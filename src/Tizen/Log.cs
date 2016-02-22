c02c436890e14ef6e78acb4b898e8e6ff62205ed
WonYoung Choi <wy80.choi@samsung.com>
2016-02-22 11:43:36 +0900

    Add Logging classes to Tizen namespace
    Add Tizen.Log and Tizen.LogTraceListener to support dlog system.
    
    Change-Id: Ibe1fd321d3df3a6be1c5abe0997edc585acc5b58

---
+++ src/Tizen/Log.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Tizen
+{
+    /// <summary>
+    /// Provides methods to print log messages to Tizen logging system.
+    /// </summary>
+    public class Log
+    {
+        /// <summary>
+        /// Prints a log message with the DEBUG priority.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        /// <param name="msg">The log message to print.</param>
+        /// <param name="file">The source file path of the caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="func">The function name of caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="line">The line number of calling position. This argument will be set automatically by the compiler.</param>
+        public static void Debug(string tag, string msg, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            if (String.IsNullOrEmpty(file))
+            {
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_DEBUG, tag, "%s", msg);
+            }
+            else
+            {
+                Uri f = new Uri(file);
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_DEBUG, tag, "%s: %s(%d) > %s", Path.GetFileName(f.AbsolutePath), func, line, msg);
+            }
+        }
+
+        /// <summary>
+        /// Prints a log message with the VERBOSE priority.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        /// <param name="msg">The log message to print.</param>
+        /// <param name="file">The source file path of the caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="func">The function name of caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="line">The line number of calling position. This argument will be set automatically by the compiler.</param>
+        public static void Verbose(string tag, string msg, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            if (String.IsNullOrEmpty(file))
+            {
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_VERBOSE, tag, "%s", msg);
+            }
+            else
+            {
+                Uri f = new Uri(file);
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_VERBOSE, tag, "%s: %s(%d) > %s", Path.GetFileName(f.AbsolutePath), func, line, msg);
+            }
+        }
+
+        /// <summary>
+        /// Prints a log message with the INFO priority.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        /// <param name="msg">The log message to print.</param>
+        /// <param name="file">The source file path of the caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="func">The function name of caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="line">The line number of calling position. This argument will be set automatically by the compiler.</param>
+        public static void Info(string tag, string msg, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            if (String.IsNullOrEmpty(file))
+            {
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_INFO, tag, "%s", msg);
+            }
+            else
+            {
+                Uri f = new Uri(file);
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_INFO, tag, "%s: %s(%d) > %s", Path.GetFileName(f.AbsolutePath), func, line, msg);
+            }
+        }
+
+        /// <summary>
+        /// Prints a log message with the WARNING priority.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        /// <param name="msg">The log message to print.</param>
+        /// <param name="file">The source file path of the caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="func">The function name of caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="line">The line number of calling position. This argument will be set automatically by the compiler.</param>
+        public static void Warn(string tag, string msg, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            if (String.IsNullOrEmpty(file))
+            {
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_WARN, tag, "%s", msg);
+            }
+            else
+            {
+                Uri f = new Uri(file);
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_WARN, tag, "%s: %s(%d) > %s", Path.GetFileName(f.AbsolutePath), func, line, msg);
+            }
+        }
+
+        /// <summary>
+        /// Prints a log message with the ERROR priority.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        /// <param name="msg">The log message to print.</param>
+        /// <param name="file">The source file path of the caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="func">The function name of caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="line">The line number of calling position. This argument will be set automatically by the compiler.</param>
+        public static void Error(string tag, string msg, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            if (String.IsNullOrEmpty(file))
+            {
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_ERROR, tag, "%s", msg);
+            }
+            else
+            {
+                Uri f = new Uri(file);
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_ERROR, tag, "%s: %s(%d) > %s", Path.GetFileName(f.AbsolutePath), func, line, msg);
+            }
+        }
+
+        /// <summary>
+        /// Prints a log message with the FATAL priority.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        /// <param name="msg">The log message to print.</param>
+        /// <param name="file">The source file path of the caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="func">The function name of caller function. This argument will be set automatically by the compiler.</param>
+        /// <param name="line">The line number of calling position. This argument will be set automatically by the compiler.</param>
+        public static void Fatal(string tag, string msg, [CallerFilePath] string file = "", [CallerMemberName] string func = "", [CallerLineNumber] int line = 0)
+        {
+            if (String.IsNullOrEmpty(file))
+            {
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_FATAL, tag, "%s", msg);
+            }
+            else
+            {
+                Uri f = new Uri(file);
+                NativeMethods.DlogPrint(NativeMethods.LogPriority.DLOG_FATAL, tag, "%s: %s(%d) > %s", Path.GetFileName(f.AbsolutePath), func, line, msg);
+            }
+        }
+    }
+}
---
+++ src/Tizen/LogTraceListener.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace Tizen
+{
+    /// <summary>
+    /// Directs tracing or debugging output to Tizen logging system.
+    /// </summary>
+    public class LogTraceListener : TraceListener
+    {
+        private string tagName = null;
+        private StringBuilder buf = new StringBuilder();
+
+        /// <summary>
+        /// Initializes a new instance of the LogTraceListener class, using the specfied tag name of the logging system.
+        /// </summary>
+        /// <param name="tag">The tag name of the log message.</param>
+        public LogTraceListener(string tag) : base("DLOG")
+        {
+            this.tagName = tag;
+        }
+
+        /// <summary>
+        /// The tag name of the log message.
+        /// </summary>
+        public string Tag
+        {
+            get { return tagName; }
+            set { tagName = value; }
+        }
+
+        private void WriteImpl(string message)
+        {
+            if (NeedIndent)
+            {
+                WriteIndent();
+            }
+            buf.Append(message);
+        }
+
+        /// <summary>
+        /// Writes an error message to the logging system.
+        /// </summary>
+        /// <param name="message">The error message to print.</param>
+        public override void Fail(string message)
+        {
+            Fail(message, null);
+        }
+
+        /// <summary>
+        /// Writes an error message and a detailed error message to the logging system.
+        /// </summary>
+        /// <param name="message">The error message to print.</param>
+        /// <param name="detailMessage">The detailed error message to print.</param>
+        public override void Fail(string message, string detailMessage)
+        {
+            StringBuilder failBuf = new StringBuilder();
+            failBuf.Append("Fail: ");
+            failBuf.Append(message);
+            if (!String.IsNullOrEmpty(detailMessage))
+            {
+                failBuf.Append(" ");
+                failBuf.Append(detailMessage);
+            }
+            Log.Error(tagName, failBuf.ToString(), null);
+        }
+
+        /// <summary>
+        /// Writes a log message to the logging system.
+        /// </summary>
+        /// <param name="message">The log message to print.</param>
+        public override void Write(string message)
+        {
+            WriteImpl(message);
+        }
+
+        /// <summary>
+        /// Writes a log message followed by the current line terminator to the logging system.
+        /// </summary>
+        /// <param name="message">The log message to print.</param>
+        public override void WriteLine(string message)
+        {
+            WriteImpl(message + Environment.NewLine);
+            NeedIndent = true;
+            Flush();
+        }
+
+        /// <summary>
+        /// Causes buffered data to be written to the logging system.
+        /// </summary>
+        public override void Flush()
+        {
+            Log.Debug(tagName, buf.ToString(), null);
+            buf.Clear();
+        }
+    }
+}
