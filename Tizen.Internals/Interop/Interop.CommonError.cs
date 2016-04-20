b1acde248ced07132498d3730fa784ccece1baf8
WonYoung Choi <wy80.choi@samsung.com>
2016-04-20 17:14:04 +0900

    Include Interop class to Tizen.Internals namespace
    Change-Id: I957b249f8336419419ed25d246c9adfe817dcdf9

--- Tizen.Internals/Interop/Interop.CommonError.cs
+++ Tizen.Internals/Interop/Interop.CommonError.cs
@@ -1,4 +1,4 @@
-﻿/// Copyright 2016 by Samsung Electronics, Inc.,
+/// Copyright 2016 by Samsung Electronics, Inc.,
 ///
 /// This software is the confidential and proprietary information
 /// of Samsung Electronics, Inc. ("Confidential Information"). You
@@ -10,19 +10,24 @@
 using System;
 using System.Runtime.InteropServices;
 
-internal static partial class Interop
+// Because Tizen.Internals assembly defines friends assembly attribute,
+// Include the Interop class to the Tizen.Internals namespace to avoid conflicts with Interop classes in other assembly.
+namespace Tizen.Internals
 {
-    internal static partial class Libraries
+    internal static partial class Interop
     {
-        public const string Base = "libcapi-base-common.so.0";
-    }
+        internal static partial class Libraries
+        {
+            public const string Base = "libcapi-base-common.so.0";
+        }
 
-    internal static partial class CommonError
-    {
-        [DllImport(Libraries.Base, EntryPoint = "get_last_result")]
-        internal static extern int GetLastResult();
+        internal static partial class CommonError
+        {
+            [DllImport(Libraries.Base, EntryPoint = "get_last_result")]
+            internal static extern int GetLastResult();
 
-        [DllImport(Libraries.Base, EntryPoint = "get_error_message")]
-        internal static extern IntPtr GetErrorMessage(int errorCode);
+            [DllImport(Libraries.Base, EntryPoint = "get_error_message")]
+            internal static extern IntPtr GetErrorMessage(int errorCode);
+        }
     }
 }
