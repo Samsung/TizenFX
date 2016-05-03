11b03e42ea177d8a01906f300bd8e70902193eae
Sidharth Gupta <sid92.gupta@samsung.com>
2016-05-03 13:52:48 +0900

    [Errors] Make error codes and utility functions public
    This is temporary so that other device APIs can access
    the error features during the implementation phase.
    
    Signed-off-by: Sidharth Gupta <sid92.gupta@samsung.com>
    Change-Id: Idc7f99d845878778d407eb6c62147fce1ac0ab06

--- Tizen.Internals/Tizen.Internals.Errors/ErrorFacts.cs
+++ Tizen.Internals/Tizen.Internals.Errors/ErrorFacts.cs
@@ -15,13 +15,13 @@ namespace Tizen.Internals.Errors
     /// <summary>
     /// 
     /// </summary>
-    internal static class ErrorFacts
+    public static class ErrorFacts
     {
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        internal static int GetLastResult()
+        public static int GetLastResult()
         {
             return Interop.CommonError.GetLastResult();
         }
@@ -31,7 +31,7 @@ namespace Tizen.Internals.Errors
         /// </summary>
         /// <param name="errorCode"></param>
         /// <returns></returns>
-        internal static string GetErrorMessage(int errorCode)
+        public static string GetErrorMessage(int errorCode)
         {
             IntPtr errorPtr = Interop.CommonError.GetErrorMessage(errorCode);
             return Marshal.PtrToStringAuto(errorPtr);
