e7bb65fb4347bb7e5db6aeca98265f6f4abe9f71
vivek ellur <vivek.ellur@samsung.com>
2016-06-03 15:40:13 +0530

    Recorder: Updated recorder according to the new design.
    1) Combined Recorder Attributes and Recorder Capabilities to
    the main Recorder class.
    2) Updated the tct according to the new design.
    3) Testing of video resolution not yet done due to dependency on
    video recorder.
    4) Fixed review comments
    
    patch2 : Fixed build issue as .csproj file was not updated.
    patch3 : Fixed videoresolution issue.
    
    Change-Id: I5e3efe8aaa01e17275b573287c935795dc05dd10
    Signed-off-by: vivek ellur <vivek.ellur@samsung.com>

--- src/Tizen.Multimedia/Recorder/Recorder.cs
+++ src/Tizen.Multimedia/Recorder/Recorder.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Tizen.Internals.Errors;
 
 namespace Tizen.Multimedia
 {
@@ -13,15 +15,13 @@ namespace Tizen.Multimedia
 	///  to start, stop and save the recorded content. It also provides methods
 	///  to get/set various attributes and capabilities of recorder.
     /// </summary>
-	/// /// <privilege>
+	/// <privilege>
 	/// http://tizen.org/privilege/recorder
 	/// </privilege>
 	public class Recorder : IDisposable
 	{
         private IntPtr _handle;
         private bool _disposed = false;
-		private RecorderAttribute _attribute = null;
-		private RecorderCapability _capability = null;
 		private EventHandler<RecorderStateChangedEventArgs> _recorderStateChanged;
 		private Interop.Recorder.StatechangedCallback _recorderStateChangedCallback;
 		private EventHandler<RecordingStatusChangedEventArgs> _recordingStatusChanged;
@@ -34,6 +34,11 @@ namespace Tizen.Multimedia
 		private Interop.Recorder.InterruptedCallback _interruptedCallback;
 		private EventHandler<RecordingErrorOccurredEventArgs> _recordingErrorOccured;
 		private Interop.Recorder.RecorderErrorCallback _recorderErrorCallback;
+		private List<RecorderFileFormat> _formats;
+		private List<RecorderAudioCodec> _audioCodec;
+		private List<RecorderVideoCodec> _videoCodec;
+		private List<RecorderVideoResolution> _resolutions;
+		RecorderVideoResolution _videoResolution = null;
 
         /// <summary>
         /// Audio recorder constructor.
@@ -45,9 +50,11 @@ namespace Tizen.Multimedia
 			{
 				RecorderErrorFactory.ThrowException (ret, "Failed to create Audio recorder");
 			}
-
-			_attribute = new RecorderAttribute (_handle);
-			_capability = new RecorderCapability (_handle);
+			_formats = new List<RecorderFileFormat>();
+			_audioCodec = new List<RecorderAudioCodec>();
+			_videoCodec = new List<RecorderVideoCodec> ();
+			_resolutions = new List<RecorderVideoResolution> ();
+			_videoResolution = new RecorderVideoResolution (_handle);
 		}
 
         /// <summary>
@@ -63,9 +70,11 @@ namespace Tizen.Multimedia
 			{
 				RecorderErrorFactory.ThrowException (ret, "Failed to create Video recorder");
 			}
-
-			_attribute = new RecorderAttribute (_handle);
-			_capability = new RecorderCapability (_handle);
+			_formats = new List<RecorderFileFormat>();
+			_audioCodec = new List<RecorderAudioCodec>();
+			_videoCodec = new List<RecorderVideoCodec> ();
+			_resolutions = new List<RecorderVideoResolution> ();
+			_videoResolution = new RecorderVideoResolution (_handle);
         }
 
         /// <summary>
@@ -367,38 +376,425 @@ namespace Tizen.Multimedia
             }
         }
 
-        /// <summary>
-        /// The various attributes of audio recorder.
-        /// </summary>
-        public RecorderAttribute Attribute
-        {
-            get
-            {
-				return _attribute;
-            }
-        }
+		/// <summary>
+		/// The maximum size of a recording file in KB(kilobytes). If 0 means
+		/// unlimited recording size.
+		/// </summary>
+		/// <remarks>
+		/// After reaching the limitation, the data which is being recorded will
+		/// be discarded and not written to the file.
+		/// The recorder state must be in 'Ready' or 'Created' state.
+		/// </remarks>
+		///
+		public int SizeLimit
+		{
+			get
+			{
+				int val = 0;
 
-        /// <summary>
-        /// The various capabilites of recorder.
-        /// </summary>
-        public RecorderCapability Capability
-        {
-            get
-            {
-				return _capability;
-            }
-        }
+				int ret = Interop.RecorderAttribute.GetSizeLimit (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get sizelimit, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetSizeLimit (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set sizelimit, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set sizelimit");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The time limit of a recording file in Seconds. If 0 means unlimited recording
+		/// time.
+		/// </summary>
+		/// <remarks>
+		/// After reaching the limitation, the data which is being recorded will
+		/// be discarded and not written to the file.
+		/// The recorder state must be in 'Ready' or 'Created' state.
+		/// </remarks>
+		public int TimeLimit
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetTimeLimit (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get timelimit, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetTimeLimit (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set timelimit, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set timelimit");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The sampling rate of an audio stream in hertz.
+		/// </summary>
+		public int AudioSampleRate
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetAudioSampleRate (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get audio samplerate, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetAudioSampleRate (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set audio samplerate, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set audio samplerate");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The bitrate of an audio encoder in bits per second.
+		/// </summary>
+		public int AudioBitRate
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetAudioEncoderBitrate (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get audio bitrate, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetAudioEncoderBitrate (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set audio bitrate, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set audio bitrate");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The bitrate of an video encoder in bits per second.
+		/// </summary>
+		public int VideoBitRate
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetVideoEncoderBitrate (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get video bitrate, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetVideoEncoderBitrate (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set video bitrate, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set video bitrate");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The mute state of a recorder.
+		/// </summary>
+		public bool Mute
+		{
+			get
+			{
+				bool val;
+
+				val = Interop.RecorderAttribute.GetMute (_handle);
+				int ret = ErrorFacts.GetLastResult ();
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					RecorderError err;
+					err = (RecorderError) ret;
+					Log.Error (RecorderLog.Tag, "Failed to get the mute state of recorder, return " + err.ToString());
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetMute (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set mute, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set mute");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The video recording motion rate
+		/// </summary>
+		/// <remarks>
+		/// The attribute is valid only in a video recorder.
+		/// If the rate is in range of 0-1, video is recorded in a slow motion mode.
+		/// If the rate is bigger than 1, video is recorded in a fast motion mode.
+		/// </remarks>
+		public double MotionRate
+		{
+			get
+			{
+				double val = 0.0;
+
+				int ret = Interop.RecorderAttribute.GetMotionRate (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get video motionrate, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetMotionRate (_handle, (double)value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set video motionrate, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set video motionrate");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The number of audio channel.
+		/// </summary>
+		/// <remarks>
+		/// The attribute is applied only in Created state.
+		/// For mono recording, set channel to 1.
+		/// For stereo recording, set channel to 2.
+		/// </remarks>
+		public int AudioChannel
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetAudioChannel (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get audio channel, " + (RecorderError)ret);
+				}
+				return val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetAudioChannel (_handle, value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set audio channel, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set audio channel");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The audio device for recording.
+		/// </summary>
+		public RecorderAudioDevice AudioDevice
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetAudioDevice (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get audio device, " + (RecorderError)ret);
+				}
+				return (RecorderAudioDevice)val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetAudioDevice (_handle, (int)value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set audio device, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set audio device");
+				}
+			}
+		}
+
+		/// <summary>
+		/// The orientation in a video metadata tag.
+		/// </summary>
+		public RecorderOrientation OrientationTag
+		{
+			get
+			{
+				int val = 0;
+
+				int ret = Interop.RecorderAttribute.GetOrientationTag (_handle, out val);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to get recorder orientation, " + (RecorderError)ret);
+				}
+				return (RecorderOrientation)val;
+			}
+			set
+			{
+				int ret = Interop.RecorderAttribute.SetOrientationTag (_handle, (int)value);
+				if ((RecorderError)ret != RecorderError.None)
+				{
+					Log.Error (RecorderLog.Tag, "Failed to set recorder orientation, " + (RecorderError)ret);
+					RecorderErrorFactory.ThrowException (ret, "Failed to set audio orientation");
+				}
+			}
+		}
 
         /// <summary>
         /// Video resolution of the video recording.
         /// </summary>
-        public VideoResolution Resolution
+        public RecorderVideoResolution Resolution
         {
             get
             {
-                return null;
+				return _videoResolution;
             }
         }
+
+		/// <summary>
+		/// Retrieves all the file formats supported by the recorder.
+		/// </summary>
+		/// <returns>
+		/// It returns a list containing all the supported file
+		/// formats by recorder.
+		/// </returns>
+		public IEnumerable<RecorderFileFormat> SupportedFileFormats
+		{
+			get
+			{
+				if (_formats.Count == 0)
+				{
+					Interop.RecorderCapablity.FileFormatCallback callback = (RecorderFileFormat format, IntPtr userData) =>
+					{
+						_formats.Add (format);
+						return true;
+					};
+					int ret = Interop.RecorderCapablity.FileFormats (_handle, callback, IntPtr.Zero);
+					if (ret != (int)RecorderError.None)
+					{
+						RecorderErrorFactory.ThrowException (ret, "Failed to get the supported fileformats");
+					}
+				}
+				return _formats;
+			}
+		}
+
+		/// <summary>
+		/// Retrieves all the audio encoders supported by the recorder.
+		/// </summary>
+		/// <returns>
+		/// It returns a list containing all the supported audio encoders
+		/// by recorder.
+		/// </returns>
+		public IEnumerable<RecorderAudioCodec> SupportedAudioEncodings
+		{
+			get
+			{
+				if (_audioCodec.Count == 0)
+				{
+					Interop.RecorderCapablity.AudioEncoderCallback callback = (RecorderAudioCodec codec, IntPtr userData) =>
+					{
+						_audioCodec.Add(codec);
+						return true;
+					};
+					int ret = Interop.RecorderCapablity.AudioEncoders (_handle, callback, IntPtr.Zero);
+					if (ret != (int)RecorderError.None)
+					{
+						RecorderErrorFactory.ThrowException (ret, "Failed to get the supported audio encoders");
+					}
+				}
+				return _audioCodec;
+			}
+		}
+
+		/// <summary>
+		/// Retrieves all the video encoders supported by the recorder.
+		/// </summary>
+		/// <returns>
+		/// It returns a list containing all the supported video encoders
+		/// by recorder.
+		/// </returns>
+		public IEnumerable<RecorderVideoCodec> SupportedVideoEncodings
+		{
+			get
+			{
+				if (_videoCodec.Count == 0)
+				{
+					Interop.RecorderCapablity.VideoEncoderCallback callback = (RecorderVideoCodec codec, IntPtr userData) =>
+					{
+						_videoCodec.Add(codec);
+						return true;
+					};
+					int ret = Interop.RecorderCapablity.VideoEncoders (_handle, callback, IntPtr.Zero);
+					if (ret != (int)RecorderError.None)
+					{
+						RecorderErrorFactory.ThrowException (ret, "Failed to get the supported video encoders");
+					}
+				}
+				return _videoCodec;
+			}
+		}
+
+		/// <summary>
+		/// Retrieves all the video resolutions supported by the recorder.
+		/// </summary>
+		/// <returns>
+		/// It returns videoresolution list containing the width and height of
+		/// different resolutions supported by recorder.
+		/// </returns>
+		public IEnumerable<RecorderVideoResolution> SupportedVideoResolutions
+		{
+			get
+			{
+				if (_resolutions.Count == 0)
+				{
+					Interop.RecorderCapablity.VideoResolutionCallback callback = (int width, int height, IntPtr userData) =>
+					{
+						RecorderVideoResolution temp = new RecorderVideoResolution(width, height);
+						_resolutions.Add(temp);
+						return true;
+					};
+					int ret = Interop.RecorderCapablity.VideoResolution (_handle, callback, IntPtr.Zero);
+					if (ret != (int)RecorderError.None)
+					{
+						RecorderErrorFactory.ThrowException (ret, "Failed to get the supported video resolutions");
+					}
+				}
+				return _resolutions;
+			}
+		}
 
 		/// <summary>
 		/// Prepare the media recorder for recording.
@@ -512,12 +908,12 @@ namespace Tizen.Multimedia
 		{
 			int ret = Interop.Recorder.SetAudioStreamPolicy (_handle, policy.Handle);
 
-			if (ret != (int)RecorderError.None) 
+			if (ret != (int)RecorderError.None)
 			{
 				RecorderErrorFactory.ThrowException (ret, "Failed to set audio stream policy");
 			}
-     	}
-			
+		}
+
 		/// <summary>
 		/// Release any unmanaged resources used by this object.
 		/// </summary>
@@ -611,7 +1007,7 @@ namespace Tizen.Multimedia
 
 		private void UnregisterStateChangedEvent()
 		{
-			int ret = Interop.Recorder.UnsetStateChangedCallback (_handle);	
+			int ret = Interop.Recorder.UnsetStateChangedCallback (_handle);
 			if (ret != (int)RecorderError.None)
 			{
 				RecorderErrorFactory.ThrowException (ret, "Unsetting state changed callback failed");
