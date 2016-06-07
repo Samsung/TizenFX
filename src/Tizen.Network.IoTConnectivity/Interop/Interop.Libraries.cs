a48eb73ba2f48575c56c8e9717cddedeb6c04fe8
rajeev ranjan <rajeev.ran@samsung.com>
2016-06-07 19:20:24 +0530

    Modified IoTCon library name
    Change-Id: Ibd1ab94c6465bc13f86527e90a0c5205f7cd357b
    Signed-off-by: rajeev ranjan <rajeev.ran@samsung.com>

--- src/Tizen.Network.IoTConnectivity/Interop/Interop.Libraries.cs
+++ src/Tizen.Network.IoTConnectivity/Interop/Interop.Libraries.cs
@@ -10,7 +10,7 @@ internal static partial class Interop
 {
     internal static partial class Libraries
     {
-        public const string IoTCon = "libiotcon.so.0.0.12";
+        public const string IoTCon = "libiotcon.so.0";
         public const string Glib = "libglib-2.0.so.0";
         public const string Libc = "libc.so.6";
     }
