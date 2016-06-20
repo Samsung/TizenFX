a9746878deca88911f66bd0aeed623d46ea565e2
Aditya <a.aswani@samsung.com>
2016-06-20 15:53:06 +0530

    Adding system-settings module Implementation + TCTs Modified the Tizen.System.SystemSettings namespace to Tizen.System
    Change-Id: I2f5cfae46a328a816b3ac5b686f9b4a62d498911
    Signed-off-by: Aditya <a.aswani@samsung.com>

---
+++ src/Tizen.System/SystemSettings/SystemSettingsExceptionFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Tizen.System
+{
+    internal enum SystemSettingsError
+    {
+        None = Tizen.Internals.Errors.ErrorCode.None,
+        InvalidParameter = Tizen.Internals.Errors.ErrorCode.InvalidParameter,
+        OutOfMemory = Tizen.Internals.Errors.ErrorCode.OutOfMemory,
+        IoError = Tizen.Internals.Errors.ErrorCode.IoError,
+        PermissionDenied = Tizen.Internals.Errors.ErrorCode.PermissionDenied,
+        NotSupported = Tizen.Internals.Errors.ErrorCode.NotSupported,
+        LockScreenAppPasswordMode = -0x01140000 | 0x01
+    };
+    internal class SystemSettingsExceptionFactory
+    {
+        internal const string LogTag = "Tizen.System.SystemSettings";
+
+        internal static Exception CreateException(SystemSettingsError err, string msg)
+        {
+            Exception exp;
+            switch (err)
+            {
+                case SystemSettingsError.InvalidParameter:
+                    exp = new ArgumentException(msg);
+                    break;
+                case SystemSettingsError.OutOfMemory:
+                //fall through
+                case SystemSettingsError.IoError:
+                //fall through
+                case SystemSettingsError.PermissionDenied:
+                //fall through
+                case SystemSettingsError.NotSupported:
+                //fall through
+                case SystemSettingsError.LockScreenAppPasswordMode:
+                //fall through
+                default:
+                    exp = new InvalidOperationException(msg);
+                    break;
+            }
+            return exp;
+        }
+    }
+}
