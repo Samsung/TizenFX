17834e787b411dfc74baa4966e22e62d7d32e1c7
Jooseok Park <jooseok.park@samsung.com>
2016-11-01 08:53:14 +0900

    change license
    Change-Id: Ibcdbeb0b5f6efaea72b74453af83a1a658bd7338

--- src/Tizen.Network.IoTConnectivity/Tizen.Network.IoTConnectivity/Attributes.cs
+++ src/Tizen.Network.IoTConnectivity/Tizen.Network.IoTConnectivity/Attributes.cs
@@ -1,10 +1,21 @@
 /// Copyright 2016 by Samsung Electronics, Inc.,
 ///
-/// This software is the confidential and proprietary information
-/// of Samsung Electronics, Inc. ("Confidential Information"). You
-/// shall not disclose such Confidential Information and shall use
-/// it only in accordance with the terms of the license agreement
-/// you entered into with Samsung.
+ /*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 using System;
 using System.Collections;
