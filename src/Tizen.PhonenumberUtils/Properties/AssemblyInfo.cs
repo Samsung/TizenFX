aec68ae1044c31bc70c089962d2ee83545678d30
Kyeonghun Lee <kh9090.lee@samsung.com>
2017-04-03 16:53:27 +0900

    Initial commit
    Change-Id: I6e470bbe0b9e95c8a9833a10f0b800bfc325517a
    Signed-off-by: Kyeonghun Lee <kh9090.lee@samsung.com>

---
+++ src/Tizen.PhonenumberUtils/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tizen.PhonenumberUtils")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Samsung Electronics")]
+[assembly: AssemblyProduct("Tizen.PhonenumberUtils")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Samsung Electronics Co., Ltd")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b8ec773-abdf-4e51-8158-71c56ce25bb9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Tizen.PhonenumberUtils/Tizen.PhonenumberUtils/PhonenumberUtils.cs
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+
+namespace Tizen.PhonenumberUtils
+{
+    /// <summary>
+    /// This class makes applications can use phonenumber utils.
+    /// </summary>
+    public class PhonenumberUtils : IDisposable
+    {
+        private bool disposed = false;
+
+        public PhonenumberUtils()
+        {
+            int ret;
+
+            ret = Interop.PhonenumberUtils.Connect();
+            if (ret != (int)PhonenumberUtilsError.None)
+            {
+                Log.Error(Globals.LogTag, "Failed to connect, Error - " + (PhonenumberUtilsError)ret);
+                PhonenumberUtilsErrorFactory.ThrowPhonenumberUtilsException(ret);
+            }
+        }
+
+        ~PhonenumberUtils()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free managed objects
+            }
+
+            // Free unmanaged objects
+            int ret;
+
+            ret = Interop.PhonenumberUtils.Disconnect();
+            if (ret != (int)PhonenumberUtilsError.None)
+            {
+                Log.Error(Globals.LogTag, "Failed to disconnect, Error - " + (PhonenumberUtilsError)ret);
+                PhonenumberUtilsErrorFactory.ThrowPhonenumberUtilsException(ret);
+            }
+
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Gets the location string from number, region, and language.
+        /// </summary>
+        /// <param name="number">The number</param>
+        /// <param name="region">The region of number</param>
+        /// <param name="language">The language of location</param>
+        /// <returns>The location string</returns>
+        /// <exception cref="InvalidOperationException">Thrown when method failed due to invalid operation</exception>
+        /// <exception cref="NotSupportedException">Thrown when phonenumber-utils is not supported</exception>
+        /// <exception cref="ArgumentException">Thrown when input coordinates are invalid</exception>
+        /// <exception cref="OutOfMemoryException">Thrown when failed due to out of memory</exception>
+        public string GetLocationFromNumber(string number, Region region, Language language)
+        {
+            int ret;
+            string result;
+
+            ret = Interop.PhonenumberUtils.GetLocationFromNumber(number, (int)region, (int)language, out result);
+            if (ret != (int)PhonenumberUtilsError.None)
+            {
+                Log.Error(Globals.LogTag, "Failed to get location, Error - " + (PhonenumberUtilsError)ret);
+                PhonenumberUtilsErrorFactory.ThrowPhonenumberUtilsException(ret);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the formatted number.
+        /// </summary>
+        /// <param name="number">The number</param>
+        /// <param name="region">The region of number</param>
+        /// <returns>The formatted number string</returns>
+        /// <exception cref="InvalidOperationException">Thrown when method failed due to invalid operation</exception>
+        /// <exception cref="NotSupportedException">Thrown when phonenumber-utils is not supported</exception>
+        /// <exception cref="ArgumentException">Thrown when input coordinates are invalid</exception>
+        /// <exception cref="OutOfMemoryException">Thrown when failed due to out of memory</exception>
+        public string GetFormattedNumber(string number, Region region)
+        {
+            int ret;
+            string result;
+
+            ret = Interop.PhonenumberUtils.GetFormmatedNumber(number, (int)region, out result);
+            if (ret != (int)PhonenumberUtilsError.None)
+            {
+                Log.Error(Globals.LogTag, "Failed to get formatted number, Error - " + (PhonenumberUtilsError)ret);
+                PhonenumberUtilsErrorFactory.ThrowPhonenumberUtilsException(ret);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the normalized number.
+        /// </summary>
+        /// <privilege>http://tizen.org/privilege/telephony</privilege>
+        /// <param name="number">The number</param>
+        /// <returns>The normalized number</returns>
+        /// <exception cref="InvalidOperationException">Thrown when method failed due to invalid operation</exception>
+        /// <exception cref="NotSupportedException">Thrown when phonenumber-utils is not supported</exception>
+        /// <exception cref="ArgumentException">Thrown when input coordinates are invalid</exception>
+        /// <exception cref="OutOfMemoryException">Thrown when failed due to out of memory</exception>
+        /// <exception cref="UnauthorizedAccessException">Thrown when application does not have proper privileges</exception>
+        public string GetNormalizedNumber(string number)
+        {
+            int ret;
+            string result;
+
+            ret = Interop.PhonenumberUtils.GetNormailizedNumber(number, out result);
+            if (ret != (int)PhonenumberUtilsError.None)
+            {
+                Log.Error(Globals.LogTag, "Failed to get normalized number, Error - " + (PhonenumberUtilsError)ret);
+                PhonenumberUtilsErrorFactory.ThrowPhonenumberUtilsException(ret);
+            }
+
+            return result;
+        }
+    }
+}
