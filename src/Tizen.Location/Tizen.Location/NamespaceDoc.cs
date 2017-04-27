692c77b66b8d12cf1a2f320909281231aa942d54
kj7.sung <kj7.sung@samsung.com>
2017-04-27 15:37:58 +0900

    Add NamespaceDoc
    Change-Id: I45a87fa61694636fc03e38674d873532959ccb98
    Signed-off-by: kj7.sung <kj7.sung@samsung.com>

---
+++ src/Tizen.Location/Tizen.Location/NamespaceDoc.cs
@@ -0,0 +1,21 @@
+/**
+<summary>
+The Tizen.Location namespace provides classes for obtaining information related to geographical location.
+Notifications on events like service becoming enabled or disabled,
+new position data being available and others can also be acquired.
+</summary>
+<remarks>
+<h2>Overview</h2>
+<para>The Tizen.Location namespace provides classes for obtaining information related to geographical location.
+Notifications on events like service becoming enabled or disabled,
+new position data being available and others can also be acquired.</para>
+<h2>Related Features</h2>
+<para>To guarantee that the Location application runs on a device with location profile feature,
+declare the following feature requirements in the config file:<br/>
+http://tizen.org/feature/location<br/>
+http://tizen.org/feature/location.gps<br/>
+http://tizen.org/feature/location.wps
+</para>
+</remarks>
+*/
+namespace Tizen.Location {}
