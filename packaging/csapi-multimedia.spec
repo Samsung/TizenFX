bc78bc8968a98c3a95c67752ba3b026346fb200e
coderhyme <jhyo.kim@samsung.com>
2017-05-12 14:08:20 +0900

    Added InternalsVisbleToAttribute for MediaCodec assembly.
    MediaCodec will be a seperated assembly.
    This path must be merged first.
    
    Change-Id: I10b2ed68275e2887068b9fcf2014f61a5c66bdd5
    Signed-off-by: coderhyme <jhyo.kim@samsung.com>

--- packaging/csapi-multimedia.spec
+++ packaging/csapi-multimedia.spec
@@ -1,6 +1,6 @@
 Name:       csapi-multimedia
 Summary:    Tizen Multimedia API for C#
-Version:    1.0.55
+Version:    1.1.0
 Release:    0
 Group:      Development/Libraries
 License:    Apache-2.0
---
+++ src/Tizen.Multimedia/Common/InternalsVisibleDefinitions.cs
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tizen.Multimedia.MediaCodec, " +
+    "PublicKey=002400000480000094000000060200000024000052534131000400000100010085f7b7a5984e7e995" +
+    "d5f86234bfd1d4b2609b9334d5cb6cd3076c5faab98f8c4922d88fec96bea747fec274bceedc2bd1adb28c38a33" +
+    "c66182245adec7fbcee69cc556c337758646936500b770e19a562ebd10a527787f176eba8fdcf58c8e220042580" +
+    "75fe2903b3b5090953ac1d41c587993377cde63269a7cdd6cbf98efda")]
