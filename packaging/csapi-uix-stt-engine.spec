296b70e43b126e0a5557db1f34abdddd563b3d76
manish.r <manish.r@samsung.com>
2017-05-26 11:31:27 +0530

    Base Code
    Change-Id: I96b20e693b27dfe09c675937ffe8e1abdc266fe9
    Signed-off-by: manish.r <manish.r@samsung.com>

---
+++ packaging/csapi-uix-stt-engine.spec
@@ -0,0 +1,44 @@
+Name:       csapi-uix-stt-engine
+Summary:    Tizen Uix stt engine API for C#
+Version:    1.0.2
+Release:    1
+Group:      Development/Libraries
+License:    Apache-2.0
+URL:        https://www.tizen.org
+Source0:    %{name}-%{version}.tar.gz
+Source1:    %{name}.manifest
+
+AutoReqProv: no
+ExcludeArch: aarch64
+
+BuildRequires: dotnet-build-tools
+
+# C# API Requires
+BuildRequires: csapi-tizen-nuget
+
+%define Assemblies Tizen.Uix.SttEngine
+
+%description
+%{summary}
+
+%dotnet_import_sub_packages
+
+%prep
+%setup -q
+cp %{SOURCE1} .
+
+%build
+for ASM in %{Assemblies}; do
+%dotnet_build $ASM
+%dotnet_pack $ASM/$ASM.nuspec %{version}
+done
+
+%install
+for ASM in %{Assemblies}; do
+%dotnet_install $ASM
+done
+
+%files
+%manifest %{name}.manifest
+%license LICENSE
+%attr(644,root,root) %{dotnet_assembly_files}
---
+++ src/Tizen.Uix.SttEngine/Tizen.Uix.SttEngine/ExceptionFactory.cs
@@ -0,0 +1,107 @@
+﻿/*
+* Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+*
+* Licensed under the Apache License, Version 2.0 (the License);
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an AS IS BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using static Interop.SttEngine;
+
+
+namespace Tizen.Uix.SttEngine
+{
+    internal static class ExceptionFactory
+    {
+        internal static Exception CreateException(ErrorCode err)
+        {
+            Tizen.Log.Error(LogTag, "Error " + err);
+            Exception exp;
+            switch (err)
+            {
+                case ErrorCode.OutOfMemory:
+                {
+                    exp = new OutOfMemoryException("Out Of Memory");
+                    break;
+                }
+
+                case ErrorCode.IoError:
+                {
+                    exp = new InvalidOperationException("I/O Error Occured");
+                    break;
+                }
+
+                case ErrorCode.InvalidParameter:
+                {
+                    exp = new ArgumentException("Invalid Parameters Provided");
+                    break;
+                }
+
+                case ErrorCode.NetworkDown:
+                {
+                    exp = new InvalidOperationException("Network down(Out of network)");
+                    break;
+                }
+
+                case ErrorCode.InvalidState:
+                {
+                    exp = new InvalidOperationException("Invalid state");
+                    break;
+                }
+
+                case ErrorCode.InvalidLanguage:
+                {
+                    exp = new InvalidOperationException("Invalid language");
+                    break;
+                }
+
+                case ErrorCode.OperationFailed:
+                {
+                    exp = new InvalidOperationException("Operation Failed");
+                    break;
+                }
+
+                case ErrorCode.NotSupportedFeature:
+                {
+                    exp = new InvalidOperationException("Not supported feature");
+                    break;
+                }
+
+                case ErrorCode.NotSupported:
+                {
+                    exp = new NotSupportedException("Not supported");
+                    break;
+                }
+
+                case ErrorCode.PermissionDenied:
+                {
+                    exp = new UnauthorizedAccessException("Permission Denied");
+                    break;
+                }
+
+                case ErrorCode.RecordingTimedOut:
+                {
+                    exp = new TimeoutException("Recording timed out");
+                    break;
+                }
+
+                default:
+                {
+                    exp = new Exception("");
+                    break;
+                }
+            }
+
+            return exp;
+        }
+    }
+}
