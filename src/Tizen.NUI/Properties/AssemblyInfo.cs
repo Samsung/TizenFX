ec827cb74956da09a36722ab49f1e41be47ba22e
dongsug.song <dongsug.song@samsung.com>
2017-06-21 10:43:42 +0900

    Revert "[Tizen] nui 0.2.41 release"
    This reverts commit bb81f71a0e79d43349ac54dc32a5743204d52907.
    
    Change-Id: Iaab3f7fcfa0da0bba9900e58c0c8e45bb042e9ab

--- src/Tizen.NUI/Properties/AssemblyInfo.cs
+++ src/Tizen.NUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.41.0531")]
-[assembly: AssemblyFileVersion("0.2.41.0531")]
+[assembly: AssemblyVersion("0.2.39.0519")]
+[assembly: AssemblyFileVersion("0.2.39.0519")]
