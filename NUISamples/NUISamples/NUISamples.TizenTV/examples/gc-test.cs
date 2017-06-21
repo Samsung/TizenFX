bd9825ad046bc6ab5ac6d0d99ab41f4d5d961135
dongsug.song <dongsug.song@samsung.com>
2017-06-21 14:41:20 +0900

    [Tizen] nui 0.2.41 release
    This reverts commit 3a1a6f675463e898edb74c2f057e66ebc7faeb41.
    
    Change-Id: I9bdebb98bb96bc6eeeb03c910238ac441f3e3aeb

--- NUISamples/NUISamples/NUISamples.TizenTV/examples/gc-test.cs
+++ NUISamples/NUISamples/NUISamples.TizenTV/examples/gc-test.cs
@@ -240,7 +240,7 @@ namespace TizenVDUIApplication19
             //This function is called when the app exit normally.
             base.OnTerminate();
         }
-
+/*
         protected override void OnLowMemory(LowMemoryEventArgs e)
         {
             //This function is called when the system is low on memory.
@@ -252,7 +252,7 @@ namespace TizenVDUIApplication19
             //This function is called when the language is changed.
             base.OnLocaleChanged(e);
         }
-
+*/
         private static void _Main(string[] args)
         {
             //Create an Application
