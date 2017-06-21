5edb06b4abab1386938819f446934e45ef88e432
dongsug.song <dongsug.song@samsung.com>
2017-06-21 10:42:00 +0900

    Revert "[Tizen] some APIs are changed for preview#4"
    This reverts commit 33170eb7f9d0fc8169054d4ef02dbaa835183726.
    
    Change-Id: Idf5e65bf9b659443066b4e3ecfda301b4d2f02a2

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -1772,7 +1772,7 @@ namespace Tizen.NUI.BaseComponents
         /// <remarks>
         /// Readonly.
         /// </remarks>
-        public Vector3 NaturalSize
+        internal Vector3 NaturalSize
         {
             get
             {
@@ -3589,7 +3589,7 @@ namespace Tizen.NUI.BaseComponents
 
             }
         }
-        public event EventHandlerWithReturnType<object, WheelEventArgs, bool> WheelMoved
+        public event EventHandlerWithReturnType<object, WheelEventArgs, bool> WheelRolled
         {
             add
             {
