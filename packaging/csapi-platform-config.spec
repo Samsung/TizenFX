ba288bbbe7a17c01162394b60bdd07c275772d63
Hyotaek Shim <hyotaek.shim@samsung.com>
2017-07-14 21:34:06 +0900

    Initialization for tizen-platform-config C# internal APIs
    Change-Id: I42a788e91ae4da975b1601e3e211188ecce332c3
    Signed-off-by: Hyotaek Shim <hyotaek.shim@samsung.com>

---
+++ packaging/csapi-platform-config.spec
@@ -0,0 +1,41 @@
+Name:       csapi-platform-config
+Summary:    Tizen Platform-Config APIs for C#
+Version:    1.0.0
+Release:    1
+Group:      Development/Libraries
+License:    Apache-2.0
+URL:        https://www.tizen.org
+Source0:    %{name}-%{version}.tar.gz
+Source1:    %{name}.manifest
+
+AutoReqProv: no
+ExcludeArch: aarch64
+
+BuildRequires: dotnet-build-tools
+
+%define Assemblies Tizen.System.PlatformConfig
+
+%description
+%{summary}
+
+%dotnet_import_sub_packages
+
+%prep
+%setup -q
+cp %{SOURCE1} .
+
+%build
+for ASM in %{Assemblies}; do
+%dotnet_build $ASM
+%dotnet_pack $ASM
+done
+
+%install
+for ASM in %{Assemblies}; do
+%dotnet_install $ASM
+done
+
+%files
+%manifest %{name}.manifest
+%license LICENSE.Apache-2.0
+%attr(644,root,root) %{dotnet_assembly_files}
---
+++ src/Tizen.System.PlatformConfig/Interop/Interop.PlatformConfig.cs
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Runtime.InteropServices;
+using System;
+
+internal static partial class Interop
+{
+    internal static partial class Libraries
+    {
+        public const string PlatformConfig = "libtzplatform-config-2.0.so.2";
+    }
+
+    internal static partial class PlatformConfig
+    {
+        [DllImport(Libraries.PlatformConfig, EntryPoint = "tzplatform_getenv", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr _GetEnv(int id);
+
+	internal static string GetEnv(int id)
+	{
+		var text = _GetEnv(id);
+		return Marshal.PtrToStringAnsi(text);
+	}
+    }
+}
+
