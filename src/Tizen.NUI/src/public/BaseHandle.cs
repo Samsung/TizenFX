3f9a67850c746f16018b5a12284e2523cc297e20
Umar <m.umar@partner.samsung.com>
2017-07-18 14:28:43 +0100

    [Tizen] Temporary Fix to return the Layer as a View when Parent is a Layer.
    Change-Id: I0e0805dd542bbae3f32d7e047bc7792ec37e1d79

--- src/Tizen.NUI/src/public/BaseHandle.cs
+++ src/Tizen.NUI/src/public/BaseHandle.cs
@@ -21,22 +21,26 @@ namespace Tizen.NUI
     {
         private global::System.Runtime.InteropServices.HandleRef swigCPtr;
         protected bool swigCMemOwn;
+        private bool _registerMe;
 
         internal BaseHandle(global::System.IntPtr cPtr, bool cMemoryOwn)
         {
-            swigCMemOwn = cMemoryOwn;
+            _registerMe = swigCMemOwn = cMemoryOwn;
             swigCPtr = new global::System.Runtime.InteropServices.HandleRef(this, cPtr);
 
             // using copy constructor to create another native handle so Registry.Unregister works fine.
             swigCPtr = new global::System.Runtime.InteropServices.HandleRef(this, NDalicPINVOKE.new_BaseHandle__SWIG_2(swigCPtr));
 
-            // Register this instance of BaseHandle in the registry.
-            Registry.Register(this);
+            if (_registerMe)
+            {
+                // Register this instance of BaseHandle in the registry.
+                Registry.Register(this);
+            }
         }
 
         internal BaseHandle(global::System.IntPtr cPtr)
         {
-            swigCMemOwn = true;
+            _registerMe = swigCMemOwn = true;
             swigCPtr = new global::System.Runtime.InteropServices.HandleRef(this, cPtr);
 
             // Register this instance of BaseHandle in the registry.
@@ -101,7 +105,10 @@ namespace Tizen.NUI
             //because the execution order of Finalizes is non-deterministic.
 
             //Unreference this instance from Registry.
-            Registry.Unregister(this);
+            if (_registerMe)
+            {
+                Registry.Unregister(this);
+            }
 
             if (swigCPtr.Handle != global::System.IntPtr.Zero)
             {
