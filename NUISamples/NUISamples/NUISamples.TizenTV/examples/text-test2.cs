92dd605e0456e4f4931ca87fd9d9bc904b827213
minho.sun <minho.sun@samsung.com>
2017-08-14 16:47:32 +0900

    Revert "[Tizen]Update ImfManager and fix Resized&LanguageChanged event"
    This reverts commit f419245b112ffd855ed59b25be8a0264ea1e02bf.
    
    Change-Id: I59b5c809a880eb4bce90dd92db6271d005f9cbd3

--- NUISamples/NUISamples/NUISamples.TizenTV/examples/text-test2.cs
+++ NUISamples/NUISamples/NUISamples.TizenTV/examples/text-test2.cs
@@ -44,15 +44,25 @@ namespace TextTest2
             Initialize();
         }
 
-        private PushButton button;
+        private PushButton _button;
+        private PushButton button2;
+        private TextField _field;
         private TextEditor editor;
-        private ImfManager imfManager;
 
 
         public void Initialize()
         {
             Window window = Window.Instance;
             window.BackgroundColor = Color.White;
+
+            _field = new TextField();
+            _field.Size2D = new Size2D(500, 300);
+            _field.Position2D = new Position2D(530, 550);
+            _field.BackgroundColor = Color.Cyan;
+            _field.PlaceholderText = "TextField input someth...";
+            _field.Focusable = true;
+            _field.EnableSelection = true;
+            window.Add(_field);
 
             PropertyMap propertyMap = new PropertyMap();
             propertyMap.Add("placeholderText", new PropertyValue("TextEditor Placeholder Text"));
@@ -66,17 +76,14 @@ namespace TextTest2
             propertyMap.Add("placeholderFontStyle", new PropertyValue(fontStyleMap));
 
 
-            editor = new TextEditor()
-            {
-                Size2D = new Size2D(500, 300),
-                Position2D = new Position2D(10, 550),
-                BackgroundColor = Color.Magenta,
-                Focusable = true,
-                Placeholder = propertyMap
-            };
+            editor = new TextEditor();
+            editor.Size2D = new Size2D(500, 300);
+            editor.Position2D = new Position2D(10, 550);
+            editor.BackgroundColor = Color.Magenta;
+            editor.Focusable = true;
+            editor.Placeholder = propertyMap;
 
             window.Add(editor);
-
             editor.TextChanged += (obj, e) => {
                 Tizen.Log.Fatal("NUI", "editor line count: " + e.TextEditor.LineCount);
             };
@@ -85,113 +92,56 @@ namespace TextTest2
                 Tizen.Log.Fatal("NUI", "editor scroll state:" + e.ScrollState);
             };
 
-            editor.KeyEvent += OnKeyEvent;
-
-
             Tizen.Log.Debug("NUI", "editor id: " + editor.ID);
 
-            imfManager = ImfManager.Get();
-            imfManager.AutoEnableInputPanel(false);
-            imfManager.SetInputPanelUserData("layouttype = 1 & entrylimit = 255 & action = clearall_for_voice_commit & caller = org.volt.search - all");
-
-
+            ImfManager imfManager = ImfManager.Get();
             // send privatecommand event
             ImfManager.ImfEventData imfevent = new ImfManager.ImfEventData(ImfManager.ImfEvent.PrivateCommand, "", 0, 0);
-            imfManager.EventReceived += ImfManager_ImfManagerEventReceived;
+            imfManager.ImfManagerEventReceived += ImfManager_ImfManagerEventReceived;
 
-            imfManager.LanguageChanged += ImfManager_LanguageChanged;
+            //imfmanager.imfManagerLanguageChanged += ImfManager_LanguageChanged;
 
-            imfManager.Activated += (obj, e) => {
-                Tizen.Log.Debug("NUI", "ImfManager Activated !!!!!");
-            };
-            imfManager.StatusChanged += (obj, e) => {
-                Tizen.Log.Debug("NUI", "ImfManager StatusChanged: !!!!!" + e.StatusChanged);
-            };
-            imfManager.Resized += (obj, e) =>
-            {
-                Tizen.Log.Debug("NUI", "ImfManager Resized !!!!!");
-            };
-            imfManager.KeyboardTypeChanged += (obj, e) => {
-                Tizen.Log.Debug("NUI", "ImfManager KeyboardTypeChanged: !!!!!" + e.KeyboardType);
-            };
+            _button = new PushButton();
+            _button.LabelText = "Button1";
+            _button.Size2D = new Size2D(400, 200);
+            _button.Position2D = new Position2D(10, -10);
+            _button.ParentOrigin = ParentOrigin.BottomLeft;
+            _button.PivotPoint = PivotPoint.BottomLeft;
+            _button.PositionUsesPivotPoint = true;
+            _button.Focusable = true;
+            window.Add(_button);
 
-            button = new PushButton()
-            {
-                LabelText = "Button",
-                Size2D = new Size2D(400, 200),
-                Position2D = new Position2D(10, -10),
-                ParentOrigin = ParentOrigin.BottomLeft,
-                PivotPoint = PivotPoint.BottomLeft,
-                PositionUsesPivotPoint = true,
-                Focusable = true
-            };
+            button2 = new PushButton();
+            button2.LabelText = "Button2";
+            button2.Size2D = new Size2D(400, 200);
+            button2.ParentOrigin = ParentOrigin.BottomLeft;
+            button2.PivotPoint = PivotPoint.BottomLeft;
+            button2.Position2D = new Position2D(420, -10);
+            button2.PositionUsesPivotPoint = true;
+            button2.Focusable = true;
+            window.Add(button2);
 
-            window.Add(button);
-
-            button.UpFocusableView = editor;
-            editor.DownFocusableView = button;
-
-            FocusManager.Instance.SetCurrentFocusView(button);
+            _button.UpFocusableView = editor;
+            FocusManager.Instance.PreFocusChange += OnPreFocusChange;
         }
 
-        private bool OnKeyEvent(object source, View.KeyEventArgs e)
+        private View OnPreFocusChange(object source, FocusManager.PreFocusChangeEventArgs e)
         {
-            Tizen.Log.Debug("NUI", "KeyEvent called !!!!!");
-            if (e.Key.State != Key.StateType.Down || editor.Focusable == false)
+            if (!e.ProposedView && !e.CurrentView)
             {
-                Tizen.Log.Debug("NUI", "KeyEvent ignored !!!!!");
-                return false;
+                e.ProposedView = _button;
             }
-            switch (e.Key.KeyPressedName)
-            {
-                case "Return":
-                    Tizen.Log.Debug("NUI", "KeyPressedName: Return !!!!!");
-                    ShowImf();
-                    return true;
-                case "Select":
-                    Tizen.Log.Debug("NUI", "KeyPressedName: Select !!!!!");
-                    HideImf();
-                    return true;
-                case "Cancel":
-                    Tizen.Log.Debug("NUI", "KeyPressedName: Cancel !!!!!");
-                    HideImf();
-                    return true;
-                case "Down":
-                    Tizen.Log.Debug("NUI", "KeyPressedName: Down !!!!!");
-                    HideImf();
-                    return MoveFocusTo(button);
-            }
-            return false;
+            return e.ProposedView;
         }
 
-        private void ShowImf()
+        public ImfManager.ImfCallbackData ImfManager_ImfManagerEventReceived(object sender, ImfManager.ImfManagerEventReceivedEventArgs e)
         {
-            imfManager.Activate();
-            imfManager.ShowInputPanel();
-            Tizen.Log.Debug("NUI", "IME showed !!!!!");
-        }
+            Tizen.Log.Fatal("NUI", "ImfManager_ImfManagerEventReceived()!");
 
-        private bool MoveFocusTo(View view)
-        {
-            if (view == null) return false;
-            return FocusManager.Instance.SetCurrentFocusView(view);
-        }
-
-        private void HideImf()
-        {
-            imfManager.Deactivate();
-            imfManager.HideInputPanel();
-            Tizen.Log.Debug("NUI", "IME hided !!!!!");
-        }
-
-        public ImfManager.ImfCallbackData ImfManager_ImfManagerEventReceived(object sender, ImfManager.EventReceivedEventArgs e)
-        {
-            Tizen.Log.Debug("NUI", "ImfManager_ImfManagerEventReceived()!");
-
-            Tizen.Log.Debug("NUI", "e.ImfEventData.PredictiveString= " + e?.ImfEventData?.PredictiveString);
-            Tizen.Log.Debug("NUI", "e.ImfEventData.CursorOffset= " + e?.ImfEventData?.CursorOffset);
-            Tizen.Log.Debug("NUI", "e.ImfEventData.EventName= " + e?.ImfEventData?.EventName);
-            Tizen.Log.Debug("NUI", "e.ImfEventData.NumberOfChars= " + e?.ImfEventData?.NumberOfChars);
+            Tizen.Log.Fatal("NUI", "e.ImfEventData.PredictiveString= " + e?.ImfEventData?.PredictiveString);
+            Tizen.Log.Fatal("NUI", "e.ImfEventData.PredictiveString= " + e?.ImfEventData?.CursorOffset);
+            Tizen.Log.Fatal("NUI", "e.ImfEventData.PredictiveString= " + e?.ImfEventData?.EventName);
+            Tizen.Log.Fatal("NUI", "e.ImfEventData.PredictiveString= " + e?.ImfEventData?.NumberOfChars);
 
             //Be able to compare VD specific private command with ImfEventData.predictiveString
             if (e.ImfEventData.PredictiveString == "IME_F31")
@@ -199,18 +149,18 @@ namespace TextTest2
                 ImfManager.Get().Deactivate();
                 ImfManager.Get().HideInputPanel();
                 // Do Something the user wants
-                Tizen.Log.Debug("NUI", "ImfManager ImfEventData.PredictiveString: IME_F31!!!");
+                Tizen.Log.Fatal("NUI", "ImfManager ImfEventData.PredictiveString: IME_F31!!!");
             }
             ImfManager.ImfCallbackData callbackData = new ImfManager.ImfCallbackData(true, 0, e.ImfEventData.PredictiveString, false);
-            Tizen.Log.Debug("NUI", "ImfManager return callbackData!!!");
+            Tizen.Log.Fatal("NUI", "ImfManager return callbackData!!!");
             return callbackData;
         }
 
-        public void ImfManager_LanguageChanged(object sender, EventArgs args)
-        {
-            Tizen.Log.Debug("NUI", "ImfManager LanguageChanged!!!");
-            return;
-        }
+        //public void ImfManager_LanguageChanged(object sender, EventArgs args)
+        //{
+        //    Tizen.Log.Fatal("NUI", "ImfManager LanguageChanged!!!");
+        //    return;
+        //}
 
         [STAThread]
         static void _Main(string[] args)
