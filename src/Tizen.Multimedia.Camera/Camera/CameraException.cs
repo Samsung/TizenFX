124bab99450b73ba40bf2dd462ec05ddb000ce41
Haesu Gwon <haesu.gwon@samsung.com>
2017-09-28 14:43:16 +0900

    [Camera] Fix XML documentation warnings
    Change-Id: I4767ce5f9485e3e98c55528e51c50064af56ca9d
    Signed-off-by: Haesu Gwon <haesu.gwon@samsung.com>

--- src/Tizen.Multimedia.Camera/Camera/CameraException.cs
+++ src/Tizen.Multimedia.Camera/Camera/CameraException.cs
@@ -18,34 +18,61 @@ using System;
 
 namespace Tizen.Multimedia
 {
+    /// <summary>
+    /// The base exception class that is thrown when specific camera related error occurs.
+    /// </summary>
     public class CameraException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraException"/> class.
+        /// </summary>
         public CameraException() : base()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraException"/> class with a specified error message.
+        /// </summary>
         public CameraException(string message) : base(message)
         {
         }
     }
 
+    /// <summary>
+    /// The exception that is thrown when a camera device-related error occurs.
+    /// </summary>
     public class CameraDeviceException : CameraException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraDeviceException"/> class.
+        /// </summary>
         public CameraDeviceException() : base()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraDeviceException"/> class with a specified error message.
+        /// </summary>
         public CameraDeviceException(string message) : base(message)
         {
         }
     }
 
+    /// <summary>
+    /// The exception that is thrown when a camera device is not available.
+    /// </summary>
     public class CameraDeviceNotFoundException : CameraException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraDeviceNotFoundException"/> class.
+        /// </summary>
         public CameraDeviceNotFoundException() : base()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CameraDeviceNotFoundException"/> class with a specified error message.
+        /// </summary>
         public CameraDeviceNotFoundException(string message) : base(message)
         {
         }
