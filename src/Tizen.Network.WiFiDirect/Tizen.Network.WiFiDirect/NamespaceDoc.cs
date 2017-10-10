61916b87335b4bcecad08e7b0cd6ffbe07fb1e51
Jiung <jiung.yu@samsung.com>
2017-10-10 10:35:26 +0900

    [Tizen.Network.WiFiDirect] Fix build warning
    Change-Id: I91aacf72b3b474b1b9c14dc414a4278c212d58e9
    Signed-off-by: Jiung <jiung.yu@samsung.com>

--- src/Tizen.Network.WiFiDirect/Tizen.Network.WiFiDirect/NamespaceDoc.cs
+++ src/Tizen.Network.WiFiDirect/Tizen.Network.WiFiDirect/NamespaceDoc.cs
@@ -1,7 +1,5 @@
-/**
-<summary>
-The Tizen.Network.WiFiDirect namespace provides classes to manage the settings of Wi-Fi Direct.
-In addition, this namespace provides classes to connect and disconnect remote devices using Wi-Fi Direct.
-</summary>
-*/
+/// <summary>
+/// The Tizen.Network.WiFiDirect namespace provides classes to manage the settings of Wi-Fi Direct.
+/// In addition, this namespace provides classes to connect and disconnect remote devices using Wi-Fi Direct.
+/// </summary>
 namespace Tizen.Network.WiFiDirect {}
