feda501cab9f83e64dc8f83221616ed12f5b1267
coderhyme <jhyo.kim@samsung.com>
2017-10-12 11:48:15 +0900

    [Multimedia] Added InternalsVisibleTo attribute for Radio.
    Change-Id: Icdabccea5bfb65913ac650b2dadead1bc57072fa
    Signed-off-by: coderhyme <jhyo.kim@samsung.com>

--- src/Tizen.Multimedia/AssemblyAttrs.cs
+++ src/Tizen.Multimedia/AssemblyAttrs.cs
@@ -19,56 +19,31 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.AudioIO, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.AudioIO, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.Camera, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.Camera, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.MediaCodec, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.MediaCodec, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.MediaPlayer, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.MediaPlayer, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.Recorder, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.Recorder, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.StreamRecorder, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.StreamRecorder, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.Remoting, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.Remoting, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.Util, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.Util, " + PublicKey.Value)]
 
-[assembly: InternalsVisibleTo("Tizen.Multimedia.Vision, " +
-    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
-    "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
-    "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
-    "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2")]
+[assembly: InternalsVisibleTo("Tizen.Multimedia.Vision, " + PublicKey.Value)]
+
+[assembly: InternalsVisibleTo("Tizen.Multimedia.Radio, " + PublicKey.Value)]
+
+internal static class PublicKey
+{
+    internal const string Value =
+        "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
+        "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
+        "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
+        "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2";
+}
