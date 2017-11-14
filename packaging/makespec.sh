944f48c06cf10a02e486b249e47368fab5ffb3b9
Wonyoung Choi <wy80.choi@samsung.com>
2017-11-14 13:32:56 +0900

    Add packaging/version.txt
    Change-Id: I9a2236bff9302429668bbc7f61e7fc629e8c10b5

---
+++ packaging/makespec.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_FILE=$(readlink -f $0)
+SCRIPT_DIR=$(dirname $SCRIPT_FILE)
+
+VERSION_FILE=$SCRIPT_DIR/version.txt
+RPMSPEC=$SCRIPT_DIR/csapi-tizenfx.spec
+RPMSPEC_IN=$RPMSPEC.in
+
+source $VERSION_FILE
+
+while getopts ":r:n:i:" opt; do
+  case $opt in
+    r) RPM_VERSION=$OPTARG ;;
+    n) NUGET_VERSION=$OPTARG ;;
+    i) INTERNAL_NUGET_VERSION=$OPTARG ;;
+    :) echo "Option -$OPTARG requires an argument."; exit 1 ;;
+  esac
+done
+
+RPM_VERSION=$RPM_VERSION+$RPM_VERSION_SUFFIX
+
+# Update RPM Spec
+echo "# Auto-generated from $(basename $RPMSPEC_IN) by makespec.sh" | cat - $RPMSPEC_IN > $RPMSPEC
+sed -i -e "s/@api_version@/$API_VERSION/g" $RPMSPEC
+sed -i -e "s/@rpm_version@/$RPM_VERSION/g" $RPMSPEC
+sed -i -e "s/@nuget_version@/$NUGET_VERSION/g" $RPMSPEC
+sed -i -e "s/@internal_nuget_version@/$INTERNAL_NUGET_VERSION/g" $RPMSPEC
