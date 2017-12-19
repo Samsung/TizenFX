eb5305473b9e1d273283e829dff69b5fa71e0ad5
Wonyoung Choi <wy80.choi@samsung.com>
2017-12-19 16:53:33 +0900

    [Build] Add a flag to skip native deps checking
    Change-Id: Id74d546589782df51fd7b9fc0ef33165a22e3555

--- packaging/makespec.sh
+++ packaging/makespec.sh
@@ -7,13 +7,16 @@ VERSION_FILE=$SCRIPT_DIR/version.txt
 RPMSPEC=$SCRIPT_DIR/csapi-tizenfx.spec
 RPMSPEC_IN=$RPMSPEC.in
 
+CHECK_NATIVE_DEPS=1
+
 source $VERSION_FILE
 
-while getopts ":r:n:i:" opt; do
+while getopts ":r:n:i:c:" opt; do
   case $opt in
     r) RPM_VERSION=$OPTARG ;;
     n) NUGET_VERSION=$OPTARG ;;
     i) INTERNAL_NUGET_VERSION=$OPTARG ;;
+    c) CHECK_NATIVE_DEPS=$OPTARG ;;
     :) echo "Option -$OPTARG requires an argument."; exit 1 ;;
   esac
 done
@@ -27,3 +30,4 @@ sed -i -e "s/@rpm_version@/$RPM_VERSION/g" $RPMSPEC
 sed -i -e "s/@nuget_version@/$NUGET_VERSION/g" $RPMSPEC
 sed -i -e "s/@internal_nuget_version@/$INTERNAL_NUGET_VERSION/g" $RPMSPEC
 sed -i -e "s/@dali_version@/$DALI_VERSION/g" $RPMSPEC
+sed -i -e "s/@check_native_deps@/$CHECK_NATIVE_DEPS/g" $RPMSPEC
