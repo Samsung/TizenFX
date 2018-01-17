5428555f003a8f5e9ee47248f986eb7ede9bb9e4
Chungryeol Lim <cdark.lim@samsung.com>
2018-01-17 20:30:32 +0900

    [ElmSharp] fix svace issue

--- src/ElmSharp/ElmSharp/GenItemClass.cs
+++ src/ElmSharp/ElmSharp/GenItemClass.cs
@@ -217,8 +217,8 @@ namespace ElmSharp
 
         void EvasObjectDeleted(object sender, EventArgs e)
         {
-            IntPtr handle = (sender as EvasObject).Handle;
-            s_HandleToEvasObject.Remove(handle);
+            if(sender is EvasObject evasObject)
+                s_HandleToEvasObject.Remove(evasObject);
         }
 
         IntPtr GetReusableContentCallback(IntPtr data, IntPtr obj, IntPtr part, IntPtr old)
