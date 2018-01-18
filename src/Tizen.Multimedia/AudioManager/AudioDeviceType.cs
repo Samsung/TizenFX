713fdc7ae2679a43efef28d64b523d7faf1b3b2a
coderhyme <jhyo.kim@samsung.com>
2018-01-18 17:37:33 +0900

    [Multimedia] Moved enums of AudioManager into separate files.
    Signed-off-by: coderhyme <jhyo.kim@samsung.com>

---
+++ src/Tizen.Multimedia/AudioManager/AudioDeviceType.cs
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Tizen.Multimedia
+{
+    /// <summary>
+    /// Specifies the audio device types.
+    /// </summary>
+    /// <since_tizen> 3 </since_tizen>
+    public enum AudioDeviceType
+    {
+        /// <summary>
+        /// Built-in speaker.
+        /// </summary>
+        BuiltinSpeaker,
+
+        /// <summary>
+        /// Built-in receiver.
+        /// </summary>
+        BuiltinReceiver,
+
+        /// <summary>
+        /// Built-in microphone.
+        /// </summary>
+        BuiltinMic,
+
+        /// <summary>
+        /// Audio jack that can be connected to wired accessories such as headphones and headsets.
+        /// </summary>
+        AudioJack,
+
+        /// <summary>
+        /// Bluetooth media (A2DP).
+        /// </summary>
+        BluetoothMedia,
+
+        /// <summary>
+        /// HDMI.
+        /// </summary>
+        Hdmi,
+
+        /// <summary>
+        /// Device for forwarding.
+        /// </summary>
+        Forwarding,
+
+        /// <summary>
+        /// USB audio.
+        /// </summary>
+        UsbAudio,
+
+        /// <summary>
+        /// Bluetooth voice (SCO).
+        /// </summary>
+        BluetoothVoice
+    }
+}
---
+++ src/Tizen.Multimedia/AudioManager/AudioStreamType.cs
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Tizen.Multimedia
+{
+    /// <summary>
+    /// Specifies the audio stream types.
+    /// </summary>
+    /// <since_tizen> 3 </since_tizen>
+    public enum AudioStreamType
+    {
+        /// <summary>
+        /// Media.
+        /// </summary>
+        Media,
+
+        /// <summary>
+        /// System.
+        /// </summary>
+        System,
+
+        /// <summary>
+        /// Alarm.
+        /// </summary>
+        Alarm,
+
+        /// <summary>
+        /// Notification.
+        /// </summary>
+        Notification,
+
+        /// <summary>
+        /// Emergency.
+        /// </summary>
+        Emergency,
+
+        /// <summary>
+        /// Voice information.
+        /// </summary>
+        VoiceInformation,
+
+        /// <summary>
+        /// Voice recognition.
+        /// </summary>
+        VoiceRecognition,
+
+        /// <summary>
+        /// Ringtone for VoIP.
+        /// </summary>
+        RingtoneVoip,
+
+        /// <summary>
+        /// VoIP.
+        /// </summary>
+        Voip,
+
+        /// <summary>
+        /// Media only for external devices.
+        /// </summary>
+        MediaExternalOnly
+    }
+}
