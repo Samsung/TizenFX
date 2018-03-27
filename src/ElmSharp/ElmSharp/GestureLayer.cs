bd84f34ad5e7673be0f9293d27237cb65ccb6a9a
arosis78 <35049857+arosis78@users.noreply.github.com>
2018-03-27 15:52:09 +0900

    [ElmSharp] cherry-pick from API4 (#28)(#42)(#58) (#174)
    * [ElmSharp] Remove layout for Background (#28)
    
    Signed-off-by: Jeonghyun Yun <jh0506.yun@samsung.com>
    
    * [ElmSharp] fix svace issue (#42)
    
    Add an optional extended description…
    
    * [ElmSharp] Changed Background Color property internal implementation to Widget BackgroundColor property (#58)
    
    Signed-off-by: Jeonghyun Yun <jh0506.yun@samsung.com>

--- src/ElmSharp/ElmSharp/GestureLayer.cs
+++ src/ElmSharp/ElmSharp/GestureLayer.cs
@@ -559,11 +559,14 @@ namespace ElmSharp
             // thanks to the fact that we never remove item from _handlers, we don't need a lock here
             if (handlerIndex < 0 || handlerIndex >= _handlers.Count)
                 return;
-            Action<object> action = _handlers[handlerIndex].Action;
+
+            var currentHandler = _handlers[handlerIndex];
+            Action<object> action = currentHandler.Action;
             if (action == null)
                 return;
+
             // the interpretation of the event_info struct pointer depends on the GestureType
-            switch (_handlers[handlerIndex].Type)
+            switch (currentHandler.Type)
             {
                 case GestureType.Tap:
                 case GestureType.LongTap:
