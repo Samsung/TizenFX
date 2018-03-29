bd6a533008dd1b21cbbf6d916e1ef7eaf0b4b43a
Jay Cho <chojoong@gmail.com>
2018-03-29 18:54:14 +0900

    Update Button.cs

--- src/ElmSharp/ElmSharp/Button.cs
+++ src/ElmSharp/ElmSharp/Button.cs
@@ -105,13 +105,9 @@ namespace ElmSharp
             {
                 return Interop.Elementary.elm_button_autorepeat_get(RealHandle);
             }
-        }
-        
-        public bool AutoRepeatTest
-        {
-            get
+            set
             {
-                return Interop.Elementary.elm_button_autorepeat_get(RealHandle);
+                Interop.Elementary.elm_button_autorepeat_set(RealHandle, value);
             }
         }
 
