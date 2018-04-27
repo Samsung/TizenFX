635080e99398ee95601fd165438c0dd04afe15f8
chleun.moon <chleun.moon@samsung.com>
2018-04-27 09:31:38 +0900

    [Connection] Do not add events in case of error

--- src/Tizen.Network.Connection/Tizen.Network.Connection/ConnectionProfile.cs
+++ src/Tizen.Network.Connection/Tizen.Network.Connection/ConnectionProfile.cs
@@ -110,6 +110,7 @@ namespace Tizen.Network.Connection
             if ((ConnectionError)ret != ConnectionError.None)
             {
                 Log.Error(Globals.LogTag, "It failed to register callback for changing profile state, " + (ConnectionError)ret);
+                ConnectionErrorFactory.ThrowConnectionException(ret);
             }
         }
 
@@ -120,6 +121,7 @@ namespace Tizen.Network.Connection
             if ((ConnectionError)ret != ConnectionError.None)
             {
                 Log.Error(Globals.LogTag, "It failed to unregister callback for changing profile state, " + (ConnectionError)ret);
+                ConnectionErrorFactory.ThrowConnectionException(ret);
             }
         }
 
