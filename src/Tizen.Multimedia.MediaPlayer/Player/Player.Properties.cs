f1074a5db10b3a66fa848623534800710b914391
nam <just.nam@samsung.com>
2018-06-20 11:03:08 +0900

    [MediaPlayer] before progressing ACR, modified structure
    1. remove StreamingSetting class for previous API sets.
    2. remove replaygain to consider extendabilities.

--- src/Tizen.Multimedia.MediaPlayer/Player/Player.Properties.cs
+++ src/Tizen.Multimedia.MediaPlayer/Player/Player.Properties.cs
@@ -42,6 +42,148 @@ namespace Tizen.Multimedia
             }
         }
 
+        #region Network configuration
+        /// <summary>
+        /// Represents properties for streaming buffering time
+        /// </summary>
+        /// <since_tizen> 5 </since_tizen>
+        public struct StreamingBufferingTime
+        {
+            /// <summary>
+            /// Initializes a new instance of the StreamingBufferingTime struct.
+            /// </summary>
+            /// <param name="preBuffMs">The buffer time to start playback.</param>
+            /// <param name="reBuffMs">The buffer time during playback if player enter pause state for buffering.</param>
+            /// <since_tizen> 5 </since_tizen>
+            public StreamingBufferingTime(int preBuffMs, int reBuffMs)
+            {
+                PreBuffMs = preBuffMs;
+                ReBuffMs = reBuffMs;
+            }
+
+            /// <summary>
+            /// Gets or sets the buffer time to start playback.
+            /// </summary>
+            /// <since_tizen> 5 </since_tizen>
+            public int PreBuffMs
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// Gets or sets the buffer time during playback if player enter pause state for buffering.
+            /// </summary>
+            /// <since_tizen> 5 </since_tizen>
+            public int ReBuffMs
+            {
+                get;
+                set;
+            }
+        }
+        private string _cookie = "";
+        private string _userAgent = "";
+
+        /// <summary>
+        /// Gets or sets the cookie for streaming playback.
+        /// </summary>
+        /// <remarks>To set, the player must be in the <see cref="PlayerState.Idle"/> state.</remarks>
+        /// <exception cref="InvalidOperationException">The player is not in the valid state.</exception>
+        /// <exception cref="ObjectDisposedException">The player has already been disposed of.</exception>
+        /// <exception cref="ArgumentNullException">The value to set is null.</exception>
+        /// <since_tizen> 3 </since_tizen>
+        public string Cookie
+        {
+            get
+            {
+                return _cookie;
+            }
+            set
+            {
+                ValidatePlayerState(PlayerState.Idle);
+
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value), "Cookie can't be null.");
+                }
+
+                NativePlayer.SetStreamingCookie(Handle, value, value.Length).
+                    ThrowIfFailed(this, "Failed to set the cookie to the player");
+
+                _cookie = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the user agent for streaming playback.
+        /// </summary>
+        /// <remarks>To set, the player must be in the <see cref="PlayerState.Idle"/> state.</remarks>
+        /// <exception cref="InvalidOperationException">The player is not in the valid state.</exception>
+        /// <exception cref="ObjectDisposedException">The player has already been disposed of.</exception>
+        /// <exception cref="ArgumentNullException">The value to set is null.</exception>
+        /// <since_tizen> 3 </since_tizen>
+        public string UserAgent
+        {
+            get
+            {
+                return _userAgent;
+            }
+            set
+            {
+                ValidatePlayerState(PlayerState.Idle);
+
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value), "UserAgent can't be null.");
+                }
+
+                NativePlayer.SetStreamingUserAgent(Handle, value, value.Length).
+                    ThrowIfFailed(this, "Failed to set the user agent to the player");
+
+                _userAgent = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the streaming buffering time.
+        /// </summary>
+        /// <remarks>To set, the player must be in the <see cref="PlayerState.Idle"/> state.</remarks>
+        /// <exception cref="InvalidOperationException">The player is not in the valid state.</exception>
+        /// <exception cref="ObjectDisposedException">The player has already been disposed of.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     <pramref name="PreBuffMs"/> is less than 0.<br/>
+        ///     -or-<br/>
+        ///     <pramref name="ReBuffMs"/> is less than 0.<br/>
+        /// </exception>
+        /// <exception cref="ArgumentException">The value is not valid.</exception>
+        /// <seealso cref="StreamingBufferingTime"/>
+        /// <since_tizen> 5 </since_tizen>
+        public StreamingBufferingTime BuffTime
+        {
+            get
+            {
+                ValidateNotDisposed();
+
+                NativePlayer.GetStreamingBufferingTime(Handle, out var PreBuffMs, out var ReBuffMs).
+                        ThrowIfFailed(this, "Failed to get the buffering time of the player");
+
+                return new StreamingBufferingTime(PreBuffMs, ReBuffMs);
+            }
+            set
+            {
+                ValidatePlayerState(PlayerState.Idle);
+
+                if (value.PreBuffMs < 0 || value.ReBuffMs < 0)
+                {
+                    throw new ArgumentOutOfRangeException("invalid range");
+                }
+
+                NativePlayer.SetStreamingBufferingTime(Handle, value.PreBuffMs, value.ReBuffMs).
+                    ThrowIfFailed(this, "Failed to set the buffering time of the player");
+            }
+        }
+        #endregion
+
         /// <summary>
         /// Gets the state of the player.
         /// </summary>
@@ -380,31 +522,6 @@ namespace Tizen.Multimedia
             }
         }
 
-        /// <summary>
-        /// Gets or sets the player's replaygain state.
-        /// </summary>
-        /// <value>If the replaygain status is true, replaygain is applied (if contents has a replaygain tag);
-        /// otherwise, the replaygain isn't affected by tag and properties.</value>
-        /// <exception cref="ObjectDisposedException">The player has already been disposed of.</exception>
-        /// <exception cref="ArgumentException">The value is not valid.</exception>
-        /// <since_tizen> 5 </since_tizen>
-        public bool Replaygain
-        {
-            get
-            {
-                ValidateNotDisposed();
-                NativePlayer.IsReplaygain(Handle, out var value).
-                    ThrowIfFailed(this, "Failed to get the replaygain of the player");
-                return value;
-            }
-            set
-            {
-                ValidateNotDisposed();
-                NativePlayer.SetReplaygain(Handle, value).
-                    ThrowIfFailed(this, "Failed to set the replaygain of the player");
-            }
-        }
-
         private SphericalVideo _sphericalVideo;
 
         /// <summary>
@@ -421,25 +538,6 @@ namespace Tizen.Multimedia
                 }
 
                 return _sphericalVideo;
-            }
-        }
-
-        private StreamingSettings _streamingSettings;
-
-        /// <summary>
-        /// Gets the streaming settings.
-        /// </summary>
-        /// <since_tizen> 5 </since_tizen>
-        public StreamingSettings StreamingSettings
-        {
-            get
-            {
-                if (_streamingSettings == null)
-                {
-                    _streamingSettings = new StreamingSettings(this);
-                }
-
-                return _streamingSettings;
             }
         }
 
