6d459a8509404a0b24939453a285234785516d29
Haesu Gwon <haesu.gwon@samsung.com>
2018-08-09 11:06:12 +0900

    [Camera] Add new preview data format and type

--- src/Tizen.Multimedia.Camera/Camera/PreviewFrame.cs
+++ src/Tizen.Multimedia.Camera/Camera/PreviewFrame.cs
@@ -46,6 +46,10 @@ namespace Tizen.Multimedia
                 {
                     return new EncodedPlane(unmanagedStruct.Plane.EncodedPlane);
                 }
+                else if (unmanagedStruct.Format == CameraPixelFormat.Invz)
+                {
+                    return new DepthPlane(unmanagedStruct.Plane.DepthPlane);
+                }
                 else
                 {
                     return new SinglePlane(unmanagedStruct.Plane.SinglePlane);
@@ -71,6 +75,10 @@ namespace Tizen.Multimedia
                 if (unmanagedStruct.Format == CameraPixelFormat.H264 || unmanagedStruct.Format == CameraPixelFormat.Jpeg)
                 {
                     return PlaneType.EncodedPlane;
+                }
+                else if (unmanagedStruct.Format == CameraPixelFormat.Invz)
+                {
+                    return PlaneType.DepthPlane;
                 }
                 else
                 {
