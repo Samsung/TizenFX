e91105670e748059c0836ce72869b310b346ccc3
prjung <33618885+prjung@users.noreply.github.com>
2018-08-13 11:15:25 +0900

    [Device]Add new apis (#382)
    * [Device]Add new apis and deprecate old apis
    
    - Add set function for State property on Display class
    - Add PowerLock enumeration
    - Add RequestLock and ReleaseLock on Power class
    - Deprecate RequestCpuLock and ReleaseCpuLock on Power class
    
    Signed-off-by: pr.jung <pr.jung@samsung.com>
    
    * Update Power.cs
    
    * Update Power.cs

--- src/Tizen.System/Device/Power.cs
+++ src/Tizen.System/Device/Power.cs
@@ -19,6 +19,29 @@ using System;
 namespace Tizen.System
 {
     /// <summary>
+    /// Enumeration for power lock type.
+    /// </summary>
+    /// <since_tizen> 5 </since_tizen>
+    public enum PowerLock
+    {
+        /// <summary>
+        /// CPU lock.
+        /// </summary>
+        /// <since_tizen> 5 </since_tizen>
+        Cpu = Interop.Device.PowerLock.Cpu,
+        /// <summary>
+        /// Display normal lock
+        /// </summary>
+        /// <since_tizen> 5 </since_tizen>
+        DisplayNormal = Interop.Device.PowerLock.DisplayNormal,
+        /// <summary>
+        /// Display dim lock
+        /// </summary>
+        /// <since_tizen> 5 </since_tizen>
+        DisplayDim = Interop.Device.PowerLock.DisplayDim,
+    }
+
+    /// <summary>
     /// The Power class provides methods to control the power service.
     /// </summary>
     /// <remarks>
@@ -46,34 +69,80 @@ namespace Tizen.System
         /// <exception cref="ArgumentException">When an invalid parameter value is set.</exception>
         /// <exception cref="UnauthorizedAccessException">If the privilege is not set.</exception>
         /// <exception cref="InvalidOperationException">In case of any system error.</exception>
-        /// <example>
-        /// <code>
-        /// Tizen.System.Power.RequestCpuLock(2000);
-        /// </code>
-        /// </example>
-
         public static void RequestCpuLock(int timeout)
         {
-            DeviceError res = (DeviceError)Interop.Device.DevicePowerRequestLock(0, timeout);
+            DeviceError res = (DeviceError)Interop.Device.DevicePowerRequestLock(Interop.Device.PowerLock.Cpu, timeout);
             if (res != DeviceError.None)
             {
                 throw DeviceExceptionFactory.CreateException(res, "unable to acquire power lock.");
             }
         }
         /// <summary>
-        /// Releases the CPU lock state.
+        /// Release the CPU lock state.
         /// </summary>
         /// <since_tizen> 3 </since_tizen>
         /// <exception cref="UnauthorizedAccessException">If the privilege is not set.</exception>
         /// <exception cref="InvalidOperationException">In case of any system error.</exception>
-        /// <example>
-        /// <code>
-        /// Tizen.System.Power.ReleaseCpuLock();
-        /// </code>
-        /// </example>
         public static void ReleaseCpuLock()
         {
-            DeviceError res = (DeviceError)Interop.Device.DevicePowerReleaseLock(0);
+            DeviceError res = (DeviceError)Interop.Device.DevicePowerReleaseLock(Interop.Device.PowerLock.Cpu);
+            if (res != DeviceError.None)
+            {
+                throw DeviceExceptionFactory.CreateException(res, "unable to release power lock.");
+            }
+        }
+
+        /// <summary>
+        /// Locks the given lock state for a specified time.
+        /// After the given timeout (in milliseconds), unlock the given lock state automatically.
+        /// </summary>
+        /// <remarks>
+        /// If the process dies, then every lock will be removed.
+        /// </remarks>
+        /// <since_tizen> 5 </since_tizen>
+        /// <param name="type">
+        /// The power type to request lock
+        /// </param>
+        /// <param name="timeout">
+        /// The positive number in milliseconds or 0 for the permanent lock.
+        /// So you must release the permanent lock of the power state with ReleaseLock() if timeout_ms is zero.
+        /// </param>
+        /// <exception cref="ArgumentException">When an invalid parameter value is set.</exception>
+        /// <exception cref="UnauthorizedAccessException">If the privilege is not set.</exception>
+        /// <exception cref="NotSupportedException">If power lock is not supported.</exception>
+        /// <exception cref="InvalidOperationException">In case of any system error.</exception>
+        /// <example>
+        /// <code>
+        /// Tizen.System.Power.RequestLock(Tizen.System.Power.PowerLock.Cpu, 2000);
+        /// </code>
+        /// </example>
+        public static void RequestLock(PowerLock type, int timeout)
+        {
+            DeviceError res = (DeviceError)Interop.Device.DevicePowerRequestLock((Interop.Device.PowerLock)type, timeout);
+            if (res != DeviceError.None)
+            {
+                throw DeviceExceptionFactory.CreateException(res, "unable to acquire power lock.");
+            }
+        }
+        /// <summary>
+        /// Releases the lock state.
+        /// </summary>
+        /// <since_tizen> 5 </since_tizen>
+        /// <param name="type">
+        /// The power type to request lock
+        /// </param>
+        /// <exception cref="ArgumentException">When an invalid parameter value is set.</exception>
+        /// <exception cref="UnauthorizedAccessException">If the privilege is not set.</exception>
+        /// <exception cref="NotSupportedException">If power lock is not supported.</exception>
+        /// <exception cref="InvalidOperationException">In case of any system error.</exception>
+        /// <example>
+        /// <code>
+        /// Tizen.System.Power.ReleaseLock(Tizen.System.Power.PowerLock.Cpu);
+        /// </code>
+        /// </example>
+        public static void ReleaseLock(PowerLock type)
+        {
+            DeviceError res = (DeviceError)Interop.Device.DevicePowerReleaseLock((Interop.Device.PowerLock)type);
             if (res != DeviceError.None)
             {
                 throw DeviceExceptionFactory.CreateException(res, "unable to release power lock.");
