ded00fd5b538db3af958509fa6e575a2ebaf7132
dongsug-song <35130733+dongsug-song@users.noreply.github.com>
2018-08-22 17:14:41 +0900

    [NUI] VisualMap, friend assembly, InputMethodContext related changes (#406)
    * [NUI] Add DestroyContext call before disposing TextField/TextEditor
    
    In order to speed up IME hide, temporarily add DestroyContext call.
    
    Change-Id: Id93c99f4d45061d4e406d36ced7a0ec09c8f2c34
    
    * [NUI] VisualMap revising
    
    Here are some changes:
    Add VisualFittingMode property
    Add Atlasing property
    Add Shadow,Underline,Outline,Background property
    Add RenderIfTransparent property
    
    Change-Id: I8d85f9d1a438cb6fbb636f79c7438e9134e1df8f
    
    * [NUI] Add visual test example
    
    Change-Id: I25f41d969f7b98a494a4fa1716b0335c09ab5026
    
    * [NUI] Add friend assembly for Tizen.TV.NUI.Component
    
    Change-Id: I2fdc02205c03aa98260035fb81998d68514c030e

--- src/Tizen.NUI/src/public/BaseComponents/TextField.cs
+++ src/Tizen.NUI/src/public/BaseComponents/TextField.cs
@@ -1025,6 +1025,8 @@ namespace Tizen.NUI.BaseComponents
                 if (swigCMemOwn)
                 {
                     swigCMemOwn = false;
+                    // In order to speed up IME hide, temporarily add
+                    GetInputMethodContext()?.DestroyContext();
                     NDalicPINVOKE.delete_TextField(swigCPtr);
                 }
                 swigCPtr = new global::System.Runtime.InteropServices.HandleRef(null, global::System.IntPtr.Zero);
