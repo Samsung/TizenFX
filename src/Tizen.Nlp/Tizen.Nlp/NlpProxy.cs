5537afeb7065bb46df6f1651f55028e99ffb757c
WonYoung Choi <wy80.choi@samsung.com>
2018-09-04 19:00:41 +0900

    [NLP] hot fix to avoid dummy build error

--- src/Tizen.Nlp/Tizen.Nlp/NlpProxy.cs
+++ src/Tizen.Nlp/Tizen.Nlp/NlpProxy.cs
@@ -168,7 +168,10 @@ namespace Tizen.Nlp
             } while (true);
         }
 
-        public Message(string appId) => _appId = appId;
+        public Message(string appId)
+        {
+            _appId = appId;
+        }
 
         public void Connect()
         {
