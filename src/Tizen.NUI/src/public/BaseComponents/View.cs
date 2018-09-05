1e4d96db1446ea5a1bf522d66c2e4899baf5be6f
Seoyeon2Kim <34738918+Seoyeon2Kim@users.noreply.github.com>
2018-09-05 17:52:44 +0900

    Update View.cs
    - Changed notes to <remarks> </remarks>

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -5339,10 +5339,12 @@ namespace Tizen.NUI.BaseComponents
         }
 
         /// <summary>
-        /// Gets or sets the Margin for use in layout.
-        /// Note : Margin property is supported by Layout algorithms and containers.
-        ///        Please Set Layout if you want to use Margin property.
+        /// Gets or sets the Margin for use in layout.   
         /// </summary>
+        /// <remarks>
+        /// Margin property is supported by Layout algorithms and containers.
+        /// Please Set Layout if you want to use Margin property.
+        /// </remarks>
         /// <since_tizen> 4 </since_tizen>
         public Extents Margin
         {
