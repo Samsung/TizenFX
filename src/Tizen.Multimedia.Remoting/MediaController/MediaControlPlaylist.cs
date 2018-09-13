5abb655d88f13b1b69c2d59af1f236f31329093c
Haesu Gwon <haesu.gwon@samsung.com>
2018-09-13 18:23:42 +0900

    [MediaController] fix bug

--- src/Tizen.Multimedia.Remoting/MediaController/MediaControlPlaylist.cs
+++ src/Tizen.Multimedia.Remoting/MediaController/MediaControlPlaylist.cs
@@ -29,7 +29,7 @@ namespace Tizen.Multimedia.Remoting
     public class MediaControlPlaylist : IDisposable
     {
         private IntPtr _handle;
-        private Dictionary<string, MediaControlMetadata> _metadata;
+        private Dictionary<string, MediaControlMetadata> _metadata = null;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MediaControlPlaylist"/> class by server side.
