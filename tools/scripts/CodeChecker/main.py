5ff399254086f98898591c1fdd73280a36e1d472
Jay Cho <chojoong@gmail.com>
2018-09-18 15:22:23 +0900

    Update main.py

--- tools/scripts/CodeChecker/main.py
+++ tools/scripts/CodeChecker/main.py
@@ -5,12 +5,12 @@ from PRManager import PRManager
 logPath = "./build.log"
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print("Execute with a PR number")
-        print(" ~$ python main.py [PR Number]")
+    if len(sys.argv) < 3:
+        print("Execute with token and the PR number")
+        print(" ~$ python main.py [Token] [PR Number]")
         exit(1)
     logs = BuildLog(logPath)
-    pr = PRManager(int(sys.argv[1]))
+    pr = PRManager(sys.argv[1], int(sys.argv[2]))
     warningsInFile = []
     for file in pr.changedFiles:
         warningsInFile = [warning for warning in logs.warnings if file.filename.endswith(warning[logs.FILE])]
