d331682eaf8045f7e8fdb19ce78b91b37bdc5c83
Joonghyun Cho <jh5.cho@samsung.com>
2018-10-10 15:04:23 +0900

    [ElmSharp] Fix Focus Clearing issue

--- src/ElmSharp/Interop/Interop.Elementary.cs
+++ src/ElmSharp/Interop/Interop.Elementary.cs
@@ -91,6 +91,33 @@ internal static partial class Interop
             return Marshal.PtrToStringAnsi(_elm_config_profile_get());
         }
 
+        internal static void elm_object_focused_clear(IntPtr handle)
+        {
+            if (elm_widget_is(handle))
+            {
+                efl_ui_widget_focused_object_clear(handle);
+            }
+            else
+            {
+                Evas.evas_object_focus_set(handle, false);
+            }
+
+            IntPtr win = elm_widget_top_get(handle);
+            if (win != IntPtr.Zero && Eo.efl_class_name_get(Eo.efl_class_get(win)) == "Efl.Ui.Win")
+            {
+                Evas.evas_object_focus_set(win, true);
+            }
+        }
+
+        [DllImport(Libraries.Elementary)]
+        internal static extern IntPtr elm_widget_top_get(IntPtr handle);
+
+        [DllImport(Libraries.Elementary)]
+        internal static extern bool elm_widget_is(IntPtr handle);
+
+        [DllImport(Libraries.Elementary)]
+        internal static extern void efl_ui_widget_focused_object_clear(IntPtr handle);
+
         [DllImport(Libraries.Elementary)]
         internal static extern void elm_config_preferred_engine_set(string name);
 
