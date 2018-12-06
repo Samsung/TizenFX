d99899e838a799b7e27ef21de6db641ab00a5ab0
yeji kim <yeji01.kim@samsung.com>
2018-12-06 18:31:27 +0900

    Initial Commit
    - Interop
    - DevicePolicyManager class
    
    Signed-off-by: yeji kim <yeji01.kim@samsung.com>

---
+++ src/Tizen.Security.DevicePolicyManager/Interop/Interop.DevicePolicyManager.cs
@@ -0,0 +1,82 @@
+﻿/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class DevicePolicyManager
+    {
+        internal enum DpmError
+        {
+            None = Tizen.Internals.Errors.ErrorCode.None,
+            InvalidParameter = Tizen.Internals.Errors.ErrorCode.InvalidParameter,
+            TimeOut = Tizen.Internals.Errors.ErrorCode.TimedOut,
+            PermissionDenied = Tizen.Internals.Errors.ErrorCode.PermissionDenied,
+            ConnectionRefused = Tizen.Internals.Errors.ErrorCode.ConnectionRefused,
+            OutOfMemory = Tizen.Internals.Errors.ErrorCode.OutOfMemory
+        }
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_manager_create", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr CreateHandle();
+        // device_policy_manager_h dpm_manager_create(void)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_manager_destroy", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int DestroyHandle(IntPtr handle);
+        // int dpm_manager_destroy(device_policy_manager_h handle)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_expires", CallingConvention = CallingConvention.Cdecl), ]
+        internal static extern int PasswordGetExpires(IntPtr handle, out int value);
+        // int dpm_password_get_expires(device_policy_manager_h handle, int* value)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_history", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int PasswordGetHistory(IntPtr handle, out int value);
+        // int dpm_password_get_history(device_policy_manager_h handle, int* value)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_max_inactivity_time_device_lock", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int PasswordGetMaxInactivityTimeDeviceLock(IntPtr handle, out int value);
+        // int dpm_password_get_max_inactivity_time_device_lock(device_policy_manager_h handle, int* value)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_maximum_failed_attempts_for_wipe", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int PasswordGetMaximumFailedAttemptsForWipe(IntPtr handle, out int value);
+        // int dpm_password_get_maximum_failed_attempts_for_wipe(device_policy_manager_h handle, int* value)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_min_complex_chars", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int PasswordGetMinComplexChars(IntPtr handle, out int value);
+        // int dpm_password_get_min_complex_chars(device_policy_manager_h handle, int* value)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_minimum_length", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int PasswordGetMinimumLength(IntPtr handle, out int value);
+        // int dpm_password_get_minimum_length(device_policy_manager_h handle, int* value)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_password_get_quality", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int PasswordGetQuality(IntPtr handle, out int quality);
+        // int dpm_password_get_quality(device_policy_manager_h handle, int* quality)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_restriction_get_messaging_state", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int RestrictionGetMessagingState(IntPtr handle, string simId, out int allowed);
+        // int dpm_restriction_get_messaging_state(device_policy_manager_h handle, const char* sim_id, int* is_allowed)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_restriction_get_popimap_email_state", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int RestrictionGetPopimapEmailState(IntPtr handle, out int allowed);
+        // int dpm_restriction_get_popimap_email_state(device_policy_manager_h handle, int* is_allowed)
+
+        [DllImport(Libraries.DevicePolicyManager, EntryPoint = "dpm_restriction_get_wifi_state", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int RestrictionGetWifiState(IntPtr handle, out int allowed);
+        // int dpm_restriction_get_wifi_state(device_policy_manager_h handle, int* is_allowed)
+    }
+}
---
+++ src/Tizen.Security.DevicePolicyManager/Tizen.Security.DevicePolicyManager/DevicePolicyManager.cs
@@ -0,0 +1,133 @@
+﻿/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+using System;
+using System.Collections.Generic;
+using Tizen.Internals.Errors;
+
+namespace Tizen.Security.DevicePolicyManager
+{
+    /// <summary>
+    /// The DevicePolicyManager provides the methods to create handle for device policy.
+    /// </summary>
+    /// <since_tizen> 6 </since_tizen>
+    public class DevicePolicyManager : IDisposable
+    {
+        private IntPtr handle = IntPtr.Zero;
+        private bool disposed = false;
+
+        /// <summary>
+        /// A constructor of DevicePolicyManager that creates handle.
+        /// </summary>
+        /// <since_tizen> 6 </since_tizen>
+        public DevicePolicyManager()
+        {
+            handle = Interop.DevicePolicyManager.CreateHandle();
+        }
+
+        /// <summary>
+        /// Method to creates an instance of Device Policy.
+        /// </summary>
+        /// <typeparam name="T">The generic type to create.</typeparam>
+        /// <returns>An instance of policy.</returns>
+        /// <since_tizen> 6 </since_tizen>
+        public T GetPolicy<T>() where T : class
+        {
+            T policy = Activator.CreateInstance(typeof(T),
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
+                null, new object[] { this }, null) as T;
+
+            if (policy == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                return policy;
+            }
+        }
+
+        internal IntPtr GetHandle()
+        {
+            if (disposed)
+            {
+                return IntPtr.Zero;
+            }
+
+            return handle;
+        }
+
+        /// <summary>
+        /// A Destructor of DevicePolicyManager.
+        /// </summary>
+        ~DevicePolicyManager()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Releases any unmanaged resources used by this object. Can also dispose any other disposable objects.
+        /// </summary>
+        /// <param name="disposing">If true, disposes any disposable objects. If false, does not dispose disposable objects.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+ 
+            if (handle != IntPtr.Zero)
+            {
+                Interop.DevicePolicyManager.DestroyHandle(handle);
+                handle = IntPtr.Zero;
+            }
+
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Releases any unmanaged resources used by this object.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    internal static class DevicePolicyManagerErrorFactory
+    {
+        static internal Exception GetException(int error)
+        {
+            Interop.DevicePolicyManager.DpmError errCode = (Interop.DevicePolicyManager.DpmError)error;
+            switch (errCode)
+            {
+                case Interop.DevicePolicyManager.DpmError.InvalidParameter:
+                    return new ArgumentException("Invalid parameter");
+                case Interop.DevicePolicyManager.DpmError.TimeOut:
+                    return new TimeoutException("Timeout");
+                case Interop.DevicePolicyManager.DpmError.PermissionDenied:
+                    return new UnauthorizedAccessException("Permission Denied");
+                case Interop.DevicePolicyManager.DpmError.OutOfMemory:
+                    return new OutOfMemoryException("Out of memory");
+                case Interop.DevicePolicyManager.DpmError.ConnectionRefused:
+                    return new InvalidOperationException("Connection refused");
+                default:
+                    return new InvalidOperationException("Unknown Error Code");
+            }
+        }
+    }
+}
