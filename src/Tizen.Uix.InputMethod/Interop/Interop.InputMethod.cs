fb10c2494fa1da0d0837a23bf80c05fc7e136858
Inhong <tukkong123@naver.com>
2019-01-11 15:57:29 +0900

    [Inputmethod] Add missing APIs (#647)

--- src/Tizen.Uix.InputMethod/Interop/Interop.InputMethod.cs
+++ src/Tizen.Uix.InputMethod/Interop/Interop.InputMethod.cs
@@ -273,6 +273,24 @@ internal static partial class Interop
         [DllImport(Libraries.InputMethod, EntryPoint = "ime_update_input_panel_event")]
         internal static extern ErrorCode ImeUpdateInputPanelEvent(ImeEventType type, uint value);
 
+        [DllImport(Libraries.InputMethod, EntryPoint = "ime_get_selected_text")]
+        internal static extern ErrorCode ImeGetSelectedText(out IntPtr text);
+
+        [DllImport(Libraries.InputMethod, EntryPoint = "ime_event_set_prediction_hint_set_cb")]
+        internal static extern ErrorCode ImeEventSetPredictionHintSetCb(ImePredictionHintSetCb callbackFunction, IntPtr userData);
+
+        [DllImport(Libraries.InputMethod, EntryPoint = "ime_event_set_prediction_hint_data_set_cb")]
+        internal static extern ErrorCode ImeEventSetPredictionHintDataSetCb(ImePredictionHintDataSetCb callbackFunction, IntPtr userData);
+
+        [DllImport(Libraries.InputMethod, EntryPoint = "ime_event_set_mime_type_set_request_cb")]
+        internal static extern ErrorCode ImeEventSetMimeTypeSetRequestCb(ImeMimeTypeSetRequestCb callbackFunction, IntPtr userData);
+
+        [DllImport(Libraries.InputMethod, EntryPoint = "ime_send_private_command")]
+        internal static extern ErrorCode ImeSendPrivateCommand(string command);
+
+        [DllImport(Libraries.InputMethod, EntryPoint = "ime_commit_content")]
+        internal static extern ErrorCode ImeCommitContent(string content, string description, string mimeType);
+
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         internal delegate void ImeCreateCb(IntPtr userData);
 
@@ -335,5 +353,14 @@ internal static partial class Interop
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         internal delegate void ImeAccessibilityStateChangedCb(bool state, IntPtr userData);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void ImePredictionHintSetCb(IntPtr predictionHint, IntPtr userData);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void ImePredictionHintDataSetCb(IntPtr key, IntPtr keyValue, IntPtr userData);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void ImeMimeTypeSetRequestCb(IntPtr mimeType, IntPtr userData);
     }
 }
