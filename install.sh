ccc4a657b5936ac80c39994295f960d9c41752b6
Cheoleun Moon <chleun.moon@samsung.com>
2019-02-15 14:09:00 +0900

    remove unnecessary file

--- install.sh
+++
@@ -1,4 +0,0 @@
-sdb root on
-sdb shell mount -o remount,rw /
-sdb push ./Artifacts/bin/public/$1.dll /usr/share/dotnet.tizen/framework/
-sdb shell chsmack -a "_" /usr/share/dotnet.tizen/framework/$1.dll
