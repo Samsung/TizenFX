7cee2105880626c59894cb3c1f6f9e71c3543ec0
huiyueun <35286162+huiyueun@users.noreply.github.com>
2019-05-02 14:34:49 +0900

    Update ToggleButton.cs

--- src/Tizen.NUI/src/public/UIComponents/ToggleButton.cs
+++ src/Tizen.NUI/src/public/UIComponents/ToggleButton.cs
@@ -43,8 +43,7 @@ namespace Tizen.NUI
             return temp;
         });
 
-        /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
+        /// Only for XAML property binding. This will be changed as Inhouse API by ACR later.
         public static readonly BindableProperty TooltipsProperty = BindableProperty.Create("Tooltips", typeof(PropertyArray), typeof(ToggleButton), new PropertyArray(), propertyChanged: (bindable, oldValue, newValue) =>
         {
             var toggleButton = (ToggleButton)bindable;
