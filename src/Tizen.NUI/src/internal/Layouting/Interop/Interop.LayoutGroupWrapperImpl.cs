a9f9d0e56eda973b346b5838c1be06cbbb43b30c
huiyu.eun <huiyu.eun@samsung.com>
2019-05-03 13:28:13 +0900

    [NUI] Modify Layout interop
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

---
+++ src/Tizen.NUI/src/internal/Layouting/Interop/Interop.LayoutGroupWrapperImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class LayoutGroupWrapperImpl
+        {
+
+
+        }
+    }
+}
