96d46d1704c1711a2d81e2ea8d3291190c9c0d2a
Sangjung Woo <sangjung.woo@samsung.com>
2019-06-12 15:25:08 +0900

    [MachineLearning.Nnstreamer] Add Testcase for Nnstreamer Pipeline APIs
    This patch initially adds the Testcase for Nnstreamer Pipeline APIs.
    
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

---
+++ test/Tizen.MachineLearning.Nnstreamer.Test/App.cs
@@ -0,0 +1,106 @@
+﻿/*
+ *  Copyright (c) 2019 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+using System;
+
+using Xamarin.Forms;
+using Tizen.MachineLearning.Nnstreamer.Test;
+
+namespace XamarinForTizen.Tizen
+{
+    public class App : Application
+    {
+        Button btnPipeline;
+        Button btnSingle;
+        Label lblResult;
+        
+        
+        public App()
+        {
+            btnPipeline = new Button
+            {
+                Text = "Pipeline Test",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+            };
+            btnPipeline.Clicked += OnBtnPilelineClicked;
+
+            btnSingle = new Button
+            {
+                Text = "Single Test",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+            };
+            btnSingle.Clicked += OnBtnSingleClicked;
+
+            lblResult = new Label
+            {
+                Text = "",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Start,
+                    Children = {
+                        btnPipeline,
+                        btnSingle,
+                        lblResult,
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        private void OnBtnPilelineClicked(object s, EventArgs e)
+        {
+            string retMsg = "";
+            retMsg += "Pipeline Test Started\n\n";
+
+            retMsg += "  * ConstructDestructTest_Success00: ";
+            retMsg += PipelineTest.ConstructDestructTest_Success00() ? "OK\n" : "Failed!\n";
+
+            retMsg += "  * ConstructDestructTest_Failure00: ";
+            retMsg += PipelineTest.ConstructDestructTest_Failure00() ? "OK\n" : "Failed!\n";
+
+            retMsg += "\nPipeline Test Done";
+
+            lblResult.Text = retMsg;
+        }
+
+        private void OnBtnSingleClicked(object s, EventArgs e)
+        {
+            lblResult.Text = "Single Test Started\n";
+            lblResult.Text += "Single Test is Done\n";
+        }
+    }
+}
---
+++ test/Tizen.MachineLearning.Nnstreamer.Test/PipelineTest.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.IO;
+using Tizen.MachineLearning.Nnstreamer;
+
+namespace Tizen.MachineLearning.Nnstreamer.Test
+{
+    public static class PipelineTest
+    {
+        const string TAG = "PipelineTest";
+
+        public static bool ConstructDestructTest_Success00()
+        {
+            bool ret = true;
+            Pipeline p = new Pipeline();
+
+            try
+            {
+                p.Construct("videotestsrc num_buffers=2 ! fakesink");
+                p.Destroy();
+            }
+            catch (Exception e)
+            {
+                Tizen.Log.Error(TAG, "Fail to ConstructDestructTest_Success00: " + e.Message);
+                ret = false;
+            }
+            return ret;
+        }
+
+        public static bool ConstructDestructTest_Failure00()
+        {
+            bool ret = false;
+            Pipeline p = new Pipeline();
+
+            try
+            {
+                p.Construct("nonexistsrc ! fakesink");
+            }
+            catch (IOException)
+            {
+                Tizen.Log.Error(TAG, "ConstructDestructTest_Failure00 test passed!!");
+                ret = true;
+            }
+
+            if (!ret)
+                Tizen.Log.Error(TAG, "Fail to ConstructDestructTest_Failure00");
+            return ret;
+        }
+    }
+}
