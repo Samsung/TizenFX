874f2cdc3bf39b603bbef6c757eb4d6d9424b17c
Seoyeon2Kim <34738918+Seoyeon2Kim@users.noreply.github.com>
2019-07-02 21:29:57 +0900

    [NUI] Add AutofillContainer and autofill implementation (#909)
    Change-Id: Ifc219498c53577e6560b84a8065c22c3b03e2859
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

---
+++ src/Tizen.NUI/src/internal/Interop/Interop.ListEventSignalType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class ListEventSignalType
+        {
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListShownSignalType_Empty")]
+            public static extern bool ListEventSignalType_Empty(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListShownSignalType_GetConnectionCount")]
+            public static extern uint ListEventSignalType_GetConnectionCount(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListShownSignalType_Connect")]
+            public static extern void ListEventSignalType_Connect(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListShownSignalType_Disconnect")]
+            public static extern void ListEventSignalType_Disconnect(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListShownSignalType_Emit")]
+            public static extern void ListEventSignalType_Emit(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_ListShownSignalType")]
+            public static extern global::System.IntPtr new_ListEventSignalType();
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_ListShownSignalType")]
+            public static extern void delete_ListEventSignalType(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+        }
+    }
+}
