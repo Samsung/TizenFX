e96e398309b5cdaa05bf2cbb85fb7c1c1ead6097
Haesu Gwon <haesu.gwon@samsung.com>
2019-07-17 15:02:39 +0900

    [ThumbnailExtractor] Add description for Extract method

--- src/Tizen.Multimedia.Util/ThumbnailExtractor/ThumbnailExtractor.cs
+++ src/Tizen.Multimedia.Util/ThumbnailExtractor/ThumbnailExtractor.cs
@@ -246,6 +246,8 @@ namespace Tizen.Multimedia.Util
         /// </summary>
         /// <remarks>
         /// The size of generated thumbnail will be 320x240.<br/>
+        /// But, if input image have different ratio with 320x240 (approximately 1.33:1),<br/>
+        /// thumbnail could be generated with different size. (Modify based on short axis)<br/>
         /// If you want to set the size of generated thumbnail, please use <see cref="Extract(string, Size)"/><br/>
         /// <br/>
         /// If you want to access internal storage, you should add privilege http://tizen.org/privilege/mediastorage. <br/>
@@ -336,6 +338,8 @@ namespace Tizen.Multimedia.Util
         /// </summary>
         /// <remarks>
         /// The size of generated thumbnail will be 320x240.<br/>
+        /// But, if input image have different ratio with 320x240 (approximately 1.33:1),<br/>
+        /// thumbnail could be generated with different size. (Modify based on short axis)<br/>
         /// If you want to set the size of generated thumbnail, please use <see cref="Extract(string, Size, string)"/><br/>
         /// <br/>
         /// If you want to access internal storage, you should add privilege http://tizen.org/privilege/mediastorage. <br/>
