3525162109ca6ab1fb73520e6b7bcb56c3aa1dfc
hyunho <hhstark.kang@samsung.com>
2019-08-08 17:26:52 +0900

    Sync with native api name modification
    - frame_component_get_resource_id
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>

--- src/Tizen.Applications.ComponentBased.Default/Interop/Interop.EflCbApplication.cs
+++ src/Tizen.Applications.ComponentBased.Default/Interop/Interop.EflCbApplication.cs
@@ -7,7 +7,7 @@ internal static partial class Interop
 {
     internal static partial class EflCBApplication
     {
-        [DllImport(Libraries.CompApplication, EntryPoint = "component_based_app_get_resource_id")]
+        [DllImport(Libraries.CompApplication, EntryPoint = "frame_component_get_resource_id")]
         internal static extern int GetResourceId(IntPtr win, out int resId);
     }
 }
