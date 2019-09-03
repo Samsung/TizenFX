59a3d5c48d508826ca4289e4fb76518618d8f004
Hyunho Kang <hhstark.kang@samsung.com>
2019-09-03 15:12:06 +0900

    [Tizen.Applications.ComponentBased][TCSACR-265][Add] Add ComponentBased application APIs (#977)
    * Add component-based application
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Remove error file
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * [ComponentBased] The initial impelementation of ComponentBased Application
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Sync with native api name modification
    
    - frame_component_get_resource_id
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Add component-type to base-component
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Add app-event handling logic
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Remove OnAction from component lifecycle
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Add launch request API
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Add missing implementations
    
    - Get Parent
    - FrameComponent's DisplayStatus
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Fix naming issue
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Modify unecessary internel setter to private
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Modify component parent property setter to internal
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Fix typo
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Change OnFini to OnFinished
    
    To use verbose field name
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Change a property name Window to WindowInfo
    
    It contains more proper meaning and parent interface name is changed to
    IWindowInfo.
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Fix type compare bug
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Modify application class naming
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Add missing files
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Fix some issues
    
    - Naming issue Factory -> StateManager
    - encapsulation issue -> add CreateWindowInfo method
    - Circular dependancy -> public to protected
    - Base Component creation -> Do not allow BaseComponent creation
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Fix desc
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Add some logs
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Remove not used Dispose method override
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>
    
    * Remove unused value
    
    Signed-off-by: hyunho <hhstark.kang@samsung.com>

---
+++ src/Tizen.Applications.ComponentBased.Default/Tizen.Applications.ComponentBased.Default/EFLWindowInfo.cs
@@ -0,0 +1,49 @@
+﻿using ElmSharp;
+using System;
+using Tizen.Applications.ComponentBased.Common;
+
+namespace Tizen.Applications.ComponentBased.Default
+{
+    /// <summary>
+    /// Window information class for ComponentBasedApplication
+    /// </summary>
+    /// <since_tizen> 6 </since_tizen>
+    public class EFLWindowInfo : IWindowInfo
+    {
+        private const string LogTag = "Tizen.Applications.EFLWindow";
+        private Window _win;
+        private int _resId;
+
+        /// <summary>
+        /// Initializes the EFLWindow class.
+        /// </summary>
+        /// <param name="win">The window object of component.</param>
+        /// <since_tizen> 6 </since_tizen>
+        public EFLWindowInfo(Window win)
+        {
+            _win = win;
+        }
+
+        /// <summary>
+        /// Gets the resource ID of window
+        /// </summary>
+        /// <returns>The native handle of window</returns>
+        /// <since_tizen> 6 </since_tizen>
+        public int ResourceId
+        {
+            get
+            {
+                if (_resId == 0)
+                {
+                    int err = Interop.EflCBApplication.GetResourceId(_win.RealHandle, out _resId);
+                    if (err != 0)
+                        Log.Info(LogTag, "Fail to get resource ID");
+                }
+
+                return _resId;
+            }
+        }
+
+    }
+
+}
