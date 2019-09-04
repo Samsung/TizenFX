39d1991cfbd279b71658e4eb5e5e78d317604b01
Haesu Gwon <haesu.gwon@samsung.com>
2019-09-04 16:37:19 +0900

    [MediaTool] Add audio channel mapping APIs for PCM format

--- src/Tizen.Multimedia/MediaTool/AudioMediaFormat.cs
+++ src/Tizen.Multimedia/MediaTool/AudioMediaFormat.cs
@@ -14,8 +14,12 @@
  * limitations under the License.
  */
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Diagnostics;
 using Tizen.Internals.Errors;
+using Native = Tizen.Multimedia.Interop.MediaFormat;
 
 namespace Tizen.Multimedia
 {
@@ -66,6 +70,91 @@ namespace Tizen.Multimedia
         /// <since_tizen> 3 </since_tizen>
         public AudioMediaFormat(MediaFormatAudioMimeType mimeType,
             int channel, int sampleRate, int bit, int bitRate, MediaFormatAacType aacType)
+            : this(mimeType, channel, sampleRate, bit, bitRate, aacType, 0, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the AudioMediaFormat class with the specified mime type,
+        /// channel, sample rate, bit, bit rate, bit depth, and audio channel map.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="audioChannelMap"/> contains <see cref="MediaFormatAudioChannelPosition.None"/>,
+        /// <paramref name="channel"/> should be set greater than 0.<br/>
+        /// If <paramref name="audioChannelMap"/> contains <see cref="MediaFormatAudioChannelPosition.Mono"/>,
+        /// <paramref name="channel"/> should be set 1.<br/>
+        /// User can not set <see cref="MediaFormatAudioChannelPosition.None"/> with another channel positions.<br/>
+        /// User can not set <see cref="MediaFormatAudioChannelPosition.Mono"/> with another channel positions.<br/>
+        /// If same channel position is added in <paramref name="audioChannelMap"/> more than twice, its duplicaiton will be removed.
+        /// </remarks>
+        /// <param name="mimeType">The mime type of the format.</param>
+        /// <param name="channel">The channel value of the format.</param>
+        /// <param name="sampleRate">The sample rate value of the format.</param>
+        /// <param name="bit">The bit value of the format.</param>
+        /// <param name="bitRate">The bit rate value of the format.</param>
+        /// <param name="bitDepth">The bit depth value of the PCM audio format.</param>
+        /// <param name="audioChannelMap">The loudspeaker position in PCM audio format.</param>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="mimeType"/> is invalid (i.e. undefined value).<br/>
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="audioChannelMap"/> is invalid or mismatched with <paramref name="channel"/> like the following:<br/>
+        ///     <paramref name="audioChannelMap"/> is not matched correctly with <paramref name="channel"/>.
+        ///     -or-<br/>
+        ///     <paramref name="audioChannelMap"/> is set to <see cref="MediaFormatAudioChannelPosition.Invaild"/>.
+        ///     -or-<br/>
+        ///     <see cref="MediaFormatAudioChannelPosition.Mono"/> or <see cref="MediaFormatAudioChannelPosition.None"/> is set with another channel position.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     <paramref name="channel"/>, <paramref name="sampleRate"/>, <paramref name="bit"/>, or <paramref name="bitRate"/> is less than zero.
+        /// </exception>
+        /// <since_tizen> 6 </since_tizen>
+        public AudioMediaFormat(MediaFormatAudioMimeType mimeType,
+            int channel, int sampleRate, int bit, int bitRate, int bitDepth, IList<MediaFormatAudioChannelPosition> audioChannelMap)
+            : this(mimeType, channel, sampleRate, bit, bitRate, MediaFormatAacType.None, bitDepth, audioChannelMap)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the AudioMediaFormat class with the specified mime type,
+        /// channel, sample rate, bit, bit rate, bit depth, and audio channel map.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="audioChannelMap"/> contains <see cref="MediaFormatAudioChannelPosition.None"/>,
+        /// <paramref name="channel"/> should be set greater than 0.<br/>
+        /// If <paramref name="audioChannelMap"/> contains <see cref="MediaFormatAudioChannelPosition.Mono"/>,
+        /// <paramref name="channel"/> should be set 1.<br/>
+        /// User can not set <see cref="MediaFormatAudioChannelPosition.None"/> with another channel positions.<br/>
+        /// User can not set <see cref="MediaFormatAudioChannelPosition.Mono"/> with another channel positions.<br/>
+        /// If same channel position is added in <paramref name="audioChannelMap"/> more than twice, its duplicaiton will be removed.
+        /// </remarks>
+        /// <param name="mimeType">The mime type of the format.</param>
+        /// <param name="channel">The channel value of the format.</param>
+        /// <param name="sampleRate">The sample rate value of the format.</param>
+        /// <param name="bit">The bit value of the format.</param>
+        /// <param name="bitRate">The bit rate value of the format.</param>
+        /// <param name="aacType">The AAC bitstream format(ADIF or ADTS).</param>
+        /// <param name="bitDepth">The bit depth value of the PCM audio format.</param>
+        /// <param name="audioChannelMap">The loudspeaker position in PCM audio format.</param>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="mimeType"/> or <paramref name="aacType"/> is invalid (i.e. undefined value).<br/>
+        ///     -or-<br/>
+        ///     <paramref name="aacType"/> is not <see cref="MediaFormatAacType.None"/>, but <paramref name="mimeType"/> is one of the AAC types.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="audioChannelMap"/> is invalid or mismatched with <paramref name="channel"/> like the following:<br/>
+        ///     <paramref name="audioChannelMap"/> is not matched correctly with <paramref name="channel"/>.
+        ///     -or-<br/>
+        ///     <paramref name="audioChannelMap"/> is set to <see cref="MediaFormatAudioChannelPosition.Invaild"/>.
+        ///     -or-<br/>
+        ///     <see cref="MediaFormatAudioChannelPosition.Mono"/> or <see cref="MediaFormatAudioChannelPosition.None"/> is set with another channel position.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     <paramref name="channel"/>, <paramref name="sampleRate"/>, <paramref name="bit"/>, or <paramref name="bitRate"/> is less than zero.
+        /// </exception>
+        /// <since_tizen> 6 </since_tizen>
+        public AudioMediaFormat(MediaFormatAudioMimeType mimeType,
+            int channel, int sampleRate, int bit, int bitRate, MediaFormatAacType aacType, int bitDepth, IList<MediaFormatAudioChannelPosition> audioChannelMap)
             : base(MediaFormatType.Audio)
         {
             ValidationUtil.ValidateEnum(typeof(MediaFormatAudioMimeType), mimeType, nameof(mimeType));
@@ -99,11 +188,53 @@ namespace Tizen.Multimedia
             }
 
             MimeType = mimeType;
+            AacType = aacType;
             Channel = channel;
             SampleRate = sampleRate;
             Bit = bit;
             BitRate = bitRate;
-            AacType = aacType;
+            BitDepth = bitDepth;
+
+            if (audioChannelMap != null)
+            {
+                audioChannelMap = audioChannelMap.Distinct().OrderBy(p => p).ToList();
+
+                ValidateAudioChannelMap(audioChannelMap);
+
+                AudioChannelMap = new ReadOnlyCollection<MediaFormatAudioChannelPosition>(audioChannelMap);
+            }
+        }
+
+        private void ValidateAudioChannelMap(IList<MediaFormatAudioChannelPosition> audioChannelMap)
+        {
+            if (audioChannelMap.Contains(MediaFormatAudioChannelPosition.Invaild))
+            {
+                throw new ArgumentException("Invalid channel position.", nameof(audioChannelMap));
+            }
+
+            if ((audioChannelMap.Contains(MediaFormatAudioChannelPosition.Mono) && audioChannelMap.Count > 1) ||
+                (audioChannelMap.Contains(MediaFormatAudioChannelPosition.None) && audioChannelMap.Count > 1))
+            {
+                throw new ArgumentException($"Mono and None can not be set with another channel position.",
+                    nameof(audioChannelMap));
+            }
+
+            if (audioChannelMap.Contains(MediaFormatAudioChannelPosition.None))
+            {
+                if (Channel <= 0)
+                {
+                    throw new ArgumentException($"Channel should be greater than 0 in {MediaFormatAudioChannelPosition.None}.",
+                        nameof(audioChannelMap));
+                }
+            }
+            else
+            {
+                if (audioChannelMap.Count != Channel)
+                {
+                    throw new ArgumentException("Channel should be the same with number of audioChannelMap.",
+                        nameof(audioChannelMap));
+                }
+            }
         }
 
         /// <summary>
@@ -123,6 +254,19 @@ namespace Tizen.Multimedia
             Bit = bit;
             BitRate = bitRate;
             AacType = IsAacSupportedMimeType(mimeType) ? GetAacType(handle) : MediaFormatAacType.None;
+            AudioChannelMap = GetAudioChannelMap(handle);
+        }
+
+        private static ReadOnlyCollection<MediaFormatAudioChannelPosition> GetAudioChannelMap(IntPtr handle)
+        {
+            var ret = Native.GetAudioChannelMask(handle, out ulong mask);
+            MultimediaDebug.AssertNoError(ret);
+
+            ret = Native.GetChannelPositionFromMask(handle, mask, out MediaFormatAudioChannelPosition[] positions);
+            MultimediaDebug.AssertNoError(ret);
+
+            return positions == null ? null :
+                new ReadOnlyCollection<MediaFormatAudioChannelPosition>(positions.Distinct().OrderBy(p => p).ToList());
         }
 
         /// <summary>
@@ -150,7 +294,7 @@ namespace Tizen.Multimedia
         {
             Debug.Assert(handle != IntPtr.Zero, "The handle is invalid!");
 
-            int ret = Interop.MediaFormat.GetAudioInfo(handle,
+            int ret = Native.GetAudioInfo(handle,
                 out mimeType, out channel, out sampleRate, out bit, out bitRate);
 
             MultimediaDebug.AssertNoError(ret);
@@ -167,7 +311,7 @@ namespace Tizen.Multimedia
         {
             Debug.Assert(handle != IntPtr.Zero, "The handle is invalid!");
 
-            int ret = Interop.MediaFormat.GetAudioAacType(handle, out var aacType);
+            int ret = Native.GetAudioAacType(handle, out var aacType);
 
             MultimediaDebug.AssertNoError(ret);
 
@@ -180,23 +324,38 @@ namespace Tizen.Multimedia
         {
             Debug.Assert(Type == MediaFormatType.Audio);
 
-            int ret = Interop.MediaFormat.SetAudioMimeType(handle, MimeType);
+            int ret = Native.SetAudioMimeType(handle, MimeType);
             MultimediaDebug.AssertNoError(ret);
 
-            ret = Interop.MediaFormat.SetAudioChannel(handle, Channel);
+            ret = Native.SetAudioChannel(handle, Channel);
             MultimediaDebug.AssertNoError(ret);
 
-            ret = Interop.MediaFormat.SetAudioSampleRate(handle, SampleRate);
+            ret = Native.SetAudioSampleRate(handle, SampleRate);
             MultimediaDebug.AssertNoError(ret);
 
-            ret = Interop.MediaFormat.SetAudioBit(handle, Bit);
+            ret = Native.SetAudioBit(handle, Bit);
             MultimediaDebug.AssertNoError(ret);
 
-            ret = Interop.MediaFormat.SetAudioAverageBps(handle, BitRate);
+            ret = Native.SetAudioAverageBps(handle, BitRate);
             MultimediaDebug.AssertNoError(ret);
 
-            ret = Interop.MediaFormat.SetAudioAacType(handle, AacType);
+            ret = Native.SetAudioAacType(handle, AacType);
             MultimediaDebug.AssertNoError(ret);
+
+            if (AudioChannelMap != null)
+            {
+                ret = Native.SetAudioChannelMask(handle, GetAudioChannelMask(handle));
+                MultimediaDebug.AssertNoError(ret);
+            }
+        }
+
+        private ulong GetAudioChannelMask(IntPtr handle)
+        {
+            int ret = Native.GetMaskFromChannelPosition(handle, AudioChannelMap.ToArray(),
+                out ulong mask);
+            MultimediaDebug.AssertNoError(ret);
+
+            return mask;
         }
 
         /// <summary>
@@ -210,6 +369,18 @@ namespace Tizen.Multimedia
         /// </summary>
         /// <since_tizen> 3 </since_tizen>
         public int Channel { get; }
+
+        /// <summary>
+        /// Gets or sets the list of channel position value of PCM audio format.
+        /// </summary>
+        /// <remarks>
+        /// The channel mask specifies the mapping of channels to speakers.
+        /// default value is 0.
+        /// </remarks>
+        /// <seealso cref="Channel"/>
+        /// <seealso cref="MediaFormatAudioChannelPosition"/>
+        /// <since_tizen> 6 </since_tizen>
+        public ReadOnlyCollection<MediaFormatAudioChannelPosition> AudioChannelMap { get; }
 
         /// <summary>
         /// Gets the sample rate value of the current format.
@@ -230,6 +401,12 @@ namespace Tizen.Multimedia
         public int BitRate { get; }
 
         /// <summary>
+        /// Gets the bit depth value of the current foramt.
+        /// </summary>
+        /// <since_tizen> 6 </since_tizen>
+        public int BitDepth { get; }
+
+        /// <summary>
         /// Gets the AAC type of the current format.
         /// </summary>
         /// <since_tizen> 3 </since_tizen>
@@ -241,8 +418,17 @@ namespace Tizen.Multimedia
         /// <returns>A string that represents the current object.</returns>
         /// <since_tizen> 3 </since_tizen>
         public override string ToString()
-            => $@"MimeType={ MimeType.ToString() }, Channel={ Channel.ToString() }, SampleRate=
-                { SampleRate }, Bit={ Bit.ToString() }, BitRate={ BitRate.ToString() }, AacType={ AacType.ToString() }";
+        {
+            var toString = $@"MimeType={ MimeType.ToString() }, Channel={ Channel.ToString() }, SampleRate={ SampleRate },
+                Bit={ Bit.ToString() }, BitRate={ BitRate.ToString() }, BitDepth={ BitDepth.ToString() }, AacType={ AacType.ToString()}";
+
+            if (AudioChannelMap != null)
+            {
+                toString += ", AudioChannelMap=" + $"{string.Join(",", AudioChannelMap)}";
+            }
+
+            return toString;
+        }
 
         /// <summary>
         /// Compares an object to an instance of <see cref="AudioMediaFormat"/> for equality.
@@ -258,8 +444,23 @@ namespace Tizen.Multimedia
                 return false;
             }
 
+            var mapCompare = true;
+            // We don't care the case of both properties are null.
+            if (AudioChannelMap != null && rhs.AudioChannelMap != null)
+            {
+                for (int i = 0; i < AudioChannelMap.Count; i++)
+                {
+                    mapCompare = AudioChannelMap[i].Equals(rhs.AudioChannelMap[i]);
+                }
+            }
+            else if ((AudioChannelMap == null && rhs.AudioChannelMap != null) ||
+                (AudioChannelMap != null && rhs.AudioChannelMap == null))
+            {
+                mapCompare = false;
+            }
+
             return MimeType == rhs.MimeType && Channel == rhs.Channel && SampleRate == rhs.SampleRate &&
-                Bit == rhs.Bit && BitRate == rhs.BitRate && AacType == rhs.AacType;
+                Bit == rhs.Bit && BitRate == rhs.BitRate && BitDepth == rhs.BitDepth && AacType == rhs.AacType && mapCompare;
         }
 
         /// <summary>
@@ -268,6 +469,6 @@ namespace Tizen.Multimedia
         /// <returns>The hash code for this instance of <see cref="AudioMediaFormat"/>.</returns>
         /// <since_tizen> 3 </since_tizen>
         public override int GetHashCode()
-            => new { MimeType, Channel, SampleRate, Bit, BitRate, AacType }.GetHashCode();
+            => new { MimeType, Channel, SampleRate, Bit, BitRate, BitDepth, AacType, AudioChannelMap }.GetHashCode();
     }
 }
