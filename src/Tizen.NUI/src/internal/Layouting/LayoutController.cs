a5c1a70e6fc159edd6e09bcda9576bb5c9fdcc57
Agnelo Vaz <agnelo.vaz@samsung.com>
2019-09-20 15:48:35 +0100

    Layout Animation fix
    Change-Id: I85fbb7af0e86860d3d2d667796b89ba29944b384

--- src/Tizen.NUI/src/internal/Layouting/LayoutController.cs
+++ src/Tizen.NUI/src/internal/Layouting/LayoutController.cs
@@ -356,8 +356,8 @@ namespace Tizen.NUI
         /// </summary>
         private void PlayAnimation()
         {
+            Debug.WriteLineIf( LayoutDebugController, "LayoutController Playing, Core Duration:" + _coreAnimation.Duration);
             _coreAnimation.Play();
-            Debug.WriteLineIf( LayoutDebugController, "LayoutController Core Duration:" + _coreAnimation.Duration);
         }
 
         private void AnimationFinished(object sender, EventArgs e)
@@ -390,6 +390,8 @@ namespace Tizen.NUI
                 // of the other stack.  Then the main removal stack iterated when AnimationFinished
                 // occurs again.
             }
+            Debug.WriteLineIf( LayoutDebugController, "LayoutController AnimationFinished");
+            _coreAnimation?.Clear();
         }
 
         /// <summary>
@@ -401,9 +403,15 @@ namespace Tizen.NUI
             // Initialize animation for this layout run.
             bool animationPending = false;
 
+            Debug.WriteLineIf( LayoutDebugController,
+                               "LayoutController SetupCoreAnimation for:" + _layoutTransitionDataQueue.Count);
+
             if (_layoutTransitionDataQueue.Count > 0 ) // Something to animate
             {
-                _coreAnimation = new Animation();
+                if (!_coreAnimation)
+                {
+                    _coreAnimation = new Animation();
+                }
                 _coreAnimation.EndAction = Animation.EndActions.StopFinal;
                 _coreAnimation.Finished += AnimationFinished;
 
