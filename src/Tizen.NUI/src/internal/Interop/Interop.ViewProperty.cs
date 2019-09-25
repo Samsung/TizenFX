66750919753250274542768d7b4aadb7c1813744
dongsug-song <35130733+dongsug-song@users.noreply.github.com>
2019-09-25 16:52:31 +0900

    [NUI] Integration from dalihub (#1039)
    * [NUI] Add UPATE_SIZE_HINT property
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI] Make public-API as Hidden-API before ACR
    
    -View::RotateBy
    -Touch::GetAngle
    
    Required for handling rotation of objects.
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI] Modify BackgroundImageSynchronosLoading issue
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI]Fix UPDATE_SIZE_HINT crash issue
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI] Fix transition inheritance from parent
    
    Change of logic so if child has a transition for the condition
    then don't copy parents.
    
    Previously only checked if transitions list was empty.
    
    Change-Id: If168d493b70e236fc22845bb0a09e4e6ff5ca77a
    
    * [NUI] Overriding Animation API
    
    Change-Id: Ief7d52b1108729f0216c11d495770be2af2920d1
    
    * [NUI] Layout Animation fix
    
    Change-Id: I85fbb7af0e86860d3d2d667796b89ba29944b384

--- src/Tizen.NUI/src/internal/Interop/Interop.ViewProperty.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.ViewProperty.cs
@@ -49,6 +49,9 @@ namespace Tizen.NUI
 
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_View_Property_DOWN_FOCUSABLE_ACTOR_ID_get")]
             public static extern int View_Property_DOWN_FOCUSABLE_ACTOR_ID_get();
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_View_Property_UPDATE_SIZE_HINT_get")]
+            public static extern int View_Property_UPDATE_SIZE_HINT_get();
         }
     }
 }
