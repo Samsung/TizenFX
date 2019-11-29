553e4e32554a37365d1a7b68a3e8d7c703bb114d
huiyueun <35286162+huiyueun@users.noreply.github.com>
2019-11-29 14:25:42 +0900

    [NUI] Add Component Application (#1148)
    * Add component
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Change Bug
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI]Modify Last
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Add to mange frame component
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Change Sln file
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Change mod
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Change sln
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI] fix comment
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

---
+++ src/Tizen.Applications.ComponentBased/AssemblyAttr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tizen.NUI, " + PublicKey.Value)]
+
+internal static class PublicKey
+{
+    internal const string Value =
+        "PublicKey=0024000004800000940000000602000000240000525341310004000001000100d115b100424841" +
+        "6b12d21b626cfb17149c9303fe394693fd3b32d7872e89559a4fa96c98110c2e62eea48aca693b" +
+        "ddbe17094ca8ea2e2cd79970ca590fb672b9b371b5d7002076817321f62d6483ea50c56dbd1f37" +
+        "b185a4c24c47718876e6ae6d266508c551170d4cbdda3f82edaff9405ee3d7857282d8269e8e518d2f0fb2";
+}
--- src/Tizen.NUI/src/public/Window.cs
+++ src/Tizen.NUI/src/public/Window.cs
@@ -207,6 +207,19 @@ namespace Tizen.NUI
         }
 
         /// <summary>
+        /// Get Resource ID of window
+        /// </summary>
+        internal int ResourceID
+        {
+            get
+            {
+                int ret = Interop.Window.GetResouceID(swigCPtr);
+                if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+                return ret;
+            }
+        }
+
+        /// <summary>
         /// Gets or sets a window type.
         /// </summary>
         /// <since_tizen> 3 </since_tizen>
