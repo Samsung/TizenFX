590a95c6098659061db1a69c5d58cba9116521cc
zhouleonlei <56956725+zhouleonlei@users.noreply.github.com>
2019-12-18 18:14:13 +0800

    [NUI] Merge master#1206 to API7 (#1209)

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -665,11 +665,6 @@ namespace Tizen.NUI.BaseComponents
         /// <summary>
         /// Retrieves and sets the view's opacity.<br />
         /// </summary>
-        /// <remarks>
-        /// <para>
-        /// Animatable - This property can be animated using <c>Animation</c> class.
-        /// </para>
-        /// </remarks>
         /// <since_tizen> 3 </since_tizen>
         public float Opacity
         {
@@ -1204,6 +1199,9 @@ namespace Tizen.NUI.BaseComponents
         /// <para>
         /// If the view is not visible, then the view and its children will not be rendered.
         /// This is regardless of the individual visibility values of the children, i.e., the view will only be rendered if all of its parents have visibility set to true.
+        /// </para>
+        /// <para>
+        /// Animatable - This property can be animated using <c>Animation</c> class.
         /// </para>
         /// </remarks>
         /// <since_tizen> 3 </since_tizen>
