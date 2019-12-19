2f6392f0cef3b995ce801815208005a554bcd687
minho sun <minho.sun@samsung.com>
2019-12-19 14:35:46 +0900

    [NUI] Support adding multiple callback to gesture

--- src/Tizen.NUI/src/public/PanGestureDetector.cs
+++ src/Tizen.NUI/src/public/PanGestureDetector.cs
@@ -28,10 +28,6 @@ namespace Tizen.NUI
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class PanGestureDetector : GestureDetector
     {
-
-        private DaliEventHandler<object, DetectedEventArgs> _panGestureEventHandler;
-        private DetectedCallbackDelegate _panGestureCallbackDelegate;
-
         /// <summary>
         /// Creates an initialized PanGestureDetector.
         /// </summary>
@@ -58,9 +54,10 @@ namespace Tizen.NUI
         {
         }
 
-
+        private DaliEventHandler<object, DetectedEventArgs> _detectedEventHandler;
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        private delegate void DetectedCallbackDelegate(IntPtr actor, IntPtr panGesture);
+        private delegate void DetectedCallbackType(IntPtr actor, IntPtr panGesture);
+        private DetectedCallbackType _detectedCallback;
 
         /// <summary>
         /// This signal is emitted when the specified pan is detected on the attached view.
@@ -71,31 +68,25 @@ namespace Tizen.NUI
         {
             add
             {
-                lock (this)
+                if (_detectedEventHandler == null)
                 {
-                    // Restricted to only one listener
-                    if (_panGestureEventHandler == null)
-                    {
-                        _panGestureEventHandler += value;
-
-                        _panGestureCallbackDelegate = new DetectedCallbackDelegate(OnPanGestureDetected);
-                        this.DetectedSignal().Connect(_panGestureCallbackDelegate);
-                    }
+                    _detectedCallback = OnPanGestureDetected;
+                    DetectedSignal().Connect(_detectedCallback);
                 }
+
+                _detectedEventHandler += value;
             }
 
             remove
             {
-                lock (this)
-                {
-                    if (_panGestureEventHandler != null)
-                    {
-                        this.DetectedSignal().Disconnect(_panGestureCallbackDelegate);
-                    }
+                _detectedEventHandler -= value;
 
-                    _panGestureEventHandler -= value;
+                if (_detectedEventHandler == null && DetectedSignal().Empty() == false)
+                {
+                    DetectedSignal().Disconnect(_detectedCallback);
                 }
             }
+        }
         }
 
         /// <summary>
@@ -527,6 +518,11 @@ namespace Tizen.NUI
         [EditorBrowsable(EditorBrowsableState.Never)]
         protected override void ReleaseSwigCPtr(System.Runtime.InteropServices.HandleRef swigCPtr)
         {
+            if (_detectedCallback != null)
+            {
+                DetectedSignal().Disconnect(_detectedCallback);
+            }
+
             Interop.PanGestureDetector.delete_PanGestureDetector(swigCPtr);
         }
 
@@ -543,10 +539,9 @@ namespace Tizen.NUI
 
             e.PanGesture = Tizen.NUI.PanGesture.GetPanGestureFromPtr(panGesture);
 
-            if (_panGestureEventHandler != null)
+            if (_detectedEventHandler != null)
             {
-                //here we send all data to user event handlers
-                _panGestureEventHandler(this, e);
+                _detectedEventHandler(this, e);
             }
         }
 
