6de352330562667c4c2332d09bc4dd44c443fa83
dongsug-song <35130733+dongsug-song@users.noreply.github.com>
2019-12-20 20:37:53 +0900

    [NUI] NUI samples (#1225) (#1226)
    * [NUI] Initial upload the samples of NUI and NUI.Components (#1207)
    
    * [NUI] Merge NUI.Samples to TizenFX (#1222)
    
    * [NUI] Revise NUI samples (#1224)
    
    Co-authored-by: zhouleonlei <56956725+zhouleonlei@users.noreply.github.com>
    
    Co-authored-by: zhouleonlei <56956725+zhouleonlei@users.noreply.github.com>

---
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/ContactCard/MaskedImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.UIComponents;
+
+namespace Tizen.NUI.Samples
+{
+    public class MaskedImage
+    {
+        private static readonly string IMAGE_MASK = CommonResource.GetFHResourcePath() + "ContactCard/contact-cards-mask.png";
+
+        public static View Create(string imagePath)
+        {
+            View maskedImage = new ImageView();
+            ImageVisual imageVisual = new ImageVisual();
+            imageVisual.URL = imagePath;
+            imageVisual.AlphaMaskURL = IMAGE_MASK;
+            maskedImage.Background = imageVisual.OutputVisualMap;
+
+            return maskedImage;
+        }
+    }
+}
