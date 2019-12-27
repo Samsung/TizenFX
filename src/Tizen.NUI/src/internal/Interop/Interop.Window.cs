632c6a80756757be09a20907da771e773bff373f
Wonsik Jung <sidein@samsung.com>
2019-12-27 18:59:14 +0900

    [NUI] Add to get current window orientation. (#1249)
    Add the API to get current window orientation.
    It is to return the logical orientation

--- src/Tizen.NUI/src/internal/Interop/Interop.Window.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.Window.cs
@@ -58,6 +58,9 @@ namespace Tizen.NUI
 
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Window_GetPreferredOrientation")]
             public static extern int Window_GetPreferredOrientation(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Window_GetCurrentOrientation")]
+            public static extern int Window_GetCurrentOrientation(global::System.Runtime.InteropServices.HandleRef jarg1);
             
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Window_SetPositionSize")]
             public static extern void Window_SetPositionSize(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
