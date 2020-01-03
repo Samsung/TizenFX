0fab5dd3d7f04570adbab1b659fb4a598d00331b
Jiyun Yang <ji.yang@samsung.com>
2020-01-03 16:43:21 +0900

    [NUI] Enable BackgroundImageBorder in View
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/Tizen.NUI/src/public/Rectangle.cs
+++ src/Tizen.NUI/src/public/Rectangle.cs
@@ -58,6 +58,10 @@ namespace Tizen.NUI
             if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
         }
 
+        internal Rectangle(RectangleChangedCallback cb) : this()
+        {
+        }
+
         internal Rectangle(RectangleChangedCallback cb, Rectangle other) : this(cb, other.x, other.y, other.width, other.height)
         {
         }
@@ -449,5 +453,10 @@ namespace Tizen.NUI
         {
             Interop.Rectangle.delete_Rectangle(swigCPtr);
         }
+
+        /// <summary>
+        /// Determines whether the reference is null or the Rectangle has all 0 properties.
+        /// </summary>
+        internal static bool IsNullOrZero(Rectangle rectangle) => (rectangle == null || (rectangle.top == 0 && rectangle.right == 0 && rectangle.bottom == 0 && rectangle.left == 0));
     }
 }
