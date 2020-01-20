31894087855a25e8b0d0d673694d2b1087a15b9d
huiyueun <35286162+huiyueun@users.noreply.github.com>
2020-01-20 11:18:51 +0900

    [NUI] Fix FittingMode bug (#1319)
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/ImageView.cs
+++ src/Tizen.NUI/src/public/BaseComponents/ImageView.cs
@@ -783,7 +783,7 @@ namespace Tizen.NUI.BaseComponents
             }
             set
             {
-                UpdateImage(ImageVisualProperty.CropToMask, new PropertyValue((int)value));
+                UpdateImage(ImageVisualProperty.FittingMode, new PropertyValue((int)value));
             }
         }
 
