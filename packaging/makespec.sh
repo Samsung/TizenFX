4bad4a3ce0832087fe223688128d7b2c87c2f524
j-h.choi <j-h.choi@samsung.com>
2020-02-18 14:40:14 +0900

    [Build] Add RIDs of Tizen.6.0.0 and Set RIDs to vconf automatically

--- packaging/makespec.sh
+++ packaging/makespec.sh
@@ -24,3 +24,20 @@ echo "# Auto-generated from $(basename $RPMSPEC_IN) by makespec.sh" | cat - $RPM
 sed -i -e "s/@api_version@/$API_VERSION/g" $RPMSPEC
 sed -i -e "s/@rpm_version@/$RPM_VERSION/g" $RPMSPEC
 sed -i -e "s/@nuget_version@/$NUGET_VERSION/g" $RPMSPEC
+
+# Update RID
+cd ${SCRIPT_DIR}/..
+RUNTIME_JSON_FILE=$PWD/pkg/Tizen.NET/runtime.json
+
+if [ $(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        echo ""
+        echo "[!] JSON processor(jq) package should install"
+        echo "[!] Starting install JSON processor(jq) ... "
+        sudo apt-get install jq
+fi
+
+PARSING_DATA=$(cat $RUNTIME_JSON_FILE | jq '.runtimes | keys' | grep 'tizen\.' | grep -Ev 'armel|x86')
+RID_VERSION=$(echo $PARSING_DATA | sed -e "s/\"tizen\.//g" | sed -e "s/\"\, /:/g" | sed -e "s/\"\,//g")
+
+sed -i -e "s/@rid_version@/$RID_VERSION/g" $RPMSPEC
