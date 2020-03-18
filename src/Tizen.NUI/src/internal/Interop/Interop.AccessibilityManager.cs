caab049ed1be1588034372bbc3eeb2de4c35ee3a
Seoyeon Kim <seoyeon2.kim@samsung.com>
2020-03-18 10:25:22 +0900

    Revert "[NUI] Make AccessibilityManager APIs as public ones (#1460)"
    This reverts commit 5056acf4fae2e2bbba0e1251c9fcb626a01ed039.

--- src/Tizen.NUI/src/internal/Interop/Interop.AccessibilityManager.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.AccessibilityManager.cs
@@ -167,6 +167,9 @@ namespace Tizen.NUI
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AccessibilityManager_ActionZoomSignal")]
             public static extern global::System.IntPtr AccessibilityManager_ActionZoomSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
 
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AccessibilityManager_ActionReadIndicatorInformationSignal")]
+            public static extern global::System.IntPtr AccessibilityManager_ActionReadIndicatorInformationSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
+
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AccessibilityManager_ActionReadPauseResumeSignal")]
             public static extern global::System.IntPtr AccessibilityManager_ActionReadPauseResumeSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
 
