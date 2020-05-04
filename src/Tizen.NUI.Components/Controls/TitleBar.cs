40988676b2edc5ca59bae316e93224c77ec7ac36
Joogab Yun <joogab.yun@samsung.com>
2020-05-04 15:05:16 +0900

    [NUI] Add TitleBar
    The TitleBar inherits TextLabel.
    TitleBar can has a fadeOut effects on the left and right side.

---
+++ src/Tizen.NUI.Components/Controls/TitleBar.cs
@@ -0,0 +1,342 @@
+﻿/*
+ * Copyright(c) 2020 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Binding;
+
+namespace Tizen.NUI.Components
+{
+    /// <summary>
+    /// Use it to set the title. this has a fadeout effect.
+    /// You can also set the color of the fadeout.
+    /// </summary>
+    /// <since_tizen> 8 </since_tizen>
+    /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class TitleBar : TextLabel
+    {
+        private static readonly string VERTEX_SHADER =
+                        "attribute mediump vec2 aPosition;\n" +
+                        "varying mediump vec2 vTexCoord;\n" +
+                        "uniform highp mat4 uMvpMatrix;\n" +
+                        "uniform mediump vec3 uSize;\n" +
+                        "varying mediump vec2 sTexCoordRect;\n" +
+                        "void main()\n" +
+                        "{\n" +
+                        "   gl_Position = uMvpMatrix * vec4(aPosition * uSize.xy, 0.0, 1.0);\n" +
+                        "   vTexCoord = aPosition + vec2(0.5);\n" +
+                        "}\n";
+
+        private static readonly string FRAGMENT_SHADER =
+                        "uniform lowp vec4 uColor;\n" +
+                        "varying mediump vec2 vTexCoord;\n" +
+                        "uniform sampler2D sTexture;\n" +
+                        "void main()\n" +
+                        "{\n" +
+                        "   gl_FragColor = texture2D(sTexture, vTexCoord) * uColor;\n" +
+                        "}\n";
+
+        private ImageView leftImage = null;
+        private ImageView rightImage = null;
+        private Geometry geometry = null;
+        private Shader shader = null;
+        private Renderer mLeftRenderer = null;
+        private Renderer mRightRenderer = null;
+        private Color startColor = new Vector4(0, 0, 0, 0.6F); // default fadeout color
+        private Color endColor = new Vector4(0, 0, 0, 0);
+        private int fadeOutWidth = 32; // default size
+        private int fadeOutHeight = 0;
+
+        /// <summary>
+        /// </summary>
+        /// <since_tizen> 8 </since_tizen>
+        static TitleBar() { }
+
+        /// <summary>
+        /// Construct TitleBar with null.
+        /// </summary>
+        /// <since_tizen> 8 </since_tizen>
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TitleBar() : base()
+        {
+            Initialize();
+        }
+
+        public TextLabelStyle Style => ViewStyle as TextLabelStyle;
+        /// <summary>
+        /// The constructor of the TitleBar class with specific Style.
+        /// </summary>
+        /// <param name="textLabelStyle">Construct Style</param>
+        /// <since_tizen> 8 </since_tizen>
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TitleBar(TextLabelStyle textLabelStyle) : base(textLabelStyle)
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Constructor of the TitleBar class with special style.
+        /// </summary>
+        /// <param name="style"> style name </param>
+        /// <since_tizen> 8 </since_tizen>
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TitleBar(string style) : base(style)
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Sets the start and end color of the fadeout.
+        /// </summary>
+        /// <since_tizen> 8 </since_tizen>
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public void FadeOutColor(Color startColor, Color endColor)
+        {
+            this.startColor = startColor;
+            this.endColor = endColor;
+            UpdateImage();
+        }
+
+        /// <summary>
+        /// Gets or Sets the width of the fadeout effect.
+        /// </summary>
+        /// <since_tizen> 8 </since_tizen>
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public int FadeOutWidth
+        {
+            get
+            {
+                return fadeOutWidth;
+            }
+            set
+            {
+                fadeOutWidth = value;
+                UpdateImage();
+            }
+        }
+
+        /// <summary>
+        /// Gets or Sets the height of the fadeout effect.
+        /// </summary>
+        /// <since_tizen> 8 </since_tizen>
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public int FadeOutHeight
+        {
+            get
+            {
+                return fadeOutHeight;
+            }
+            set
+            {
+                fadeOutHeight = value;
+                UpdateImage();
+            }
+        }
+
+        /// <summary>
+        /// Dispose TitleBar.
+        /// </summary>
+        /// <param name="type">dispose types.</param>
+        /// <since_tizen> 8 </since_tizen>
+        protected override void Dispose(DisposeTypes type)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (type == DisposeTypes.Explicit)
+            {
+                if (null != leftImage)
+                {
+                    Utility.Dispose(leftImage);
+                }
+                if (null != rightImage)
+                {
+                    Utility.Dispose(rightImage);
+                }
+            }
+            base.Dispose(type);
+        }
+
+        private void UpdateImage()
+        {
+            leftImage.Size.Width = fadeOutWidth;
+            rightImage.Size.Width = fadeOutWidth;
+
+            if(fadeOutWidth > 0)
+            {
+                TextureSet outerTextureSet = CreateTexture(this.startColor, this.endColor);
+                TextureSet insiderTextureSet = CreateTexture(this.endColor, this.startColor);
+
+                mLeftRenderer.SetTextures(outerTextureSet);
+                mRightRenderer.SetTextures(insiderTextureSet);
+            }
+
+            if(fadeOutHeight > 0)
+            {
+                leftImage.Size.Height = fadeOutHeight;
+                rightImage.Size.Height = fadeOutHeight;
+            }
+        }
+
+        private void Initialize()
+        {
+            if (null == leftImage)
+            {
+                leftImage = new ImageView();
+                leftImage.PositionUsesPivotPoint = true;
+                leftImage.ParentOrigin = Tizen.NUI.ParentOrigin.TopLeft;
+                leftImage.PivotPoint = Tizen.NUI.PivotPoint.TopLeft;
+                leftImage.WidthResizePolicy = ResizePolicyType.FillToParent;
+                leftImage.HeightResizePolicy = ResizePolicyType.FillToParent;
+                this.Add(leftImage);
+            }
+            if (null == rightImage)
+            {
+                rightImage = new ImageView();
+                rightImage.PositionUsesPivotPoint = true;
+                rightImage.ParentOrigin = Tizen.NUI.ParentOrigin.TopRight;
+                rightImage.PivotPoint = Tizen.NUI.PivotPoint.TopRight;
+                rightImage.WidthResizePolicy = ResizePolicyType.FillToParent;
+                rightImage.HeightResizePolicy = ResizePolicyType.FillToParent;
+                this.Add(rightImage);
+            }
+            if (null == geometry)
+            {
+                geometry = CreateQuadGeometry();
+            }
+            if (null == shader)
+            {
+                shader = new Shader(VERTEX_SHADER, FRAGMENT_SHADER);
+            }
+            if (null == mLeftRenderer)
+            {
+                mLeftRenderer = new Renderer(geometry, shader);
+            }
+            if (null == mRightRenderer)
+            {
+                mRightRenderer = new Renderer(geometry, shader);
+            }
+
+            leftImage.AddRenderer(mLeftRenderer);
+            rightImage.AddRenderer(mRightRenderer);
+            this.Ellipsis = false;
+
+            UpdateImage();
+        }
+
+        TextureSet CreateTexture(Vector4 color1, Vector4 color2)
+        {
+            TextureSet textureSet = new TextureSet();
+            const int width = 2;
+            const int height = 1;
+            uint size = width * height * 4;
+            byte[] pixelBuffer = new byte[size];
+            pixelBuffer[0] = (byte)(0xFF * color1.X);
+            pixelBuffer[1] = (byte)(0xFF * color1.Y);
+            pixelBuffer[2] = (byte)(0xFF * color1.Z);
+            pixelBuffer[3] = (byte)(0xFF * color1.W);
+            pixelBuffer[4] = (byte)(0xFF * color2.X);
+            pixelBuffer[5] = (byte)(0xFF * color2.Y);
+            pixelBuffer[6] = (byte)(0xFF * color2.Z);
+            pixelBuffer[7] = (byte)(0xFF * color2.W);
+
+            PixelData pixelData = new PixelData(pixelBuffer, size, width, height, PixelFormat.RGBA8888, PixelData.ReleaseFunction.DeleteArray );
+            Texture texture = new Texture(TextureType.TEXTURE_2D, PixelFormat.RGBA8888, width, height);
+            texture.Upload(pixelData);
+
+            textureSet.SetTexture(0u, texture);
+            return textureSet;
+        }
+
+        private PropertyBuffer CreatePropertyBuffer()
+        {
+            PropertyMap vertexFormat = new PropertyMap();
+            vertexFormat.Add("aPosition", new PropertyValue((int)PropertyType.Vector2));
+            PropertyBuffer vertexBuffer = new PropertyBuffer(vertexFormat);
+            return vertexBuffer;
+        }
+
+        private struct Vec2
+        {
+            float x;
+            float y;
+            public Vec2(float xIn, float yIn)
+            {
+                x = xIn;
+                y = yIn;
+            }
+        }
+
+        private struct TexturedQuadVertex
+        {
+            public Vec2 position;
+        };
+
+        private byte[] Struct2Bytes(TexturedQuadVertex[] obj)
+        {
+            int size = Marshal.SizeOf(obj);
+            byte[] bytes = new byte[size];
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(obj, ptr, false);
+            Marshal.Copy(ptr, bytes, 0, size);
+            Marshal.FreeHGlobal(ptr);
+            return bytes;
+        }
+
+        private Geometry CreateQuadGeometry()
+        {
+            PropertyBuffer vertexData = CreatePropertyBuffer();
+
+            TexturedQuadVertex vertex1 = new TexturedQuadVertex();
+            TexturedQuadVertex vertex2 = new TexturedQuadVertex();
+            TexturedQuadVertex vertex3 = new TexturedQuadVertex();
+            TexturedQuadVertex vertex4 = new TexturedQuadVertex();
+            vertex1.position = new Vec2(-0.5f, -0.5f);
+            vertex2.position = new Vec2(-0.5f, 0.5f);
+            vertex3.position = new Vec2(0.5f, -0.5f);
+            vertex4.position = new Vec2(0.5f, 0.5f);
+
+
+            TexturedQuadVertex[] texturedQuadVertexData = new TexturedQuadVertex[4] { vertex1, vertex2, vertex3, vertex4 };
+
+            int lenght = Marshal.SizeOf(vertex1);
+            IntPtr pA = Marshal.AllocHGlobal(lenght * 4);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Marshal.StructureToPtr(texturedQuadVertexData[i], pA + i * lenght, true);
+            }
+            vertexData.SetData(pA, 4);
+
+            Geometry geometry = new Geometry();
+            geometry.AddVertexBuffer(vertexData);
+            geometry.SetType(Geometry.Type.TRIANGLE_STRIP);
+            return geometry;
+        }
+
+    }
+}
---
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/TitleBarSample.cs
@@ -0,0 +1,47 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace Tizen.NUI.Samples
+{
+    public class TitleBarSample : IExample
+    {
+        private TitleBar titleBar;
+        private View root;
+
+        public void Activate()
+        {
+            Window window = NUIApplication.GetDefaultWindow();
+
+            TextLabelStyle attr = new TextLabelStyle
+            {
+                Size = new Size(300, 39),
+            };
+
+            /**
+             * The title bar inherits textlabel.
+             * Refer to textlabel.
+             */
+            titleBar = new TitleBar(attr);
+            titleBar.Text = "The title bar can has a fadeOut effect.";
+
+            /**
+             * The title bar has a fadeout effect.
+             * The default width is 32. You can change the value through FadeOutWidth.
+             * ex) titleBar.FadeOutWidth = 10;
+             * The default color is Vector4(0, 0, 0, 0.6F). You can change the color value through FadeOutColor.
+             * ex) titleBar.FadeOutColor(new Vector4(1, 0, 0, 0.6F), new Vector4(1, 0, 0, 0));
+             */
+            window.Add(titleBar);
+        }
+
+        public void Deactivate()
+        {
+            if (root != null)
+            {
+                NUIApplication.GetDefaultWindow().Remove(root);
+                root.Dispose();
+            }
+        }
+    }
+}
