11d193dfd264a2dda09886f73371bfb13687c3ad
Wootak <wootak.jung@samsung.com>
2020-05-21 10:59:07 +0900

    Update BluetoothLeAdapterImpl.cs

--- src/Tizen.Network.Bluetooth/Tizen.Network.Bluetooth/BluetoothLeAdapterImpl.cs
+++ src/Tizen.Network.Bluetooth/Tizen.Network.Bluetooth/BluetoothLeAdapterImpl.cs
@@ -398,13 +398,13 @@ namespace Tizen.Network.Bluetooth
             for (int i = 0; i < _serviceListCount; i++)
             {
                 var svc = (BluetoothLeServiceDataStruct)Marshal.PtrToStructure(new IntPtr(serviceListArray.ToInt32() + (i * sizePointerToABC)), typeof(BluetoothLeServiceDataStruct));
-                _list.Add(BluetoothUtils.ConvertStructToLeServiceData(svc));
+                list.Add(BluetoothUtils.ConvertStructToLeServiceData(svc));
             }
 
             Interop.Bluetooth.FreeServiceDataList(serviceListArray, _serviceListCount);
             Marshal.FreeHGlobal(scanDataStruct.AdvData);
             Marshal.FreeHGlobal(scanDataStruct.ScanData);
-            return _list;
+            return list;
         }
 
         internal int GetScanResultAppearance(BluetoothLeScanData scanData, BluetoothLePacketType packetType)
