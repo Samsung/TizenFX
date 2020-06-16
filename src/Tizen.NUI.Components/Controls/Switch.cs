bf3593ee8eeef2493b8c70d22e2a50379a619991
zhouleonlei <zhouleon.lei@samsung.com>
2020-06-16 15:53:58 +0800

    [NUI] Remove deprecated APIs in Slider and Switch

--- src/Tizen.NUI.Components/Controls/Switch.cs
+++ src/Tizen.NUI.Components/Controls/Switch.cs
@@ -168,27 +168,6 @@ namespace Tizen.NUI.Components
         }
 
         /// <summary>
-        /// Switch's track part.
-        /// </summary>
-        /// <since_tizen> 6 </since_tizen>
-        /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public string SwitchBackgroundImageURL
-        {
-            get
-            {
-                return Style?.Track?.ResourceUrl?.All;
-            }
-            set
-            {
-                if (null != value && null != Style?.Track)
-                {
-                    Style.Track.ResourceUrl = value;
-                }
-            }
-        }
-
-        /// <summary>
         /// Background image's resource url selector in Switch.
         /// </summary>
         /// <since_tizen> 6 </since_tizen>
