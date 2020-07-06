c78fe21f24eb4f09408e8ccb9d896bc13d14dd54
huayong.xu <huayong.xu@samsung.com>
2020-07-06 14:11:55 +0800

    Fix the issue that theme could not be changed

--- src/Tizen.NUI.Components/Utils/StyleManager.cs
+++ src/Tizen.NUI.Components/Utils/StyleManager.cs
@@ -94,9 +94,9 @@ namespace Tizen.NUI.Components
 
             set
             {
-                if (currentThemeName != value)
+                if (value != null && currentThemeName != value)
                 {
-                    currentThemeName = value.ToLower();
+                    currentThemeName = value.ToUpperInvariant();
                     themeChangeHander?.Invoke(null, new ThemeChangeEventArgs { CurrentTheme = currentThemeName });
 
                     UpdateTheme();
@@ -131,6 +131,8 @@ namespace Tizen.NUI.Components
                 }
                 return;
             }
+
+            theme = theme.ToUpperInvariant();
 
             if (themeStyleSet.ContainsKey(style) && themeStyleSet[style].ContainsKey(theme))
             {
