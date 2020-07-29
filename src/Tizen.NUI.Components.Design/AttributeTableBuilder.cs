c5c4ffe4e83b662a6e55cf532fdd270819063e02
Yeongjong Lee <yj34.lee@samsung.com>
2020-07-29 14:51:32 +0900

    [NUI] add Xaml intellisense support for NUI.Components

---
+++ src/Tizen.NUI.Components.Design/AttributeTableBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace Tizen.NUI.Components
+{
+    internal class AttributeTableBuilder : Microsoft.Windows.Design.Metadata.AttributeTableBuilder
+    {
+        private void AddAttributesForTypes()
+        {
+            Type typeFromHandle = typeof(Control);
+            AddTypeAttributes(typeFromHandle, new EditorBrowsableAttribute(EditorBrowsableState.Always));
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public AttributeTableBuilder() : base()
+        {
+            Assembly assembly = typeof(Control).Assembly;
+            AddAssemblyAttributes(assembly, new global::System.Windows.Markup.XmlnsDefinitionAttribute("http://tizen.org/Tizen.NUI/2018/XAML", "Tizen.NUI.Components"));
+            AddAttributesForTypes();
+        }
+
+        private void AddTypeAttributes(Type type, params Attribute[] attribs)
+        {
+            this.AddCallback(type, builder => builder.AddCustomAttributes(attribs));
+        }
+
+        private void AddMemberAttributes(Type type, string memberName, params Attribute[] attribs)
+        {
+            this.AddCallback(type, builder => builder.AddCustomAttributes(attribs));
+        }
+
+        private void AddAssemblyAttributes(Assembly assembly, params Attribute[] attribs)
+        {
+            this.AddCustomAttributes(assembly, attribs);
+        }
+    }
+}
---
+++ src/Tizen.NUI.Components.Design/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using Microsoft.Windows.Design.Metadata;
+
+[assembly: ProvideMetadata(typeof(Tizen.NUI.Components.RegisterMetadata))]
+
