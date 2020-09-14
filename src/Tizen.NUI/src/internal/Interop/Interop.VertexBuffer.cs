114d09295e8deedbfd31ff825164f0e8a6defe9a
DavidIanSteele <david.steele@samsung.com>
2020-09-14 13:33:28 +0100

    [NUI] Adding VertexBuffer to replace PropertyBuffer (#1983)
    * [NUI] Adding VertexBuffer to replace PropertyBuffer
    
    Native PropertyBuffer has been renamed to VertexBuffer.
    Creating a new object to mirror this name change.
    
    Changed SetData API to take an array of structs, and create
    an unmanaged buffer before passing it to native side.
    
    Signed-off-by: David Steele <david.steele@samsung.com>
    
    * Update src/Tizen.NUI/src/public/Geometry.cs
    
    Co-authored-by: Marcin Macias <40991900+m-macias@users.noreply.github.com>
    
    * Update src/Tizen.NUI/src/public/VertexBuffer.cs
    
    Co-authored-by: Marcin Macias <40991900+m-macias@users.noreply.github.com>
    
    * Update VertexBuffer.cs
    
    Fixed the tizen version in the documentation
    
    * Update Geometry.cs
    
    Changed if() throw style.
    
    Co-authored-by: Marcin Macias <40991900+m-macias@users.noreply.github.com>
    Co-authored-by: Adeel Kazmi <adeel.kazmi@samsung.com>

---
+++ src/Tizen.NUI/src/internal/Interop/Interop.VertexBuffer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class VertexBuffer
+        {
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_VertexBuffer_SWIGUpcast")]
+            public static extern global::System.IntPtr VertexBuffer_SWIGUpcast(global::System.IntPtr jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_VertexBuffer_New")]
+            public static extern global::System.IntPtr VertexBuffer_New(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_VertexBuffer__SWIG_0")]
+            public static extern global::System.IntPtr new_VertexBuffer__SWIG_0();
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_VertexBuffer")]
+            public static extern void delete_VertexBuffer(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_VertexBuffer__SWIG_1")]
+            public static extern global::System.IntPtr new_VertexBuffer__SWIG_1(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_VertexBuffer_DownCast")]
+            public static extern global::System.IntPtr VertexBuffer_DownCast(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_VertexBuffer_Assign")]
+            public static extern global::System.IntPtr VertexBuffer_Assign(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_VertexBuffer_SetData")]
+            public static extern void VertexBuffer_SetData(global::System.Runtime.InteropServices.HandleRef jarg1, System.IntPtr jarg2, uint jarg3);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_VertexBuffer_GetSize")]
+            public static extern uint VertexBuffer_GetSize(global::System.Runtime.InteropServices.HandleRef jarg1);
+        }
+    }
+}
