f700d777aacd3ee70138ec95d8aa1846213763e7
Dongsug Song <dongsug.song@samsung.com>
2020-09-15 21:17:15 +0900

    Revert "[NUI] Change parent class of Hover, Key, Wheel and Gesture to BaseHandle (#2001)"
    This reverts commit f57729e8b0e69dbab9864ff8248e9d86584072ad.

--- src/Tizen.NUI/src/public/LongPressGesture.cs
+++ src/Tizen.NUI/src/public/LongPressGesture.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2020 Samsung Electronics Co., Ltd.
+ * Copyright(c) 2017 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -135,11 +135,7 @@ namespace Tizen.NUI
 
         internal static LongPressGesture GetLongPressGestureFromPtr(global::System.IntPtr cPtr)
         {
-            LongPressGesture ret = Registry.GetManagedBaseHandleFromNativePtr(cPtr) as LongPressGesture;
-            if (ret == null)
-            {
-                ret = new LongPressGesture(cPtr, false);
-            }
+            LongPressGesture ret = new LongPressGesture(cPtr, false);
             if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
             return ret;
         }
