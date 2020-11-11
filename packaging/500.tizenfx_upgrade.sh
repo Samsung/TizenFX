7725f4908ba490db3df608a8b51f77bdf0b929a6
Wonyoung Choi <wy80.choi@samsung.com>
2020-11-11 18:58:30 +0900

    [Build] Add FOTA script for TizenFX

---
+++ packaging/500.tizenfx_upgrade.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# RW Update Script for 4.0 -> 5.5
+
+/usr/bin/vconftool set -f -t int db/dotnet/tizen_api_version 9
+/usr/bin/vconftool set -f -t string db/dotnet/tizen_rid_version "4.0.0:5.0.0:5.5.0:6.0.0:6.5.0"
+/usr/bin/vconftool set -f -t string db/dotnet/tizen_tfm_support "tizen90:tizen80:tizen70:tizen60:tizen50:tizen40:netstandard2.0"
