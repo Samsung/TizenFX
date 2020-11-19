769a383cbcd8e3ad1839a3b8aac60f62f775e98c
Yeongjong Lee <cleanlyj@naver.com>
2020-11-19 18:21:27 +0900

    [NUI] respect flex child margin changes
    The flex child margin is only set when a flex child is added to flexlayout,
    so margin changes after `View.Add` are ignored.
    
    To respect child margin changes, This patch add code to set flex margin in
    `OnMeasure`.

--- src/Tizen.NUI/src/public/Layouting/FlexLayout.cs
+++ src/Tizen.NUI/src/public/Layouting/FlexLayout.cs
@@ -679,6 +679,8 @@ namespace Tizen.NUI
             parentMeasureSpecificationWidth = widthMeasureSpec;
             parentMeasureSpecificationHeight = heightMeasureSpec;
 
+            Extents zeroMargin = new Extents();
+
             // Assign child properties
             for (int i = 0; i < LayoutChildren.Count; i++)
             {
@@ -700,7 +702,9 @@ namespace Tizen.NUI
                 float flexBasis = GetFlexBasis(Child);
                 float flexShrink = GetFlexShrink(Child);
                 float flexGrow = GetFlexGrow(Child);
+                Extents childMargin = Child.ExcludeLayouting ? zeroMargin : layoutItem.Margin;
 
+                Interop.FlexLayout.FlexLayout_SetMargin(childHandleRef, Extents.getCPtr(childMargin));
                 Interop.FlexLayout.FlexLayout_SetFlexAlignmentSelf(childHandleRef, (int)flexAlignemnt);
                 Interop.FlexLayout.FlexLayout_SetFlexPositionType(childHandleRef, (int)positionType);
                 Interop.FlexLayout.FlexLayout_SetFlexAspectRatio(childHandleRef, flexAspectRatio);
@@ -710,6 +714,7 @@ namespace Tizen.NUI
             }
 
             Interop.FlexLayout.FlexLayout_CalculateLayout(swigCPtr, width, height, isLayoutRtl);
+            zeroMargin.Dispose();
 
             LayoutLength flexLayoutWidth = new LayoutLength(Interop.FlexLayout.FlexLayout_GetWidth(swigCPtr));
             LayoutLength flexLayoutHeight = new LayoutLength(Interop.FlexLayout.FlexLayout_GetHeight(swigCPtr));
