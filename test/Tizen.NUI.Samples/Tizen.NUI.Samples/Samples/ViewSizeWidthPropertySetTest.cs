62f9bae2db71ff0fbc5ebbdf7b1ed4a39774f995
huiyu.eun <huiyu.eun@samsung.com>
2020-11-26 16:49:17 +0900

    [NUI] Modify comment
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/ViewSizeWidthPropertySetTest.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/ViewSizeWidthPropertySetTest.cs
@@ -58,11 +58,11 @@ namespace Tizen.NUI.Samples
             {
                 if (e.Key.KeyPressedName == "1")
                 {
-                    view1.Size2D.Width += 10;
+                    view1.SizeWidth += 10;
                 }
                 else if (e.Key.KeyPressedName == "2")
                 {
-                    view1.Size2D.Height += 10;
+                    view1.SizeHeight += 10;
                 }
                 else if (e.Key.KeyPressedName == "3")
                 {
@@ -88,10 +88,14 @@ namespace Tizen.NUI.Samples
                 {
                     view1.MaximumSize.Width = 70;
                 }
+                else if (e.Key.KeyPressedName == "9")
+                {
+                    view1.Size += new Size(10, 10);
+                }
                 else if (e.Key.KeyPressedName == "0")
                 {
-                    view1.Size2D.Width -= 10;
-                    view1.Size2D.Height -= 10;
+                    view1.SizeWidth -= 10;
+                    view1.SizeHeight -= 10;
                 }
             }
         }
@@ -103,6 +107,7 @@ namespace Tizen.NUI.Samples
                 string log = "";
                 log += $"view1 Size2D=({view1.Size2D.Width}, {view1.Size2D.Height}) \n";
                 log += $"SizeWidth={view1.SizeWidth}, SizeHeight={view1.SizeHeight} \n";
+                log += $"Size={view1.Size.Width}, SizeHeight={view1.Size.Height} \n";
                 log += $"WidthResizePolicy={view1.WidthResizePolicy}, HeightResizePolicy={view1.HeightResizePolicy} \n";
                 log += $"WidthSpecification={view1.WidthSpecification}, HeightSpecification={view1.HeightSpecification} \n";
                 log += $"MinimumSize=({view1.MinimumSize.Width}, {view1.MinimumSize.Height}) \n";
