1fc8eb0e7abd838f4707db0f8c42f3191f749c56
Youngbok Shin <youngb.shin@samsung.com>
2020-12-02 17:06:19 +0900

    [NUI] fix CA1801 build warnings from auto-generated codes by SWIG
    The 'futureUse' parameter is never being used through the whole code.
    It removes 'futureUse' parameter from functions.

--- src/Tizen.NUI/src/internal/SWIGTYPE_p_std__type_info.cs
+++ src/Tizen.NUI/src/internal/SWIGTYPE_p_std__type_info.cs
@@ -14,7 +14,7 @@ namespace Tizen.NUI
     {
         private global::System.Runtime.InteropServices.HandleRef swigCPtr;
 
-        internal SWIGTYPE_p_std__type_info(global::System.IntPtr cPtr, bool futureUse)
+        internal SWIGTYPE_p_std__type_info(global::System.IntPtr cPtr)
         {
             swigCPtr = new global::System.Runtime.InteropServices.HandleRef(this, cPtr);
         }
