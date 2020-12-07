c5b08138e2fadcc29314728e08b84776da1c85c2
huiyueun <35286162+huiyueun@users.noreply.github.com>
2020-12-07 16:20:12 +0900

    [NUI] Modify NUI Seamless animation bug (#2334)
    * [NUI] Modify NUI Seamless animation bug
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * [NUI] Modify build warning of seamless
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Update DefaultFrameBroker.cs
    
    * Update TransitionAnimations.cs
    
    * Update DefaultFrameBroker.cs
    
    * Update SeamlessBackward.cs
    
    * Update SeamlessForward.cs
    
    * Update TransitionOptions.cs
    
    * Update NUIBrokerSample.cs
    
    * Update DefaultFrameBroker.cs
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

---
+++ test/Tizen.NUI.Seamless.Xaml/NUIBrokerSample/DefaultView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace NUIBrokerSample
+{
+    public class DefaultView : View
+    {
+        public DefaultView()
+        {
+            PositionUsesPivotPoint = true;
+            ParentOrigin = Tizen.NUI.ParentOrigin.TopCenter;
+            PivotPoint = Tizen.NUI.PivotPoint.Center;
+        }
+    }
+}
