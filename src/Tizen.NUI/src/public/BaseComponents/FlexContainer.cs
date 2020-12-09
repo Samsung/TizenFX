fd911029fa2e06e3c567812a296500d3c110c848
Jiyun Yang <ji.yang@samsung.com>
2020-12-09 14:49:11 +0900

    [NUI] Fix CA1052
    CA1052
    Static holder types should be Static or NotInheritable.
    
    This patch fixes classes that holds only static members to static or sealed.
    If the class is obsolete, added suppress message attribute to hide warnings.
    
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/FlexContainer.cs
+++ src/Tizen.NUI/src/public/BaseComponents/FlexContainer.cs
@@ -16,6 +16,7 @@
  */
 using System;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using Tizen.NUI.Binding;
 
 namespace Tizen.NUI.BaseComponents
@@ -432,6 +433,7 @@ namespace Tizen.NUI.BaseComponents
         /// Enumeration for the instance of child properties belonging to the FlexContainer class.
         /// </summary>
         /// <since_tizen> 3 </since_tizen>
+        [SuppressMessage("Microsoft.Design", "CA1052:StaticHolderTypesShouldBeStaticOrNotInheritable")]
         [Obsolete("Deprecated in API6, Will be removed in API9, " +
             "Please use View.Flex, View.AlignSelf, View.FlexMargin instead!")]
         [EditorBrowsable(EditorBrowsableState.Never)]
