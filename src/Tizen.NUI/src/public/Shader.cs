12096b90ce23673ab4dfd6a86e694d387c58a344
Dongsug Song <dongsug.song@samsung.com>
2020-12-11 10:17:08 +0900

    [NUI] Suppress CA1716, CA1052, CA1034 for deprecated APIs
    - Since the detected classes will be deprecated, CA1716 warning message is suppressed.
    https://docs.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1716
    - This is conflict resolving patch for https://github.com/Samsung/TizenFX/pull/2374

--- src/Tizen.NUI/src/public/Shader.cs
+++ src/Tizen.NUI/src/public/Shader.cs
@@ -120,11 +120,11 @@ namespace Tizen.NUI
         /// Enumeration for instances of properties belonging to the Shader class.
         /// </summary>
         /// <since_tizen> 3 </since_tizen>
-        [SuppressMessage("Microsoft.Design", "CA1052:StaticHolderTypesShouldBeStaticOrNotInheritable")]
         [EditorBrowsable(EditorBrowsableState.Never)]
         [Obsolete("Deprecated in API6; Will be removed in API9.")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1034:NestedTypesShouldNotBeVisible")]
+#pragma warning disable CA1716, CA1052, CA1034 // Identifiers should not match keywords
         public class Property
+#pragma warning restore CA1716, CA1052, CA1034 // Identifiers should not match keywords
         {
             /// <summary>
             /// The default value is empty.
