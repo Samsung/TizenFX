2d2f1f10865fb2513479e24d7d00da34727c9253
huiyu <huiyu.eun@samsung.com>
2020-12-15 11:10:34 +0900

    Revert "[NUI] Fix build warning[CA1000]"
    This reverts commit 4cef848feaaefe4abac0b5f7890b8402e37fbcf6.

--- src/Tizen.NUI/src/internal/DisposeQueue.cs
+++ src/Tizen.NUI/src/internal/DisposeQueue.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 
 namespace Tizen.NUI
 {
-    internal class DisposeQueue : IDisposable
+    internal class DisposeQueue
     {
         private static readonly DisposeQueue _disposableQueue = new DisposeQueue();
         private List<IDisposable> _disposables = new List<IDisposable>();
@@ -68,22 +68,5 @@ namespace Tizen.NUI
                 _disposables.Clear();
             }
         }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                // Dispose managed resources.
-                _disposableQueue?.Dispose();
-            }
-            // Free native resources.
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
     }
 }
