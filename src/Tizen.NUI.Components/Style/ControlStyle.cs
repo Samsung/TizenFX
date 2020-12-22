4785f732b6448900f629bc4982fbd4c7fce2bd67
huiyu.eun <huiyu.eun@samsung.com>
2020-12-22 12:46:26 +0900

    [NUI]Fix build warning[CA1001]
    https://docs.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1001
    CA1001: Types that own disposable fields should be disposable
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

--- src/Tizen.NUI.Components/Style/ControlStyle.cs
+++ src/Tizen.NUI.Components/Style/ControlStyle.cs
@@ -25,10 +25,8 @@ namespace Tizen.NUI.Components
     /// <since_tizen> 6 </since_tizen>
     /// This will be public opened in tizen_5.5 after ACR done. Before ACR, need to be hidden as inhouse API.
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public class ControlStyle : ViewStyle, global::System.IDisposable
+    public class ControlStyle : ViewStyle
     {
-        private bool isDisposeQueued = false;
-
         static ControlStyle()
         {
             ThemeManager.AddPackageTheme(new DefaultThemeCreator());
@@ -54,69 +52,6 @@ namespace Tizen.NUI.Components
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ControlStyle(ControlStyle style) : base(style)
         {
-        }
-
-        /// <summary>
-        /// Dispose.
-        /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        ~ControlStyle()
-        {
-            if (!isDisposeQueued)
-            {
-                isDisposeQueued = true;
-                DisposeQueue.Instance.Add(this);
-            }
-        }
-
-        /// <summary>
-        /// A Flag to check if it is already disposed.
-        /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        protected bool Disposed { get; set; }
-
-        /// <summary>
-        /// Dispose.
-        /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public void Dispose()
-        {
-            //Throw excpetion if Dispose() is called in separate thread.
-            if (!Window.IsInstalled())
-            {
-                throw new global::System.InvalidOperationException("This API called from separate thread. This API must be called from MainThread.");
-            }
-
-            if (isDisposeQueued)
-            {
-                Dispose(DisposeTypes.Implicit);
-            }
-            else
-            {
-                Dispose(DisposeTypes.Explicit);
-                global::System.GC.SuppressFinalize(this);
-            }
-        }
-
-        /// <summary>
-        /// Dispose.
-        /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        protected virtual void Dispose(DisposeTypes type)
-        {
-            if (Disposed)
-            {
-                return;
-            }
-
-            if (type == DisposeTypes.Explicit)
-            {
-                //Called by User
-                //Release your own managed resources here.
-                //You should release all of your own disposable objects here.
-            }
-
-            Disposed = true;
         }
 
         private void SubStyleCalledEvent(object sender, global::System.EventArgs e)
