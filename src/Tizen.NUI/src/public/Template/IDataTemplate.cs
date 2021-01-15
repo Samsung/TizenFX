5a7b42068e156a8d68b23a5638f4eafcede5d1e7
SangHyeon Jade Lee <sh10233.lee@samsung.com>
2021-01-15 15:02:47 +0900

    [NUI] Update templates to EditorBrowsableState.Never

--- src/Tizen.NUI/src/public/Template/IDataTemplate.cs
+++ src/Tizen.NUI/src/public/Template/IDataTemplate.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 
 namespace Tizen.NUI.Binding
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IDataTemplate
     {
         Func<object> LoadTemplate { get; set; }
