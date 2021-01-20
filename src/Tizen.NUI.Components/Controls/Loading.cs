f7d7dcb551baecb5c07304ee3b4f060b502fc807
huiyu.eun <huiyu.eun@samsung.com>
2021-01-20 18:17:12 +0900

    [NUI] Add DeleteAccessibilityAttribute
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

--- src/Tizen.NUI.Components/Controls/Loading.cs
+++ src/Tizen.NUI.Components/Controls/Loading.cs
@@ -218,6 +218,7 @@ namespace Tizen.NUI.Components
                 //Called by User
                 //Release your own managed resources here.
                 //You should release all of your own disposable objects here.
+                AccessibilityManager.Instance.DeleteAccessibilityAttribute(this);
                 RemoveVisual("loadingImageVisual");
             }
 
