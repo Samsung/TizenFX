9658c85eecda991e83809dc87534d4277f96ed85
Dongsug Song <dongsug.song@samsung.com>
2021-02-25 17:06:26 +0900

    Revert "[NUI] Remove APIs which have been deprecated in API7"
    This reverts commit 4c6d61473e4a0fadde471ab1e6291810df4ec23a.

--- src/Tizen.NUI/src/public/CustomView/Spin.cs
+++ src/Tizen.NUI/src/public/CustomView/Spin.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Runtime.InteropServices;
 using Tizen.NUI;
+using Tizen.NUI.UIComponents;
 using Tizen.NUI.BaseComponents;
 
 // A spin control (for continously changing values when users can easily predict a set of values)
