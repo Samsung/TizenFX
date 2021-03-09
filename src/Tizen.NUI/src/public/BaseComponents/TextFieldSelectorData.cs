581b6d5a248e8de1398ab3e9c2b63d7518bd5eac
Jiyun Yang <ji.yang@samsung.com>
2021-03-09 12:19:34 +0900

    [NUI] Improve performance in theme and styles (#2704)
    * [NUI] Improve performance: Make selectors lazily generated.
    
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>
    
    * [NUI] Improve performance: Remove redundant clone
    
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>
    
    * [NUI] Improve performance: Add dirty style property set
    
    This may reduce time of ViewStyle.Clone() and View.ApplyStyle().
    But may increase runtime memory.
    
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/TextFieldSelectorData.cs
+++ src/Tizen.NUI/src/public/BaseComponents/TextFieldSelectorData.cs
@@ -22,54 +22,37 @@ namespace Tizen.NUI.BaseComponents
     /// </summary>
     internal class TextFieldSelectorData
     {
-        public TriggerableSelector<string> TranslatableText { get; } = new TriggerableSelector<string>(TextField.TranslatableTextProperty);
-        public TriggerableSelector<string> Text { get; } = new TriggerableSelector<string>(TextField.TextProperty);
-        public TriggerableSelector<string> FontFamily { get; } = new TriggerableSelector<string>(TextField.FontFamilyProperty);
-        public TriggerableSelector<Color> TextColor { get; } = new TriggerableSelector<Color>(TextField.TextColorProperty, GetTextColor);
-        public TriggerableSelector<float?> PointSize { get; } = new TriggerableSelector<float?>(TextField.PointSizeProperty);
-        public TriggerableSelector<float?> PixelSize { get; } = new TriggerableSelector<float?>(TextField.PixelSizeProperty);
-        public TriggerableSelector<string> TranslatablePlaceholderText { get; } = new TriggerableSelector<string>(TextField.TranslatablePlaceholderTextProperty);
-        public TriggerableSelector<Vector4> PlaceholderTextColor { get; } = new TriggerableSelector<Vector4>(TextField.PlaceholderTextColorProperty, delegate (View view)
-        {
-            Vector4 color = new Vector4();
-            if (view.GetProperty(TextField.Property.PlaceholderTextColor).Get(color))
-            {
-                return color;
-            }
-            return null;
-        });
+        public TriggerableSelector<string> TranslatableText { get; private set; }
+        public TriggerableSelector<string> Text { get; private set; }
+        public TriggerableSelector<string> FontFamily { get; private set; }
+        public TriggerableSelector<Color> TextColor { get; private set; }
+        public TriggerableSelector<float?> PointSize { get; private set; }
+        public TriggerableSelector<float?> PixelSize { get; private set; }
+        public TriggerableSelector<string> TranslatablePlaceholderText { get; private set; }
+        public TriggerableSelector<Vector4> PlaceholderTextColor { get; private set; }
+        public TriggerableSelector<Vector4> PrimaryCursorColor { get; private set; }
 
-        public TriggerableSelector<Vector4> PrimaryCursorColor { get; } = new TriggerableSelector<Vector4>(TextField.PrimaryCursorColorProperty, delegate (View view)
-        {
-            Vector4 color = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
-            if (view.GetProperty(TextField.Property.PrimaryCursorColor).Get(color))
-            {
-                return color;
-            }
-            return null;
-        });
+        public TriggerableSelector<string> EnsureTranslatableText() => TranslatableText ?? (TranslatableText = new TriggerableSelector<string>(TextField.TranslatableTextProperty));
+        public TriggerableSelector<string> EnsureText() => Text ?? (Text = new TriggerableSelector<string>(TextField.TextProperty));
+        public TriggerableSelector<string> EnsureFontFamily() => FontFamily ?? (FontFamily = new TriggerableSelector<string>(TextField.FontFamilyProperty));
+        public TriggerableSelector<Color> EnsureTextColor() => TextColor ?? (TextColor = new TriggerableSelector<Color>(TextField.TextColorProperty));
+        public TriggerableSelector<float?> EnsurePointSize() => PointSize ?? (PointSize = new TriggerableSelector<float?>(TextField.PointSizeProperty));
+        public TriggerableSelector<float?> EnsurePixelSize() => PixelSize ?? (PixelSize = new TriggerableSelector<float?>(TextField.PixelSizeProperty));
+        public TriggerableSelector<string> EnsureTranslatablePlaceholderText() => TranslatablePlaceholderText ?? (TranslatablePlaceholderText = new TriggerableSelector<string>(TextField.TranslatablePlaceholderTextProperty));
+        public TriggerableSelector<Vector4> EnsurePlaceholderTextColor() =>  PlaceholderTextColor ?? (PlaceholderTextColor = new TriggerableSelector<Vector4>(TextField.PlaceholderTextColorProperty));
+        public TriggerableSelector<Vector4> EnsurePrimaryCursorColor() => PrimaryCursorColor ?? (PrimaryCursorColor = new TriggerableSelector<Vector4>(TextField.PrimaryCursorColorProperty));
 
         public void Reset(View view)
         {
-            TranslatableText.Reset(view);
-            Text.Reset(view);
-            FontFamily.Reset(view);
-            TextColor.Reset(view);
-            PointSize.Reset(view);
-            PixelSize.Reset(view);
-            TranslatablePlaceholderText.Reset(view);
-            PlaceholderTextColor.Reset(view);
-            PrimaryCursorColor.Reset(view);
-        }
-
-        private static Color GetTextColor(View view)
-        {
-            Color color = new Color();
-            if (view.GetProperty(TextField.Property.TextColor).Get(color))
-            {
-                return color;
-            }
-            return null;
+            TranslatableText?.Reset(view);
+            Text?.Reset(view);
+            FontFamily?.Reset(view);
+            TextColor?.Reset(view);
+            PointSize?.Reset(view);
+            PixelSize?.Reset(view);
+            TranslatablePlaceholderText?.Reset(view);
+            PlaceholderTextColor?.Reset(view);
+            PrimaryCursorColor?.Reset(view);
         }
     }
 }
--- src/Tizen.NUI/src/public/BaseComponents/ViewInternal.cs
+++ src/Tizen.NUI/src/public/BaseComponents/ViewInternal.cs
@@ -27,6 +27,7 @@ namespace Tizen.NUI.BaseComponents
     public partial class View
     {
         private ViewSelectorData selectorData;
+
         internal string styleName;
 
         /// <summary>
@@ -332,18 +333,6 @@ namespace Tizen.NUI.BaseComponents
                 PropertyValue setValue = new Tizen.NUI.PropertyValue(value);
                 SetProperty(View.Property.DownFocusableViewId, setValue);
                 setValue.Dispose();
-            }
-        }
-
-        private ViewSelectorData SelectorData
-        {
-            get
-            {
-                if (selectorData == null)
-                {
-                    selectorData = new ViewSelectorData();
-                }
-                return selectorData;
             }
         }
 
@@ -1098,6 +1087,21 @@ namespace Tizen.NUI.BaseComponents
         }
 
         /// <summary>
+        /// Get selector value from the triggerable selector or related property.
+        /// </summary>
+        internal Selector<T> GetSelector<T>(TriggerableSelector<T> triggerableSelector, NUI.Binding.BindableProperty relatedProperty)
+        {
+            var selector = triggerableSelector?.Get();
+            if (selector != null)
+            {
+                return selector;
+            }
+
+            var value = (T)GetValue(relatedProperty);
+            return value == null ? null : new Selector<T>(value);
+        }
+
+        /// <summary>
         /// you can override it to clean-up your own resources.
         /// </summary>
         /// <param name="type">DisposeTypes</param>
@@ -1374,5 +1378,7 @@ namespace Tizen.NUI.BaseComponents
             if (style != null) ApplyStyle(style.Clone());   // Use given style
             else if (ThemeManager.ThemeApplied) UpdateStyle(); // Use style in the current theme
         }
+
+        private ViewSelectorData EnsureSelectorData() => selectorData ?? (selectorData = new ViewSelectorData());
     }
 }
