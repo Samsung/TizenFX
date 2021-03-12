d1e30e0dc40a0b731c48aca0e5f8a3514b3e42a7
Joogab Yun <joogab.yun@samsung.com>
2021-03-12 10:50:58 +0900

    1. [NUI] Add TouchArea property.
    Default touchable area is view's size.
    TouchArea can reset the view's touchable area.
    
    This is usefull when the view is small, but it should have a larger touch area.
    
    for example
    
          View view = new View();
          view.Size = new Size(10, 10);
          view.TouchEvent += OnTouchEvent;
          view.TouchArea = new Size2D(200, 200);
    
    The view is small, If you want to set the touch area to a larger area,
    you can use the TouchArea property.
    
    2. [NUI] change type from Size2D to Size at TouchArea api
    
    3. [NUI] Change  TouchArea to TouchAreaOffset (#2948)
    
        You can expand the touch area.
    
        for example)
          View view = new View();
          view.Size = new Size(10, 10);
          view.TouchEvent += OnTouch;
          view.TouchAreaOffset = new Offset(-100, 100, 100, -100); // left, right, bottom, top
    
        then touch area is 210x210.
        this is view.width  -touchAreaOffset.left + touchAreaOffset.right
        and view.height + touchAreaOffset.bottom  -touchAreaOffset.top

--- src/Tizen.NUI/src/public/BaseComponents/ViewEvent.cs
+++ src/Tizen.NUI/src/public/BaseComponents/ViewEvent.cs
@@ -1278,5 +1278,51 @@ namespace Tizen.NUI.BaseComponents
             }
         }
 
+        /// <summary>
+        /// TouchArea can expand the view's touchable area.<br/>
+        /// If you set the TouchAreaOffset on an view, when you touch the view, the touch area is used rather than the size of the view.<br/>
+        /// This is based on the top left x, y coordinates.<br/>
+        /// example) <br/>
+        ///  view.Size = new Size(100, 100);<br/>
+        ///  view.TouchAreaOffset = new Offset(-10, 20, 30, -40); // left, right, bottom, top <br/>
+        /// then touch area is 130x170.<br/>
+        /// this is view.width + TouchAreaOffset.right - TouchAreaOffset.left and view.height + TouchAreaOffset.bottom - TouchAreaOffset.top <br/>
+        /// +---------------------+ <br/>
+        /// |         ^           | <br/>
+        /// |         |           | <br/>
+        /// |         | -40       | <br/>
+        /// |         |           | <br/>
+        /// |         |           | <br/>
+        /// |    +----+----+      | <br/>
+        /// |    |         |      | <br/>
+        /// | -10|         | 20   | <br/>
+        /// |<---+         +----->| <br/>
+        /// |    |         |      | <br/>
+        /// |    |         |      | <br/>
+        /// |    +----+----+      | <br/>
+        /// |         |           | <br/>
+        /// |         | 30        | <br/>
+        /// |         |           | <br/>
+        /// |         v           | <br/>
+        /// +---------------------+ <br/>
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public Offset TouchAreaOffset
+        {
+            get
+            {
+                Interop.ActorInternal.GetTouchAreaOffset(SwigCPtr, out int left, out int right, out int bottom, out int top);
+                if (NDalicPINVOKE.SWIGPendingException.Pending)
+                    throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+                return new Offset(left, right, bottom, top);
+            }
+            set
+            {
+                Interop.ActorInternal.SetTouchAreaOffset(SwigCPtr, value.Left, value.Right, value.Bottom, value.Top);
+                if (NDalicPINVOKE.SWIGPendingException.Pending)
+                    throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+            }
+        }
+
     }
 }
