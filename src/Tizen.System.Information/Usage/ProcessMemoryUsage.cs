551e45ce60f1cd5dc6c8f4abd362baf015fe88cd
yunmiha <49266695+yunmiha@users.noreply.github.com>
2021-03-15 16:19:54 +0900

    Update src/Tizen.System.Information/Usage/ProcessMemoryUsage.cs
    Co-authored-by: WonYoung Choi <wy80.choi@samsung.com>

--- src/Tizen.System.Information/Usage/ProcessMemoryUsage.cs
+++ src/Tizen.System.Information/Usage/ProcessMemoryUsage.cs
@@ -232,7 +232,6 @@ namespace Tizen.System
         /// <summary>
         /// Gets the SWAP memory size of a process.
         /// </summary>
-        /// <since_tizen> 6.5 </since_tizen>
         /// <param name="pid">The process id.</param>
         /// <returns>The SWAP memory size <paramref name="pid"/> is using (KiB).</returns>
         /// <exception cref="ArgumentException">Thrown when the <paramref name="pid"/> is invalid.</exception>
