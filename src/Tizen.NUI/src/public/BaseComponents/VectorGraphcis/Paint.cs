56e24db8566e9b2872fb6a57e4dd756d5aa130a8
JunsuChoi <jsuya.choi@samsung.com>
2021-03-18 17:40:22 +0900

    BaseComponent: Introduce CanvasView component for vector primitives.
    CanvasView draws Vector Primitives using ThorVG library.
    CanvasView can draw a shape in its internal buffer by adding a Shape object.
    Shape is a command list based on Path. Fill and Stroke properties can be set using setters and getters.
    Paint is a parent class of Shape and it has common class of classes to be added later (Scene, etc.).
    
    Classes
      Tizen.NUI.BaseComponents.VectorGraphics.CanvasView
      Tizen.NUI.BaseComponents.VectorGraphics.Paint
      Tizen.NUI.BaseComponents.VectorGraphics.Shape
    
    Dependancy
      Dali-Toolkit : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-toolkit/+/250398/
      Dali-Adaptor : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-adaptor/+/250466/
      ThorVG : https://github.com/Samsung/thorvg
    
    Example
      test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CanvasViewSamsple.cs

---
+++ src/Tizen.NUI/src/public/BaseComponents/VectorGraphcis/Paint.cs
@@ -0,0 +1,118 @@
+/*
+* Copyright(c) 2019 Samsung Electronics Co., Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+using System.ComponentModel;
+
+namespace Tizen.NUI.BaseComponents
+{
+
+    namespace VectorGraphics
+    {
+        /// <summary>
+        /// Paint is a object class for drawing a vector primitive.<br />
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public class Paint : BaseHandle
+        {
+            private Paint()
+            {
+
+            }
+
+            internal Paint(global::System.IntPtr cPtr, bool cMemoryOwn) : base(cPtr, cMemoryOwn)
+            {
+            }
+
+            /// <summary>
+            /// Set the transparency value
+            /// </summary>
+            /// <param name="opacity">The transparency level [0 ~ 1.0], 0 means totally transparent, while 1 means opaque.</param>
+            /// <returns>True when it's successful. False otherwise.</returns>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public bool SetOpacity(float opacity)
+            {   
+                if (Interop.Paint.SetOpacity(BaseHandle.getCPtr(this), opacity))
+                    return true;
+                return false;
+            }
+
+            /// <summary>
+            /// Get the transparency value
+            /// </summary>
+            /// <returns>The transparency level</returns>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public float GetOpacity()
+            {   
+                return Interop.Paint.GetOpacity(BaseHandle.getCPtr(this));
+            }
+
+            /// <summary>
+            /// Set the x, y movement value of translate transformation.
+            /// </summary>
+            /// <param name="x">The x-axis movement value.</param>
+            /// <param name="y">The y-axis movement value.</param>
+            /// <returns>True when it's successful. False otherwise.</returns>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public bool Translate(float x, float y)
+            {   
+                if (Interop.Paint.Translate(BaseHandle.getCPtr(this), x, y))
+                    return true;
+                return false;
+            }
+            
+            /// <summary>
+            /// Set the scale value of scale transformation.
+            /// </summary>
+            /// <param name="factor">The scale factor value.</param>
+            /// <returns>True when it's successful. False otherwise.</returns>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public bool Scale(float factor)
+            {   
+                if (Interop.Paint.Scale(BaseHandle.getCPtr(this), factor))
+                    return true;
+                return false;
+            }
+
+            /// <summary>
+            /// Set the angle of rotation transformation.
+            /// </summary>
+            /// <param name="degree">The degree value of angle.</param>
+            /// <returns>True when it's successful. False otherwise.</returns>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public bool Rotate(float degree)
+            {   
+                if (Interop.Paint.Rotate(BaseHandle.getCPtr(this), degree))
+                    return true;
+                return false;
+            }
+
+            /// <summary>
+            /// Set the matrix value for affine transform.
+            /// </summary>
+            /// <param name="matrix">The float type array of 3x3 matrix.</param>
+            /// <returns>True when it's successful. False otherwise.</returns>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public bool Transform(float[] matrix)
+            {   
+                if (Interop.Paint.Transform(BaseHandle.getCPtr(this), matrix))
+                    return true;
+                return false;
+            }
+        }
+        
+
+    }
+}
