dd43b894c72c68b00d723fdb1e8e05f5c444e481
JunsuChoi <jsuya.choi@samsung.com>
2021-03-18 17:40:22 +0900

    BaseComponent: Introduce CanvasView component for vector primitives.
    CanvasView draws Vector Primitives using ThorVG library.
    CanvasView can draw a shape in its internal buffer by adding a Shape object.
    Shape is a command list based on Path. Fill and Stroke properties can be set using setters and getters.
    Paint is a parent class of Shape and it has common class of classes to be added later (Scene, etc.).
    
    Classes
      Tizen.NUI.BaseComponents.VectorGraphics.CanvasView
      Tizen.NUI.BaseComponents.VectorGraphics.Paint
      Tizen.NUI.BaseComponents.VectorGraphics.Shape
    
    Dependancy
      Dali-Toolkit : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-toolkit/+/250398/
      Dali-Adaptor : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-adaptor/+/250466/
      ThorVG : https://github.com/Samsung/thorvg
    
    Example
      test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CanvasViewSamsple.cs

---
+++ src/Tizen.NUI/src/internal/Interop/Interop.Paint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class Paint
+        {
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Paint_Translate")]
+            public static extern bool Translate(global::System.Runtime.InteropServices.HandleRef jarg1, float jarg2, float jarg3);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Paint_SetOpacity")]
+            public static extern bool SetOpacity(global::System.Runtime.InteropServices.HandleRef jarg1, float jarg2);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Paint_GetOpacity")]
+            public static extern float GetOpacity(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Paint_Scale")]
+            public static extern bool Scale(global::System.Runtime.InteropServices.HandleRef jarg1, float jarg2);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Paint_Rotate")]
+            public static extern bool Rotate(global::System.Runtime.InteropServices.HandleRef jarg1, float jarg2);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Paint_Transform")]
+            public static extern bool Transform(global::System.Runtime.InteropServices.HandleRef jarg1, [global::System.Runtime.InteropServices.In, global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.LPArray)] float[] jarg2);
+        }
+}
+    }
