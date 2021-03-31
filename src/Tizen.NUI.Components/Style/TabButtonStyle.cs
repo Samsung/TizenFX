61b7f4124e874c9751df0b9ecf70e22d02ebcbaf
Jaehyun Cho <jaehyun0cho@gmail.com>
2021-03-31 16:38:59 +0900

    [NUI] Add TabButtonStyle class with the latest TabButton UX (#2780)
    To apply the latest TabButton UX, TabButtonStyle class is added.
    
    TabButtonStyle.TopLine and TabButtonStyle.BottomLine are displayed when
    TabButton is pressed or selected.
    
    Co-authored-by: Jaehyun Cho <jae_hyun.cho@samsung.com>

---
+++ src/Tizen.NUI.Components/Style/TabButtonStyle.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Copyright(c) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System.ComponentModel;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Binding;
+
+namespace Tizen.NUI.Components
+{
+    /// <summary>
+    /// TabButtonStyle is a class which saves TabButton's ux data.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class TabButtonStyle : ButtonStyle
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        static TabButtonStyle() { }
+
+        /// <summary>
+        /// Creates a new instance of a TabButtonStyle.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TabButtonStyle() : base()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of a TabButtonStyle with style.
+        /// </summary>
+        /// <param name="style">Create TabButtonStyle by style customized by user.</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TabButtonStyle(TabButtonStyle style) : base(style)
+        {
+        }
+
+        /// <summary>
+        /// Gets or Sets the Line Style at the top of TabButton.
+        /// </summary>
+        internal ViewStyle TopLine { get; set; } = new ViewStyle();
+
+        /// <summary>
+        /// Gets or Sets the Line Style at the bottom of TabButton.
+        /// </summary>
+        internal ViewStyle BottomLine { get; set; } = new ViewStyle();
+
+        /// <summary>
+        /// Style's clone function.
+        /// </summary>
+        /// <param name="bindableObject">The style that need to copy.</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override void CopyFrom(BindableObject bindableObject)
+        {
+            base.CopyFrom(bindableObject);
+
+            if (bindableObject is TabButtonStyle tabButtonStyle)
+            {
+                TopLine.CopyFrom(tabButtonStyle.TopLine);
+                BottomLine.CopyFrom(tabButtonStyle.BottomLine);
+            }
+        }
+    }
+}
