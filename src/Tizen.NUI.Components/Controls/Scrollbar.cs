1762a0f11c5d7f524ae246219a507d207085a0aa
Jaehyun Cho <jae_hyun.cho@samsung.com>
2021-04-06 18:07:53 +0900

    [NUI] Fix not to block touch event by Scrollbar
    Now, Scrollbar blocks touch events to ScrollableBase's Contents.
    
    Since Scrollbar does not support Handler mode now, Scrollbar's
    EnableControlState is set to be false not to catch TouchEvent.

--- src/Tizen.NUI.Components/Controls/Scrollbar.cs
+++ src/Tizen.NUI.Components/Controls/Scrollbar.cs
@@ -244,6 +244,8 @@ namespace Tizen.NUI.Components
 
             WidthResizePolicy = ResizePolicyType.FillToParent;
             HeightResizePolicy = ResizePolicyType.FillToParent;
+
+            EnableControlState = false;
         }
 
         /// <inheritdoc/>
