9283016721813ba3d550754a3a1a8fe6ea7fcb2c
Bowon Ryu <bowon.ryu@samsung.com>
2021-04-07 19:26:26 +0900

    [NUI] Support markup anchor to TextLabel, TextField, TextEditor (#2813)
    * [NUI] Support markup anchor to TextLabel, TextField, TextEditor
    
    User can set href on anchors using the anchor tag <a> in markup.
    and also can register AnchorClicked callback in Text components.
    the AnchorClicked signal is emitted when the anchor is clicked.
    
    example:
    label.Text = "<a href='https://www.tizen.org'>Tizen</a>";
    label.EnableMarkup = true;
    label.AnchorClicked += (sender, e) =>
    {
      Tizen.Log.Info("NUI", e.Href + "\n");
    };
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>
    
    [NUI] Remove unnecessary codes.
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>
    
    * [NUI] Change the name of signal to AnchorClicked.
    
    To prevent confusion with TouchSignal(),
    the name of signal was changed to AnchorClicked.
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- src/Tizen.NUI/src/internal/Interop/Interop.TextEditor.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.TextEditor.cs
@@ -180,6 +180,9 @@ namespace Tizen.NUI
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_TextEditor_MaxLengthReachedSignal")]
             public static extern global::System.IntPtr MaxLengthReachedSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
 
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_TextEditor_AnchorClickedSignal")]
+            public static extern global::System.IntPtr AnchorClickedSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
+
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_TextEditorSignal_Empty")]
             public static extern bool TextEditorSignalEmpty(global::System.Runtime.InteropServices.HandleRef jarg1);
 
