43a1dd73f316e84aeb887a656df9a6364384d8c9
Seungkeun Lee <sngn.lee@samsung.com>
2021-04-07 11:40:49 +0900

    Add Extension method to help use PropertyMap class

--- src/Tizen.NUI/src/public/Common/PropertyMap.cs
+++ src/Tizen.NUI/src/public/Common/PropertyMap.cs
@@ -317,4 +317,25 @@ namespace Tizen.NUI
             Interop.PropertyMap.DeletePropertyMap(swigCPtr);
         }
     }
+
+    internal static class PropertyMapSetterHelper
+    {
+        internal static PropertyMap Add<T>(this PropertyMap propertyMap, int key, T value)
+        {
+            using (var pv = PropertyValue.CreateFromObject(value))
+            {
+                propertyMap.Add(key, pv);
+            }
+            return propertyMap;
+        }
+
+        internal static PropertyMap Add<T>(this PropertyMap propertyMap, string key, T value)
+        {
+            using (var pv = PropertyValue.CreateFromObject(value))
+            {
+                propertyMap.Add(key, pv);
+            }
+            return propertyMap;
+        }
+    }
 }
