d38226e5327e596306e932c061ae68e2434a0574
huayong.xu <huayong.xu@samsung.com>
2021-05-10 16:06:35 +0800

    [NUI] Fix crash issue when favicon is null.

--- src/Tizen.NUI/src/internal/WebView.cs
+++ src/Tizen.NUI/src/internal/WebView.cs
@@ -645,7 +645,7 @@ namespace Tizen.NUI
             get
             {
                 global::System.IntPtr imageView = Interop.WebView.WebView_GetFavicon(swigCPtr);
-                if (NDalicPINVOKE.SWIGPendingException.Pending)
+                if (imageView == IntPtr.Zero)
                     return null;
                 return new ImageView(imageView, false);
             }
