40e5f23d776d4d3e191de1b76fc4fa6fca750e6f
Artur Świgoń <aswigon@yandex.com>
2021-05-25 03:41:07 +0200

    [NUI][AT-SPI] Fix DllImport typo in Interop.Accessibility.SuppressScreenReader (#3070)
    Co-authored-by: Artur Świgoń <a.swigon@samsung.com>
    Co-authored-by: Seoyeon2Kim <34738918+Seoyeon2Kim@users.noreply.github.com>

--- src/Tizen.NUI/src/internal/Interop/Interop.Accessibility.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.Accessibility.cs
@@ -33,7 +33,7 @@ namespace Tizen.NUI
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "csharp_dali_accessibility_stop_reading")]
             public static extern void StopReading(global::System.Runtime.InteropServices.HandleRef jarg1, bool jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "csharp_dali_accessibility_supress_screen_reader")]
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "csharp_dali_accessibility_suppress_screen_reader")]
             public static extern bool SuppressScreenReader(global::System.Runtime.InteropServices.HandleRef jarg1, bool jarg2);
 
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "csharp_dali_accessibility_BridgeEnableAutoInit")]
