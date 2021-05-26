ac1f3bac1cf96712994cdaf0b8a49631383a8658
Woochanlee <wc0917.lee@samsung.com>
2021-05-26 14:39:51 +0900

    [NUI][ATSPI] Default accessibility support for Picker components

--- src/Tizen.NUI.Components/Controls/DatePicker.cs
+++ src/Tizen.NUI.Components/Controls/DatePicker.cs
@@ -211,6 +211,7 @@ namespace Tizen.NUI.Components
         public override void OnInitialize()
         {
             base.OnInitialize();
+            SetAccessibilityConstructor(Role.DateEditor, AccessibilityInterface.Value);
 
             dayPicker = new Picker()
             {
@@ -244,6 +245,7 @@ namespace Tizen.NUI.Components
                          Justification = "The CellPadding will be dispose when the date picker disposed")]
         private void Initialize()
         {
+            AccessibilityHighlightable = true;
             HeightSpecification = LayoutParamPolicies.MatchParent;
 
             Layout = new LinearLayout() { 
