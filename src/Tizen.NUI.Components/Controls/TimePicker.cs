cb2381bab47ba6936bc3b9103cfbab1ad4d84b1d
Woochanlee <wc0917.lee@samsung.com>
2021-05-27 13:21:29 +0900

    [NUI][ATSPI] Modify Accessbility for picker components

--- src/Tizen.NUI.Components/Controls/TimePicker.cs
+++ src/Tizen.NUI.Components/Controls/TimePicker.cs
@@ -208,7 +208,7 @@ namespace Tizen.NUI.Components
         public override void OnInitialize()
         {
             base.OnInitialize();
-            SetAccessibilityConstructor(Role.DateEditor, AccessibilityInterface.Value);
+            SetAccessibilityConstructor(Role.DateEditor);
 
             hourPicker = new Picker()
             {
@@ -271,7 +271,6 @@ namespace Tizen.NUI.Components
                          Justification = "The CellPadding will be dispose when the time picker disposed")]
         private void Initialize()
         {
-            AccessibilityHighlightable = true;
             HeightSpecification = LayoutParamPolicies.MatchParent;
 
             Layout = new LinearLayout() { 
