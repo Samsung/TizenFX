c0f47a64549cf6e060f223128cbd810f6dfee690
zhouleonlei <zhouleon.lei@samsung.com>
2021-05-28 16:54:44 +0800

    [NUI] Add TCs for Public Events

--- test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/public/Events/TSGestureDetectorManager.cs
+++ test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/public/Events/TSGestureDetectorManager.cs
@@ -3,6 +3,7 @@ using NUnit.Framework.TUnit;
 using System;
 using Tizen.NUI;
 using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Events;
 
 namespace Tizen.NUI.Devel.Tests
 {
@@ -37,6 +38,12 @@ namespace Tizen.NUI.Devel.Tests
         {
             tlog.Debug(tag, $"GestureDetectorManagerConstructor START");
             View view = new View();
+
+            GestureDetectorManager a1 = new GestureDetectorManager(view, new GestureDetectorManager.GestureListener());
+            a1.Dispose();
+
+            GestureDetectorManager b1 = new GestureDetectorManager(null, new GestureDetectorManager.GestureListener());
+            GestureDetectorManager c1 = new GestureDetectorManager(view, null);
             view.Dispose();
 
             tlog.Debug(tag, $"GestureDetectorManagerConstructor END (OK)");
@@ -53,6 +60,14 @@ namespace Tizen.NUI.Devel.Tests
         public void GestrueFeedTouchEvent()
         {
             tlog.Debug(tag, $"GestrueFeedTouchEvent START");
+            object sender = new object();
+            View view = new View();
+            View.TouchEventArgs e = new View.TouchEventArgs();
+            GestureDetectorManager a1 = new GestureDetectorManager(view, new GestureDetectorManager.GestureListener());
+
+            a1.FeedTouchEvent(sender, e);
+
+            a1.Dispose();
             tlog.Debug(tag, $"GestrueFeedTouchEvent END (OK)");
             Assert.Pass("GestrueFeedTouchEvent");
         }
