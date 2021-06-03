8c65834ca68455b53bb27fcf2092468d2f97dc19
EverLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2021-06-03 16:44:18 +0900

    make IsGrouped setter internal as only the GroupHeaderTemplate can enable it

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CollectionViewDemo/Group/CollectionViewLinearGroupSample.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CollectionViewDemo/Group/CollectionViewLinearGroupSample.cs
@@ -80,7 +80,6 @@ namespace Tizen.NUI.Samples
                     return group;
                 }),
                 Header = myTitle,
-                IsGrouped = true,
                 ScrollingDirection = ScrollableBase.Direction.Vertical,
                 WidthSpecification = LayoutParamPolicies.MatchParent,
                 HeightSpecification = LayoutParamPolicies.MatchParent,
