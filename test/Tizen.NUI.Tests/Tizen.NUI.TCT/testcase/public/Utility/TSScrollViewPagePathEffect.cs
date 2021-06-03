ed7018ea5bb926299a4cb57f8178a146b5d7a8b3
guowei.wang <guowei.wang@samsung.com>
2021-06-03 19:08:02 +0800

    [NUI] Add Utility(public) TCs.

---
+++ test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/public/Utility/TSScrollViewPagePathEffect.cs
@@ -0,0 +1,179 @@
+﻿using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+using System.Collections.Generic;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("public/Utility/ScrollViewPagePathEffect")]
+    public class PublicScrollViewPagePathEffectTest
+    {
+        private const string tag = "NUITEST";
+
+        internal class MyScrollViewPagePathEffect : ScrollViewPagePathEffect
+        {
+            private static Path path = new Path();
+            private static Vector3 forward = new Vector3(2, 4, 6);
+            private static int inputPropertyIndex = 1;
+            private static Vector3 pageSize = new Vector3(6, 8, 10);
+            private static uint pageCount = 5;
+
+            public MyScrollViewPagePathEffect() : base(path, forward, inputPropertyIndex, pageSize, pageCount)
+            { }
+
+            public void OnReleaseSwigCPtr(global::System.Runtime.InteropServices.HandleRef swigCPtr)
+            {
+                base.ReleaseSwigCPtr(swigCPtr);
+            }
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("ScrollViewPagePathEffect constructor.")]
+        [Property("SPEC", "Tizen.NUI.ScrollViewPagePathEffect.ScrollViewPagePathEffect C")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "CONSTR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void ScrollViewPagePathEffectConstructor()
+        {
+            tlog.Debug(tag, $"ScrollViewPagePathEffectConstructor START");
+
+            using (Path path = new Path())
+            {
+                using (Vector3 forword = new Vector3(2, 4, 6))
+                {
+                    using (Vector3 pageSize = new Vector3(6, 8, 10))
+                    {
+                        var testingTarget = new ScrollViewPagePathEffect(path, forword, 1, pageSize, 5);
+                        Assert.IsNotNull(testingTarget, "Can't create success object ScrollViewPagePathEffect");
+                        Assert.IsInstanceOf<ScrollViewPagePathEffect>(testingTarget, "Should be an instance of ScrollViewPagePathEffect type.");
+
+                        testingTarget.Dispose();
+                    }
+                }
+            }
+
+            tlog.Debug(tag, $"ScrollViewPagePathEffectConstructor END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("ScrollViewPagePathEffect DownCast.")]
+        [Property("SPEC", "Tizen.NUI.ScrollViewPagePathEffect.DownCast M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void ScrollViewPagePathEffectDownCast()
+        {
+            tlog.Debug(tag, $"ScrollViewPagePathEffectDownCast START");
+
+            using (Path path = new Path())
+            {
+                using (Vector3 forword = new Vector3(2, 4, 6))
+                {
+                    using (Vector3 pageSize = new Vector3(6, 8, 10))
+                    {
+                        var testingTarget = new ScrollViewPagePathEffect(path, forword, 1, pageSize, 5);
+                        Assert.IsNotNull(testingTarget, "Can't create success object ScrollViewPagePathEffect");
+                        Assert.IsInstanceOf<ScrollViewPagePathEffect>(testingTarget, "Should be an instance of ScrollViewPagePathEffect type.");
+
+                        var result = ScrollViewPagePathEffect.DownCast(testingTarget);
+                        Assert.IsNotNull(result, "Can't create success object ScrollViewPagePathEffect");
+                        Assert.IsInstanceOf<ScrollViewPagePathEffect>(result, "Should be an instance of ScrollViewPagePathEffect type.");
+
+                        testingTarget.Dispose();
+                    }
+                }
+            }
+
+            tlog.Debug(tag, $"ScrollViewPagePathEffectDownCast END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("ScrollViewPagePathEffect ApplyToPage.")]
+        [Property("SPEC", "Tizen.NUI.ScrollViewPagePathEffect.ApplyToPage M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void ScrollViewPagePathEffectApplyToPage()
+        {
+            tlog.Debug(tag, $"ScrollViewPagePathEffectApplyToPage START");
+
+            using (Path path = new Path())
+            {
+                using (Vector3 forword = new Vector3(2, 4, 6))
+                {
+                    using (Vector3 pageSize = new Vector3(6, 8, 10))
+                    {
+                        var testingTarget = new ScrollViewPagePathEffect(path, forword, 1, pageSize, 5);
+                        Assert.IsNotNull(testingTarget, "Can't create success object ScrollViewPagePathEffect");
+                        Assert.IsInstanceOf<ScrollViewPagePathEffect>(testingTarget, "Should be an instance of ScrollViewPagePathEffect type.");
+
+                        try
+                        {
+                            using (View view = new View())
+                            {
+                                testingTarget.ApplyToPage(view, 2);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            tlog.Debug(tag, e.Message.ToString());
+                            Assert.Fail("Caught Exception: Failed!");
+                        }
+
+                        testingTarget.Dispose();
+                    }
+                }
+            }
+
+            tlog.Debug(tag, $"ScrollViewPagePathEffectApplyToPage END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("ScrollViewPagePathEffect ReleaseSwigCPtr.")]
+        [Property("SPEC", "Tizen.NUI.ScrollViewPagePathEffect.ReleaseSwigCPtr M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void ScrollViewPagePathEffectReleaseSwigCPtr()
+        {
+            tlog.Debug(tag, $"ScrollViewPagePathEffectReleaseSwigCPtr START");
+
+            var testingTarget = new MyScrollViewPagePathEffect();
+            Assert.IsNotNull(testingTarget, "Can't create success object ScrollViewPagePathEffect");
+            Assert.IsInstanceOf<ScrollViewPagePathEffect>(testingTarget, "Should be an instance of ScrollViewPagePathEffect type.");
+
+            try
+            {
+                testingTarget.OnReleaseSwigCPtr(testingTarget.SwigCPtr);
+            }
+            catch (Exception e)
+            {
+                tlog.Debug(tag, e.Message.ToString());
+                Assert.Fail("Caught Exception: Failed!");
+            }
+
+            tlog.Debug(tag, $"ScrollViewPagePathEffectReleaseSwigCPtr END (OK)");
+        }
+    }
+}
---
+++ test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/public/Utility/TSTTSPlayer.cs
@@ -0,0 +1,182 @@
+﻿using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("public/Utility/TTSPlayer")]
+    class PublicTTSPlayerTest
+    {
+        private const string tag = "NUITEST";
+        private bool flagStateChanged = false;
+        private string image_path = Tizen.Applications.Application.Current.DirectoryInfo.Resource + "picture.png";
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TTSPlayer constructor. With TTSPlayer handler.")]
+        [Property("SPEC", "Tizen.NUI.TTSPlayer.TTSPlayer C")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "CONSTR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TTSPlayerConstructorWithTTSPlayerHandler()
+        {
+            tlog.Debug(tag, $"TTSPlayerConstructorWithTTSPlayerHandler START");
+
+            using (TTSPlayer player = TTSPlayer.Get())
+            {
+                var testingTarget = new TTSPlayer(player);
+                Assert.IsNotNull(testingTarget, "Can't create success object TTSPlayer");
+                Assert.IsInstanceOf<TTSPlayer>(testingTarget, "Should be an instance of TTSPlayer type.");
+
+                testingTarget.Dispose();
+            }
+ 
+            tlog.Debug(tag, $"TTSPlayerConstructorWithTTSPlayerHandler END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TTSPlayer StateChanged.")]
+        [Property("SPEC", "Tizen.NUI.TTSPlayer.StateChanged A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PRW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TTSPlayerStateChanged()
+        {
+            tlog.Debug(tag, $"TTSPlayerStateChanged START");
+
+            using (TTSPlayer player = TTSPlayer.Get())
+            {
+                var testingTarget = new TTSPlayer(player);
+                Assert.IsNotNull(testingTarget, "Can't create success object TTSPlayer");
+                Assert.IsInstanceOf<TTSPlayer>(testingTarget, "Should be an instance of TTSPlayer type.");
+
+                testingTarget.StateChanged += StateChangedEvent;
+                testingTarget.StateChanged -= StateChangedEvent;
+
+                testingTarget.Dispose();
+            }
+
+            tlog.Debug(tag, $"TTSPlayerStateChanged END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TTSPlayer StateChangedSignal.")]
+        [Property("SPEC", "Tizen.NUI.TTSPlayer.StateChangedSignal M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TTSPlayerStateChangedSignal()
+        {
+            tlog.Debug(tag, $"TTSPlayerStateChangedSignal START");
+
+            using (TTSPlayer player = TTSPlayer.Get())
+            {
+                var testingTarget = new TTSPlayer(player);
+                Assert.IsNotNull(testingTarget, "Can't create success object TTSPlayer");
+                Assert.IsInstanceOf<TTSPlayer>(testingTarget, "Should be an instance of TTSPlayer type.");
+
+                var result = testingTarget.StateChangedSignal();
+                Assert.IsNotNull(result, "Can't create success object StateChangedSignalType");
+                Assert.IsInstanceOf<StateChangedSignalType>(result, "Should be an instance of StateChangedSignalType type.");
+
+                testingTarget.Dispose();
+            }
+
+            tlog.Debug(tag, $"TTSPlayerStateChangedSignal END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TTSPlayer Assign.")]
+        [Property("SPEC", "Tizen.NUI.TTSPlayer.Assign M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TTSPlayerAssign()
+        {
+            tlog.Debug(tag, $"TTSPlayerAssign START");
+
+            using (TTSPlayer player = new TTSPlayer())
+            {
+                using (TTSPlayer rhs = TTSPlayer.Get(TTSPlayer.TTSMode.ScreenReader))
+                {
+                    var testingTarget = player.Assign(rhs);
+                    Assert.IsNotNull(testingTarget, "Can't create success object TTSPlayer");
+                    Assert.IsInstanceOf<TTSPlayer>(testingTarget, "Should be an instance of TTSPlayer type.");
+
+                    testingTarget.Dispose();
+                }
+            }
+
+            tlog.Debug(tag, $"TTSPlayerAssign END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("StateChangedEventArgs PrevState.")]
+        [Property("SPEC", "Tizen.NUI.StateChangedEventArgs.PrevState A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PRW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TTSPlayerStateChangedEventArgsPrevState()
+        {
+            tlog.Debug(tag, $"TTSPlayerStateChangedEventArgsPrevState START");
+
+            var testingTarget = new TTSPlayer.StateChangedEventArgs();
+            Assert.IsNotNull(testingTarget, "Can't create success object StateChangedEventArgs");
+            Assert.IsInstanceOf<TTSPlayer.StateChangedEventArgs>(testingTarget, "Should be an instance of StateChangedEventArgs type.");
+
+            testingTarget.PrevState = TTSPlayer.TTSState.Ready;
+            Assert.AreEqual(TTSPlayer.TTSState.Ready, testingTarget.PrevState);
+
+            tlog.Debug(tag, $"TTSPlayerStateChangedEventArgsPrevState END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("StateChangedEventArgs NextState.")]
+        [Property("SPEC", "Tizen.NUI.StateChangedEventArgs.NextState A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PRW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TTSPlayerStateChangedEventArgsNextState()
+        {
+            tlog.Debug(tag, $"TTSPlayerStateChangedEventArgsNextState START");
+
+            var testingTarget = new TTSPlayer.StateChangedEventArgs();
+            Assert.IsNotNull(testingTarget, "Can't create success object StateChangedEventArgs");
+            Assert.IsInstanceOf<TTSPlayer.StateChangedEventArgs>(testingTarget, "Should be an instance of StateChangedEventArgs type.");
+
+            testingTarget.NextState = TTSPlayer.TTSState.Playing;
+            Assert.AreEqual(TTSPlayer.TTSState.Playing, testingTarget.NextState);
+
+            tlog.Debug(tag, $"TTSPlayerStateChangedEventArgsNextState END (OK)");
+        }
+
+        private void StateChangedEvent(object sender, TTSPlayer.StateChangedEventArgs args)
+        {
+            flagStateChanged = true;
+        }
+    }
+}
