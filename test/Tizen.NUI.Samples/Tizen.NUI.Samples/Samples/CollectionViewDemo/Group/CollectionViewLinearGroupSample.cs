fcb6b90aa05b9ba73fcea3c2cddf97ef2b5173d8
SangHyeon Jade Lee <sh10233.lee@samsung.com>
2021-06-07 16:38:10 +0900

    Revert "make IsGrouped setter internal as only the GroupHeaderTemplate can enable it"
    This reverts commit 9ab325a08818ec3d492f91fe5bd70f4aaee3f614.

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CollectionViewDemo/Group/CollectionViewLinearGroupSample.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/CollectionViewDemo/Group/CollectionViewLinearGroupSample.cs
@@ -80,6 +80,7 @@ namespace Tizen.NUI.Samples
                     return group;
                 }),
                 Header = myTitle,
+                IsGrouped = true,
                 ScrollingDirection = ScrollableBase.Direction.Vertical,
                 WidthSpecification = LayoutParamPolicies.MatchParent,
                 HeightSpecification = LayoutParamPolicies.MatchParent,
