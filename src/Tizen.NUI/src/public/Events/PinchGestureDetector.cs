73bb77df713d5c257b04bc230fc92385323868d5
JoogabYun <40262755+JoogabYun@users.noreply.github.com>
2021-06-07 12:03:43 +0900

    [NUI] Add NeedGesturePropagation (#3130)
    This is used when the parent view wants to listen to gesture events.

--- src/Tizen.NUI/src/public/Events/PinchGestureDetector.cs
+++ src/Tizen.NUI/src/public/Events/PinchGestureDetector.cs
@@ -163,6 +163,7 @@ namespace Tizen.NUI
         {
             private View view;
             private PinchGesture pinchGesture;
+            private bool handled = true;
 
             /// <summary>
             /// The attached view.
@@ -197,6 +198,22 @@ namespace Tizen.NUI
                 set
                 {
                     pinchGesture = value;
+                }
+            }
+
+            /// <summary>
+            /// Gets or sets a value that indicates whether the event handler has completely handled the event or whether the system should continue its own processing.
+            /// </summary>
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            public bool Handled
+            {
+                get => handled;
+                set
+                {
+                    handled = value;
+                    Interop.Actor.SetNeedGesturePropagation(View.getCPtr(view), !value);
+                    if (NDalicPINVOKE.SWIGPendingException.Pending)
+                        throw NDalicPINVOKE.SWIGPendingException.Retrieve();
                 }
             }
         }
