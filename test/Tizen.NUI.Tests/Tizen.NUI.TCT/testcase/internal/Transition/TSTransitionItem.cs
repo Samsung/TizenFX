7a1fe193b51237aa1ee8191ecc323764b679b9fa
guowei.wang <guowei.wang@samsung.com>
2021-06-10 11:14:02 +0800

    [NUI] Add Transition(internal & public) TCs.

---
+++ test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/internal/Transition/TSTransitionItem.cs
@@ -0,0 +1,225 @@
+﻿using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("Internal/Transition/TransitionItem")]
+    class TSTransitionItem
+    {
+        private const string tag = "NUITEST";
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItem constructor.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItem.TransitionItem C")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "CONSTR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemConstructor()
+        {
+            tlog.Debug(tag, $"TransitionItemConstructor START");
+
+            View currentView = new View()
+            {
+                Name = "currentPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            currentView.TransitionOptions.TransitionTag = "Transition";
+            currentView.TransitionOptions.EnableTransition = true;
+
+            View newView = new View()
+            {
+                Name = "newPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            newView.TransitionOptions.TransitionTag = "Transition";
+            newView.TransitionOptions.EnableTransition = true;
+
+            AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default);
+            TimePeriod timePeriod = new TimePeriod(500);
+
+            var testingTarget = new TransitionItem(currentView, newView, timePeriod, alphaFunction);
+            Assert.IsNotNull(testingTarget, "Should be not null!");
+            Assert.IsInstanceOf<TransitionItem>(testingTarget, "Should be an Instance of TransitionItem!");
+
+            newView.Dispose();
+            currentView.Dispose();
+            timePeriod.Dispose();
+            alphaFunction.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemConstructor END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItem ShowSourceAfterFinished.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItem.ShowSourceAfterFinished A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PROW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemShowSourceAfterFinished()
+        {
+            tlog.Debug(tag, $"TransitionItemShowSourceAfterFinished START");
+
+            View currentView = new View()
+            {
+                Name = "currentPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            currentView.TransitionOptions.TransitionTag = "Transition";
+            currentView.TransitionOptions.EnableTransition = true;
+
+            View newView = new View()
+            {
+                Name = "newPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            newView.TransitionOptions.TransitionTag = "Transition";
+            newView.TransitionOptions.EnableTransition = true;
+
+            AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default);
+            TimePeriod timePeriod = new TimePeriod(500);
+
+            var testingTarget = new TransitionItem(currentView, newView, timePeriod, alphaFunction);
+            Assert.IsNotNull(testingTarget, "Should be not null!");
+            Assert.IsInstanceOf<TransitionItem>(testingTarget, "Should be an Instance of TransitionItem!");
+
+            try
+            {
+                testingTarget.ShowSourceAfterFinished = true;
+            }
+            catch (Exception e)
+            {
+                tlog.Error(tag, "Caught Exception" + e.ToString());
+                LogUtils.Write(LogUtils.DEBUG, LogUtils.TAG, "Caught Exception" + e.ToString());
+                Assert.Fail("Caught Exception" + e.ToString());
+            }
+
+            newView.Dispose();
+            currentView.Dispose();
+            timePeriod.Dispose();
+            alphaFunction.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemShowSourceAfterFinished END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItem Assign.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItem.Assign M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemAssign()
+        {
+            tlog.Debug(tag, $"TransitionItemAssign START");
+
+            View currentView = new View()
+            {
+                Name = "currentPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            currentView.TransitionOptions.TransitionTag = "Transition";
+            currentView.TransitionOptions.EnableTransition = true;
+
+            View newView = new View()
+            {
+                Name = "newPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            newView.TransitionOptions.TransitionTag = "Transition";
+            newView.TransitionOptions.EnableTransition = true;
+
+            AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default);
+            TimePeriod timePeriod = new TimePeriod(500);
+
+            var testingTarget = new TransitionItem(currentView, newView, timePeriod, alphaFunction);
+            Assert.IsNotNull(testingTarget, "Should be not null!");
+            Assert.IsInstanceOf<TransitionItem>(testingTarget, "Should be an Instance of TransitionItem!");
+
+            using (TransitionItem rhs = new TransitionItem(testingTarget))
+            {
+                var result = testingTarget.Assign(rhs);
+                Assert.IsNotNull(result, "Should be not null!");
+                Assert.IsInstanceOf<TransitionItem>(result, "Should be an Instance of TransitionItem!");
+            }
+
+            currentView?.Dispose();
+            newView?.Dispose();
+            timePeriod?.Dispose();
+            alphaFunction?.Dispose();
+            testingTarget?.Dispose();
+            tlog.Debug(tag, $"TransitionItemAssign END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItem Dispose.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItem.Dispose M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemDispose()
+        {
+            tlog.Debug(tag, $"TransitionItemDispose START");
+
+            View currentView = new View()
+            {
+                Name = "currentPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            currentView.TransitionOptions.TransitionTag = "Transition";
+            currentView.TransitionOptions.EnableTransition = true;
+
+            View newView = new View()
+            {
+                Name = "newPage",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            newView.TransitionOptions.TransitionTag = "Transition";
+            newView.TransitionOptions.EnableTransition = true;
+
+            AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default);
+            TimePeriod timePeriod = new TimePeriod(500);
+
+            var testingTarget = new TransitionItem(currentView, newView, timePeriod, alphaFunction);
+            Assert.IsNotNull(testingTarget, "Should be not null!");
+            Assert.IsInstanceOf<TransitionItem>(testingTarget, "Should be an Instance of TransitionItem!");
+
+            try
+            {
+                testingTarget.Dispose();
+            }
+            catch (Exception e)
+            {
+                tlog.Error(tag, "Caught Exception" + e.ToString());
+                LogUtils.Write(LogUtils.DEBUG, LogUtils.TAG, "Caught Exception" + e.ToString());
+                Assert.Fail("Caught Exception" + e.ToString());
+            }
+
+            currentView?.Dispose();
+            newView?.Dispose();
+            timePeriod?.Dispose();
+            alphaFunction?.Dispose();
+            tlog.Debug(tag, $"TransitionItemDispose END (OK)");
+        }
+    }
+}
---
+++ test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/internal/Transition/TSTransitionItemBase.cs
@@ -0,0 +1,385 @@
+﻿using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("Internal/Transition/TransitionItemBase")]
+    class TSTransitionItemBase
+    {
+        private const string tag = "NUITEST";
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase constructor.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.TransitionItemBase C")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "CONSTR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseConstructor()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseConstructor START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseConstructor END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase Duration.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.Duration A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PRW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseDuration()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseDuration START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            testingTarget.Duration = 300;
+            Assert.IsTrue(300 == testingTarget.Duration);
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseDuration END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase Delay.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.Delay A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PRW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseDelay()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseDelay START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            testingTarget.Delay = 300;
+            Assert.IsTrue(300 == testingTarget.Delay);
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseDelay END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase TimePeriod.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.TimePeriod A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseTimePeriod()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseTimePeriod START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            try
+            {
+                using (TimePeriod setValue = new TimePeriod(300))
+                {
+                    testingTarget.TimePeriod = setValue;
+                }
+            }
+            catch (Exception e)
+            {
+                tlog.Error(tag, "Caught Exception" + e.ToString());
+                LogUtils.Write(LogUtils.DEBUG, LogUtils.TAG, "Caught Exception" + e.ToString());
+                Assert.Fail("Caught Exception" + e.ToString());
+            }
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseTimePeriod END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase AlphaFunction.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.AlphaFunction A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PRW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseAlphaFunction()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseAlphaFunction START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            using (AlphaFunction setValue = new AlphaFunction(AlphaFunction.BuiltinFunctions.EaseOut)) 
+            {
+                testingTarget.AlphaFunction = setValue;
+                var result = testingTarget.AlphaFunction;
+                Assert.IsTrue(result.GetBuiltinFunction() == AlphaFunction.BuiltinFunctions.EaseOut);
+            }
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseAlphaFunction END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase TransitionWithChild.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.TransitionWithChild A")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "PW")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseTransitionWithChild()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseTransitionWithChild START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            try
+            {
+                testingTarget.TransitionWithChild = true;
+            }
+            catch (Exception e)
+            {
+                tlog.Error(tag, "Caught Exception" + e.ToString());
+                LogUtils.Write(LogUtils.DEBUG, LogUtils.TAG, "Caught Exception" + e.ToString());
+                Assert.Fail("Caught Exception" + e.ToString());
+            }
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseTransitionWithChild END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase DownCast.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.DownCast M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseDownCast()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseDownCast START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            var result = TransitionItemBase.DownCast(testingTarget);
+            Assert.IsNotNull(result, "Should be not null!");
+            Assert.IsInstanceOf<TransitionItemBase>(result, "Should be an Instance of TransitionItemBase!");
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseDownCast END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase Assign.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.Assign M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseAssign()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseAssign START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            using (TransitionItemBase rhs = new TransitionItemBase(testingTarget))
+            {
+                var result = testingTarget.Assign(rhs);
+                Assert.IsNotNull(result, "Should be not null!");
+                Assert.IsInstanceOf<TransitionItemBase>(result, "Should be an Instance of TransitionItemBase!");
+            }
+
+            view.Dispose();
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseAssign END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TransitionItemBase Dispose.")]
+        [Property("SPEC", "Tizen.NUI.TransitionItemBase.Dispose M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void TransitionItemBaseDispose()
+        {
+            tlog.Debug(tag, $"TransitionItemBaseDispose START");
+
+            View view = new View()
+            {
+                Name = "view",
+                TransitionOptions = new TransitionOptions(Window.Instance)
+            };
+            view.TransitionOptions.TransitionTag = "Transition";
+            view.TransitionOptions.EnableTransition = true;
+
+            TransitionItemBase testingTarget = null;
+            using (TimePeriod timePeriod = new TimePeriod(500))
+            {
+                using (AlphaFunction alphaFunction = new AlphaFunction(AlphaFunction.BuiltinFunctions.Default))
+                {
+                    testingTarget = new TransitionItemBase(view, true, timePeriod, alphaFunction);
+                }
+            }
+
+            try
+            {
+                testingTarget.Dispose();
+            }
+            catch (Exception e)
+            {
+                tlog.Error(tag, "Caught Exception" + e.ToString());
+                LogUtils.Write(LogUtils.DEBUG, LogUtils.TAG, "Caught Exception" + e.ToString());
+                Assert.Fail("Caught Exception" + e.ToString());
+            }
+
+            view?.Dispose();
+            tlog.Debug(tag, $"TransitionItemBaseDispose END (OK)");
+        }
+    }
+}
