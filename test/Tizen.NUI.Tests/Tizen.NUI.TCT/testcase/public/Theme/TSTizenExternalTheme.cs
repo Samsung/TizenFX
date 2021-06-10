659cbf75a098a1231f633843090c409e36fce592
zhouleonlei <zhouleon.lei@samsung.com>
2021-06-10 14:24:50 +0800

    [NUI] Add public theme TCs

---
+++ test/Tizen.NUI.Tests/Tizen.NUI.TCT/testcase/public/Theme/TSTizenExternalTheme.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("public/Theme/TizenExternalTheme")]
+    internal class PublicTizenExternalThemeTest
+    {
+        private const string tag = "NUITEST";
+        private readonly object themeLoader;
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("Test TizenExternalTheme GetValue.")]
+        [Property("SPEC", "Tizen.NUI.TizenExternalTheme.GetValue  M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("COVPARAM", "")]
+        public void TizenExternalThemeGetValue()
+        {
+            tlog.Debug(tag, $"TizenExternalThemeGetValue START");
+
+            Tizen.Applications.ThemeManager.ThemeLoader themeLoader = new Tizen.Applications.ThemeManager.ThemeLoader();
+
+            Tizen.Applications.ThemeManager.Theme a1 = null;
+            string id = "myId";
+
+            a1 = themeLoader.LoadTheme(id);
+
+            TizenExternalTheme b1 = new TizenExternalTheme(a1);
+
+
+
+            tlog.Debug(tag, $"TizenExternalThemeGetValue END (OK)");
+            Assert.Pass("TizenExternalThemeGetValue");
+        }
+    }
+}
+
