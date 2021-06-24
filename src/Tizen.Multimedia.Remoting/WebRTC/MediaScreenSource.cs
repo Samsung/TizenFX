8de45e87f09d9666764d931ce05044fc9a34f65d
Haesu Gwon <haesu.gwon@samsung.com>
2021-06-24 12:43:57 +0900

    [WebRTC] Add MediaScreenSource

---
+++ src/Tizen.Multimedia.Remoting/WebRTC/MediaScreenSource.cs
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Diagnostics;
+using NativeWebRTC = Interop.NativeWebRTC;
+
+namespace Tizen.Multimedia.Remoting
+{
+    /// <summary>
+    /// Represents a screen source.
+    /// </summary>
+    /// <seealso cref="WebRTC.AddSource"/>
+    /// <seealso cref="WebRTC.AddSources"/>
+    /// <since_tizen> 9 </since_tizen>
+    public sealed class MediaScreenSource : MediaSource
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaScreenSource"/> class.
+        /// </summary>
+        /// <since_tizen> 9 </since_tizen>
+        public MediaScreenSource() : base(MediaType.Video) {}
+
+        internal override void OnAttached(WebRTC webRtc)
+        {
+            Debug.Assert(webRtc != null);
+
+            if (WebRtc != null)
+            {
+                throw new InvalidOperationException("The source is has already been assigned to another WebRTC.");
+            }
+
+            NativeWebRTC.AddMediaSource(webRtc.Handle, MediaSourceType.Screen, out uint sourceId).
+                ThrowIfFailed("Failed to add media source for screen.");
+
+            WebRtc = webRtc;
+            SourceId = sourceId;
+        }
+
+        internal override void OnDetached(WebRTC webRtc)
+        {
+            NativeWebRTC.RemoveMediaSource(webRtc.Handle, SourceId.Value).
+                ThrowIfFailed("Failed to remove media source for screen.");
+
+            WebRtc = null;
+        }
+    }
+}
