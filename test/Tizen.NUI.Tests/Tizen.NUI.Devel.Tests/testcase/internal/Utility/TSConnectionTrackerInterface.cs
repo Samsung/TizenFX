ed418a2d66a52d27408074532a3287766996b400
guowei.wang <guowei.wang@samsung.com>
2021-07-02 09:40:42 +0800

    [NUI] Add Utility(internal) TCs.

---
+++ test/Tizen.NUI.Tests/Tizen.NUI.Devel.Tests/testcase/internal/Utility/TSConnectionTrackerInterface.cs
@@ -0,0 +1,131 @@
+﻿using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("Internal/Utility/ConnectionTrackerInterface")]
+    public class InternalConnectionTrackerInterfaceTest
+    {
+        private const string tag = "NUITEST";
+        private delegate bool dummyCallback(IntPtr pageTurnSignal);
+        private bool OnDummyCallback(IntPtr data)
+        {
+            return false;
+        }
+
+        internal class MyConnectionTrackerInterface : ConnectionTrackerInterface
+        {
+            public MyConnectionTrackerInterface(global::System.IntPtr cPtr, bool cMemoryOwn) : base(cPtr, cMemoryOwn)
+            { }
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("ConnectionTrackerInterface constructor.")]
+        [Property("SPEC", "Tizen.NUI.ConnectionTrackerInterface.ConnectionTrackerInterface C")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "CONSTR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void ConnectionTrackerInterfaceConstructor()
+        {
+            tlog.Debug(tag, $"ConnectionTrackerInterfaceConstructor START");
+
+            using (View view = new View())
+            {
+                var testingTarget = new ConnectionTrackerInterface(view.SwigCPtr.Handle, false);
+                Assert.IsNotNull(testingTarget, "Should be not null!");
+                Assert.IsInstanceOf<ConnectionTrackerInterface>(testingTarget, "Should be an Instance of ConnectionTrackerInterface!");
+
+                testingTarget.Dispose();
+            }
+
+            tlog.Debug(tag, $"ConnectionTrackerInterfaceConstructor END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("ConnectionTrackerInterface getCPtr.")]
+        [Property("SPEC", "Tizen.NUI.ConnectionTrackerInterface.getCPtr M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void ConnectionTrackerInterfacegetCPtr()
+        {
+            tlog.Debug(tag, $"ConnectionTrackerInterfacegetCPtr START");
+
+            using (View view = new View())
+            {
+                var testingTarget = new MyConnectionTrackerInterface(view.SwigCPtr.Handle, false);
+                Assert.IsNotNull(testingTarget, "Should be not null!");
+                Assert.IsInstanceOf<ConnectionTrackerInterface>(testingTarget, "Should be an Instance of ConnectionTrackerInterface!");
+
+                try
+                {
+                    ConnectionTrackerInterface.getCPtr(testingTarget);
+                }
+                catch (Exception e)
+                {
+                    tlog.Debug(tag, e.Message.ToString());
+                    Assert.Fail("Caught Exception: Failed!");
+                }
+
+                testingTarget.Dispose();
+            }
+
+            tlog.Debug(tag, $"ConnectionTrackerInterfacegetCPtr END (OK)");
+        }
+
+        //[Test]
+        //[Category("P1")]
+        //[Description("ConnectionTrackerInterface SignalConnected.")]
+        //[Property("SPEC", "Tizen.NUI.ConnectionTrackerInterface.SignalConnected M")]
+        //[Property("SPEC_URL", "-")]
+        //[Property("CRITERIA", "MR")]
+        //[Property("AUTHOR", "guowei.wang@samsung.com")]
+        //public void ConnectionTrackerInterfaceSignalConnected()
+        //{
+        //    tlog.Debug(tag, $"ConnectionTrackerInterfaceSignalConnected START");
+
+        //    using (View view = new View())
+        //    {
+        //        var testingTarget = new ConnectionTrackerInterface(view.SwigCPtr.Handle, false);
+        //        Assert.IsNotNull(testingTarget, "Should be not null!");
+        //        Assert.IsInstanceOf<ConnectionTrackerInterface>(testingTarget, "Should be an Instance of ConnectionTrackerInterface!");
+
+        //        try
+        //        {
+        //            var slotObserver = new SlotObserver(testingTarget.SwigCPtr.Handle, false);
+        //            var callback = new SWIGTYPE_p_CallbackBase(testingTarget.SwigCPtr.Handle);
+        //            testingTarget.SignalConnected(slotObserver, callback);
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            tlog.Debug(tag, e.Message.ToString());
+        //            Assert.Fail("Caught Exception: Failed!");
+        //        }
+
+        //        testingTarget.Dispose();
+        //    }
+
+        //    tlog.Debug(tag, $"ConnectionTrackerInterfaceSignalConnected END (OK)");
+        //}
+    }
+}
---
+++ test/Tizen.NUI.Tests/Tizen.NUI.Devel.Tests/testcase/internal/Utility/TSPageFactory.cs
@@ -0,0 +1,97 @@
+﻿using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("Internal/Utility/PageFactory")]
+    public class InternalPageFactoryTest
+    {
+        private const string tag = "NUITEST";
+
+        internal class MyPageFactory : PageFactory
+        {
+            public MyPageFactory(global::System.IntPtr cPtr, bool cMemoryOwn) : base(cPtr, cMemoryOwn)
+            { }
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("PageFactory constructor.")]
+        [Property("SPEC", "Tizen.NUI.PageFactory.PageFactory C")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "CONSTR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void PageFactoryConstructor()
+        {
+            tlog.Debug(tag, $"PageFactoryConstructor START");
+
+            using (View view = new View())
+            {
+                view.Size = new Size(20, 40);
+                view.BackgroundColor = Color.Cyan;
+
+                var testingTarget = new PageFactory(view.SwigCPtr.Handle, false);
+                Assert.IsNotNull(testingTarget, "Should be not null!");
+                Assert.IsInstanceOf<PageFactory>(testingTarget, "Should be an Instance of PageFactory!");
+
+                testingTarget.Dispose();
+            }
+
+            tlog.Debug(tag, $"PageFactoryConstructor END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("PageFactory getCPtr.")]
+        [Property("SPEC", "Tizen.NUI.PageFactory.getCPtr M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "guowei.wang@samsung.com")]
+        public void PageFactorygetCPtr()
+        {
+            tlog.Debug(tag, $"PageFactorygetCPtr START");
+
+            using (View view = new View())
+            {
+                view.Size = new Size(20, 40);
+                view.BackgroundColor = Color.Cyan;
+
+                var testingTarget = new MyPageFactory(view.SwigCPtr.Handle, false);
+                Assert.IsNotNull(testingTarget, "Should be not null!");
+                Assert.IsInstanceOf<PageFactory>(testingTarget, "Should be an Instance of PageFactory!");
+
+                try
+                {
+                    MyPageFactory.getCPtr(testingTarget);
+                }
+                catch (Exception e)
+                {
+                    tlog.Debug(tag, e.Message.ToString());
+                    Assert.Fail("Caught Exception: Failed!");
+                }
+
+                testingTarget.Dispose();
+            }
+
+            tlog.Debug(tag, $"PageFactorygetCPtr END (OK)");
+        }
+    }
+}
