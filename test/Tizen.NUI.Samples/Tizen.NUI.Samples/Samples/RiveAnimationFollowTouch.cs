b656cdfbb464cb7f8674b9b0b690cd45cecf58b6
Taehyub Kim <taehyub.kim@samsung.com>
2021-08-05 01:49:28 -0700

    [NUI] RiveAnimationView Samples: Add More RiveAnimationView Samples
    1. RiveAnimationUniverse.cs: This sample shows the view animation using user interactive rive animation.
    2. RiveAnimationFollowTouch.cs: This sample shows the node position control of rive animation.
    3. RiveAnimationRollInOut.cs: This sample shows the animation state change by the  user interaction.

---
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/RiveAnimationFollowTouch.cs
@@ -0,0 +1,63 @@
+﻿using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace Tizen.NUI.Samples
+{
+    public class RiveAnimationFollowTouch : IExample
+    {
+        private Window window;
+        private Layer defaultLayer;
+
+        RiveAnimationView rav;
+        Button playButton, stopButton;
+        Button bounceButton, brokeButton;
+        Button fillButton, strokeButton, opacityButton;
+        Button scaleButton, rotationButton, positionButton;
+        public void Activate()
+        {
+            window = NUIApplication.GetDefaultWindow();
+            defaultLayer = window.GetDefaultLayer();
+            window.TouchEvent += OnRiveWindowTouchEvent;
+
+            // Load RiveAnimation File
+            rav = new RiveAnimationView(Tizen.Applications.Application.Current.DirectoryInfo.Resource + "rive/flame-and-spark.riv")
+            {
+                Size = new Size(720, 720),
+            };
+
+            // Enable RiveAnimation and Play
+            rav.EnableAnimation("idle", true);
+            rav.Play();
+
+            playButton = new Button()
+            {
+                Size = new Size(200, 100),
+                Position = new Position(0, 0),
+                Text = "Play"
+            };
+            playButton.Clicked += (object source, ClickedEventArgs args) =>
+            {
+                rav.Play();
+            };
+
+            defaultLayer.Add(rav);
+            defaultLayer.Add(playButton);
+        }
+
+         private void OnRiveWindowTouchEvent(object source, Window.TouchEventArgs e)
+        {
+            Vector2 lp = e.Touch.GetLocalPosition(0);
+            Vector2 sp = e.Touch.GetScreenPosition(0);
+            float scale = (1000.0f /720.0f);
+
+            // Set root and spark node position
+            rav.SetNodePosition("root", new Position(lp.X * scale, lp.Y * scale));
+            rav.SetNodePosition("spark", new Position((lp.X - 288) * scale, lp.Y) * scale);
+        }
+        public void Deactivate()
+        {
+            defaultLayer.Remove(rav);
+            defaultLayer.Remove(playButton);
+        }
+    }
+}
