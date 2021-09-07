412a2629f7fb769e538f1bcd5aa1bdbd3d090efc
Wonsik Jung <sidein@samsung.com>
2021-09-07 17:17:46 +0900

    [NUI] Add NUIApplication construct.
    Add NUIApplication construct with related windowType.
    It is for IME Window type.

--- src/Tizen.NUI/src/public/Common/NUIConstants.cs
+++ src/Tizen.NUI/src/public/Common/NUIConstants.cs
@@ -743,6 +743,10 @@ namespace Tizen.NUI
     /// <summary>
     /// An enum of window types.
     /// </summary>
+    /// <remarks>
+    /// Most of window type can be set, except for IME type.<br />
+    /// IME type can only be used in one of NUIApplication's constrcutors.<br />
+    /// </remarks>
     /// <since_tizen> 3 </since_tizen>
     public enum WindowType
     {
@@ -766,10 +770,12 @@ namespace Tizen.NUI
         Dialog,
         /// <summary>
         /// Used for IME window that is used for keyboard window.
-        /// It should be set in Application's New input param when application is created.
-        /// Because IME window type can not change in runtime.
-        /// So, It does not work with Window.Type.
+        /// It should be set in NUIApplication constructor.
+        /// It does not work with Window.Type, because IME window type can not change in runtime.
         /// </summary>
+        /// <remarks>
+        /// See <see cref="NUIApplication" /> for this type. <br />
+        /// </remarks>
         Ime
     }
 
