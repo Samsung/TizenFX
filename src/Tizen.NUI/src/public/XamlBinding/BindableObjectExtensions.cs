363fde4a0d0c06b467c12d6893bc748245b3fedc
Fang Xiaohui <xiaohui.fang@samsung.com>
2021-09-14 09:21:51 +0800

    [NUI] Roll back to fix build error of TCT code

--- src/Tizen.NUI/src/public/XamlBinding/BindableObjectExtensions.cs
+++ src/Tizen.NUI/src/public/XamlBinding/BindableObjectExtensions.cs
@@ -38,5 +38,22 @@ namespace Tizen.NUI.Binding
             var binding = new Binding(path, mode, converter, stringFormat: stringFormat);
             self.SetBinding(targetProperty, binding);
         }
+
+        /// This will be public opened in tizen_6.0 after ACR done. Before ACR, need to be hidden as inhouse API.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [ObsoleteAttribute(" ", false)]
+        public static void SetBinding<TSource>(this BindableObject self, BindableProperty targetProperty, Expression<Func<TSource, object>> sourceProperty, BindingMode mode = BindingMode.Default,
+                                               IValueConverter converter = null, string stringFormat = null)
+        {
+            if (self == null)
+                throw new ArgumentNullException(nameof(self));
+            if (targetProperty == null)
+                throw new ArgumentNullException(nameof(targetProperty));
+            if (sourceProperty == null)
+                throw new ArgumentNullException(nameof(sourceProperty));
+
+            Binding binding = Binding.Create(sourceProperty, mode, converter, stringFormat: stringFormat);
+            self.SetBinding(targetProperty, binding);
+        }
     }
 }
