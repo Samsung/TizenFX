cf62b6417d2b7eea62e40a66f2fd2b4750b52b99
guowei.wang <guowei.wang@samsung.com>
2021-10-18 17:37:48 +0800

    [NUI] Fix dependencies issue of Components.Devel project.

---
+++ test/Tizen.NUI.Tests/Tizen.NUI.Components.Devel.Tests/testcase/Controls/TSToast.cs
@@ -0,0 +1,31 @@
+
+using global::System;
+using NUnit.Framework;
+using NUnit.Framework.TUnit;
+using Tizen.NUI;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Components.Devel.Tests
+{
+    using tlog = Tizen.Log;
+
+    [TestFixture]
+    [Description("Control/Toast")]
+    public class ControlToastTest
+    {
+        private const string tag = "NUITEST";
+
+        [SetUp]
+        public void Init()
+        {
+            tlog.Info(tag, "Init() is called!");
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            tlog.Info(tag, "Destroy() is called!");
+        }
+    }
+}
