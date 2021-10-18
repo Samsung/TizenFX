d3df0449f3c2816aebbcdfe29df9058a8fcff2ee
Sangyoon Jang <jeremy.jang@samsung.com>
2021-10-18 18:49:00 +0900

    [Tizen.Applications.Cion] Fix Cion
    - Fix handling returned data of ServerBase
    - Fix parameter of CionClientSendPayloadAsync
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>

--- src/Tizen.Applications.Cion/Interop/Interop.Libraries.cs
+++ src/Tizen.Applications.Cion/Interop/Interop.Libraries.cs
@@ -19,5 +19,6 @@ internal static partial class Interop
     internal static partial class Libraries
     {
         public const string Cion = "libcion.so.1";
+        public const string Libc = "libc.so.6";
     }
 }
