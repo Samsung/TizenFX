1f29ee37d64fc59c158bb5a882bfaee764cba5c1
dongsug.song <dongsug.song@samsung.com>
2021-11-09 20:43:52 +0900

    [NUI] Change Color, Position2D, and etc properties to own only one instance

--- src/Tizen.NUI/src/public/Common/Position.cs
+++ src/Tizen.NUI/src/public/Common/Position.cs
@@ -540,7 +540,7 @@ namespace Tizen.NUI
                 Interop.Vector3.XSet(SwigCPtr, value);
                 if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
 
-                callback?.Invoke(X, Y, Z);
+                callback?.Invoke(value, Y, Z);
             }
             get
             {
@@ -573,7 +573,7 @@ namespace Tizen.NUI
                 Interop.Vector3.YSet(SwigCPtr, value);
                 if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
 
-                callback?.Invoke(X, Y, Z);
+                callback?.Invoke(X, value, Z);
             }
             get
             {
@@ -606,7 +606,7 @@ namespace Tizen.NUI
                 Interop.Vector3.ZSet(SwigCPtr, value);
                 if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
 
-                callback?.Invoke(X, Y, Z);
+                callback?.Invoke(X, Y, value);
             }
             get
             {
