31c3180daa66d39380be89ada461e2bc3adcbb9a
joogab.yun <joogab.yun@samsung.com>
2021-11-19 13:57:43 +0900

    [NUI] Binding TextFitChanged event

---
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/TextFitChangedSample.cs
@@ -0,0 +1,43 @@
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+using Tizen.NUI.Text;
+
+namespace Tizen.NUI.Samples
+{
+    public class TextFitChangedSample : IExample
+    {
+        private TextLabel label;
+
+        public void Activate()
+        {
+            Window window = NUIApplication.GetDefaultWindow();
+            var fit = new TextFit();
+            fit.Enable = true;
+            fit.MinSize = 5.0f;
+            fit.MaxSize = 50.0f;
+
+            label = new TextLabel()
+            {
+                Text = "ABCDE",
+                Size = new Size(300, 100),
+                PointSize = 10,
+                Position = new Position(100, 100),
+                BackgroundColor = Color.Yellow,
+            };
+            label.SetTextFit(fit);
+
+            window.Add(label);
+
+            label.TextFitChanged +=(s, e) =>
+            {
+                TextFit textfit = label.GetTextFit();
+                Tizen.Log.Error("NUI", $"FontSize : {textfit.FontSize}\n");
+            };
+        }
+
+        public void Deactivate()
+        {
+        }
+    }
+}
