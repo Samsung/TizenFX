bb83449537e56a7d5aa6369013c44318e9d77ade
Taehyub Kim <taehyub.kim@samsung.com>
2021-12-17 18:32:16 +0900

    [NUI] Add New NUI DnD Class and Demo
    DragAndDrop Class supports the drag and drop functionality in the single window and sub-windows in an application.
    Also It supports the application to application drag and drop functionality for multi-window feature.
    
    Class
       Tizen.NUI.DragAndDrop
    
    Dependency
       dali-adaptor : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-adaptor/+/263704/
       dali-csharp-binder : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-csharp-binder/+/268209/
    
    API Changes
     public bool StartDragAndDrop(View sourceView, View shadowView, string data)
     - Start drag and drop to send the data form sourceView to targetView.
     - sourceView means the NUI View to move.
     - shadowView means the half-transparent NUI ImageView of drag object, user should be add this image.
     - data means the data to send to the target view.
     public bool AddListener(View targetView, DragAndDropEventHandler callback)
     - Add listener to receive the drag event.
     - targetView means the NUI View to receive drag event
     - callback will be called when the drag event is emitted.
     public enum DragType
     public struct DragEvent
     public delegate void DragAndDropEventHandler(DragEvent dragEvent)

---
+++ src/Tizen.NUI/src/internal/Interop/Interop.DragAndDrop.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Copyright(c) 2022 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class DragAndDrop
+        {
+            internal enum DragType
+            {
+                Enter,
+                Leave,
+                Move,
+                Drop
+            }
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragAndDrop_New__SWIG_0")]
+            public static extern global::System.IntPtr New();
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragAndDrop_StartDragAndDrop")]
+            public static extern bool StartDragAndDrop(global::System.Runtime.InteropServices.HandleRef dragAndDrop, global::System.Runtime.InteropServices.HandleRef sourceView, global::System.Runtime.InteropServices.HandleRef shadow, string mimeType, string data);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragAndDrop_AddListener")]
+            public static extern bool AddListener(global::System.Runtime.InteropServices.HandleRef dragAndDrop, global::System.Runtime.InteropServices.HandleRef targetView, global::System.Runtime.InteropServices.HandleRef callback);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragAndDrop_RemoveListener")]
+            public static extern bool RemoveListener(global::System.Runtime.InteropServices.HandleRef dragAndDrop, global::System.Runtime.InteropServices.HandleRef targetView, global::System.Runtime.InteropServices.HandleRef callback);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragEvent_GetAction")]
+            public static extern int GetAction(global::System.IntPtr dragAndDrop);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragEvent_GetPosition")]
+            public static extern global::System.IntPtr GetPosition(global::System.IntPtr dragAndDrop);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragEvent_GetMimeType")]
+            public static extern string GetMimeType(global::System.IntPtr dragAndDrop);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_DragEvent_GetData")]
+            public static extern string GetData(global::System.IntPtr dragAndDrop);
+        }
+    }
+}
