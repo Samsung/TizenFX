cc4ef6d5126b5f93572d45bfef2bbe4462f60eed
Taehyub Kim <taehyub.kim@samsung.com>
2021-12-17 18:32:16 +0900

    [NUI] Add New NUI DnD Class and Demo
    DragAndDrop Class supports the drag and drop functionality in the single window and sub-windows in an application.
    Also It supports the application to application drag and drop functionality for multi-window feature.
    
    Class
       Tizen.NUI.DragAndDrop
    
    Dependency
       dali-adaptor : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-adaptor/+/263704/
       dali-csharp-binder : https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-csharp-binder/+/268209/
    
    API Changes
     public bool StartDragAndDrop(View sourceView, View shadowView, string data)
     - Start drag and drop to send the data form sourceView to targetView.
     - sourceView means the NUI View to move.
     - shadowView means the half-transparent NUI ImageView of drag object, user should be add this image.
     - data means the data to send to the target view.
     public bool AddListener(View targetView, DragAndDropEventHandler callback)
     - Add listener to receive the drag event.
     - targetView means the NUI View to receive drag event
     - callback will be called when the drag event is emitted.
     public enum DragType
     public struct DragEvent
     public delegate void DragAndDropEventHandler(DragEvent dragEvent)

---
+++ test/NUIDnDTarget/NUIDnDTarget.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+
+namespace NUIDnDTarget
+{
+    class Program : NUIApplication
+    {
+        ImageView targetViewA;
+        ImageView targetViewB;
+        DragAndDrop dnd;
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            // Create DnD Instance
+            dnd = DragAndDrop.Instance;
+
+            Window.Instance.KeyEvent += OnKeyEvent;
+            Window.Instance.WindowPosition = new Position(1020, 0); 
+            Window.Instance.WindowSize = new Size(900, 1080); 
+            Window.Instance.BackgroundColor = Color.White;
+
+            TextLabel text = new TextLabel("DropTarget Application");
+            text.Position = new Position(0, 0);
+            text.TextColor = Color.Black;
+            text.PointSize = 10.0f;
+            Window.Instance.GetDefaultLayer().Add(text);
+
+            TextLabel text2 = new TextLabel("This sample demonstrates NUI DnD functionality and is the 'target' app for this sample.");
+            text2.Position = new Position(0, 70);
+            text2.TextColor = Color.Black;
+            text2.PointSize = 7.0f;
+            text2.MultiLine = true;
+            Window.Instance.GetDefaultLayer().Add(text2);
+
+            // Create Target View A
+            targetViewA = new ImageView(Tizen.Applications.Application.Current.DirectoryInfo.Resource + "droptarget.png");
+            targetViewA.Size = new Size(300, 300);
+            targetViewA.Position = new Position(100, 235);
+            Window.Instance.GetDefaultLayer().Add(targetViewA);
+
+            // Add Drop Target A
+            dnd.AddListener(targetViewA, OnTargetAppDnDFuncA);
+
+            // Create Target View B
+            targetViewB = new ImageView(Tizen.Applications.Application.Current.DirectoryInfo.Resource + "droptarget.png");
+            targetViewB.Size = new Size(300, 300);
+            targetViewB.Position = new Position(100, 585);
+            Window.Instance.GetDefaultLayer().Add(targetViewB);
+
+            // Add Drop Target B
+            dnd.AddListener(targetViewB, OnTargetAppDnDFuncB);
+
+            TextLabel text3 = new TextLabel("Drop your image here.");
+            text3.Position = new Position(500, 355);
+            text3.TextColor = Color.Black;
+            text3.PointSize = 7.0f;
+            Window.Instance.GetDefaultLayer().Add(text3);
+        }
+
+        public void OnTargetAppDnDFuncA(View targetView, DragEvent dragEvent)
+        {
+            if (dragEvent.DragType == DragType.Enter)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target A [Enter]");
+            }
+            else if (dragEvent.DragType == DragType.Leave)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target A [Leave]");
+            }
+            else if (dragEvent.DragType == DragType.Move)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target A [Move]: " + dragEvent.Position.X + " " + dragEvent.Position.Y);
+            }
+            else if (dragEvent.DragType == DragType.Drop)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target A [Drop] MimeType: " +  dragEvent.MimeType + " Data: " + dragEvent.Data);
+              targetViewA.ResourceUrl = dragEvent.Data;
+            }
+        }
+
+        public void OnTargetAppDnDFuncB(View targetView, DragEvent dragEvent)
+        {
+            if (dragEvent.DragType == DragType.Enter)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target B [Enter]");
+            }
+            else if (dragEvent.DragType == DragType.Leave)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target B [Leave]");
+            }
+            else if (dragEvent.DragType == DragType.Move)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target B [Move]: " + dragEvent.Position.X + " " + dragEvent.Position.Y);
+            }
+            else if (dragEvent.DragType == DragType.Drop)
+            {
+              Tizen.Log.Error("NUIDnDTarget", "Target App Target B [Drop] MimeType: " +  dragEvent.MimeType + " Data: " + dragEvent.Data);
+              targetViewB.ResourceUrl = dragEvent.Data;
+            }
+        }
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            if (e.Key.State == Key.StateType.Down && (e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "Escape"))
+            {
+                Exit();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
