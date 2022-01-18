2f4c653007861b936c7f091f0890b650ad6be8ee
EverLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2022-01-18 14:47:28 +0900

    [NUI] introduce SectionIndexer class

---
+++ src/Tizen.NUI.Components/Controls/SectionIndexer.cs
@@ -0,0 +1,42 @@
+/* Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Tizen.NUI.Binding;
+
+namespace Tizen.NUI.Components
+{
+    /// <summary>
+    /// An section indexer for fast scrolling over the indexes on the scroller.
+    /// </summary>
+    /// <since_tizen> tizen_next </since_tizen>
+    public abstract class SectionIndexer : Control
+    {
+        public virtual IEnumerable Sections{ get; set; }
+
+        public virtual float getPositionForSection(int sectionIndex)
+        {
+            return 0.0f;
+        }
+
+        public virtual int getSectionForPosition(float position)
+        {
+            return 0;
+        }
+    }
+}
