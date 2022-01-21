6dfac2d2ea73eaee426bed7079299f54f7dc1e3f
dongsug.song <dongsug.song@samsung.com>
2022-01-21 22:10:41 +0900

    [NUI] Add Tizen.NUI.Devel.Tests.Ubuntu project in order to run testcase in a local Ubuntu PC

---
+++ test/Tizen.NUI.Devel.Tests.Ubuntu/nunit.framework/Constraints/ExceptionNotThrownConstraint.cs
@@ -0,0 +1,33 @@
+#define PORTABLE
+#define TIZEN
+#define NUNIT_FRAMEWORK
+#define NUNITLITE
+#define NET_4_5
+#define PARALLEL
+using System;
+
+namespace NUnit.Framework.Constraints
+{
+    internal class ExceptionNotThrownConstraint : Constraint
+    {
+        /// <summary>
+        /// The Description of what this constraint tests, for
+        /// use in messages and in the ConstraintResult.
+        /// </summary>
+        public override string Description
+        {
+            get { return "No Exception to be thrown"; }
+        }
+
+        /// <summary>
+        /// Applies the constraint to an actual value, returning a ConstraintResult.
+        /// </summary>
+        /// <param name="actual">The value to be tested</param>
+        /// <returns>A ConstraintResult</returns>
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            var exception = actual as Exception;
+            return new ConstraintResult(this, exception, exception == null);
+        }
+    }
+}
