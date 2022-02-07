d6bc31a89e0b8de6260b0e9ef5cc37f48a03576c
Eunki, Hong <eunkiki.hong@samsung.com>
2022-02-07 22:14:25 +0900

    [Test] bind PerformanceLogger class
    DO NOT MERGE.
    Only for test.
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

---
+++ src/Tizen.NUI/src/internal/Common/PerformanceLogger.cs
@@ -0,0 +1,81 @@
+/*
+ * Copyright(c) 2021 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System.ComponentModel;
+
+namespace Tizen.NUI
+{
+    using global::System;
+    using global::System.Runtime.InteropServices;
+
+    /// <summary>
+    /// </summary>
+    internal class PerformanceLogger : BaseHandle
+    {
+        internal PerformanceLogger(string name) : base(Interop.PerformanceLogger.NewPerformanceLogger(name), true)
+        {
+        }
+
+
+        /// <summary>
+        /// Apply current constraint.
+        /// Constraint will work until Remove called.
+        /// </summary>
+        internal void Start()
+        {
+            Interop.PerformanceLogger.Start(SwigCPtr);
+            if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+        }
+        /// <summary>
+        /// Remove current constraint.
+        /// </summary>
+        internal void End()
+        {
+            Interop.PerformanceLogger.End(SwigCPtr);
+            if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+        }
+
+
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        /// <param name="type"></param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected override void Dispose(DisposeTypes type)
+        {
+            if(disposed)
+            {
+                return;
+            }
+            if(type == DisposeTypes.Explicit)
+            {
+                //Called by User
+                //Release your own managed resources here.
+                //You should release all of your own disposable objects here.
+            }
+            base.Dispose(type);
+        }
+
+        /// This will not be public opened.
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected override void ReleaseSwigCPtr(HandleRef swigCPtr)
+        {
+            Interop.PerformanceLogger.DeletePerformanceLogger(swigCPtr);
+            if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+        }
+    }
+}
