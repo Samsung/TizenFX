55991621548551fae11af45a4f0c3dd1b62b2062
SangHyeon Jade Lee <sh10233.lee@samsung.com>
2022-02-13 23:21:23 -0800

    [NUI] add style guide sample. (#3944)
    * NUI: Add style guide sample.
    
    * NUI : add codeowners on styleguide.
    
    * [NUI] adding comments.
    
    * [NUI] remove xaml build config and remove unecessary dir.
    
    * [NUI] modify main logics and adding empty 2-depth scrollableBase examples.
    
    * [NUI] add scrollableBase examples

---
+++ test/Tizen.NUI.StyleGuide/Examples/ButtonExample.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Copyright(c) 2022 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+using System;
+using System.ComponentModel;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace Tizen.NUI.StyleGuide
+{
+    // IExample inehrited class will be automatically added in the main examples list.
+    internal class ButtonExample : ContentPage, IExample
+    {
+        private Window window;
+        public void Activate()
+        {
+           Log.Info(this.GetType().Name, $"@@@ this.GetType().Name={this.GetType().Name}, Activate()\n");
+        }
+        public void Deactivate()
+        {
+            Log.Info(this.GetType().Name, $"@@@ this.GetType().Name={this.GetType().Name}, Deactivate()\n");
+            window = null;
+        }
+
+        /// Modify this method for adding other examples.
+        public ButtonExample() : base()
+        {
+            Log.Info(this.GetType().Name, $"{this.GetType().Name} is contructed\n");
+
+            WidthSpecification = LayoutParamPolicies.MatchParent;
+            HeightSpecification = LayoutParamPolicies.MatchParent;
+            // Navigator bar title is added here.
+            AppBar = new AppBar()
+            {
+                Title = "Button Default Style",
+            };
+
+            // Example root content view.
+            // you can decorate, add children on this view.
+            var rootContent = new View()
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    CellPadding = new Size2D(10, 20),
+                },
+            };
+
+            // Button style examples.
+
+            var enabledButton = new Button()
+            {
+                Text = "Enabled"
+            };
+            enabledButton.Clicked += (object obj, ClickedEventArgs ev) =>
+            {
+                Log.Info(this.GetType().Name, "Enabled Button Clicked\n");
+            };
+            rootContent.Add(enabledButton);
+
+            var disabledButton = new Button()
+            {
+                Text = "Disabled",
+                IsEnabled = false,
+            };
+            disabledButton.Clicked += (object obj, ClickedEventArgs ev) =>
+            {
+                // This event should not be recieved. button is disabled.
+                Log.Info(this.GetType().Name, "Disabled Button Clicked\n");
+
+            };
+            rootContent.Add(disabledButton);
+
+            var selectableButton = new Button()
+            {
+                Text = "Unselected",
+                IsSelectable = true,
+            };
+            selectableButton.Clicked += (object obj, ClickedEventArgs ev) =>
+            {
+                Log.Info(this.GetType().Name, "Selected Button Clicked\n");
+                if (obj is Button button)
+                {
+                   if (button.IsSelected)
+                    {
+                        button.Text = "Selected";
+                    }
+                    else
+                    {
+                        button.Text = "Unselected";
+                    }
+                }
+            };
+            rootContent.Add(selectableButton);
+            Content = rootContent;
+        }
+    }
+}
---
+++ test/Tizen.NUI.StyleGuide/Tizen.NUI.StyleGuide.cs
@@ -0,0 +1,355 @@
+﻿/*
+ * Copyright(c) 2022 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Tizen.NUI;
+using Tizen.NUI.Components;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Binding;
+using System.Reflection;
+
+namespace Tizen.NUI.StyleGuide
+{
+    public class SearchField : View
+    {
+        public TextField SearchTextField;
+        public Button SearchButton;
+        public SearchField() : base()
+        {
+            Layout = new LinearLayout()
+            {
+                LinearOrientation = LinearLayout.Orientation.Horizontal,
+                LinearAlignment = LinearLayout.Alignment.CenterVertical,
+                CellPadding = new Size2D(40, 0),
+            };
+
+            BackgroundColor = Color.White;
+
+            WidthSpecification = LayoutParamPolicies.MatchParent;
+            HeightSpecification = LayoutParamPolicies.WrapContent;
+            Padding = new Extents(64, 64, 0, 0);
+
+            var searchTextBox = CreateSearchText();
+            SearchTextField = CreateTextField();
+            var underline = CreateUnderline();
+
+            searchTextBox.Add(SearchTextField);
+            searchTextBox.Add(underline);
+
+            SearchButton = CreateSearchButton();
+
+            Add(searchTextBox);
+            Add(SearchButton);
+        }
+
+        private View CreateSearchText()
+        {
+            return new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    CellPadding = new Size2D(0, 20),
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+            };
+        }
+
+        private TextField CreateTextField()
+        {
+            return new TextField()
+            {
+                PlaceholderText = "Search",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                MinimumSize = new Size2D(0, 40),
+            };
+        }
+
+        private View CreateUnderline()
+        {
+            return new View()
+            {
+                BackgroundColor = new Color("#0A0E4AFF"),
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = 2,
+            };
+        }
+
+        private Button CreateSearchButton()
+        {
+            return new Button()
+            {
+                Text = "Run",
+                WidthSpecification = 120,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+            };
+        }
+    }
+
+    public class ControlMenu
+    {
+        public ControlMenu(string name, string fullName = null)
+        {
+            Name = name;
+            FullName = fullName;
+        }
+
+        public string Name { get; set; }
+
+        public string ViewLabel
+        {
+            get
+            {
+                return Name;
+            }
+        }
+
+        public bool Selected { get; set; }
+
+        internal string FullName { get; set; }
+    }
+
+    public class ControlMenuViewModel
+    {
+        public List<Tuple<string, string>> NamePool = new List<Tuple<string, string>>();
+
+        public ControlMenuViewModel()
+        {
+            //CreateData();
+        }
+
+        public List<ControlMenu> CreateData()
+        {
+            GetXamlPages();
+
+            List<ControlMenu> result = new List<ControlMenu>();
+            foreach (var name in NamePool)
+            {
+                result.Add(new ControlMenu(name.Item1, name.Item2));
+            }
+            return result;
+        }
+
+        private void GetXamlPages()
+        {
+            Assembly assembly = this.GetType().Assembly;
+            Type exampleType = assembly.GetType("Tizen.NUI.StyleGuide.IExample");
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                Console.WriteLine($"@@@ type.Name={type.Name}, type.FullName={type.FullName}");
+                if (exampleType.IsAssignableFrom(type) && type.Name != "SampleMain" && this.GetType() != type && type.IsClass)
+                {
+                    NamePool.Add(new Tuple<string, string>(type.Name, type.FullName));
+                }
+            }
+        }
+    }
+
+    class Program : NUIApplication
+    {
+        private Window window;
+        private Navigator navigator;
+        private CollectionView colView;
+        private ItemSelectionMode selMode;
+        private ContentPage page;
+        private SearchField field;
+        private List<ControlMenu> testSource;
+
+        public void OnKeyEvent(object sender, Window.KeyEventArgs e)
+        {
+            // FIXME:: Navigator should provide Back/Escape event processing.
+            if (e.Key.State == Key.StateType.Up)
+            {
+                Log.Info("StyleGuide", $"[{e.Key.KeyPressedName}] is pressed!\n");
+                if (e.Key.KeyPressedName == "Escape" || e.Key.KeyPressedName == "XF86Back" || e.Key.KeyPressedName == "BackSpace")
+                {
+                    if (navigator == null) return;
+
+                    ExitSample();
+
+                    if (navigator.PageCount == 0)
+                    {
+                         Exit();
+                    }
+                }
+            }
+        }
+
+        public void OnSelectionChanged(object sender, SelectionChangedEventArgs ev)
+        {
+            Console.WriteLine($"@@@ OnSelectionChanged() {ev.CurrentSelection}");
+
+            if (ev.CurrentSelection.Count == 0) return;
+
+            if (ev.CurrentSelection[0] is ControlMenu selItem)
+            {
+                Console.WriteLine($"@@@ selItem.Name={selItem.Name}, selItem.FullName={selItem.FullName}");
+                RunSample(selItem?.FullName);
+            }
+            colView.SelectedItem = null;
+        }
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Initialize();
+            SetMainPage();
+        }
+        private void Initialize()
+        {
+            window = GetDefaultWindow();
+            window.Title = "NUI Style Guide";
+            window.KeyEvent += OnKeyEvent;
+
+            navigator = window.GetDefaultNavigator();
+        }
+
+        void OnSearchBtnClicked(object sender, ClickedEventArgs e)
+        {
+            var filteredSource = from filter in testSource
+                                 where filter.Name.ToLower().Contains(field.SearchTextField?.Text?.ToLower())
+                                 select filter;
+
+            colView.Header = new DefaultTitleItem()
+            {
+                Text = "result",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+            };
+            colView.ItemsSource = filteredSource;
+        }
+
+        private void SetMainPage()
+        {
+            var appBar = new AppBar()
+            {
+                Title = "NUI Style Guide",
+                AutoNavigationContent = false,
+            };
+
+            var appBarStyle = ThemeManager.GetStyle("Tizen.NUI.Components.AppBar");
+            var moreButton = new Button(((AppBarStyle)appBarStyle).BackButton);
+            moreButton.Icon.ResourceUrl = Tizen.Applications.Application.Current.DirectoryInfo.Resource + "menu.png";
+            appBar.NavigationContent = moreButton;
+
+
+            var pageContent = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+            };
+
+            field = new SearchField()
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+            };
+            field.SearchButton.Clicked += OnSearchBtnClicked;
+
+            testSource = new ControlMenuViewModel().CreateData();
+            selMode = ItemSelectionMode.SingleAlways;
+            var myTitle = new DefaultTitleItem()
+            {
+                Text = "TestCase",
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+            };
+
+            colView = new CollectionView()
+            {
+                ItemsSource = testSource,
+                ItemsLayouter = new LinearLayouter(),
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    DefaultLinearItem item = new DefaultLinearItem()
+                    {
+                        WidthSpecification = LayoutParamPolicies.MatchParent,
+                    };
+                    item.Label.SetBinding(TextLabel.TextProperty, "ViewLabel");
+                    item.Label.HorizontalAlignment = HorizontalAlignment.Begin;
+                    return item;
+                }),
+                Header = myTitle,
+                ScrollingDirection = ScrollableBase.Direction.Vertical,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                SelectionMode = selMode,
+            };
+            colView.SelectionChanged += OnSelectionChanged;
+
+            pageContent.Add(field);
+            pageContent.Add(colView);
+
+            page = new ContentPage()
+            {
+                AppBar = appBar,
+                Content = pageContent,
+            };
+            navigator.Push(page);
+        }
+
+        private void RunSample(string name)
+        {
+            IExample example = typeof(Program).Assembly?.CreateInstance(name) as IExample;
+
+            Console.WriteLine($"@@@ typeof(Program).Assembly={typeof(Program).Assembly}, name={name}");
+
+            if (example != null)
+            {
+                example.Activate();
+                if (example is Page examplePage)
+                {
+                    navigator.Push((examplePage));
+                }
+            }
+            else
+            {
+                Console.WriteLine($"@@@ examle is null!");
+            }
+        }
+
+        private void ExitSample()
+        {
+            if (navigator.Peek() is IExample currentExample)
+            {
+                currentExample.Deactivate();
+            }
+
+            navigator.Pop();
+            // FullGC();
+        }
+
+        private void FullGC()
+        {
+            global::System.GC.Collect();
+            global::System.GC.WaitForPendingFinalizers();
+            global::System.GC.Collect();
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
