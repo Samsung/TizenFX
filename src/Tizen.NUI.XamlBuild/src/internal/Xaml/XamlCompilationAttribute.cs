70318934d810b91d8c36d3441965ac5613a31c59
Xianbing Teng <xb.teng@samsung.com>
2022-02-24 16:54:50 +0800

    [NUI] Add file comment and end empty line

--- src/Tizen.NUI.XamlBuild/src/internal/Xaml/XamlCompilationAttribute.cs
+++ src/Tizen.NUI.XamlBuild/src/internal/Xaml/XamlCompilationAttribute.cs
@@ -10,33 +10,5 @@ namespace Tizen.NUI.Xaml
         Skip = 1 << 0,
         Compile = 1 << 1
     }
-
-    //[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Module | AttributeTargets.Class, Inherited = false)]
-    //public sealed class XamlCompilationAttribute : Attribute
-    //{
-    //    public XamlCompilationAttribute(XamlCompilationOptions xamlCompilationOptions)
-    //    {
-    //        XamlCompilationOptions = xamlCompilationOptions;
-    //    }
-
-    //    public XamlCompilationOptions XamlCompilationOptions { get; set; }
-    //}
-
-    //internal static class XamlCExtensions
-    //{
-    //    public static bool IsCompiled(this Type type)
-    //    {
-    //        var attr = type.GetTypeInfo().GetCustomAttribute<XamlCompilationAttribute>();
-    //        if (attr != null)
-    //            return attr.XamlCompilationOptions == XamlCompilationOptions.Compile;
-    //        attr = type.GetTypeInfo().Module.GetCustomAttribute<XamlCompilationAttribute>();
-    //        if (attr != null)
-    //            return attr.XamlCompilationOptions == XamlCompilationOptions.Compile;
-    //        attr = type.GetTypeInfo().Assembly.GetCustomAttribute<XamlCompilationAttribute>();
-    //        if (attr != null)
-    //            return attr.XamlCompilationOptions == XamlCompilationOptions.Compile;
-
-    //        return false;
-    //    }
-    //}
 }
+ 
