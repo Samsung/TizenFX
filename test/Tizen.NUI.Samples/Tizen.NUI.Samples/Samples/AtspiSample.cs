b4f698055967497d07d226b9d02b702a1cc7659f
Shinwoo Kim <cinoo.kim@samsung.com>
2022-03-11 00:22:48 +0900

    [NUI][AT-SPI] Introduce AtspiEvents
    The AtspiEvents can be used for the event mask to suppress
    specific event.
    
    This is the second version to provide AtspiEvents.
    The first version is
    https://github.com/Samsung/TizenFX/pull/4019
    
    Dependency:
    https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-csharp-binder/+/272794/
    https://review.tizen.org/gerrit/#/c/platform/core/uifw/dali-adaptor/+/272184/

---
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/AtspiSample.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.Samples
+{
+    public class AtspiSample : IExample
+    {
+        public void Activate()
+        {
+            Window window = NUIApplication.GetDefaultWindow();
+            window.BackgroundColor = Color.White;
+
+            TextLabel text = new TextLabel("PASS");
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            text.VerticalAlignment = VerticalAlignment.Center;
+            text.TextColor = Color.Blue;
+            text.PointSize = 12.0f;
+            text.HeightResizePolicy = ResizePolicyType.FillToParent;
+            text.WidthResizePolicy = ResizePolicyType.FillToParent;
+            window.Add(text);
+
+            if (text.IsAccessibilityEventSuppressed(AccessibilityEvent.MovedOut))
+            {
+                text.Text = "FAIL";
+                Tizen.Log.Error("NUITEST", "Default value shoud be false\n");
+                return;
+            }
+
+            text.SetAccessibilityEventSuppressed(AccessibilityEvent.MovedOut, true);
+            if (!text.IsAccessibilityEventSuppressed(AccessibilityEvent.MovedOut))
+            {
+                text.Text = "FAIL";
+                Tizen.Log.Error("NUITEST", "Cannot set to true\n");
+                return;
+            }
+
+            text.SetAccessibilityEventSuppressed(AccessibilityEvent.MovedOut, false);
+            if (text.IsAccessibilityEventSuppressed(AccessibilityEvent.MovedOut))
+            {
+                text.Text = "FAIL";
+                Tizen.Log.Error("NUITEST", "Cannot set to false\n");
+                return;
+            }
+        }
+
+        public void Deactivate()
+        {
+        }
+    }
+}
