Samsung/TizenFX

commit 89314b85a0e2eb71627ebea74fed67e24b5410be

[NUI][SVACE] Fix SVACE issues

src/Tizen.NUI/src/public/Layouting/FlexLayout.cs
@@ -672,8 +672,8 @@ namespace Tizen.NUI
 
             childLayout?.Measure(childWidthMeasureSpec, childHeightMeasureSpec);
 
-            measureSize.width = childLayout.MeasuredWidth.Size.AsRoundedValue();
-            measureSize.height = childLayout.MeasuredHeight.Size.AsRoundedValue();
+            measureSize.width = (childLayout == null) ? 0 : MeasuredWidth.Size.AsRoundedValue();
+            measureSize.height = (childLayout == null) ? 0 : MeasuredHeight.Size.AsRoundedValue();
         }
 
         void InsertChild(LayoutItem child)

src/Tizen.NUI.Components/Controls/RecyclerView/Layouter/GridLayouter.cs
@@ -310,18 +310,24 @@ namespace Tizen.NUI.Components
                         else if (source.IsGroupFooter(i))
                         {
                             //currentGroup.hasFooter = true;
-                            currentGroup.Count++;
-                            currentGroup.GroupSize += groupFooterSize;
-                            Current += groupFooterSize;
+                            if (currentGroup != null)
+                            {
+                                currentGroup.Count++;
+                                currentGroup.GroupSize += groupFooterSize;
+                                Current += groupFooterSize;
+                            }
                         }
                         else
                         {
-                            currentGroup.Count++;
-                            int index = i - currentGroup.StartIndex - ((colView.GroupHeaderTemplate != null)? 1: 0);
-                            if ((index % spanSize) == 0)
+                            if (currentGroup != null)
                             {
-                                currentGroup.GroupSize += StepCandidate;
-                                Current += StepCandidate;
+                                currentGroup.Count++;
+                                int index = i - currentGroup.StartIndex - ((colView.GroupHeaderTemplate != null) ? 1 : 0);
+                                if ((index % spanSize) == 0)
+                                {
+                                    currentGroup.GroupSize += StepCandidate;
+                                    Current += StepCandidate;
+                                }
                             }
                         }
                     }

src/Tizen.NUI.Wearable/src/public/CircularPagination.cs
@@ -709,7 +709,7 @@ namespace Tizen.NUI.Wearable
                     // If the center image is set before, then should update the center visual separately.
                     if (isCurrentIndicatorCentered)
                     {
-                        indicator.URL = circularPaginationStyle.CenterIndicatorImageURL.Selected;
+                        indicator.URL = circularPaginationStyle.CenterIndicatorImageURL?.Selected;
                     }
                     else
                     {
@@ -722,7 +722,7 @@ namespace Tizen.NUI.Wearable
                     // If the center image is set before, then should update the center visual separately.
                     if (isCurrentIndicatorCentered)
                     {
-                        indicator.URL = circularPaginationStyle.CenterIndicatorImageURL.Normal;
+                        indicator.URL = circularPaginationStyle.CenterIndicatorImageURL?.Normal;
                     }
                     else
                     {
@@ -774,7 +774,7 @@ namespace Tizen.NUI.Wearable
 
                 if (isCenterImageSet && !isSymmetrical && (i == leftIndicatorCount))
                 {
-                    newOne.URL = circularPaginationStyle.CenterIndicatorImageURL.Normal;
+                    newOne.URL = circularPaginationStyle.CenterIndicatorImageURL?.Normal;
                 }
                 else
                 {
@@ -793,7 +793,7 @@ namespace Tizen.NUI.Wearable
 
             if (isCenterImageSet && (selectedIndex == leftIndicatorCount))
             {
-                indicatorList[selectedIndex].URL = circularPaginationStyle.CenterIndicatorImageURL.Selected;
+                indicatorList[selectedIndex].URL = circularPaginationStyle.CenterIndicatorImageURL?.Selected;
                 indicatorList[selectedIndex].Opacity = 1.0f;
             }
             else

src/Tizen.NUI/src/public/Layouting/GridLayout.cs
@@ -371,7 +371,7 @@ namespace Tizen.NUI
             }
             else
             {
-                if (hLocations.Length > maxColumnConut)
+                if (hLocations?.Length > maxColumnConut)
                 {
                     widthSize = (int)(hLocations[maxColumnConut] - hLocations[0] - columnSpacing);
                 }

commit de09125705dc56d7ff2de1fd069b94b859a5f45d

[NUI] Fix FlexLayout size calculation issue caused by typo
Fix typo in FlexLayout size calculation logic.

This typo was caused by https://github.com/Samsung/TizenFX/pull/4032

src/Tizen.NUI/src/public/Layouting/FlexLayout.cs
@@ -672,8 +672,8 @@ namespace Tizen.NUI
 
             childLayout?.Measure(childWidthMeasureSpec, childHeightMeasureSpec);
 
-            measureSize.width = (childLayout == null) ? 0 : MeasuredWidth.Size.AsRoundedValue();
-            measureSize.height = (childLayout == null) ? 0 : MeasuredHeight.Size.AsRoundedValue();
+            measureSize.width = (childLayout == null) ? 0 : childLayout.MeasuredWidth.Size.AsRoundedValue();
+            measureSize.height = (childLayout == null) ? 0 : childLayout.MeasuredHeight.Size.AsRoundedValue();
         }
 
         void InsertChild(LayoutItem child)