78d16ed7443166b6a8d2b5abd46249e441c5749b
Fang Xiaohui <xiaohui.fang@samsung.com>
2022-03-22 15:49:13 +0800

    [NUI][Xaml]Fix issue of Call SetBinding in C# code

--- src/Tizen.NUI/src/public/XamlBinding/BindableObject.cs
+++ src/Tizen.NUI/src/public/XamlBinding/BindableObject.cs
@@ -272,7 +272,7 @@ namespace Tizen.NUI.Binding
             if (property == null)
                 throw new ArgumentNullException(nameof(property));
 
-            if (true == isCreateByXaml)
+            if (true == IsBinded)
             {
                 if (property.IsReadOnly)
                     throw new InvalidOperationException(string.Format("The BindableProperty \"{0}\" is readonly.", property.PropertyName));
