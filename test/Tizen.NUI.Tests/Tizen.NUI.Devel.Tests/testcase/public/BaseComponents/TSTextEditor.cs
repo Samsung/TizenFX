54917f20394325b7f29f4228675682432dd7c015
Bowon Ryu <bowon.ryu@samsung.com>
2022-04-13 16:48:11 +0900

    [NUI] add struct testcase for Text component
    added testcase for struct get/set method of text component
    : InputFilter, Strikethrough, FontStyle, Underline, Shadow, Outline, TextFit, Placeholder, HiddenInput, SelectionHandleImage
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- test/Tizen.NUI.Tests/Tizen.NUI.Devel.Tests/testcase/public/BaseComponents/TSTextEditor.cs
+++ test/Tizen.NUI.Tests/Tizen.NUI.Devel.Tests/testcase/public/BaseComponents/TSTextEditor.cs
@@ -43,6 +43,14 @@ namespace Tizen.NUI.Devel.Tests
             return false;
         }
 
+        public bool CheckVector2(Vector2 vectorSrc, Vector2 vectorDst)
+        {
+            if (vectorSrc.X == vectorDst.X && vectorSrc.Y == vectorDst.Y)
+                return true;
+
+            return false;
+        }
+
         [Test]
         [Category("P1")]
         [Description("TextEditor constructor. With status of Shown.")]
@@ -256,6 +264,312 @@ namespace Tizen.NUI.Devel.Tests
 
             testingTarget.Dispose();
             tlog.Debug(tag, $"TextEditorVerticalAlignment END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetFontStyle.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetFontStyle M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetFontStyle()
+        {
+            tlog.Debug(tag, $"TextEditorGetFontStyle START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setFontStyle = new Tizen.NUI.Text.FontStyle()
+            {
+                Width = FontWidthType.Condensed,
+                Weight = FontWeightType.Light,
+                Slant = FontSlantType.Italic,
+            };
+
+            testingTarget.SetFontStyle(setFontStyle);
+
+            var getFontStyle = testingTarget.GetFontStyle();
+            Assert.AreEqual(getFontStyle.Width, setFontStyle.Width, "Should be equal!");
+            Assert.AreEqual(getFontStyle.Weight, setFontStyle.Weight, "Should be equal!");
+            Assert.AreEqual(getFontStyle.Slant, setFontStyle.Slant, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetFontStyle END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetUnderline.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetUnderline M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetUnderline()
+        {
+            tlog.Debug(tag, $"TextEditorGetUnderline START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setUnderline = new Tizen.NUI.Text.Underline()
+            {
+                Enable = true,
+                Color = new Color("#3498DB"),
+                Height = 2.0f,
+                Type = UnderlineType.Dashed,
+                DashWidth = 4.0f,
+                DashGap = 6.0f,
+            };
+
+            testingTarget.SetUnderline(setUnderline);
+
+            var getUnderline = testingTarget.GetUnderline();
+            Assert.AreEqual(getUnderline.Enable, setUnderline.Enable, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getUnderline.Color, setUnderline.Color), "Should be true!");
+            Assert.AreEqual(getUnderline.Height, setUnderline.Height, "Should be equal!");
+            Assert.AreEqual(getUnderline.Type, setUnderline.Type, "Should be equal!");
+            Assert.AreEqual(getUnderline.DashWidth, setUnderline.DashWidth, "Should be equal!");
+            Assert.AreEqual(getUnderline.DashGap, setUnderline.DashGap, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetUnderline END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetShadow.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetShadow M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetShadow()
+        {
+            tlog.Debug(tag, $"TextEditorGetShadow START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setShadow = new Tizen.NUI.Text.Shadow()
+            {
+                BlurRadius = 5.0f,
+                Color = Color.Red,
+                Offset = new Vector2(3, 3),
+            };
+
+            testingTarget.SetShadow(setShadow);
+
+            var getShadow = testingTarget.GetShadow();
+            Assert.AreEqual(getShadow.BlurRadius, setShadow.BlurRadius, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getShadow.Color, setShadow.Color), "Should be true!");
+            Assert.AreEqual(true, CheckVector2(getShadow.Offset, setShadow.Offset), "Should be true!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetShadow END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetOutline.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetOutline M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetOutline()
+        {
+            tlog.Debug(tag, $"TextEditorGetOutline START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setOutline = new Tizen.NUI.Text.Outline()
+            {
+                Width = 2.0f,
+                Color = Color.Red,
+            };
+
+            testingTarget.SetOutline(setOutline);
+
+            var getOutline = testingTarget.GetOutline();
+            Assert.AreEqual(getOutline.Width, setOutline.Width, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getOutline.Color, setOutline.Color), "Should be true!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetOutline END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetSelectionHandleImage.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetSelectionHandleImage M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetSelectionHandleImage()
+        {
+            tlog.Debug(tag, $"TextEditorGetSelectionHandleImage START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setSelectionHandleImage = new Tizen.NUI.Text.SelectionHandleImage()
+            {
+                LeftImageUrl = "leftImage",
+                RightImageUrl = "rightImage",
+            };
+
+            testingTarget.SetSelectionHandleImage(setSelectionHandleImage);
+
+            var getSelectionHandleImage = testingTarget.GetSelectionHandleImage();
+            Assert.AreEqual(getSelectionHandleImage.LeftImageUrl, setSelectionHandleImage.LeftImageUrl, "Should be equal!");
+            Assert.AreEqual(getSelectionHandleImage.RightImageUrl, setSelectionHandleImage.RightImageUrl, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetSelectionHandleImage END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetSelectionHandlePressedImage.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetSelectionHandlePressedImage M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetSelectionHandlePressedImage()
+        {
+            tlog.Debug(tag, $"TextEditorGetSelectionHandlePressedImage START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setSelectionHandleImage = new Tizen.NUI.Text.SelectionHandleImage()
+            {
+                LeftImageUrl = "leftImage",
+                RightImageUrl = "rightImage",
+            };
+
+            testingTarget.SetSelectionHandlePressedImage(setSelectionHandleImage);
+
+            var getSelectionHandleImage = testingTarget.GetSelectionHandlePressedImage();
+            Assert.AreEqual(getSelectionHandleImage.LeftImageUrl, setSelectionHandleImage.LeftImageUrl, "Should be equal!");
+            Assert.AreEqual(getSelectionHandleImage.RightImageUrl, setSelectionHandleImage.RightImageUrl, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetSelectionHandlePressedImage END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetSelectionHandleMarkerImage.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetSelectionHandleMarkerImage M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetSelectionHandleMarkerImage()
+        {
+            tlog.Debug(tag, $"TextEditorGetSelectionHandleMarkerImage START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setSelectionHandleImage = new Tizen.NUI.Text.SelectionHandleImage()
+            {
+                LeftImageUrl = "leftImage",
+                RightImageUrl = "rightImage",
+            };
+
+            testingTarget.SetSelectionHandleMarkerImage(setSelectionHandleImage);
+
+            var getSelectionHandleImage = testingTarget.GetSelectionHandleMarkerImage();
+            Assert.AreEqual(getSelectionHandleImage.LeftImageUrl, setSelectionHandleImage.LeftImageUrl, "Should be equal!");
+            Assert.AreEqual(getSelectionHandleImage.RightImageUrl, setSelectionHandleImage.RightImageUrl, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetSelectionHandleMarkerImage END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetInputFilter.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetInputFilter M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetInputFilter()
+        {
+            tlog.Debug(tag, $"TextEditorGetInputFilter START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setInputFilter = new Tizen.NUI.Text.InputFilter()
+            {
+                Accepted = @"[\d]",
+                Rejected = "[0-3]",
+            };
+
+            testingTarget.SetInputFilter(setInputFilter);
+
+            var getInputFilter = testingTarget.GetInputFilter();
+            Assert.AreEqual(getInputFilter.Accepted, setInputFilter.Accepted, "Should be equal!");
+            Assert.AreEqual(getInputFilter.Rejected, setInputFilter.Rejected, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetInputFilter END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextEditor GetPlaceholder.")]
+        [Property("SPEC", "Tizen.NUI.TextEditor.GetPlaceholder M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextEditorGetPlaceholder()
+        {
+            tlog.Debug(tag, $"TextEditorGetPlaceholder START");
+
+            var testingTarget = new TextEditor(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextEditor");
+            Assert.IsInstanceOf<TextEditor>(testingTarget, "Should be an instance of TextEditor type.");
+
+            var setPlaceholder = new Tizen.NUI.Text.Placeholder()
+            {
+                Text = "placeholder text",
+                TextFocused = "placeholder TextFocused",
+                Color = Color.Red,
+                FontFamily = "BreezeSans",
+                FontStyle = new Tizen.NUI.Text.FontStyle()
+                {
+                    Width = FontWidthType.Expanded,
+                    Weight = FontWeightType.ExtraLight,
+                    Slant = FontSlantType.Italic,
+                },
+                PointSize = 15.0f,
+                Ellipsis = true,
+            };
+
+            testingTarget.SetPlaceholder(setPlaceholder);
+
+            var getPlaceholder = testingTarget.GetPlaceholder();
+            Assert.AreEqual(getPlaceholder.Text, setPlaceholder.Text, "Should be equal!");
+            Assert.AreEqual(getPlaceholder.TextFocused, setPlaceholder.TextFocused, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getPlaceholder.Color, setPlaceholder.Color), "Should be true!");
+            Assert.AreEqual(getPlaceholder.FontFamily, setPlaceholder.FontFamily, "Should be equal!");
+            Assert.AreEqual(true, getPlaceholder.FontStyle == setPlaceholder.FontStyle, "Should be true!");
+            Assert.AreEqual(getPlaceholder.PointSize, setPlaceholder.PointSize, "Should be equal!");
+            Assert.AreEqual(getPlaceholder.Ellipsis, setPlaceholder.Ellipsis, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextEditorGetPlaceholder END (OK)");
         }
     }
 }
--- test/Tizen.NUI.Tests/Tizen.NUI.Devel.Tests/testcase/public/BaseComponents/TSTextLabel.cs
+++ test/Tizen.NUI.Tests/Tizen.NUI.Devel.Tests/testcase/public/BaseComponents/TSTextLabel.cs
@@ -43,6 +43,14 @@ namespace Tizen.NUI.Devel.Tests
             return false;
         }
 
+        public bool CheckVector2(Vector2 vectorSrc, Vector2 vectorDst)
+        {
+            if (vectorSrc.X == vectorDst.X && vectorSrc.Y == vectorDst.Y)
+                return true;
+
+            return false;
+        }
+
         [Test]
         [Category("P1")]
         [Description("TextLabel FontSizeScale.")]
@@ -153,5 +161,177 @@ namespace Tizen.NUI.Devel.Tests
             tlog.Debug(tag, $"TextLabelCharacterSpacing END (OK)");
         }
 
+        [Test]
+        [Category("P1")]
+        [Description("TextLabel GetFontStyle.")]
+        [Property("SPEC", "Tizen.NUI.TextLabel.GetFontStyle M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextLabelGetFontStyle()
+        {
+            tlog.Debug(tag, $"TextLabelGetFontStyle START");
+
+            var testingTarget = new TextLabel(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextLabel");
+            Assert.IsInstanceOf<TextLabel>(testingTarget, "Should be an instance of TextLabel type.");
+
+            var setFontStyle = new Tizen.NUI.Text.FontStyle()
+            {
+                Width = FontWidthType.Condensed,
+                Weight = FontWeightType.Light,
+                Slant = FontSlantType.Italic,
+            };
+
+            testingTarget.SetFontStyle(setFontStyle);
+
+            var getFontStyle = testingTarget.GetFontStyle();
+            Assert.AreEqual(getFontStyle.Width, setFontStyle.Width, "Should be equal!");
+            Assert.AreEqual(getFontStyle.Weight, setFontStyle.Weight, "Should be equal!");
+            Assert.AreEqual(getFontStyle.Slant, setFontStyle.Slant, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextLabelGetFontStyle END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextLabel GetUnderline.")]
+        [Property("SPEC", "Tizen.NUI.TextLabel.GetUnderline M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextLabelGetUnderline()
+        {
+            tlog.Debug(tag, $"TextLabelGetUnderline START");
+
+            var testingTarget = new TextLabel(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextLabel");
+            Assert.IsInstanceOf<TextLabel>(testingTarget, "Should be an instance of TextLabel type.");
+
+            var setUnderline = new Tizen.NUI.Text.Underline()
+            {
+                Enable = true,
+                Color = new Color("#3498DB"),
+                Height = 2.0f,
+                Type = UnderlineType.Dashed,
+                DashWidth = 4.0f,
+                DashGap = 6.0f,
+            };
+
+            testingTarget.SetUnderline(setUnderline);
+
+            var getUnderline = testingTarget.GetUnderline();
+            Assert.AreEqual(getUnderline.Enable, setUnderline.Enable, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getUnderline.Color, setUnderline.Color), "Should be true!");
+            Assert.AreEqual(getUnderline.Height, setUnderline.Height, "Should be equal!");
+            Assert.AreEqual(getUnderline.Type, setUnderline.Type, "Should be equal!");
+            Assert.AreEqual(getUnderline.DashWidth, setUnderline.DashWidth, "Should be equal!");
+            Assert.AreEqual(getUnderline.DashGap, setUnderline.DashGap, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextLabelGetUnderline END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextLabel GetShadow.")]
+        [Property("SPEC", "Tizen.NUI.TextLabel.GetShadow M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextLabelGetShadow()
+        {
+            tlog.Debug(tag, $"TextLabelGetShadow START");
+
+            var testingTarget = new TextLabel(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextLabel");
+            Assert.IsInstanceOf<TextLabel>(testingTarget, "Should be an instance of TextLabel type.");
+
+            var setShadow = new Tizen.NUI.Text.Shadow()
+            {
+                BlurRadius = 5.0f,
+                Color = Color.Red,
+                Offset = new Vector2(3, 3),
+            };
+
+            testingTarget.SetShadow(setShadow);
+
+            var getShadow = testingTarget.GetShadow();
+            Assert.AreEqual(getShadow.BlurRadius, setShadow.BlurRadius, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getShadow.Color, setShadow.Color), "Should be true!");
+            Assert.AreEqual(true, CheckVector2(getShadow.Offset, setShadow.Offset), "Should be true!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextLabelGetShadow END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextLabel GetOutline.")]
+        [Property("SPEC", "Tizen.NUI.TextLabel.GetOutline M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextLabelGetOutline()
+        {
+            tlog.Debug(tag, $"TextLabelGetOutline START");
+
+            var testingTarget = new TextLabel(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextLabel");
+            Assert.IsInstanceOf<TextLabel>(testingTarget, "Should be an instance of TextLabel type.");
+
+            var setOutline = new Tizen.NUI.Text.Outline()
+            {
+                Width = 2.0f,
+                Color = Color.Red,
+            };
+
+            testingTarget.SetOutline(setOutline);
+
+            var getOutline = testingTarget.GetOutline();
+            Assert.AreEqual(getOutline.Width, setOutline.Width, "Should be equal!");
+            Assert.AreEqual(true, CheckColor(getOutline.Color, setOutline.Color), "Should be true!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextLabelGetOutline END (OK)");
+        }
+
+        [Test]
+        [Category("P1")]
+        [Description("TextLabel GetTextFit.")]
+        [Property("SPEC", "Tizen.NUI.TextLabel.GetTextFit M")]
+        [Property("SPEC_URL", "-")]
+        [Property("CRITERIA", "MR")]
+        [Property("AUTHOR", "bowon.ryu@samsung.com")]
+        public void TextLabelGetTextFit()
+        {
+            tlog.Debug(tag, $"TextLabelGetTextFit START");
+
+            var testingTarget = new TextLabel(true);
+            Assert.IsNotNull(testingTarget, "Can't create success object TextLabel");
+            Assert.IsInstanceOf<TextLabel>(testingTarget, "Should be an instance of TextLabel type.");
+
+            var setTextFit = new Tizen.NUI.Text.TextFit()
+            {
+                Enable = true,
+                MinSize = 30.0f,
+                MaxSize = 200.0f,
+                StepSize = 1.0f,
+                FontSizeType = FontSizeType.PointSize,
+            };
+
+            testingTarget.SetTextFit(setTextFit);
+
+            var getTextFit = testingTarget.GetTextFit();
+            Assert.AreEqual(getTextFit.Enable, setTextFit.Enable, "Should be equal!");
+            Assert.AreEqual(getTextFit.MinSize, setTextFit.MinSize, "Should be equal!");
+            Assert.AreEqual(getTextFit.MaxSize, setTextFit.MaxSize, "Should be equal!");
+            Assert.AreEqual(getTextFit.StepSize, setTextFit.StepSize, "Should be equal!");
+            Assert.AreEqual(getTextFit.FontSizeType, setTextFit.FontSizeType, "Should be equal!");
+
+            testingTarget.Dispose();
+            tlog.Debug(tag, $"TextLabelGetTextFit END (OK)");
+        }
     }
 }
