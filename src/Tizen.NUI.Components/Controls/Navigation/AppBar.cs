63eca024787a8bfe1e0afe28b29b168c0cd421e3
Seoyeon Kim <seoyeon2.kim@samsung.com>
2022-04-14 17:35:53 +0900

    [NUI] Add to set AccessibilityName and Description in some components
    - Some NUI Components have text parts and they need to be connected with
     Accessibillity features.
     (`AccessibilityName` and `AccessibiityDescription`)
    
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- src/Tizen.NUI.Components/Controls/Navigation/AppBar.cs
+++ src/Tizen.NUI.Components/Controls/Navigation/AppBar.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright(c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright(c) 2022 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -536,6 +536,15 @@ namespace Tizen.NUI.Components
 
                 return defaultActionContent;
             }
+        }
+
+        /// <summary>
+        /// Gets accessibility name.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected override string AccessibilityGetName()
+        {
+            return Title;
         }
 
         /// <inheritdoc/>
