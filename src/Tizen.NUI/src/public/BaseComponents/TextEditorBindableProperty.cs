530ac3becfd2ec4f32cd754591f25667e9472609
Bowon Ryu <bowon.ryu@samsung.com>
2022-04-20 13:50:24 +0900

    [NUI] Add RelativeLineHeight in TextLabel, TextEditor
    RelativeLineHeight is a factor that will be multiplied by text height.
    
    // line height will be 2.0 times the text height
    label.RelativeLineHeight = 2.0f;
    
    // if the value is less than 1.0f, the lines could to be overlapped
    // line height will be 0.5 times the text height
    label.RelativeLineHeight = 0.5f;
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/TextEditorBindableProperty.cs
+++ src/Tizen.NUI/src/public/BaseComponents/TextEditorBindableProperty.cs
@@ -577,6 +577,22 @@ namespace Tizen.NUI.BaseComponents
             Tizen.NUI.Object.GetProperty((System.Runtime.InteropServices.HandleRef)textEditor.SwigCPtr, TextEditor.Property.InputLineSpacing).Get(out temp);
             return temp;
         }));
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static readonly BindableProperty RelativeLineHeightProperty = BindableProperty.Create(nameof(RelativeLineHeight), typeof(float), typeof(TextEditor), default(float), propertyChanged: (BindableProperty.BindingPropertyChangedDelegate)((bindable, oldValue, newValue) =>
+        {
+            var textEditor = (TextEditor)bindable;
+            if (newValue != null)
+            {
+                Tizen.NUI.Object.SetProperty((System.Runtime.InteropServices.HandleRef)textEditor.SwigCPtr, TextEditor.Property.RelativeLineHeight, new Tizen.NUI.PropertyValue((float)newValue));
+            }
+        }),
+        defaultValueCreator: (BindableProperty.CreateDefaultValueDelegate)((bindable) =>
+        {
+            var textEditor = (TextEditor)bindable;
+            float temp = 0.0f;
+            Tizen.NUI.Object.GetProperty((System.Runtime.InteropServices.HandleRef)textEditor.SwigCPtr, TextEditor.Property.RelativeLineHeight).Get(out temp);
+            return temp;
+        }));
         /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static readonly BindableProperty UnderlineProperty = BindableProperty.Create(nameof(Underline), typeof(PropertyMap), typeof(TextEditor), null, propertyChanged: (BindableProperty.BindingPropertyChangedDelegate)((bindable, oldValue, newValue) =>
