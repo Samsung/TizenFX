b3d002bee138e1eeaca47c29b8c474dc979c2eb8
guowei.wang <guowei.wang@samsung.com>
2022-04-26 13:43:27 +0800

    [NUI][TEST] Update TCs of FlexibleView.

--- test/Tizen.NUI.Tests/Tizen.NUI.Components.Devel.Tests/testcase/Controls/FlexibleView/TSFlexibleViewHelper.cs
+++ test/Tizen.NUI.Tests/Tizen.NUI.Components.Devel.Tests/testcase/Controls/FlexibleView/TSFlexibleViewHelper.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Tizen.NUI.BaseComponents;
 
 namespace Tizen.NUI.Components.Devel.Tests
 {
@@ -29,6 +32,128 @@ namespace Tizen.NUI.Components.Devel.Tests
         {
             scrnSize?.Dispose();
             tlog.Info(tag, "Destroy() is called!");
+        }
+
+        public class ListItemData
+        {
+            private string str;
+
+            public ListItemData(int i)
+            {
+                str = "Text" + i.ToString();
+            }
+
+            public string TextString
+            {
+                get
+                {
+                    return str;
+                }
+            }
+        }
+
+        public class ListItemView : View
+        {
+            private TextLabel mText;
+
+            public ListItemView()
+            {
+                mText = new TextLabel();
+                mText.WidthResizePolicy = ResizePolicyType.FillToParent;
+                mText.HeightResizePolicy = ResizePolicyType.FillToParent;
+                mText.PointSize = 22;
+                mText.HorizontalAlignment = HorizontalAlignment.Center;
+                mText.VerticalAlignment = VerticalAlignment.Center;
+                Add(mText);
+            }
+
+            public string MainText
+            {
+                get
+                {
+                    return mText.Text;
+                }
+                set
+                {
+                    mText.Text = value;
+                }
+            }
+        }
+
+        public class ListBridge : FlexibleViewAdapter
+        {
+            private List<ListItemData> mDatas;
+            public ListBridge(List<ListItemData> datas)
+            {
+                mDatas = datas;
+            }
+
+            public void InsertData(int position)
+            {
+                mDatas.Insert(position, new ListItemData(1000 + position));
+                NotifyItemInserted(position);
+            }
+
+            public void RemoveData(int position)
+            {
+                mDatas.RemoveAt(position);
+                NotifyItemRemoved(position);
+            }
+
+            public override FlexibleViewViewHolder OnCreateViewHolder(int viewType)
+            {
+                FlexibleViewViewHolder viewHolder = new FlexibleViewViewHolder(new ListItemView());
+                return viewHolder;
+            }
+
+            public override void OnBindViewHolder(FlexibleViewViewHolder holder, int position)
+            {
+                ListItemData listItemData = mDatas[position];
+
+                ListItemView listItemView = holder.ItemView as ListItemView;
+                listItemView.Name = "Item" + position;
+                listItemView.SizeWidth = 150;
+                listItemView.SizeHeight = 60;
+                if (listItemView != null)
+                {
+                    listItemView.MainText = String.Format("{0:D2}", position) + " : " + listItemData.TextString;
+                }
+                listItemView.Margin = new Extents(2, 2, 2, 2);
+                if (position % 2 == 0)
+                    listItemView.BackgroundColor = Color.Cyan;
+                else
+                    listItemView.BackgroundColor = Color.Yellow;
+            }
+
+            public override void OnDestroyViewHolder(FlexibleViewViewHolder holder)
+            {
+                if (holder.ItemView != null)
+                {
+                    holder.ItemView.Dispose();
+                }
+            }
+
+            public override int GetItemCount()
+            {
+                return mDatas.Count;
+            }
+
+            public override void OnFocusChange(FlexibleView flexibleView, int previousFocus, int currentFocus)
+            {
+                FlexibleViewViewHolder previousFocusView = flexibleView.FindViewHolderForAdapterPosition(previousFocus);
+                if (previousFocusView != null)
+                {
+                    if (previousFocusView.AdapterPosition % 2 == 0)
+                        previousFocusView.ItemView.BackgroundColor = Color.Cyan;
+                    else
+                        previousFocusView.ItemView.BackgroundColor = Color.Yellow;
+                }
+                FlexibleViewViewHolder currentFocusView = flexibleView.FindViewHolderForAdapterPosition(currentFocus);
+                if (currentFocusView != null)
+                {
+                    currentFocusView.ItemView.BackgroundColor = Color.Magenta;
+                }
+            }
         }
 
         private FlexibleView GetHorizontalFlexibleView()
