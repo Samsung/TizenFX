6dba090902afea297d8b2ecff2db74ce34b20b42
Haesu Gwon <haesu.gwon@samsung.com>
2022-09-07 16:20:34 +0900

    [WebRTC] Add new APIs for statistics (#4508)
    * [WebRTC] Add new APIs for statistics

--- src/Tizen.Multimedia.Remoting/Interop/Interop.WebRTC.cs
+++ src/Tizen.Multimedia.Remoting/Interop/Interop.WebRTC.cs
@@ -18,6 +18,7 @@ using System;
 using System.Runtime.InteropServices;
 using Tizen;
 using Tizen.Applications;
+using Tizen.Internals;
 using Tizen.Multimedia;
 using Tizen.Multimedia.Remoting;
 
@@ -69,6 +70,9 @@ internal static partial class Interop
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         internal delegate void SdpCreatedCallback(IntPtr handle, string sdp, IntPtr userData);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate bool RetrieveStatsCallback(WebRTCStatisticsCategory category, IntPtr prop, IntPtr userData);
 
 
         [DllImport(Libraries.WebRTC, EntryPoint = "webrtc_create")]
@@ -243,6 +247,9 @@ internal static partial class Interop
         [DllImport(Libraries.WebRTC, EntryPoint = "webrtc_foreach_media_source_supported_format")]
         internal static extern WebRTCErrorCode SupportedMediaSourceFormat(IntPtr handle, SupportedMediaFormatCallback callback, IntPtr userData);
 
+        [DllImport(Libraries.WebRTC, EntryPoint = "webrtc_foreach_stats")]
+        internal static extern WebRTCErrorCode ForeachStats(IntPtr handle, int typeMask, RetrieveStatsCallback callback, IntPtr userData = default);
+
 
         [DllImport(Libraries.WebRTC, EntryPoint = "webrtc_set_error_cb")]
         internal static extern WebRTCErrorCode SetErrorOccurredCb(IntPtr handle, ErrorOccurredCallback callback, IntPtr userData = default);
@@ -315,6 +322,37 @@ internal static partial class Interop
 
         [DllImport(Libraries.WebRTC, EntryPoint = "webrtc_unset_encoded_video_frame_cb")]
         internal static extern WebRTCErrorCode UnsetVideoFrameEncodedCb(IntPtr handle);
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct StatsPropertyValueStruct
+        {
+            [FieldOffsetAttribute(0)]
+            internal bool @bool;
+            [FieldOffsetAttribute(0)]
+            internal int @int;
+            [FieldOffsetAttribute(0)]
+            internal uint @uint;
+            [FieldOffsetAttribute(0)]
+            internal long @long;
+            [FieldOffsetAttribute(0)]
+            internal ulong @ulong;
+            [FieldOffsetAttribute(0)]
+            internal float @float;
+            [FieldOffsetAttribute(0)]
+            internal double @double;
+            [FieldOffsetAttribute(0)]
+            internal IntPtr @string;
+        }
+
+        [NativeStruct("webrtc_stats_prop_info_s", Include="webrtc.h", PkgConfig="capi-media-webrtc")]
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct StatsPropertyStruct
+        {
+            internal string name;
+            internal WebRTCStatisticsProperty property;
+            internal WebRTCStatsPropertyType propertyType;
+            internal StatsPropertyValueStruct value;
+        }
     }
 
     internal class WebRTCHandle : SafeHandle
