aad2294109a0dad9e57567762b4385ec634b85c5
EverLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2022-10-31 16:36:17 +0900

    [NUI] add null type checking after as operation.

--- src/Tizen.NUI.Components/Controls/RecyclerView/CollectionView.cs
+++ src/Tizen.NUI.Components/Controls/RecyclerView/CollectionView.cs
@@ -1229,9 +1229,8 @@ namespace Tizen.NUI.Components
             colView.OnSelectionChanged(args);
         }
 
-        private static object CoerceSelectedItems(BindableObject bindable, object value)
+        private static object CoerceSelectedItems(CollectionView colView, object value)
         {
-            var colView = bindable as CollectionView;
             if (value == null)
             {
                 return new SelectionList(colView);
@@ -1245,10 +1244,8 @@ namespace Tizen.NUI.Components
             return new SelectionList(colView, value as IList<object>);
         }
 
-        private static void SelectionModePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        private static void SelectionModePropertyChanged(CollectionView colView, object oldValue, object newValue)
         {
-            var colView = bindable as CollectionView;
-
             var oldMode = (ItemSelectionMode)oldValue;
             var newMode = (ItemSelectionMode)newValue;
 
@@ -1418,7 +1415,11 @@ namespace Tizen.NUI.Components
 
             if (groupHeader == null)
             {
-                groupHeader = (RecyclerViewItem)DataTemplateExtensions.CreateContent(groupHeaderTemplate, context, this);
+                groupHeader = DataTemplateExtensions.CreateContent(groupHeaderTemplate, context, this) as RecyclerViewItem;
+                if (groupHeader == null)
+                {
+                    return null;
+                }
 
                 groupHeader.Template = templ;
                 groupHeader.isGroupHeader = true;
@@ -1448,6 +1449,10 @@ namespace Tizen.NUI.Components
             if (groupFooter == null)
             {
                 groupFooter = DataTemplateExtensions.CreateContent(groupFooterTemplate, context, this) as RecyclerViewItem;
+                if (groupFooter == null)
+                {
+                    return null;
+                }
 
                 groupFooter.Template = templ;
                 groupFooter.isGroupHeader = false;
