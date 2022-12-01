b321baa7e50afbb33d9db5cc6c6baf4718ad4be1
guowei.wang <guowei.wang@samsung.com>
2022-12-01 22:07:19 +0800

    [NUI][NUI.Components.Devel] Fix failed isseus of NUI.Components.Devel.

--- test/Tizen.NUI.Tests/Tizen.NUI.Components.Devel.Tests/Properties/Resources.Designer.cs
+++ test/Tizen.NUI.Tests/Tizen.NUI.Components.Devel.Tests/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Tizen.NUI.Components.Devel.Tests.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tizen.NUI.Components.Tests.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tizen.NUI.Components.Devel.Tests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
