3eebe20f96cb726ffd23631d66f30b4ead588d11
Xianbing Teng <xb.teng@samsung.com>
2023-01-18 15:53:31 +0800

    [NUI] Fix PropertyNotification & PanGestureDetector svace issues

--- src/Tizen.NUI/src/public/Common/PropertyNotification.cs
+++ src/Tizen.NUI/src/public/Common/PropertyNotification.cs
@@ -286,7 +286,7 @@ namespace Tizen.NUI
                     if (propertyNotificationNotifyEventHandler != null)
                     {
                         var process = global::System.Diagnostics.Process.GetCurrentProcess();
-                        var processId = process.Id;
+                        var processId = process?.Id ?? -1;
                         var thread = global::System.Threading.Thread.CurrentThread.ManagedThreadId;
                         var me = this.GetType().FullName;
 
@@ -294,7 +294,7 @@ namespace Tizen.NUI
                             $"OR the native dali object handle of NUI becomes null! \n" +
                             $" process:{processId} thread:{thread}, isDisposed:{this.Disposed}, isDisposeQueued:{this.IsDisposeQueued}, me:{me}\n");
 
-                        process.Dispose();
+                        process?.Dispose();
                     }
                 }
                 else
@@ -302,7 +302,7 @@ namespace Tizen.NUI
                     if (this.IsDisposeQueued)
                     {
                         var process = global::System.Diagnostics.Process.GetCurrentProcess();
-                        var processId = process.Id;
+                        var processId = process?.Id ?? -1;
                         var thread = global::System.Threading.Thread.CurrentThread.ManagedThreadId;
                         var me = this.GetType().FullName;
 
@@ -310,7 +310,7 @@ namespace Tizen.NUI
                         Tizen.Log.Error("NUI", "in this case, the View object is ready to be disposed waiting on DisposeQueue, so event callback should not be invoked! just return here! \n" +
                             $"process:{processId} thread:{thread}, isDisposed:{this.Disposed}, isDisposeQueued:{this.IsDisposeQueued}, me:{me}\n");
 
-                        process.Dispose();
+                        process?.Dispose();
                         return;
                     }
                 }
