6e3473e3a53901f0dfe306ee9dee68523a29afbc
Hwankyu Jhun <h.jhun@samsung.com>
2023-03-09 10:08:58 +0900

    [Applications.Common] Add new internal AppControl APIs
    Adds:
     - SetWindowPosition()
     - GetWindowPosition()
    
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

--- src/Tizen.Applications.Common/Interop/Interop.AppControl.cs
+++ src/Tizen.Applications.Common/Interop/Interop.AppControl.cs
@@ -154,5 +154,11 @@ internal static partial class Interop
 
         [DllImport(Libraries.AppControl, EntryPoint = "app_control_set_auto_restart")]
         internal static extern ErrorCode SetAutoRestart(SafeAppControlHandle handle);
+
+        [DllImport(Libraries.AppControl, EntryPoint = "app_control_set_window_position")]
+        internal static extern ErrorCode SetWindowPosition(SafeAppControlHandle handle, int x, int y, int w, int h);
+
+        [DllImport(Libraries.AppControl, EntryPoint = "app_control_get_window_position")]
+        internal static extern ErrorCode GetWindowPosition(SafeAppControlHandle handle, out int x, out int y, out int w, out int h);
     }
 }
