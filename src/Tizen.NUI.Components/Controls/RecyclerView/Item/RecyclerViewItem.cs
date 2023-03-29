0839992e35b63176e9cbe7fad95d849560272c4b
SangHyeon Jade Lee <sh10233.lee@samsung.com>
2023-03-29 14:25:55 +0900

    [NUI] refactoring collectionView and Layouters for extended class (#5123)
    * [NUI] refactoring collectionView and Layouters for extended class
    
    to create customized collectionView and Layouters,
    some properties must be extendable.
    this change includes properties access modifier
    which is internal or private to protected.
    
    * [NUI] publize reyclerviewitem properties
    
    * [NUI] fix item source getter more efficiently.
    
    * [NUI] refactoring getter.
    
    * [NUI] add GroupItems
    
    * [NUI] fix build error
    
    * [NUI] fix build error
    
    * [NUI] change property overridable.

--- src/Tizen.NUI.Components/Controls/RecyclerView/Item/RecyclerViewItem.cs
+++ src/Tizen.NUI.Components/Controls/RecyclerView/Item/RecyclerViewItem.cs
@@ -208,9 +208,37 @@ namespace Tizen.NUI.Components
         /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public bool IsRealized { get; internal set; }
-        internal bool IsHeader { get; set; }
-        internal bool IsFooter { get; set; }
-        internal bool IsPressed { get; set; } = false;
+
+        /// <summary>
+        /// State of Pressed.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool IsPressed { get; set; } = false;
+
+        /// <summary>
+        /// Boolean flag to check this item is header.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool IsHeader { get; set; }
+
+        /// <summary>
+        /// Boolean flag to check this item is footer.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool IsFooter { get; set; }
+
+        /// <summary>
+        /// Boolean flag to check this item is group header.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool IsGroupHeader { get; set; }
+
+        /// <summary>
+        /// Boolean flag to check this item is group footer.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool IsGroupFooter { get; set; }
+
 
         /// <summary>
         /// Called after a key event is received by the view that has had its focus set.
