b12198f5e0282c1fb7990c920c185b808f71d59f
jeremy-jang <35089715+jeremy-jang@users.noreply.github.com>
2023-04-25 08:22:41 +0900

    [Tdbc] Add Tizen.Data.Tdbc (#5145)
    * [Tdbc] Add Tizen.Data.Tdbc
    
    Provides a standard C# API for accessing various database for Tizen platform.
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Add a default sqlite driver
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Add enum OperationType
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix Tdbc
    
    - Fix doxygen comments
    - Fix nameing of interfaces
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix doxygen
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix Tdbc
    
    - Fix doxygen.
    - Remove rowid from RecordChangedEventArgs.
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix wrong implemented IEnumeratable
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix RecordChangedEventArgs
    
    Add a Record to event.
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix TDBC
    
    - Add namespace document.
    - Fix dictionary type of Statement class.
    - Fix some doxygen comments.
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Add remarks for privileges
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Fix Connection class
    
    - IsOpened() -> IsOpen()
    - Add readonly keyworkd at lock object
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    * [Tdbc] Rename files
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>
    
    ---------
    
    Signed-off-by: Sangyoon Jang <jeremy.jang@samsung.com>

---
+++ src/Tizen.Data.Tdbc/Tizen.Data.Tdbc.Driver.Sqlite/Interop/Interop.Sqlite.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    const string Lib = "libsqlite3.so.0";
+
+    internal static partial class Sqlite
+    {
+        internal static void Init()
+        {
+        }
+
+        [Flags]
+        internal enum OpenParameter : int
+        {
+            SQLITE_OPEN_READONLY = 0x00000001,
+            SQLITE_OPEN_READWRITE = 0x00000002,
+            SQLITE_OPEN_CREATE = 0x00000004,
+            SQLITE_OPEN_DELETEONCLOSE = 0x00000008,
+            SQLITE_OPEN_EXCLUSIVE = 0x00000010,
+            SQLITE_OPEN_AUTOPROXY = 0x00000020,
+            SQLITE_OPEN_URI = 0x00000040,
+            SQLITE_OPEN_MEMORY = 0x00000080,
+            SQLITE_OPEN_MAIN_DB = 0x00000100,
+            SQLITE_OPEN_TEMP_DB = 0x00000200,
+            SQLITE_OPEN_TRANSIENT_DB = 0x00000400,
+            SQLITE_OPEN_MAIN_JOURNAL = 0x00000800,
+            SQLITE_OPEN_TEMP_JOURNAL = 0x00001000,
+            SQLITE_OPEN_SUBJOURNAL = 0x00002000,
+            SQLITE_OPEN_MASTER_JOURNAL = 0x00004000,
+            SQLITE_OPEN_NOMUTEX = 0x00008000,
+            SQLITE_OPEN_FULLMUTEX = 0x00010000,
+            SQLITE_OPEN_SHAREDCACHE = 0x00020000,
+            SQLITE_OPEN_PRIVATECACHE = 0x00040000,
+            SQLITE_OPEN_WAL = 0x00080000
+        }
+
+        internal enum ResultCode : int
+        {
+            SQLITE_OK = 0,
+            SQLITE_ERROR = 1,
+            SQLITE_INTERNAL = 2,
+            SQLITE_PERM = 3,
+            SQLITE_ABORT = 4,
+            SQLITE_BUSY = 5,
+            SQLITE_LOCKED = 6,
+            SQLITE_NOMEM = 7,
+            SQLITE_READONLY = 8,
+            SQLITE_INTERRUPT = 9,
+            SQLITE_IOERR = 10,
+            SQLITE_CORRUPT = 11,
+            SQLITE_NOTFOUND = 12,
+            SQLITE_FULL = 13,
+            SQLITE_CANTOPEN = 14,
+            SQLITE_PROTOCOL = 15,
+            SQLITE_EMPTY = 16,
+            SQLITE_SCHEMA = 17,
+            SQLITE_TOOBIG = 18,
+            SQLITE_CONSTRAINT = 19,
+            SQLITE_MISMATCH = 20,
+            SQLITE_MISUSE = 21,
+            SQLITE_NOLFS = 22,
+            SQLITE_AUTH = 23,
+            SQLITE_FORMAT = 24,
+            SQLITE_RANGE = 25,
+            SQLITE_NOTADB = 26,
+            SQLITE_NOTICE = 27,
+            SQLITE_WARNING = 28,
+            SQLITE_ROW = 100,
+            SQLITE_DONE = 101
+        }
+
+        //int sqlite3_open_v2(const char* filename, sqlite3** ppDb, int flags, const char* zVfs)
+        [DllImport(Lib, EntryPoint = "sqlite3_open_v2")]
+        internal static extern int OpenV2(string filename, out IntPtr ppDb, int flags, IntPtr zVfs);
+
+        //int sqlite3_close_v2(sqlite3*)
+        [DllImport(Lib, EntryPoint = "sqlite3_close_v2")]
+        internal static extern int Close(IntPtr pDb);
+
+        //int sqlite3_prepare_v2(sqlite3* db, const char* zSql, int nByte, sqlite3_stmt** ppStmt, const char** pzTail)
+        [DllImport(Lib, EntryPoint = "sqlite3_prepare_v2")]
+        internal static extern int Prepare(IntPtr pDb, string zSql, int nByte, out IntPtr ppStmt, IntPtr pzTail);
+
+        //int sqlite3_bind_text(sqlite3_stmt*, int,const char*,int,void (*) (void*));
+        [DllImport(Lib, EntryPoint = "sqlite3_bind_text")]
+        internal static extern int BindText(IntPtr stmt, int pos, string text, int size, IntPtr mode);
+
+        //int sqlite3_bind_int(sqlite3_stmt*, int, int);
+        [DllImport(Lib, EntryPoint = "sqlite3_bind_int")]
+        internal static extern int BindInt(IntPtr stmt, int pos, int val);
+
+        //int sqlite3_bind_double(sqlite3_stmt*, int, double);
+        [DllImport(Lib, EntryPoint = "sqlite3_bind_double")]
+        internal static extern int BindDouble(IntPtr stmt, int pos, double val);
+
+        //int sqlite3_bind_blob(sqlite3_stmt*, int, const void*, int n, void (*) (void*));
+        [DllImport(Lib, EntryPoint = "sqlite3_bind_blob")]
+        internal static extern int BindData(IntPtr stmt, int pos, byte[] val, int size, IntPtr mode);
+
+        //int sqlite3_bind_parameter_index(sqlite3_stmt*, const char *zName);
+        [DllImport(Lib, EntryPoint = "sqlite3_bind_parameter_index")]
+        internal static extern int GetParameterIndex(IntPtr stmt, string zName);
+
+        //int sqlite3_step(sqlite3_stmt*)
+        [DllImport(Lib, EntryPoint = "sqlite3_step")]
+        internal static extern int Step(IntPtr stmt);
+
+        //int sqlite3_finalize(sqlite3_stmt *pStmt);
+        [DllImport(Lib, EntryPoint = "sqlite3_finalize")]
+        internal static extern int Finalize(IntPtr stmt);
+
+        //int sqlite3_changes(sqlite3*)
+        [DllImport(Lib, EntryPoint = "sqlite3_changes")]
+        internal static extern int Changes(IntPtr db);
+
+        //const unsigned char *sqlite3_column_text(sqlite3_stmt*, int iCol);
+        [DllImport(Lib, EntryPoint = "sqlite3_column_text")]
+        internal static extern IntPtr ColumnText(IntPtr stmt, int col);
+
+        //int sqlite3_column_int(sqlite3_stmt*, int iCol);
+        [DllImport(Lib, EntryPoint = "sqlite3_column_int")]
+        internal static extern int ColumnInt(IntPtr stmt, int col);
+
+        //double sqlite3_column_double(sqlite3_stmt*, int iCol);
+        [DllImport(Lib, EntryPoint = "sqlite3_column_double")]
+        internal static extern double ColumnDouble(IntPtr stmt, int col);
+
+        //const void* sqlite3_column_blob(sqlite3_stmt *, int iCol);
+        [DllImport(Lib, EntryPoint = "sqlite3_column_blob")]
+        internal static extern IntPtr ColumnBlob(IntPtr stmt, int col);
+
+        //int sqlite3_reset(sqlite3_stmt* pStmt);
+        [DllImport(Lib, EntryPoint = "sqlite3_reset")]
+        internal static extern int Reset(IntPtr stmt);
+
+        //int sqlite3_column_bytes(sqlite3_stmt*, int iCol);
+        [DllImport(Lib, EntryPoint = "sqlite3_column_bytes")]
+        internal static extern int ColumnBytes(IntPtr stmt, int col);
+
+        internal enum UpdateHookAction : int
+        {
+            SQLITE_DELETE = 9,
+            SQLITE_INSERT = 18,
+            SQLITE_UPDATE = 23
+        }
+
+        internal delegate void UpdateHookCallback(IntPtr data, int action, string db_name, string table_name, long rowid);
+
+        //void* sqlite3_update_hook(sqlite3*, void(*)(void* data, int action, char const* db_name, char const* table_name, sqlite3_int64 rowid), void* data);
+        [DllImport(Lib, EntryPoint = "sqlite3_update_hook")]
+        internal static extern IntPtr UpdateHook(IntPtr db, UpdateHookCallback cb, IntPtr data);
+    }
+}
---
+++ src/Tizen.Data.Tdbc/Tizen.Data.Tdbc/IRecord.cs
@@ -0,0 +1,163 @@
+﻿/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Tizen.Data.Tdbc
+{
+    /// <summary>
+    /// Record class. This class provides the result of query.
+    /// </summary>
+    /// <since_tizen> 11 </since_tizen>
+    public interface IRecord : IEnumerator<IRecord>
+    {
+        /// <summary>
+        /// Get integer type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        int GetInt(int columnIndex);
+
+        /// <summary>
+        /// Get string type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        string GetString(int columnIndex);
+
+        /// <summary>
+        /// Get double type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        double GetDouble(int columnIndex);
+
+        /// <summary>
+        /// Get bool type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        bool GetBool(int columnIndex);
+
+        /// <summary>
+        /// Get blob type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        byte[] GetData(int columnIndex);
+
+        /// <summary>
+        /// Get char type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        char GetChar(int columnIndex);
+
+        /// <summary>
+        /// Get chars type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        char[] GetChars(int columnIndex);
+
+        /// <summary>
+        /// Get date type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        DateTime GetDate(int columnIndex);
+
+        /// <summary>
+        /// Get datetime type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        DateTime GetDateTime(int columnIndex);
+
+        /// <summary>
+        /// Get decimal type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        Decimal GetDecimal(int columnIndex);
+
+        /// <summary>
+        /// Get float type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        float GetFloat(int columnIndex);
+
+        /// <summary>
+        /// Get 16-bit signed integer type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        short GetInt16(int columnIndex);
+
+        /// <summary>
+        /// Get 64-bit signed integer type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        long GetInt64(int columnIndex);
+
+        /// <summary>
+        /// Get time type value from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        TimeSpan GetTime(int columnIndex);
+
+        /// <summary>
+        /// Get the name from the record with given index.
+        /// </summary>
+        /// <param name="columnIndex">The index of value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">The given index is out of range.</exception>
+        /// <exception cref="NotSupportedException">This type is not supported.</exception>
+        /// <since_tizen> 11 </since_tizen>
+        string GetName(int columnIndex);
+    }
+}
