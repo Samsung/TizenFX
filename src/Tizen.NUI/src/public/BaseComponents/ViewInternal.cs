68c487ff3804246c5fb30ccdef9e8c158b36349f
Jiyun Yang <ji.yang@samsung.com>
2023-05-23 18:04:27 +0900

    [NUI] Fix theme applied issue
    Component style can change even if the platform theme is disabled
    
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/ViewInternal.cs
+++ src/Tizen.NUI/src/public/BaseComponents/ViewInternal.cs
@@ -1580,7 +1580,7 @@ namespace Tizen.NUI.BaseComponents
             }
 
             // Listen theme change event if needs.
-            if (ThemeManager.PlatformThemeEnabled && initialStyle != null)
+            if (initialStyle != null)
             {
                 SetThemeApplied();
             }
