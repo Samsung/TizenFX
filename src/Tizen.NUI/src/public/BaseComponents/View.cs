df21e405c3fd729477fa251a16a55621636d318f
joogab.yun <joogab.yun@samsung.com>
2023-06-09 11:12:27 +0900

    [NUI] Fix incorrect calculation of configGestureCount.

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -3559,7 +3559,7 @@ namespace Tizen.NUI.BaseComponents
 
             if (dispatch == true)
             {
-                configGestureCount = configGestureCount > 0 ? configGestureCount-- : 0;
+                configGestureCount = configGestureCount > 0 ? configGestureCount - 1 : 0;
                 if (configGestureCount == 0)
                 {
                     panGestureDetector.Detach(this);
@@ -3573,12 +3573,6 @@ namespace Tizen.NUI.BaseComponents
                     pinchGestureDetector.Detected -= OnGestureDetected;
                     tapGestureDetector.Detected -= OnGestureDetected;
                     rotationGestureDetector.Detected -= OnGestureDetected;
-
-                    panGestureDetector = null;
-                    longGestureDetector = null;
-                    pinchGestureDetector = null;
-                    tapGestureDetector = null;
-                    rotationGestureDetector = null;
                 }
             }
             else
