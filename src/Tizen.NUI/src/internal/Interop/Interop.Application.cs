30b593ae6366044eb65d39e404da74c5733ffa75
dongsug.song <dongsug.song@samsung.com>
2023-09-26 18:39:21 +0900

    [NUI] fix IME window crash issue

--- src/Tizen.NUI/src/internal/Interop/Interop.Application.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.Application.cs
@@ -90,7 +90,7 @@ namespace Tizen.NUI
             public static extern global::System.IntPtr New(int jarg1, string jarg3, int jarg4, global::System.Runtime.InteropServices.HandleRef jarg5);
 
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Application_New__SWIG_5")]
-            public static extern global::System.IntPtr New(int jarg1, string jarg3, int jarg4, global::System.Runtime.InteropServices.HandleRef jarg5, int jarg6);
+            public static extern global::System.IntPtr New(int argc, string argv, string styleSheet, int windowMode, global::System.Runtime.InteropServices.HandleRef initRectangle, int windowType);
 
             [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_Application_New__SWIG_6")]
             public static extern global::System.IntPtr New(int argc, string argv, string jarg3, int jarg4, global::System.Runtime.InteropServices.HandleRef jarg5, bool jarg7);
