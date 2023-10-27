636bdd791923801e72285f45020367c0a3c15362
Eunki, Hong <eunkiki.hong@samsung.com>
2023-10-27 18:47:55 +0900

    [NUI] Resolve DisposeRecursively crashed when we dispose scrollbase
    Due to some app's implements, ContentView can be disposed before ScrollBase
    itself disposed. In this case, ContentView is not null.
    
    So, when we try to call RemovePropertyNotification, it become crashed.
    
    To avoid these cases, let we make DisposeRecursively API makes more safety.
    
    And also, fix some crash issue in PerformanceTest1Page.xaml.cs what
    can be problem in future.
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI/src/public/Common/Container.cs
+++ src/Tizen.NUI/src/public/Common/Container.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Tizen.NUI.BaseComponents;
 using Tizen.NUI.Binding;
 using Tizen.NUI.Binding.Internals;
@@ -249,18 +250,24 @@ namespace Tizen.NUI
         public void DisposeRecursively()
         {
             // To avoid useless "OnChildRemoved" callback invoke, Dispose itself before children.
-            if(!Disposed && !IsDisposeQueued)
+            if (!Disposed && !IsDisposeQueued)
             {
                 Dispose();
             }
 
-            foreach (View child in Children)
-            {
-                child.DisposeRecursively();
-            }
+            // Copy child referecen to avoid Children changed during DisposeRecursively();
+            var copiedChildren = childViews.ToList();
 
             // Make sure that itself don't have children anymore.
             childViews?.Clear();
+
+            foreach (View child in copiedChildren)
+            {
+                if (!(child?.Disposed ?? true))
+                {
+                    child.DisposeRecursively();
+                }
+            }
         }
 
         /// <summary>
