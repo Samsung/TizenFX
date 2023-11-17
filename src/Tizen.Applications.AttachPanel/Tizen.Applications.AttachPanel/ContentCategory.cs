16f49be672c4a112478800dd0b5990a05c81f525
tarunkumarmahay1999 <113899126+tarunkumarmahay1999@users.noreply.github.com>
2023-11-17 10:39:10 +0530

    Revert "Deprecating Attach Panel APIs (#4591)"
    This reverts commit b515cba3897ff07d0b25849a36558d2549d64905.

--- src/Tizen.Applications.AttachPanel/Tizen.Applications.AttachPanel/ContentCategory.cs
+++ src/Tizen.Applications.AttachPanel/Tizen.Applications.AttachPanel/ContentCategory.cs
@@ -1,12 +1,9 @@
-using System;
-
 namespace Tizen.Applications.AttachPanel
 {
     /// <summary>
     /// Enumeration for the attach panel content category.
     /// </summary>
     /// <since_tizen> 4 </since_tizen>
-    [Obsolete("Deprecated since API Level 10. Will be removed in API Level 12.")]
     public enum ContentCategory
     {
         /// <summary>
