0cb799167b61395984ee1da9d647f326aad852fa
Eunki, Hong <eunkiki.hong@samsung.com>
2024-01-02 18:09:36 +0900

    [NUI.Scene3D] Add AddLight API
    Let we make new API for Light class for SceneView.
    
    Note : To avoid the changness of native side code minimalize, let we use
    internal API of NUI.
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI.Scene3D/src/internal/Interop/Interop.Libraries.cs
+++ src/Tizen.NUI.Scene3D/src/internal/Interop/Interop.Libraries.cs
@@ -19,6 +19,7 @@ namespace Tizen.NUI.Scene3D
     {
         internal static partial class Libraries
         {
+            public const string Lib = "libdali2-csharp-binder.so";
             public const string Scene3D = "libdali2-csharp-binder-scene3d.so";
         }
     }
