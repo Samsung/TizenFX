1b0680b5d2ebe57d37e0d05bcfb5327dc2141e0d
Eunki, Hong <eunkiki.hong@samsung.com>
2024-01-29 12:39:19 +0900

    [NUI] Allow to use null rootView for FrameUpdateCallback + Let we use null rootView if we use default API
    Let we use some global view controllable frameUpdateCallback when
    user skip to add RootView information.
    
    Previously, we detach the frameUpdateCallback automatically if rootView was disposed.
    But now, we will never remove callback until user call RemoveFrameUpdateCallback.
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/FrameUpdateCallbackTest.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/FrameUpdateCallbackTest.cs
@@ -396,6 +396,27 @@ namespace Tizen.NUI.Samples
 
         public void Deactivate()
         {
+            if(contentsView != null)
+            {
+                contentsView.Unparent();
+                contentsView.Dispose();
+            }
+            if(baseView != null)
+            {
+                baseView.Unparent();
+                baseView.Dispose();
+            }
+
+            if(frameUpdateCallback != null)
+            {
+                window.RenderingBehavior = RenderingBehaviorType.IfRequired;
+                window.RemoveFrameUpdateCallback(frameUpdateCallback);
+                animationOffTimer?.Stop();
+                animationState = TOUCH_ANIMATION_STATE.END_ANIMATION;
+
+                frameUpdateCallback = null;
+                animationOffTimer?.Dispose();
+            }
         }
 
         void Initialize()
@@ -564,7 +585,11 @@ namespace Tizen.NUI.Samples
             // Add frame callback on window.
             // OnUpdate callback of frameUpdateCallback will be called before every render frame.
             // We can set root view what given frameUpdateCallback used
+            // Or, we can skip root view as null if we don't want to make frameUpdateCallback don't have dependency with some view.
+            // For this sample, let we use controlView as root view.
+
             window.AddFrameUpdateCallback(frameUpdateCallback, controlView);
+            // window.AddFrameUpdateCallback(frameUpdateCallback, null);
 
             // compute limit position the container could go.
             leftDirectionLimit = (float)window.Size.Width - (totalSize + (float)(INITIAL_POSITION));
