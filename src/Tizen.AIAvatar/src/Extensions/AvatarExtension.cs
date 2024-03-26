5eedb69f4ab01a70d99fa25f4a3276d1b5d52a09
huiyu <35286162+huiyueun@users.noreply.github.com>
2024-03-26 16:04:42 +0900

    Add Tizen.AIAvatar project (#6014)
    * Add Tizen.AIAvatar project
    
    Add new empty project of Tizen.AIAvatar
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Add hidden
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Modify apis
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * Make internal
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    * remove lip module
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>
    
    ---------
    
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

---
+++ src/Tizen.AIAvatar/src/Extensions/AvatarExtension.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Copyright(c) 2023 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System.Collections.Generic;
+using System.IO;
+
+using static Tizen.AIAvatar.AIAvatar;
+
+namespace Tizen.AIAvatar
+{
+    internal static class AvatarExtension
+    {
+        internal static List<AvatarInfo> GetDefaultAvatarList()
+        {
+            var list = new List<AvatarInfo>();
+            var avatarModelFolders = Directory.GetDirectories(ApplicationResourcePath + EmojiAvatarResourcePath);
+            foreach (var directoryInfo in avatarModelFolders)
+            {
+                Log.Info(LogTag, $"Directory Path : {directoryInfo}");
+                var avatarInfo = new AvatarInfo(directoryInfo);
+                list.Add(avatarInfo);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ src/Tizen.AIAvatar/src/internal/LipSync/Models/SoftmaxLinqExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tizen.AIAvatar
+{
+    internal static class SoftmaxLinqExtension
+    {
+        internal static IEnumerable<float> SoftMax(this IEnumerable<float> source)
+        {
+            var exp = source.Select(x => MathF.Exp(x)).ToArray();
+            var sum = exp.Sum();
+            return exp.Select(x => x / sum);
+        }
+    }
+}
