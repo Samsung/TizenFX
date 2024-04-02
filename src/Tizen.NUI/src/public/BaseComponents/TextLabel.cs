2002e39a7e11e7183f98439718df1086de7ea886
Chihun Jeong <50663828+ANZ1217@users.noreply.github.com>
2024-04-02 12:36:30 +0900

    [NUI] Introduce RemoveFrontInset, RemoveBackInset property (#6001)
    * Bind REMOVE_FRONT_INSET, REMOVE_BACK_INSET property
    
    * Remove unused Interop functions
    
    * Set bindable property as hidden
    
    ---------
    
    Co-authored-by: ANZ1217 <chihun.jeong@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/TextLabel.cs
+++ src/Tizen.NUI/src/public/BaseComponents/TextLabel.cs
@@ -1598,6 +1598,48 @@ namespace Tizen.NUI.BaseComponents
         }
 
         /// <summary>
+        /// The RemoveFrontInset property.<br />
+        /// This property is used when the xBearing of first glyph must not be trimmed.<br />
+        /// When set to false, The gap between (0, 0) from the first glyph's leftmost pixel is included in the width of text label.
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool RemoveFrontInset
+        {
+            get
+            {
+                return (bool)GetValue(RemoveFrontInsetProperty);
+            }
+            set
+            {
+                SetValue(RemoveFrontInsetProperty, value);
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The RemoveBackInset property.<br />
+        /// This property is used when the advance of last glyph must not be trimmed.<br />
+        /// When set to false, The gap between the last glyph's rightmost pixel and X coordinate that next glyph will be placed is included in the width of text label.
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool RemoveBackInset
+        {
+            get
+            {
+                return (bool)GetValue(RemoveBackInsetProperty);
+            }
+            set
+            {
+                SetValue(RemoveBackInsetProperty, value);
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
         /// The FontSizeScale property for scaling the specified font size up or down. <br />
         /// The default value is 1.0. <br />
         /// The given font size scale value is used for multiplying the specified font size before querying fonts. <br />
@@ -1904,6 +1946,8 @@ namespace Tizen.NUI.BaseComponents
             internal static readonly int CharacterSpacing = Interop.TextLabel.CharacterSpacingGet();
             internal static readonly int AnchorColor = Interop.TextLabel.AnchorColorGet();
             internal static readonly int AnchorClickedColor = Interop.TextLabel.AnchorClickedColorGet();
+            internal static readonly int RemoveFrontInset = Interop.TextLabel.RemoveFrontInsetGet();
+            internal static readonly int RemoveBackInset = Interop.TextLabel.RemoveBackInsetGet();
 
 
             internal static void Preload()
