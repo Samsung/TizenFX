fadf4e3ecee80cc8ce37882f1500ec1dd81942f9
Wonsik Jung <sidein@samsung.com>
2024-06-10 12:38:49 +0900

    [NUI] Supports video sychronization for video player

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/VideoViewTest.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/VideoViewTest.cs
@@ -29,6 +29,19 @@ namespace Tizen.NUI.Samples
         string resourcePath;
         const string tag = "NUITEST";
         View dummy = null;
+        Animation animation;
+
+        private const string KEY_NUM_1 = "1";
+        private const string KEY_NUM_2 = "2";
+        private const string KEY_NUM_3 = "3";
+        private const string KEY_NUM_4 = "4";
+        private const string KEY_NUM_5 = "5";
+        private const string KEY_NUM_6 = "6";
+        private const string KEY_NUM_7 = "7";
+        private const string KEY_NUM_8 = "8";
+        private const string KEY_NUM_9 = "9";
+        private const string KEY_NUM_0 = "0";
+
         public void Activate()
         {
             win = NUIApplication.GetDefaultWindow();
@@ -116,42 +129,74 @@ namespace Tizen.NUI.Samples
         VideoView videoView;
         public void NUIVideoViewTest()
         {
-            videoView = new VideoView();
+            videoView = new VideoView(1);
             videoView.ResourceUrl = resourcePath;
             videoView.Looping = true;
             videoView.Size = new Size(300, 300);
             videoView.PositionUsesPivotPoint = true;
             videoView.ParentOrigin = ParentOrigin.Center;
             videoView.PivotPoint = PivotPoint.Center;
+            videoView.Underlay = true;
             win.Add(videoView);
 
-            try
-            {
-                var playerHandle = videoView.NativeHandle;
-                player = new myPlayer(playerHandle.DangerousGetHandle());
-            }
-            catch( Exception e)
-            {
-                if (e is global::System.ArgumentException)
-                {
-                    Tizen.Log.Fatal("NUI", $"[ERROR] could not get NativePlayerHandle!");
-                }
-            }
-            if (player != null)
-            {
-                player.Start();
-            }
+            animation = new Animation(1000);
+
+//            try
+//            {
+//                var playerHandle = videoView.NativeHandle;
+//                player = new myPlayer(playerHandle.DangerousGetHandle());
+//            }
+//            catch( Exception e)
+//            {
+//                if (e is global::System.ArgumentException)
+//                {
+//                    Tizen.Log.Fatal("NUI", $"[ERROR] could not get NativePlayerHandle!");
+//                }
+//            }
+//            if (player != null)
+//            {
+//                player.Start();
+//            }
         }
 
         private void Win_KeyEvent(object sender, Window.KeyEventArgs e)
         {
-            if (e.Key.State == Key.StateType.Down)
+            switch (e.Key.KeyPressedName)
             {
-                tlog.Fatal(tag, $"key pressed name={e.Key.KeyPressedName}");
-                if (e.Key.KeyPressedName == "XF86Back")
-                {
+                case "XF86Back":
+                case "Escape":
+                   tlog.Fatal(tag, $"key pressed name={e.Key.KeyPressedName}");
+                   if (e.Key.KeyPressedName == "XF86Back")
+                   {
                     Deactivate();
-                }
+                   }
+                   break;
+                case KEY_NUM_0:
+                   tlog.Fatal(tag, $"videoView Play!!");
+                   videoView.Play();
+                   break;
+
+                case KEY_NUM_1:
+                   tlog.Fatal(tag, $"Video is moving!!");
+                   Vector3 targetSize = new Vector3(0.0f, 400.0f, 0.0f );
+                   animation.Stop();
+                   animation.Clear();
+                   animation.AnimateTo(videoView, "Size", targetSize);
+                   videoView.PlayAnimation(animation);
+
+                   break;
+
+                case KEY_NUM_2:
+                   break;
+
+                case KEY_NUM_3:
+                   break;
+
+                case KEY_NUM_4:
+                   break;
+
+                case KEY_NUM_5:
+                   break;
             }
         }
 
