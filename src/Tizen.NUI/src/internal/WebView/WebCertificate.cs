39d72f7b36f22c1ae57f4d62bf86199763fcf491
dongsug.song <dongsug.song@samsung.com>
2024-08-05 21:58:58 +0900

    [NUI] Add WebView Certificate policy decision error get and suspend

--- src/Tizen.NUI/src/internal/WebView/WebCertificate.cs
+++ src/Tizen.NUI/src/internal/WebView/WebCertificate.cs
@@ -90,5 +90,51 @@ namespace Tizen.NUI
             Interop.WebCertificate.Allow(SwigCPtr, allowed);
             if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
         }
+
+        /// <summary>
+        /// Get certificate policy decision error.
+        /// </summary>
+        /// <value>PolicyDecisionErrorType</value>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public PolicyDecisionErrorType PolicyDecisionError
+        {
+            get
+            {
+                int result = Interop.WebCertificate.GetPolicyDecisionError(SwigCPtr);
+                if (NDalicPINVOKE.SWIGPendingException.Pending) return PolicyDecisionErrorType.Unknown;
+                return (PolicyDecisionErrorType)result;
+            }
+        }
+
+        /// <summary>
+        /// Suspend certification policy decision.
+        /// </summary>
+        /// <returns></returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public bool Suspend()
+        {
+            bool ret = Interop.WebCertificate.SuspendPolicyDecision(SwigCPtr);
+            if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
+            return ret;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public enum PolicyDecisionErrorType
+        {
+            CommonNameInvalid, //The server responded with a certificate whose common name did not match the host name.
+            DateInvalid, //The server responded with a certificate that by our clock, appears to either not yet be valid or to have expired.
+            AuthorityInvalid, // The server responded with a certificate that is signed by an authority we don't trust.
+            NoRevocationMechanism, // The certificate has no mechanism for determining if it is revoked. In effect, this certificate cannot be revoked.
+            UnableToCheckRevocation, // Revocation information for the security certificate for this site is not available.
+            Revoked, // The server responded with a certificate that has been revoked.
+            Invalid, // The server responded with a certificate that is invalid.
+            WeakAlgorithm, // The server responded with a certificate that is signed using a weak signature algorithm.
+            NonUniqueName, // The host name specified in the certificate is not unique.
+            WeakKey, // The server responded with a certificate that contains a weak key (e.g. a too-small RSA key).
+            PinnedKeyNotInChain, // The certificate didn't match the public key pins for the host name.
+            NameViolation, // The certificate claimed DNS names that are in violation of name constraints.
+            ValidityTooLong, // The certificate's validity period is too long.
+            Unknown = 1000 // Reported in case of wrong usage of ewk_certificate_policy_decision_error_get API.
+        }
     }
 }
