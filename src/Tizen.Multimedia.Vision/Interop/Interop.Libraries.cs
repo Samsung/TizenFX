e7acef93057281889e7e3b3c8b8fbe5c364c0dea
Haesu Gwon <haesu.gwon@samsung.com>
2024-09-19 11:26:21 +0900

    [MediaVision] Add new inference APIs (#6316)
    * [MediaVision] Add new inference APIs

--- src/Tizen.Multimedia.Vision/Interop/Interop.Libraries.cs
+++ src/Tizen.Multimedia.Vision/Interop/Interop.Libraries.cs
@@ -20,12 +20,17 @@ internal static partial class Interop
     {
         public const string MediaVisionCommon = "libmv_common.so";
         public const string MediaVisionFace = "libmv_face.so";
-        public const string MediaVisionInference = "libmv_inference.so";
         public const string MediaVisionImage = "libmv_image.so";
         public const string MediaVisionSurveillance = "libmv_surveillance.so";
         public const string MediaVisionBarcodeDetector = "libmv_barcode_detector.so";
         public const string MediaVisionBarcodeGenerator = "libmv_barcode_generator.so";
         public const string MediaVisionRoiTracker = "libmv_roi_tracker.so";
         public const string MediaVisionFaceRecognition = "libmv_face_recognition.so"; // It's based on machine learning
+        public const string MediaVisionInference = "libmv_inference.so";
+        public const string MediaVisionInferenceImageClassification = "libmv_image_classification.so"; // Inference image classification
+        public const string MediaVisionInferenceObjectDetection = "libmv_object_detection.so";
+        public const string MediaVisionInferenceFaceDetection = MediaVisionInferenceObjectDetection; // Inference object detection and face detection
+        public const string MediaVisionInferenceFacialLandmarkDetection = "libmv_landmark_detection.so";
+        public const string MediaVisionInferencePoseLandmarkDetection = "libmv_landmark_detection.so"; // Inference facial landmark detection and pose landmark detection
     }
 }
