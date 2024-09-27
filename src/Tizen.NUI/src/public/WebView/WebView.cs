c0619f568ca0718ea71d4fadf27fac598ffe5e8c
Eunki, Hong <eunkiki.hong@samsung.com>
2024-09-27 17:41:39 +0900

    [NUI.Scene3D] Support SceneView CornerRadius/Borderline Property applied
    Relative dali patch :
    
    https://review.tizen.org/gerrit/c/platform/core/uifw/dali-toolkit/+/318377
    https://review.tizen.org/gerrit/c/platform/core/uifw/dali-csharp-binder/+/318385
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI/src/public/WebView/WebView.cs
+++ src/Tizen.NUI/src/public/WebView/WebView.cs
@@ -2982,8 +2982,8 @@ namespace Tizen.NUI.BaseComponents
             if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
             return ret;
         }
-
-        internal override void ApplyCornerRadius()
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected override void ApplyCornerRadius()
         {
             base.ApplyCornerRadius();
 
