fef2bab4e538451fd5e5ea7522019738d6ccc9a2
sukhyungkang <35091460+sukhyungkang@users.noreply.github.com>
2024-10-07 14:42:05 +0900

    [Notification] fix notification when pairing type is set (#6394)
    Signed-off-by: SukhyungKang <shine.kang@samsung.com>

--- src/Tizen.Applications.Notification/Tizen.Applications.Notifications/Notification.cs
+++ src/Tizen.Applications.Notification/Tizen.Applications.Notifications/Notification.cs
@@ -528,7 +528,7 @@ namespace Tizen.Applications.Notifications
                 Bundle bundle = new Bundle(new SafeBundleHandle(extension, false));
                 foreach (string key in bundle.Keys)
                 {
-                    if (key.StartsWith("_NOTIFICATION_EXTENSION_EVENT_"))
+                    if (key.StartsWith("_NOTIFICATION_EXTENSION_EVENT_") || key.StartsWith("_NOTIFICATION_TYPE_PAIRING_"))
                         continue;
 
                     SafeBundleHandle sbh;
