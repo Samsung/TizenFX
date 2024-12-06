0a40bdac9ce11c603a2896085ca4dc8a96b1ced8
Jiyun Yang <ji.yang@samsung.com>
2024-12-06 14:48:36 +0900

    [NUI] Access managed fields only when explicit view disposal
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/ViewInternal.cs
+++ src/Tizen.NUI/src/public/BaseComponents/ViewInternal.cs
@@ -1469,13 +1469,13 @@ namespace Tizen.NUI.BaseComponents
                     heightConstraint.Remove();
                     heightConstraint.Dispose();
                 }
+
+                DisConnectFromSignals();
             }
 
             //Release your own unmanaged resources here.
             //You should not access any managed member here except static instance.
             //because the execution order of Finalizes is non-deterministic.
-
-            DisConnectFromSignals();
 
             backgroundExtraDataUpdatedFlag = BackgroundExtraDataUpdatedFlag.None;
 
