38b534beb8f3728f93a0f257cb0f45ac644feeee
Jiyun Yang <ji.yang@samsung.com>
2025-01-17 19:03:04 +0900

    [NUI] Rename struct types
    Signed-off-by: Jiyun Yang <ji.yang@samsung.com>

--- src/Tizen.NUI/src/devel/Lite/UIState.cs
+++ src/Tizen.NUI/src/devel/Lite/UIState.cs
@@ -26,81 +26,81 @@ namespace Tizen.NUI
     /// Defines a value type of view state.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public readonly struct ViewState
+    public readonly struct UIState
     {
         /// <summary>
         /// The All state is used in a selector class. It represents all states, so if this state is defined in a selector, the other states are ignored.
         /// </summary>
-        public static readonly ViewState All = new (ControlStateUtility.FullMask);
+        public static readonly UIState All = new (ControlStateUtility.FullMask);
 
         /// <summary>
         /// Normal State.
         /// </summary>
-        public static readonly ViewState Normal = new (0UL);
+        public static readonly UIState Normal = new (0UL);
 
         /// <summary>
         /// Focused State.
         /// </summary>
-        public static readonly ViewState Focused =  new (nameof(Focused));
+        public static readonly UIState Focused =  new (nameof(Focused));
 
         /// <summary>
         /// Pressed State.
         /// </summary>
-        public static readonly ViewState Pressed = new (nameof(Pressed));
+        public static readonly UIState Pressed = new (nameof(Pressed));
 
         /// <summary>
         /// Disabled State.
         /// </summary>
-        public static readonly ViewState Disabled = new (nameof(Disabled));
+        public static readonly UIState Disabled = new (nameof(Disabled));
 
         /// <summary>
         /// Selected State.
         /// </summary>
-        public static readonly ViewState Selected = new (nameof(Selected));
+        public static readonly UIState Selected = new (nameof(Selected));
 
         /// <summary>
         /// Pressed caused by key state.
         /// </summary>
-        public static readonly ViewState PressedByKey = Pressed + new ViewState(nameof(PressedByKey));
+        public static readonly UIState PressedByKey = Pressed + new UIState(nameof(PressedByKey));
 
         /// <summary>
         /// Pressed caused by touch state.
         /// </summary>
-        public static readonly ViewState PressedByTouch = Pressed + new ViewState(nameof(PressedByTouch));
+        public static readonly UIState PressedByTouch = Pressed + new UIState(nameof(PressedByTouch));
 
         /// <summary>
         /// SelectedPressed State.
         /// </summary>
-        public static readonly ViewState SelectedPressed = Selected + Pressed;
+        public static readonly UIState SelectedPressed = Selected + Pressed;
 
         /// <summary>
         /// DisabledSelected State.
         /// </summary>
-        public static readonly ViewState DisabledSelected = Disabled + Selected;
+        public static readonly UIState DisabledSelected = Disabled + Selected;
 
         /// <summary>
         /// DisabledFocused State.
         /// </summary>
-        public static readonly ViewState DisabledFocused = Disabled + Focused;
+        public static readonly UIState DisabledFocused = Disabled + Focused;
 
         /// <summary>
         /// SelectedFocused State.
         /// </summary>
-        public static readonly ViewState SelectedFocused = Selected + Focused;
+        public static readonly UIState SelectedFocused = Selected + Focused;
 
         /// <summary>
         /// This is used in a selector class. It represents all other states except for states that are already defined in a selector.
         /// </summary>
-        public static readonly ViewState Other = new ViewState(nameof(Other));
+        public static readonly UIState Other = new UIState(nameof(Other));
 
         private readonly ulong bitFlags;
 
-        private ViewState(ulong bitMask)
+        private UIState(ulong bitMask)
         {
             bitFlags = bitMask;
         }
 
-        private ViewState(string name) : this(ControlStateUtility.Register(name))
+        private UIState(string name) : this(ControlStateUtility.Register(name))
         {
         }
 
@@ -110,15 +110,15 @@ namespace Tizen.NUI
         internal bool IsCombined => (bitFlags != 0UL) && ((bitFlags & (bitFlags - 1UL)) != 0UL);
 
         /// <summary>
-        /// Create an instance of the <see cref="ViewState"/> with state name.
+        /// Create an instance of the <see cref="UIState"/> with state name.
         /// </summary>
         /// <param name="name">The state name.</param>
-        /// <returns>The <see cref="ViewState"/> instance which has single state.</returns>
+        /// <returns>The <see cref="UIState"/> instance which has single state.</returns>
         /// <exception cref="ArgumentNullException">Thrown when the given name is null.</exception>
         /// <exception cref="ArgumentException">Thrown when the given name is invalid.</exception>
-        public static ViewState Create(string name)
+        public static UIState Create(string name)
         {
-            return new ViewState(name);
+            return new UIState(name);
         }
 
         /// <summary>
@@ -126,14 +126,14 @@ namespace Tizen.NUI
         /// </summary>
         /// <param name="state">The state to search for</param>
         /// <returns>true if the state contain a specified state, otherwise, false.</returns>
-        public bool Contains(ViewState state) => (bitFlags & state.bitFlags) == state.bitFlags;
+        public bool Contains(UIState state) => (bitFlags & state.bitFlags) == state.bitFlags;
 
         /// <summary>
         /// Checks if there is a intersection part between this and the other.
         /// </summary>
         /// <param name="other">The other state to check.</param>
         /// <returns>True if an intersection exists, otherwise false.</returns>
-        public bool HasIntersectionWith(ViewState other) => (bitFlags & other.bitFlags) != 0L;
+        public bool HasIntersectionWith(UIState other) => (bitFlags & other.bitFlags) != 0L;
 
         ///  <inheritdoc/>
         public override string ToString()
@@ -165,41 +165,41 @@ namespace Tizen.NUI
         /// <summary>
         /// Compares whether the two ControlStates are same or not.
         /// </summary>
-        /// <param name="lhs">A <see cref="ViewState"/> on the left hand side.</param>
-        /// <param name="rhs">A <see cref="ViewState"/> on the right hand side.</param>
+        /// <param name="lhs">A <see cref="UIState"/> on the left hand side.</param>
+        /// <param name="rhs">A <see cref="UIState"/> on the right hand side.</param>
         /// <returns>true if the ControlStates are equal; otherwise, false.</returns>
-        public static bool operator ==(ViewState lhs, ViewState rhs) => lhs.Equals(rhs);
+        public static bool operator ==(UIState lhs, UIState rhs) => lhs.Equals(rhs);
 
         /// <summary>
         /// Compares whether the two ControlStates are different or not.
         /// </summary>
-        /// <param name="lhs">A <see cref="ViewState"/> on the left hand side.</param>
-        /// <param name="rhs">A <see cref="ViewState"/> on the right hand side.</param>
+        /// <param name="lhs">A <see cref="UIState"/> on the left hand side.</param>
+        /// <param name="rhs">A <see cref="UIState"/> on the right hand side.</param>
         /// <returns>true if the ControlStates are not equal; otherwise, false.</returns>
-        public static bool operator !=(ViewState lhs, ViewState rhs) => !lhs.Equals(rhs);
+        public static bool operator !=(UIState lhs, UIState rhs) => !lhs.Equals(rhs);
 
         /// <summary>
         /// The addition operator.
         /// </summary>
-        /// <param name="lhs">A <see cref="ViewState"/> on the left hand side.</param>
-        /// <param name="rhs">A <see cref="ViewState"/> on the right hand side.</param>
-        /// <returns>The <see cref="ViewState"/> containing the result of the addition.</returns>
-        public static ViewState operator +(ViewState lhs, ViewState rhs) => new ViewState(lhs.bitFlags | rhs.bitFlags);
+        /// <param name="lhs">A <see cref="UIState"/> on the left hand side.</param>
+        /// <param name="rhs">A <see cref="UIState"/> on the right hand side.</param>
+        /// <returns>The <see cref="UIState"/> containing the result of the addition.</returns>
+        public static UIState operator +(UIState lhs, UIState rhs) => new (lhs.bitFlags | rhs.bitFlags);
 
         /// <summary>
         /// The substraction operator.
         /// </summary>
-        /// <param name="lhs">A <see cref="ViewState"/> on the left hand side.</param>
-        /// <param name="rhs">A <see cref="ViewState"/> on the right hand side.</param>
-        /// <returns>The <see cref="ViewState"/> containing the result of the substraction.</returns>
-        public static ViewState operator -(ViewState lhs, ViewState rhs) => new ViewState(lhs.bitFlags & ~(rhs.bitFlags));
+        /// <param name="lhs">A <see cref="UIState"/> on the left hand side.</param>
+        /// <param name="rhs">A <see cref="UIState"/> on the right hand side.</param>
+        /// <returns>The <see cref="UIState"/> containing the result of the substraction.</returns>
+        public static UIState operator -(UIState lhs, UIState rhs) => new (lhs.bitFlags & ~(rhs.bitFlags));
 
-        public bool Equals(ViewState other) => bitFlags == other.bitFlags;
+        public bool Equals(UIState other) => bitFlags == other.bitFlags;
 
         ///  <inheritdoc/>
         public override bool Equals(object obj)
         {
-            if (obj is ViewState otherState)
+            if (obj is UIState otherState)
             {
                 return Equals(otherState);
             }
