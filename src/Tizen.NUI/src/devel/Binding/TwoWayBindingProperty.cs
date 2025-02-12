82b8af111b2127b5cd9dcbcf4a25c08949d864a3
everLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2025-02-12 15:57:15 +0900

    [NUI] Introduce new binding style.
    The binding in NUI cause performance and memory issue,
    so we plan to deprecate them by setting IsUsingXaml disable.
    
    This patch is aim to introduce new style of binding,
    which do not causing performance and memory seriously,
    and user can add binding property on existing view class,
    so that we do not need to add every property to bindable.

---
+++ src/Tizen.NUI/src/devel/Binding/TwoWayBindingProperty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Tizen.NUI.Binding
+{
+    /// <summary>
+    /// This class represents a two-way binding property between a view and its value.
+    /// </summary>
+    /// <typeparam name="TView">The type of the view.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class TwoWayBindingProperty<TView, TValue> : BindingProperty<TView, TValue>
+    {
+        /// <summary>
+        /// Gets or sets the function that retrieves the value from the view.
+        /// </summary>
+        public Func<TView, TValue> Getter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action that adds an observer to the view.
+        /// </summary>
+        public Action<TView, Action> AddObserver { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action that removes an observer from the view.
+        /// </summary>
+        public Action<TView, Action> RemoveObserver { get; set; }
+    }
+}
