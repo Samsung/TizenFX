32d6730fbcc7f1ee3c16c0a935baa00e34a940c6
huayong.xu <huayong.xu@samsung.com>
2025-02-25 17:43:31 +0800

    [NUI] Fix issue that disposed PropertyMap is returned in View.

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -1893,7 +1893,9 @@ namespace Tizen.NUI.BaseComponents
 
         private PropertyMap GetInternalTooltip()
         {
-            using PropertyMap temp = new PropertyMap();
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            PropertyMap temp = new PropertyMap();
+#pragma warning restore CA2000 // Dispose objects before losing scope
             using var pv = Object.GetProperty(SwigCPtr, Property.TOOLTIP);
             pv.Get(temp);
             return temp;
@@ -3900,7 +3902,9 @@ namespace Tizen.NUI.BaseComponents
 
         private Rotation GetInternalOrientation()
         {
-            using Rotation temp = new Rotation();
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            Rotation temp = new Rotation();
+#pragma warning restore CA2000 // Dispose objects before losing scope
             using var pv = Object.GetProperty(SwigCPtr, Property.ORIENTATION);
             pv.Get(temp);
             return temp;
