150ac9c85921d3f6fa600913e33d7e1aa97e3b9f
Jaehyun Cho <jae_hyun.cho@samsung.com>
2025-04-07 12:48:44 +0900

    Revert "[NUI] Add OverScrolled in WebView"
    This reverts commit f9044a6e2a3bd4e9998f03e66e5e498e6b1d6ddd.

--- src/Tizen.NUI/src/public/WebView/WebView.cs
+++ src/Tizen.NUI/src/public/WebView/WebView.cs
@@ -49,9 +49,6 @@ namespace Tizen.NUI.BaseComponents
         private EventHandler<WebViewScrollEdgeReachedEventArgs> scrollEdgeReachedEventHandler;
         private WebViewScrollEdgeReachedCallback scrollEdgeReachedCallback;
 
-        private EventHandler<WebViewOverScrolledEventArgs> overScrolledEventHandler;
-        private WebViewOverScrolledCallback overScrolledCallback;
-
         private EventHandler<WebViewUrlChangedEventArgs> urlChangedEventHandler;
         private WebViewUrlChangedCallback urlChangedCallback;
 
@@ -267,9 +264,6 @@ namespace Tizen.NUI.BaseComponents
         private delegate void WebViewScrollEdgeReachedCallback(int edge);
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
-        private delegate void WebViewOverScrolledCallback(int overscrolled);
-
-        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         private delegate void WebViewUrlChangedCallback(string pageUrl);
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
@@ -456,34 +450,6 @@ namespace Tizen.NUI.BaseComponents
                 {
                     IntPtr ip = IntPtr.Zero;
                     Interop.WebView.RegisterScrollEdgeReachedCallback(SwigCPtr, new HandleRef(this, ip));
-                }
-            }
-        }
-
-        /// <summary>
-        /// Event for the OverScrolled signal which can be used to subscribe or unsubscribe the event handler.<br />
-        /// This signal is emitted when web view is over scrolled.<br />
-        /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public event EventHandler<WebViewOverScrolledEventArgs> OverScrolled
-        {
-            add
-            {
-                if (overScrolledEventHandler == null)
-                {
-                    overScrolledCallback = OnOverScrolled;
-                    IntPtr ip = Marshal.GetFunctionPointerForDelegate(overScrolledCallback);
-                    Interop.WebView.RegisterOverScrolledCallback(SwigCPtr, new HandleRef(this, ip));
-                }
-                overScrolledEventHandler += value;
-            }
-            remove
-            {
-                overScrolledEventHandler -= value;
-                if (overScrolledEventHandler == null)
-                {
-                    IntPtr ip = IntPtr.Zero;
-                    Interop.WebView.RegisterOverScrolledCallback(SwigCPtr, new HandleRef(this, ip));
                 }
             }
         }
@@ -2548,11 +2514,6 @@ namespace Tizen.NUI.BaseComponents
         private void OnScrollEdgeReached(int edge)
         {
             scrollEdgeReachedEventHandler?.Invoke(this, new WebViewScrollEdgeReachedEventArgs((WebViewScrollEdgeReachedEventArgs.Edge)edge));
-        }
-
-        private void OnOverScrolled(int over)
-        {
-            overScrolledEventHandler?.Invoke(this, new WebViewOverScrolledEventArgs((WebViewOverScrolledEventArgs.Over)over));
         }
 
         private void OnUrlChanged(string pageUrl)
