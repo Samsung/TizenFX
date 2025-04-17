725384f5db724211709f7f695ffc044e87bc002a
dongkwan <dk3776.kim@samsung.com>
2025-04-17 19:07:31 +0900

    [PackageManager] Add RPK type
    - add PRK type to PackageType enum.
    - add "RPK" conditional statement in ToPackageType method.

--- src/Tizen.Applications.PackageManager/Tizen.Applications/PackageType.cs
+++ src/Tizen.Applications.PackageManager/Tizen.Applications/PackageType.cs
@@ -41,6 +41,10 @@ namespace Tizen.Applications
         /// Only some preloaded packages can have this type.
         /// </summary>
         RPM
+        /// <summary>
+        /// Tizen Resource Package (RPK) for sharing read-only resources with access control and version management.
+        /// </summary>
+        RPK
     }
 
     internal static class PackageTypeMethods
@@ -64,6 +68,10 @@ namespace Tizen.Applications
             else if (lowerType == "rpm")
             {
                 return PackageType.RPM;
+            }
+            else if (lowerType == "rpk")
+            {
+                return PackageType.RPK;
             }
             else
             {
