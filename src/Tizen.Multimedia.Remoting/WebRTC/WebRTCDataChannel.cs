0c0eacae984c6e5af91362de1eebe4887047d735
Haesu Gwon <haesu.gwon@samsung.com>
2025-04-18 09:01:55 +0900

    [WebRTC] Add debug log (#6856)

--- src/Tizen.Multimedia.Remoting/WebRTC/WebRTCDataChannel.cs
+++ src/Tizen.Multimedia.Remoting/WebRTC/WebRTCDataChannel.cs
@@ -227,7 +227,9 @@ namespace Tizen.Multimedia.Remoting
 
             if (_handle != IntPtr.Zero)
             {
+                Log.Info(WebRTCLog.Tag, "[Start] Native Data Channel destroy");
                 NativeDataChannel.Destroy(_handle);
+                Log.Info(WebRTCLog.Tag, "[End] Native Data Channel destroy");
                 _disposed = true;
             }
         }
