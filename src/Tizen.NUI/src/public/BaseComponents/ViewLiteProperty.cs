4ad24a52fd0a95ab70253ca8f5cc520fb67344c7
jmm <j0064423.lee@samsung.com>
2025-05-14 14:43:34 +0900

    [NUI] Temporal revert for Animatable View corner radius

--- src/Tizen.NUI/src/public/BaseComponents/ViewLiteProperty.cs
+++ src/Tizen.NUI/src/public/BaseComponents/ViewLiteProperty.cs
@@ -27,7 +27,24 @@ namespace Tizen.NUI.BaseComponents
             // Background property will be Color after now. Remove background image url information.
             backgroundImageUrl = null;
 
-            Object.InternalSetPropertyColor(SwigCPtr, Property.BACKGROUND, color);
+
+            if (backgroundExtraData == null)
+            {
+                Object.InternalSetPropertyColor(SwigCPtr, Property.BACKGROUND, color);
+            }
+            else
+            {
+                using var map = new PropertyMap()
+                    .Append(Visual.Property.Type, (int)Visual.Type.Color)
+                    .Append(ColorVisualProperty.MixColor, color)
+                    .Append(Visual.Property.CornerRadius, backgroundExtraData.CornerRadius)
+                    .Append(Visual.Property.CornerSquareness, backgroundExtraData.CornerSquareness)
+                    .Append(Visual.Property.CornerRadiusPolicy, (int)backgroundExtraData.CornerRadiusPolicy);
+
+                backgroundExtraDataUpdatedFlag &= ~BackgroundExtraDataUpdatedFlag.Background;
+
+                Object.InternalSetPropertyMap(SwigCPtr, Property.BACKGROUND, map.SwigCPtr);
+            }
 
             NotifyPropertyChanged(nameof(BackgroundColor));
             NotifyBackgroundChanged();
@@ -37,6 +54,8 @@ namespace Tizen.NUI.BaseComponents
         internal void SetBoxShadow(UIShadow shadow)
         {
             themeData?.selectorData?.ClearShadow(this);
+
+            backgroundExtraDataUpdatedFlag &= ~BackgroundExtraDataUpdatedFlag.Shadow;
 
             using var map = shadow.BuildMap(this);
 
