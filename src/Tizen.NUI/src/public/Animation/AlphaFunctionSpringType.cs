68261d058fe9fc72092f6d37769bd6bf3551a120
Eunki, Hong <eunkiki.hong@samsung.com>
2025-05-16 10:10:24 +0900

    [NUI] Fix several compile warning and error
    - AlphaFunctionSpringType was not in Tizen.NUI namespace
    - InnerShadow.Clone() defined twice. Make it as internal virtual function
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI/src/public/Animation/AlphaFunctionSpringType.cs
+++ src/Tizen.NUI/src/public/Animation/AlphaFunctionSpringType.cs
@@ -17,34 +17,37 @@
 
 using System.ComponentModel;
 
-/// <summary>
-/// Enumeration for predefined spring animation types.
-/// This presets are based on typical spring behavior tuned for different motion effects.
-/// </summary>
-[EditorBrowsable(EditorBrowsableState.Never)]
-public enum AlphaFunctionSpringType
+namespace Tizen.NUI
 {
     /// <summary>
-    /// Gentle spring. slower and smoother motion with less oscillation.
+    /// Enumeration for predefined spring animation types.
+    /// This presets are based on typical spring behavior tuned for different motion effects.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
-    Gentle,
+    public enum AlphaFunctionSpringType
+    {
+        /// <summary>
+        /// Gentle spring. slower and smoother motion with less oscillation.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Gentle,
 
-    /// <summary>
-    /// Quick spring, Fast settling animation with minimal overshoot.
-    /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    Quick,
+        /// <summary>
+        /// Quick spring, Fast settling animation with minimal overshoot.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Quick,
 
-    /// <summary>
-    /// Bouncy spring. Highly elastic and oscillatory animation.
-    /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    Bouncy,
+        /// <summary>
+        /// Bouncy spring. Highly elastic and oscillatory animation.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Bouncy,
 
-    /// <summary>
-    /// Slow spring. Smooth and relaxed motion with longer settling.
-    /// </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    Slow
+        /// <summary>
+        /// Slow spring. Smooth and relaxed motion with longer settling.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Slow
+    }
 }
