d4113ea5bc0b7665e54f0040be600c4605005625
Bowon Ryu <bowon.ryu@samsung.com>
2025-05-31 17:24:07 +0900

    [Markdown] Added MarkdownRenderer for Markdown support
    Initial commit.
    
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ src/Tizen.NUI.MarkdownRenderer/src/internal/MarkdownUIBuilder.cs
@@ -0,0 +1,470 @@
+﻿/*
+ * Copyright(c) 2025 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Binding;
+using Tizen.NUI.Text;
+
+using Markdig;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+using Markdig.Extensions.Tables;
+
+namespace Tizen.NUI.MarkdownRenderer
+{
+    /// <summary>
+    /// Indent for log.
+    /// </summary>
+    internal class Indent
+    {
+        // level 0 ~ 6
+        private static readonly string[] indent = new[] {"", "  ", "    ", "      ", "        ", "         "};
+        public static string Get(int level)
+        {
+            return level >= 0 && level < indent.Length ? indent[level] : indent[^1];
+        }
+    }
+
+    /// <summary>
+    /// MarkdownUIBuilder.
+    /// </summary>
+    internal class MarkdownUIBuilder
+    {
+        private MarkdownStyle style;
+        private List<View> uiBlocks;
+
+        public MarkdownUIBuilder(MarkdownStyle markdownStyle)
+        {
+            style = markdownStyle;
+
+            Initialize();
+        }
+
+        public void Clear()
+        {
+            for (int i = 0 ; i < uiBlocks.Count ; i ++)
+            {
+                uiBlocks[i].Dispose();
+            }
+            uiBlocks.Clear();
+        }
+
+        public void Build(Block block, View parent, int indent)
+        {
+            if (block is LeafBlock leafBlock)
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}[{indent}][{block.GetType().Name}] : LeafBlock\n");
+
+                var text = leafBlock.Inline is null ? String.Empty : GetInlineText(leafBlock.Inline);
+                AddBlockToUI(block, parent, text, indent);
+            }
+            else if (block is ContainerBlock containerBlock)
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}[{indent}][{block.GetType().Name}] : ContainerBlock\n");
+
+                var container = NewContainer(block);
+                parent.Add(container);
+
+                foreach (var subBlock in containerBlock)
+                {
+                    Build(subBlock, container, indent + 1);
+                }
+            }
+            else
+            {
+                // Something wrong.
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}[{indent}][{block.GetType().Name}] : UnknownBlock\n");
+            }
+        }
+
+        private void Initialize()
+        {
+            uiBlocks = new List<View>();
+        }
+
+        private string GetInlineText(ContainerInline inline)
+        {
+            var result = new StringBuilder();
+            foreach (var child in inline)
+            {
+                switch (child)
+                {
+                    case LiteralInline literal:
+                        result.Append(literal.Content.Text.Substring(literal.Content.Start, literal.Content.Length));
+                        break;
+
+                    case EmphasisInline emphasis:
+                        var content = GetInlineText(emphasis);
+                        if(emphasis.DelimiterChar == '~')
+                        {
+                            result.Append(emphasis.DelimiterCount == 2 ? $"<s>{content}</s>" : $"{content}");
+                        }
+                        else
+                        {
+                            // '*', '**'
+                            result.Append(emphasis.DelimiterCount == 2 ? $"<b>{content}</b>" : $"<i>{content}</i>");
+                        }
+                        break;
+
+                    case CodeInline code:
+                        result.Append($"<font family='{style.Code.FontFamily}'>{code.Content}</font>");
+                        break;
+
+                    case LineBreakInline:
+                        result.AppendLine();
+                        break;
+
+                    case LinkInline link:
+                        var label = GetInlineText(link);
+                        result.Append($"<a href='{link.Url}'>{label}</a>");
+                        break;
+
+                    default: // fallback
+                        if (child is ContainerInline container)
+                            result.Append(GetInlineText(container));
+                        break;
+                }
+            }
+            return result.ToString();
+        }
+
+        private void AddBlockToUI(Block block, View parent, string text, int indent)
+        {
+            if (block is HeadingBlock heading)
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}└[{indent}][HeadingBlock][{heading.Level}] Text:{text}\n");
+                parent.Add(NewHeading(text, heading.Level));
+            }
+            else if (block is ParagraphBlock)
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}└[{indent}][ParagraphBlock] Text:{text}\n");
+                parent.Add(NewParagraph(text));
+            }
+            else if (block is ThematicBreakBlock)
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}└[{indent}][ThematicBreakBlock] Text:{text}\n");
+                parent.Add(NewThematicBreak());
+            }
+            else if (block is FencedCodeBlock fenced)
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}└[{indent}][FencedCodeBlock] Text:{text}\n");
+                string language = fenced.Info;
+                string code = fenced.Lines.ToString();
+                parent.Add(NewCode(language, code));
+            }
+            else
+            {
+                Tizen.Log.Info("NUI", $"{Indent.Get(indent)}└[{indent}][UnknownBlock:{block.GetType().Name}] Text:{text}\n");
+                parent.Add(NewParagraph(text));
+            }
+        }
+
+        private View NewContainer(Block block)
+        {
+            if (block is MarkdownDocument)
+            {
+                return NewBase();
+            }
+            if (block is QuoteBlock)
+            {
+                return NewQuote();
+            }
+            else if (block is ListBlock)
+            {
+                return NewList();
+            }
+            else if (block is ListItemBlock)
+            {
+                return NewListItem();
+            }
+            else if (block is Table)
+            {
+                return NewTable();
+            }
+            else if (block is TableRow)
+            {
+                return NewTableRow();
+            }
+            else if (block is TableCell)
+            {
+                return NewTableItem();
+            }
+            else
+            {
+                return NewBase();
+            }
+        }
+
+        private View NewCode(string language, string text)
+        {
+            var code = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Margin = new Extents(0, 0, (ushort)style.Common.Margin, (ushort)style.Common.Margin),
+            };
+
+            var title = new TextLabel
+            {
+                Text = language,
+                FontFamily = style.Code.TitleFontFamily,
+                PixelSize = style.Code.TitleFontSize,
+                EnableMarkup = true,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                TextColor = new Color(style.Code.TitleFontColor),
+                BackgroundColor = new Color(style.Code.TitleBackgroundColor),
+                Padding = new Extents((ushort)style.Code.Padding),
+            };
+            code.Add(title);
+
+            var label = new TextLabel
+            {
+                Text = text,
+                FontFamily = style.Code.FontFamily,
+                PixelSize = style.Code.FontSize,
+                MultiLine = true,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                TextColor = new Color(style.Code.FontColor),
+                BackgroundColor = new Color(style.Code.BackgroundColor),
+                Padding = new Extents((ushort)style.Code.Padding),
+            };
+            code.Add(label);
+
+            uiBlocks.Add(code);
+            uiBlocks.Add(title);
+            uiBlocks.Add(label);
+
+            return code;
+        }
+
+        private View NewBase()
+        {
+            var view = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Padding = new Extents((ushort)style.Common.Padding),
+            };
+            uiBlocks.Add(view);
+            return view;
+        }
+
+        private View NewTable()
+        {
+            var table = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Margin = new Extents(0, 0, (ushort)style.Common.Margin, (ushort)style.Common.Margin),
+            };
+            uiBlocks.Add(table);
+            return table;
+        }
+
+        private View NewTableRow()
+        {
+            var item = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Horizontal,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                BorderlineWidth = style.Table.BorderThickness,
+                BorderlineColor = new Color(style.Table.BorderColor),
+            };
+            uiBlocks.Add(item);
+            return item;
+        }
+
+        private View NewTableItem()
+        {
+            var item = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Horizontal,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Padding = new Extents((ushort)style.Table.Padding),
+            };
+            uiBlocks.Add(item);
+            return item;
+        }
+
+        private View NewList()
+        {
+            var list = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Margin = new Extents(0, 0, (ushort)style.Common.Margin, (ushort)style.Common.Margin),
+            };
+            uiBlocks.Add(list);
+            return list;
+        }
+
+        private View NewListItem()
+        {
+            var item = new View()
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Margin = new Extents((ushort)style.Common.Indent, 0, 0, 0),
+            };
+            uiBlocks.Add(item);
+            return item;
+        }
+
+        private TextLabel NewHeading(string text, int level)
+        {
+            var heading = new TextLabel
+            {
+                Text = text,
+                FontFamily = style.Heading.FontFamily,
+                TextColor = new Color(style.Paragraph.FontColor),
+                MultiLine = true,
+                EnableMarkup = true,
+                WidthSpecification = LayoutParamPolicies.WrapContent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = Color.Transparent,
+                Margin = new Extents(0, 0, (ushort)style.Common.Margin, (ushort)style.Common.Margin),
+            };
+
+            switch(level)
+            {
+                case 1:
+                    heading.PixelSize = style.Heading.FontSizeLevel1;
+                    break;
+                case 2:
+                    heading.PixelSize = style.Heading.FontSizeLevel2;
+                    break;
+                case 3:
+                    heading.PixelSize = style.Heading.FontSizeLevel3;
+                    break;
+                case 4:
+                    heading.PixelSize = style.Heading.FontSizeLevel4;
+                    break;
+                case 5:
+                    heading.PixelSize = style.Heading.FontSizeLevel5;
+                    break;
+                default:
+                    heading.PixelSize = style.Heading.FontSizeLevel1;
+                    break;
+            }
+            uiBlocks.Add(heading);
+            return heading;
+        }
+
+        private View NewQuote()
+        {
+            var view = new View
+            {
+                Layout = new LinearLayout()
+                {
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                },
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                BackgroundColor = new Color(style.Quote.BackgroundColor),
+                Padding = new Extents((ushort)style.Quote.Padding),
+                Margin = new Extents((ushort)style.Common.Indent, 0, (ushort)style.Common.Margin, (ushort)style.Common.Margin),
+            };
+            uiBlocks.Add(view);
+            return view;
+        }
+
+        private View NewThematicBreak()
+        {
+            var view = new View
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = style.ThematicBreak.Thickness,
+                BackgroundColor = new Color(style.ThematicBreak.Color),
+                Margin = new Extents(0, 0, (ushort)style.Common.Margin, (ushort)style.Common.Margin),
+            };
+            uiBlocks.Add(view);
+            return view;
+        }
+
+        private TextLabel NewParagraph(string text)
+        {
+            var label = new TextLabel
+            {
+                Text = text,
+                FontFamily = style.Paragraph.FontFamily,
+                PixelSize = style.Paragraph.FontSize,
+                TextColor = new Color(style.Paragraph.FontColor),
+                MinLineSize = style.Paragraph.LineHeight,
+                MultiLine = true,
+                EnableMarkup = true,
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.WrapContent,
+                VerticalLineAlignment = VerticalLineAlignment.Center,
+                BackgroundColor = Color.Transparent,
+                AnchorColor = new Color(style.Link.Color),
+                AnchorClickedColor = new Color(style.Link.VisitedColor),
+            };
+            uiBlocks.Add(label);
+            return label;
+        }
+    }
+}
---
+++ src/Tizen.NUI.MarkdownRenderer/src/public/MarkdownStyle.cs
@@ -0,0 +1,226 @@
+﻿/*
+ * Copyright(c) 2025 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace Tizen.NUI.MarkdownRenderer
+{
+    /// <summary>
+    /// MarkdownStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class MarkdownStyle
+    {
+        /// <summary>
+        /// MarkdownStyle.
+        /// </summary>
+        public MarkdownStyle() { }
+
+        /// <summary>
+        /// CommonStyle.
+        /// </summary>
+        public CommonStyle Common { get; } = new CommonStyle();
+
+        /// <summary>
+        /// ParagraphStyle.
+        /// </summary>
+        public ParagraphStyle Paragraph { get; } = new ParagraphStyle();
+
+        /// <summary>
+        /// HeadingStyle.
+        /// </summary>
+        public HeadingStyle Heading { get; } = new HeadingStyle();
+
+        /// <summary>
+        /// ThematicBreakStyle.
+        /// </summary>
+        public ThematicBreakStyle ThematicBreak { get; } = new ThematicBreakStyle();
+
+        /// <summary>
+        /// QuoteStyle.
+        /// </summary>
+        public QuoteStyle Quote { get; } = new QuoteStyle();
+
+        /// <summary>
+        /// TableStyle.
+        /// </summary>
+        public TableStyle Table { get; } = new TableStyle();
+
+        /// <summary>
+        /// CodeStyle.
+        /// </summary>
+        public CodeStyle Code { get; } = new CodeStyle();
+
+        /// <summary>
+        /// LinkStyle.
+        /// </summary>
+        public LinkStyle Link { get; } = new LinkStyle();
+    }
+
+    /// <summary>
+    /// CommonStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class CommonStyle
+    {
+        /// Indent is added in pixels to the left of a Container Block (Quote, List).
+        public int Indent { get; set; } = StyleDefaults.CommonIndent;
+
+        /// Padding.
+        public int Padding { get; set; } = StyleDefaults.CommonPadding;
+
+        /// Margin is added in pixels to the top and bottom of Block (Heading, List, Table, Code, Quote, ThematicBreak).
+        public int Margin { get; set; } = StyleDefaults.CommonMargin;
+    }
+
+    /// <summary>
+    /// ParagraphStyle.
+    /// </summary>
+    /// <remark>
+    /// Most text follows ParagraphStyle, except Heading and Code.
+    /// </remark>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class ParagraphStyle
+    {
+        /// FontColor.
+        public string FontColor { get; set; } = StyleDefaults.ParagraphFontColor;
+
+        /// FontSize.
+        public float FontSize { get; set; } = StyleDefaults.ParagraphFontSize;
+
+        /// FontFamily.
+        public string FontFamily { get; set; } = StyleDefaults.ParagraphFontFamily;
+
+        /// The minimum height of a line in pixels.
+        public float LineHeight { get; set; } = StyleDefaults.ParagraphLineHeight;
+    }
+
+    /// <summary>
+    /// HeadingStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class HeadingStyle
+    {
+        /// Font size of heading level 1.
+        public float FontSizeLevel1 { get; set; } = StyleDefaults.HeadingFontSizeLevel1;
+
+        /// Font size of heading level 2.
+        public float FontSizeLevel2 { get; set; } = StyleDefaults.HeadingFontSizeLevel2;
+
+        /// Font size of heading level 3.
+        public float FontSizeLevel3 { get; set; } = StyleDefaults.HeadingFontSizeLevel3;
+
+        /// Font size of heading level 4.
+        public float FontSizeLevel4 { get; set; } = StyleDefaults.HeadingFontSizeLevel4;
+
+        /// Font size of heading level 5.
+        public float FontSizeLevel5 { get; set; } = StyleDefaults.HeadingFontSizeLevel5;
+
+        /// FontFamily.
+        public string FontFamily { get; set; } = StyleDefaults.HeadingFontFamily;
+    }
+
+    /// <summary>
+    /// ThematicBreakStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class ThematicBreakStyle
+    {
+        /// Color.
+        public string Color { get; set; } = StyleDefaults.ThematicBreakColor;
+
+        /// Thickness.
+        public int Thickness { get; set; } = StyleDefaults.ThematicBreakThickness;
+    }
+
+    /// <summary>
+    /// QuoteStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class QuoteStyle
+    {
+        /// BackgroundColor.
+        public string BackgroundColor { get; set; } = StyleDefaults.QuoteBackgroundColor;
+
+        /// Padding.
+        public int Padding { get; set; } = StyleDefaults.QuotePadding;
+    }
+
+    /// <summary>
+    /// TableStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class TableStyle
+    {
+        /// BorderColor.
+        public string BorderColor { get; set; } = StyleDefaults.TableBorderColor;
+
+        /// TitleFontFaBorderThicknessmily.
+        public int BorderThickness { get; set; } = StyleDefaults.TableBorderThickness;
+
+        /// Padding.
+        public int Padding { get; set; } = StyleDefaults.TablePadding;
+    }
+
+    /// <summary>
+    /// CodeStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class CodeStyle
+    {
+        /// TitleFontFamily.
+        public string TitleFontFamily { get; set; } = StyleDefaults.CodeTitleFontFamily;
+
+        /// TitleFontColor.
+        public string TitleFontColor { get; set; } = StyleDefaults.CodeTitleFontColor;
+
+        /// TitleBackgroundColor.
+        public string TitleBackgroundColor { get; set; } = StyleDefaults.CodeTitleBackgroundColor;
+
+        /// TitleFontSize.
+        public float TitleFontSize { get; set; } = StyleDefaults.CodeTitleFontSize;
+
+        /// FontFamily.
+        public string FontFamily { get; set; } = StyleDefaults.CodeFontFamily;
+
+        /// FontColor.
+        public string FontColor { get; set; } = StyleDefaults.CodeFontColor;
+
+        /// BackgroundColor.
+        public string BackgroundColor { get; set; } = StyleDefaults.CodeBackgroundColor;
+
+        /// FontSize.
+        public float FontSize { get; set; } = StyleDefaults.CodeFontSize;
+
+        /// Padding.
+        public int Padding { get; set; } = StyleDefaults.CodePadding;
+    }
+
+    /// <summary>
+    /// LinkStyle.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class LinkStyle
+    {
+        /// LinkStyle.
+        public string Color { get; set; } = StyleDefaults.LinkColor;
+        
+        /// VisitedColor.
+        public string VisitedColor { get; set; } = StyleDefaults.LinkVisitedColor;
+    }
+}
