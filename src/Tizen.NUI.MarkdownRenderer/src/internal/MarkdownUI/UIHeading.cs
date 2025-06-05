83eec6953898b1b0458f5bbd6e287a3666d0f3f6
Bowon Ryu <bowon.ryu@samsung.com>
2025-06-05 18:36:11 +0900

    [Markdown] Add ui heading, paragraph, list to markdown
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

---
+++ src/Tizen.NUI.MarkdownRenderer/src/internal/MarkdownUI/UIHeading.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Copyright(c) 2025 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using System.ComponentModel;
+
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI.MarkdownRenderer
+{
+    internal class UIHeading : TextLabel
+    {
+        HeadingStyle style;
+
+        public UIHeading(string text, int level, HeadingStyle headingStyle, CommonStyle commonStyle, ParagraphStyle paragraphStyle) : base()
+        {
+            style = headingStyle;
+
+            Text = text;
+            FontFamily = style.FontFamily;
+            TextColor = new Color(paragraphStyle.FontColor);
+            MultiLine = true;
+            EnableMarkup = true;
+            Ellipsis = false;
+            WidthSpecification = LayoutParamPolicies.WrapContent;
+            HeightSpecification = LayoutParamPolicies.WrapContent;
+            BackgroundColor = Color.Transparent;
+            Margin = new Extents(0, 0, (ushort)commonStyle.Margin, (ushort)commonStyle.Margin);
+
+            SetFontSize(level);
+        }
+
+        private void SetFontSize(int level)
+        {
+            switch(level)
+            {
+                case 1:
+                    PixelSize = style.FontSizeLevel1;
+                    break;
+                case 2:
+                    PixelSize = style.FontSizeLevel2;
+                    break;
+                case 3:
+                    PixelSize = style.FontSizeLevel3;
+                    break;
+                case 4:
+                    PixelSize = style.FontSizeLevel4;
+                    break;
+                case 5:
+                    PixelSize = style.FontSizeLevel5;
+                    break;
+                default:
+                    PixelSize = style.FontSizeLevel1;
+                    break;
+            }
+        }
+    }
+}
