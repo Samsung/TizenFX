edf2a703979e2d0fd91ba3d22799882869c67004
huayong.xu <huayong.xu@samsung.com>
2025-07-11 17:55:46 +0800

    [NUI] Make NUI.Samples use its own binder lib.
    - Seperated ItemView & CubeTransitionEffect from NUI.
    - Remove an unused Interop API CSharp_Dali_NewItemLayout.

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Interop/Interop.ItemFactory.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Interop/Interop.ItemFactory.cs
@@ -21,25 +21,25 @@ namespace Tizen.NUI.Samples
     {
         internal static partial class ItemFactory
         {
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_ItemFactory")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_delete_ItemFactory")]
             public static extern void DeleteItemFactory(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemFactory_GetNumberOfItems")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemFactory_GetNumberOfItems")]
             public static extern uint GetNumberOfItems(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemFactory_NewItem")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemFactory_NewItem")]
             public static extern global::System.IntPtr NewItem(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemFactory_ItemReleased")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemFactory_ItemReleased")]
             public static extern void ItemReleased(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2, global::System.Runtime.InteropServices.HandleRef jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemFactory_ItemReleasedSwigExplicitItemFactory")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemFactory_ItemReleasedSwigExplicitItemFactory")]
             public static extern void ItemReleasedSwigExplicitItemFactory(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2, global::System.Runtime.InteropServices.HandleRef jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_ItemFactory")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_new_ItemFactory")]
             public static extern global::System.IntPtr NewItemFactory();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemFactory_director_connect")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemFactory_director_connect")]
             public static extern void DirectorConnect(global::System.Runtime.InteropServices.HandleRef jarg1, Tizen.NUI.Samples.ItemFactory.SwigDelegateItemFactory0 delegate0,
                 Tizen.NUI.Samples.ItemFactory.SwigDelegateItemFactory1 delegate1, Tizen.NUI.Samples.ItemFactory.SwigDelegateItemFactory2 delegate2);
         }
--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Interop/Interop.ItemRange.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Interop/Interop.ItemRange.cs
@@ -21,35 +21,35 @@ namespace Tizen.NUI.Samples
     {
         internal static partial class ItemRange
         {
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_ItemRange__SWIG_0")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_new_ItemRange__SWIG_0")]
             public static extern global::System.IntPtr NewItemRange(uint jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_ItemRange__SWIG_1")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_new_ItemRange__SWIG_1")]
             public static extern global::System.IntPtr NewItemRange(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_Assign")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_Assign")]
             public static extern global::System.IntPtr Assign(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_Within")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_Within")]
             [return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.U1)]
             public static extern bool Within(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_Intersection")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_Intersection")]
             public static extern global::System.IntPtr Intersection(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_begin_set")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_begin_set")]
             public static extern void BeginSet(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_begin_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_begin_get")]
             public static extern uint BeginGet(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_end_set")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_end_set")]
             public static extern void EndSet(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemRange_end_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemRange_end_get")]
             public static extern uint EndGet(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_ItemRange")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_delete_ItemRange")]
             public static extern void DeleteItemRange(global::System.Runtime.InteropServices.HandleRef jarg1);
         }
     }
