6098eaad8046cdd20fa5093f7eaa5a9bc2ac514e
huayong.xu <huayong.xu@samsung.com>
2025-07-11 17:55:46 +0800

    [NUI] Make NUI.Samples use its own binder lib.
    - Seperated ItemView & CubeTransitionEffect from NUI.
    - Remove an unused Interop API CSharp_Dali_NewItemLayout.

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Interop/Interop.ItemView.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Interop/Interop.ItemView.cs
@@ -21,131 +21,131 @@ namespace Tizen.NUI.Samples
     {
         internal static partial class ItemView
         {
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_MINIMUM_SWIPE_SPEED_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_MINIMUM_SWIPE_SPEED_get")]
             public static extern int MinimumSwipeSpeedGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_MINIMUM_SWIPE_DISTANCE_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_MINIMUM_SWIPE_DISTANCE_get")]
             public static extern int MinimumSwipeDistanceGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_WHEEL_SCROLL_DISTANCE_STEP_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_WHEEL_SCROLL_DISTANCE_STEP_get")]
             public static extern int WheelScrollDistanceStepGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_SNAP_TO_ITEM_ENABLED_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_SNAP_TO_ITEM_ENABLED_get")]
             public static extern int SnapToItemEnabledGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_REFRESH_INTERVAL_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_REFRESH_INTERVAL_get")]
             public static extern int RefreshIntervalGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_LAYOUT_POSITION_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_LAYOUT_POSITION_get")]
             public static extern int LayoutPositionGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_SCROLL_SPEED_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_SCROLL_SPEED_get")]
             public static extern int ScrollSpeedGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_OVERSHOOT_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_OVERSHOOT_get")]
             public static extern int OvershootGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_SCROLL_DIRECTION_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_SCROLL_DIRECTION_get")]
             public static extern int ScrollDirectionGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_LAYOUT_ORIENTATION_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_LAYOUT_ORIENTATION_get")]
             public static extern int LayoutOrientationGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Property_SCROLL_CONTENT_SIZE_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Property_SCROLL_CONTENT_SIZE_get")]
             public static extern int ScrollContentSizeGet();
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_ItemView")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_delete_ItemView")]
             public static extern void DeleteItemView(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_New")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_New")]
             public static extern global::System.IntPtr New(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetLayoutCount")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetLayoutCount")]
             public static extern uint GetLayoutCount(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_AddLayout")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_AddLayout")]
             public static extern void AddLayout(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_RemoveLayout")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_RemoveLayout")]
             public static extern void RemoveLayout(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetLayout")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetLayout")]
             public static extern global::System.IntPtr GetLayout(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetActiveLayout")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetActiveLayout")]
             public static extern global::System.IntPtr GetActiveLayout(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetCurrentLayoutPosition")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetCurrentLayoutPosition")]
             public static extern float GetCurrentLayoutPosition(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_ActivateLayout")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_ActivateLayout")]
             public static extern void ActivateLayout(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2, global::System.Runtime.InteropServices.HandleRef jarg3, float jarg4);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_DeactivateCurrentLayout")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_DeactivateCurrentLayout")]
             public static extern void DeactivateCurrentLayout(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_SetAnchoring")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_SetAnchoring")]
             public static extern void SetAnchoring(global::System.Runtime.InteropServices.HandleRef jarg1, bool jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetAnchoring")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetAnchoring")]
             [return: global::System.Runtime.InteropServices.MarshalAs(global::System.Runtime.InteropServices.UnmanagedType.U1)]
             public static extern bool GetAnchoring(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_SetAnchoringDuration")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_SetAnchoringDuration")]
             public static extern void SetAnchoringDuration(global::System.Runtime.InteropServices.HandleRef jarg1, float jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetAnchoringDuration")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetAnchoringDuration")]
             public static extern float GetAnchoringDuration(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_ScrollToItem")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_ScrollToItem")]
             public static extern void ScrollToItem(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_Refresh")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_Refresh")]
             public static extern void Refresh(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetItem")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetItem")]
             public static extern global::System.IntPtr GetItem(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetItemId")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetItemId")]
             public static extern uint GetItemId(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_InsertItem")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_InsertItem")]
             public static extern void InsertItem(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_InsertItems")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_InsertItems")]
             public static extern void InsertItems(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_RemoveItem")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_RemoveItem")]
             public static extern void RemoveItem(global::System.Runtime.InteropServices.HandleRef jarg1, uint jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_RemoveItems")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_RemoveItems")]
             public static extern void RemoveItems(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_ReplaceItem")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_ReplaceItem")]
             public static extern void ReplaceItem(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_ReplaceItems")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_ReplaceItems")]
             public static extern void ReplaceItems(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2, float jarg3);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_SetItemsParentOrigin")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_SetItemsParentOrigin")]
             public static extern void SetItemsParentOrigin(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetItemsParentOrigin")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetItemsParentOrigin")]
             public static extern global::System.IntPtr GetItemsParentOrigin(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_SetItemsAnchorPoint")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_SetItemsAnchorPoint")]
             public static extern void SetItemsAnchorPoint(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetItemsAnchorPoint")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetItemsAnchorPoint")]
             public static extern global::System.IntPtr GetItemsAnchorPoint(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_GetItemsRange")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_GetItemsRange")]
             public static extern void GetItemsRange(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ItemView_LayoutActivatedSignal")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_Dali_ItemView_LayoutActivatedSignal")]
             public static extern global::System.IntPtr LayoutActivatedSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
 
-            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_ItemView_Property_LAYOUT_get")]
+            [global::System.Runtime.InteropServices.DllImport(SamplesNDalicPINVOKE.ToolkitDemoLib, EntryPoint = "CSharp_ItemView_Property_LAYOUT_get")]
             public static extern int LayoutGet();
         }
     }
