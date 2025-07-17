65996ecf4860aeec96b34fc893d873307cf30bc8
everLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2025-07-17 14:51:00 +0900

    Add access target in AccessibilityActionRecievedEventArgs

--- src/Tizen.NUI/src/internal/View/ViewAccessibilityData.cs
+++ src/Tizen.NUI/src/internal/View/ViewAccessibilityData.cs
@@ -224,6 +224,7 @@ namespace Tizen.NUI.BaseComponents
             var eventArgs = new AccessibilityActionReceivedEventArgs()
             {
                 ActionType = info.ActionType,
+                Target = Registry.GetManagedBaseHandleFromNativePtr(info.target) as View,
                 Handled = false
             };
             _accessibilityActionReceivedHandler?.Invoke(_owner, eventArgs);
