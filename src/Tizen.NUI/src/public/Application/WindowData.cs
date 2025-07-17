b00b1fb83c637877cd6702062d5ee00c6611856b
Eunki, Hong <eunkiki.hong@samsung.com>
2025-07-17 15:02:08 +0900

    [NUI] More Disposable Dispose at worker thread (TouchPoint / WindowData / Builtin AlphaFunction)
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI/src/public/Application/WindowData.cs
+++ src/Tizen.NUI/src/public/Application/WindowData.cs
@@ -36,7 +36,7 @@ namespace Tizen.NUI
             if (NDalicPINVOKE.SWIGPendingException.Pending) throw NDalicPINVOKE.SWIGPendingException.Retrieve();
         }
 
-        internal WindowData(global::System.IntPtr cPtr, bool cMemoryOwn) : base(cPtr, cMemoryOwn)
+        internal WindowData(global::System.IntPtr cPtr, bool cMemoryOwn) : base(cPtr, cMemoryOwn, false)
         {
         }
 
