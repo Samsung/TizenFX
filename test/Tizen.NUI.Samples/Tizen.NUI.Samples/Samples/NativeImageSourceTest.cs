b4224fce98e8b8b7ec20f7afebb0dbea444e2da2
Eunki, Hong <eunkiki.hong@samsung.com>
2025-08-26 11:55:01 +0900

    [NUI.Samples] Upgrade NativeImageSourceTest sample code
    Make we acquire buffer at worker thread.
    Also ensure to release buffer after acquire finished.
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/NativeImageSourceTest.cs
+++ test/Tizen.NUI.Samples/Tizen.NUI.Samples/Samples/NativeImageSourceTest.cs
@@ -17,6 +17,10 @@
 
 using Tizen.NUI.BaseComponents;
 
+using global::System;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace Tizen.NUI.Samples
 {
     using log = Tizen.Log;
@@ -25,6 +29,8 @@ namespace Tizen.NUI.Samples
         private const string tag = "NUITEST";
         private Window mainWin;
         private ImageView imageView;
+        private NativeImageSource source;
+        private Task bufferAssignTask;
         const int imageWidth = 300;
         const int imageHeight = 300;
 
@@ -35,6 +41,18 @@ namespace Tizen.NUI.Samples
 
         public void Deactivate()
         {
+            if (bufferAssignTask != null)
+            {
+                try
+                {
+                    bufferAssignTask.Wait();
+                }
+                catch(Exception ex)
+                {
+                    Tizen.Log.Error("NUI", $"Exception {ex.Message}\n");
+                }
+                bufferAssignTask = null;
+            }
         }
 
         private void Initialize()
@@ -46,26 +64,39 @@ namespace Tizen.NUI.Samples
             NativeImageSource source = new NativeImageSource((uint)windowSize.Width, (uint)windowSize.Height, NativeImageSource.ColorDepth.Default);
             var url = source.GenerateUrl();
 
-            int bufferWidth = 0;
-            int bufferHeight = 0;
-            int bufferStride = 0;
-            var buffer = source.AcquireBuffer(ref bufferWidth, ref bufferHeight, ref bufferStride); 
-            uint size = (uint) bufferWidth * (uint) bufferHeight * 4;
-            unsafe
-            {
-                byte* ptrData = (byte*) buffer.ToPointer();
-                for(int i=0; i< size; i+=4)
-                {
-                    ptrData[i] = 0;
-                    ptrData[i+1] = 255;
-                    ptrData[i+2] = 0;
-                    ptrData[i+3] = 255;
-                }
-            }
+            bufferAssignTask = AssignBuffer();
 
             imageView = new ImageView(url.ToString());
-            imageView.Size2D = new Size2D( imageWidth, imageHeight);
+            imageView.Size2D = new Size2D(imageWidth, imageHeight);
             mainWin.Add(imageView);
+        }
+
+        private Task AssignBuffer()
+        {
+            Task task = new Task(() =>
+            {
+                int bufferWidth = 0;
+                int bufferHeight = 0;
+                int bufferStride = 0;
+                var buffer = source.AcquireBuffer(ref bufferWidth, ref bufferHeight, ref bufferStride); 
+                Tizen.Log.Error("NUI", $"AcquireBuffer {bufferWidth} x {bufferHeight} with stride {bufferStride}\n");
+                uint size = (uint) bufferWidth * (uint) bufferHeight * 4;
+                unsafe
+                {
+                    byte* ptrData = (byte*) buffer.ToPointer();
+                    for(int i=0; i< size; i+=4)
+                    {
+                        ptrData[i] = 0;
+                        ptrData[i+1] = 255;
+                        ptrData[i+2] = 0;
+                        ptrData[i+3] = 255;
+                    }
+                }
+                Tizen.Log.Error("NUI", $"ReleaseBuffer\n");
+                source.ReleaseBuffer();
+            });
+            task.Start();
+            return task;
         }
     }
 }
