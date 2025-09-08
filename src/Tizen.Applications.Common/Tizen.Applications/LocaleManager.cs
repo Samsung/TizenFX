f85bd5b57c9c066135dcb03e89f23113b51e925b
김현욱 <43038815+Hyeon-Uk@users.noreply.github.com>
2025-09-08 17:06:15 +0900

    [Applications.Common] Add `LocaleManager` for changing app language (#7174)
    * Add API related to locale_manager
    
    These API can handle applications locale
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Implement SystemLocaleConverter to divid responsibility
    
    This class converts Locale.
    This allow us to separate the convert function
    from CoreApplication.cs
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Implement LocaleManager.cs to manage locale in Application
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Replace code with LocaleManager in CoreApplication.cs
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Add SetApplicationLanguage method in CoreApplication.cs
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Fix native api prefix
    
    `app_locale_` to `app_core_locale_`
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Fix native API EntryPoint again
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Change SetApplicationLanguage method to internal API
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Apply of Lazy Loading to ULocale Class
    
    In the constructor of ULocale, there is a logic
    to import member variables using api.
    
    Since only some member variables are used in the existing code,
    I think it is more efficient to apply lazy loading so that
    member variable can be imported only once when necessary
    than to call them using api at the time of creation.
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Remove Calling SetCurrentUICultureInfo method in LocaleManager.
    
    If call native API, CoreApplication receive the LocaleChangeEvent
    and call SetCurrentUICultureInfo.
    
    So, this line can occur duplicated calling.
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Remove unnecessary blank line
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Convert LocaleManager into public class
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Change GetSystemLocale method's return type
    
    - string to CultureInfo
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Convert LocaleManager into internal class
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Remove SetApplicationLanguage method
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Refactor getting default locale
    
    - LocaleManager.GetSystemLocale uses Vconf
    - This causes performance degradation
    - So, use ULocale.GetDefaultLocale instead of LocaleManager
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Add license
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    * Add API Description
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    
    ---------
    
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>
    Co-authored-by: hjhun <36876573+hjhun@users.noreply.github.com>

---
+++ src/Tizen.Applications.Common/Tizen.Applications/LocaleManager.cs
@@ -0,0 +1,242 @@
+﻿/*
+ * Copyright (c) 2025 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Tizen.Applications
+{
+    /// <summary>
+    /// The class for managing the app's locale.
+    /// </summary>
+    /// <since_tizen> 13 </since_tizen>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class LocaleManager
+    {
+        private static readonly string LogTag = "Tizen.Applications";
+        private static readonly string SupportedLocalesFilePath = "/usr/share/i18n/SUPPORTED";
+        private static bool _fileExists = File.Exists(SupportedLocalesFilePath);
+        private static HashSet<string> _supportedLocales;
+
+        /// <summary>
+        /// Set the application's language independently of the system language.
+        /// </summary>
+        /// <remarks>
+        /// If a device does not support the language , It will be set to the default language(en_US.UTF-8)
+        /// </remarks>
+        /// <param name="info">The locale to set language</param>
+        /// <since_tizen> 13 </since_tizen>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void SetApplicationLocale(CultureInfo info)
+        {
+            if (!_fileExists)
+            {
+                Log.Error(LogTag, $"{SupportedLocalesFilePath} not found");
+                return;
+            }
+            var converter = new SystemLocaleConverter();
+            string convertedLocale = converter.Convert(info);
+
+            if (VerifySupportedLocale(convertedLocale))
+            {
+                Interop.AppCommon.AppCommonErrorCode ret = Interop.AppCommon.AppLocaleManagerSetLanguage(convertedLocale);
+                if (ret != Interop.AppCommon.AppCommonErrorCode.None)
+                {
+                    Interop.AppCommon.AppLocaleManagerSetLanguage("en_US.UTF-8");
+                }
+            }
+            else
+            {
+                Log.Error(LogTag, $"Unsupported Language : {info.Name}");
+            }
+        }
+
+        /// <summary>
+        /// Get the application's language
+        /// </summary>
+        /// <returns>CultureInfo corresponding to the application language</returns>
+        /// <since_tizen> 13 </since_tizen>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static CultureInfo GetApplicationLocale()
+        {
+            IntPtr langPtr = IntPtr.Zero;
+
+            try
+            {
+                Interop.AppCommon.AppCommonErrorCode ret = Interop.AppCommon.AppLocaleManagerGetLanguage(out langPtr);
+
+                if (ret != Interop.AppCommon.AppCommonErrorCode.None || langPtr == IntPtr.Zero)
+                {
+                    Log.Error(LogTag, $"Failed to get application language. Result: {ret}");
+                    return null;
+                }
+
+                string language = Marshal.PtrToStringAnsi(langPtr);
+                if (string.IsNullOrEmpty(language))
+                {
+                    Log.Warn(LogTag, "Retrieved application language is null or empty.");
+                    return null;
+                }
+
+                var converter = new SystemLocaleConverter();
+                return converter.Convert(language);
+            }
+            finally
+            {
+                if (langPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeCoTaskMem(langPtr);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the system's language
+        /// </summary>
+        /// <returns>CultureInfo corresponding to the system language</returns>
+        /// <since_tizen> 13 </since_tizen>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static CultureInfo GetSystemLocale()
+        {
+            IntPtr langPtr = IntPtr.Zero;
+
+            try
+            {
+                Interop.AppCommon.AppCommonErrorCode ret = Interop.AppCommon.AppLocaleManagerGetSystemLanguage(out langPtr);
+
+                if (ret != Interop.AppCommon.AppCommonErrorCode.None || langPtr == IntPtr.Zero)
+                {
+                    Log.Error(LogTag, $"Failed to get system language. Result: {ret}");
+                    return null;
+                }
+
+                string language = Marshal.PtrToStringAnsi(langPtr);
+                if (string.IsNullOrEmpty(language))
+                {
+                    Log.Warn(LogTag, "Retrieved System language is null or empty.");
+                    return null;
+                }
+
+                var converter = new SystemLocaleConverter();
+                return converter.Convert(language);
+            }
+            finally
+            {
+                if(langPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeCoTaskMem(langPtr);
+                }
+            }
+        }
+
+        internal static void SetCurrentCultureInfo(string locale)
+        {
+            var converter = new SystemLocaleConverter();
+            CultureInfo cultureInfo = converter.Convert(locale);
+            if (cultureInfo != null)
+            {
+                CultureInfo.CurrentCulture = cultureInfo;
+            }
+            else
+            {
+                Log.Error(LogTag, $"CultureInfo is null. locale: {locale}");
+            }
+        }
+
+        internal static void SetCurrentUICultureInfo(string locale)
+        {
+            var converter = new SystemLocaleConverter();
+            CultureInfo cultureInfo = converter.Convert(locale);
+            if (cultureInfo != null)
+            {
+                CultureInfo.CurrentUICulture = cultureInfo;
+            }
+            else
+            {
+                Log.Error(LogTag, $"CultureInfo is null. locale: {locale}");
+            }
+        }
+
+        private static bool VerifySupportedLocale(string locale)
+        {
+            if (_supportedLocales == null)
+            {
+                LoadSupportedLocales();
+            }
+
+            return _supportedLocales.Contains(locale);
+        }
+
+        /// <summary>
+        /// Parses a line from the SUPPORTED file and formats it into "{language}_{country}.{encoding}".
+        /// The input lineParts can be:
+        /// 1. ["{localeString}", "{encodingString}"] e.g., ["en_US", "UTF-8"]
+        /// 2. ["{localeString}"] e.g., ["ko_KR"] (encoding will default to UTF-8)
+        /// The localeString itself might contain an encoding, e.g., "ko_KR.UTF-8", which will be stripped.
+        /// </summary>
+        /// <param name="lineParts">Array of strings from splitting a line by space.</param>
+        /// <returns>Formatted locale string or null if parsing fails.</returns>
+        private static string ParseAndFormatLocaleLine(string[] lineParts)
+        {
+            if (lineParts == null || lineParts.Length == 0 || string.IsNullOrWhiteSpace(lineParts[0]))
+            {
+                return null;
+            }
+
+            string localeString = lineParts[0];
+            string encodingString;
+
+            if (lineParts.Length == 2)
+            {
+                encodingString = lineParts[1];
+            }
+            else
+            {
+                encodingString = Encoding.UTF8.BodyName;
+            }
+
+            int dotIndex = localeString.IndexOf('.');
+            if (dotIndex != -1)
+            {
+                localeString = localeString[..dotIndex];
+            }
+
+            if (string.IsNullOrWhiteSpace(localeString) || string.IsNullOrWhiteSpace(encodingString))
+            {
+                return null;
+            }
+
+            return $"{localeString}.{encodingString}";
+        }
+
+        private static void LoadSupportedLocales()
+        {
+            _supportedLocales = File.ReadAllLines(SupportedLocalesFilePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                .Select(lineParts => ParseAndFormatLocaleLine(lineParts))
+                .Where(formattedLocale => formattedLocale != null)
+                .ToHashSet();
+        }
+    }
+}
