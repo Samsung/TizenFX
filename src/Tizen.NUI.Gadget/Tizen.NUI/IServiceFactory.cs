cc9cb9223519cb111272d5f680b580884204216b
Hyeon-Uk <rlagusdnr120@gmail.com>
2025-09-12 18:09:30 +0900

    Remove unused using.
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>

--- src/Tizen.NUI.Gadget/Tizen.NUI/IServiceFactory.cs
+++ src/Tizen.NUI.Gadget/Tizen.NUI/IServiceFactory.cs
@@ -14,12 +14,7 @@
  * limitations under the License.
  */
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tizen.NUI
 {
--- src/Tizen.NUI.Gadget/Tizen.NUI/OneShotService.cs
+++ src/Tizen.NUI.Gadget/Tizen.NUI/OneShotService.cs
@@ -15,10 +15,7 @@
  */
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using Tizen.Core;
 
 namespace Tizen.NUI
