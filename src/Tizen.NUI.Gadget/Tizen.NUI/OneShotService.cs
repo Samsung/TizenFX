88aa255e8e8f72eef946cc8c841b7153e8bc2f44
Hyeon-Uk <rlagusdnr120@gmail.com>
2025-09-15 15:38:53 +0900

    Remove wrong comment
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>

--- src/Tizen.NUI.Gadget/Tizen.NUI/OneShotService.cs
+++ src/Tizen.NUI.Gadget/Tizen.NUI/OneShotService.cs
@@ -214,13 +214,13 @@ namespace Tizen.NUI
 
         private void NotifyLifecycleChanged()
         {
-            //CoreApplication.Post(() =>
-            //{
+            CoreApplication.Post(() =>
+            {
                 var args = new OneShotServiceLifecycleChangedEventArgs();
                 args.State = State;
                 args.OneShotService = this;
                 LifecycleStateChanged?.Invoke(this, args);
-            //});
+            });
         }
 
         /// <summary>
