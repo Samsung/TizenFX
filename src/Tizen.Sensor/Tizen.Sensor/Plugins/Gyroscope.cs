c2696c70755fff12e822f230cbeefd6977ea02a0
SangYoun Kwak <sy.kwak@samsung.com>
2025-10-13 17:01:28 +0900

    Tizen.Sensor: Fix to use Marshal.FreeHGlobal instead of Interop.Libc.Free
    The allocation in the native function call is called from libgwpasan and
    free is called from glibc, and it causes crash on ASAN environment.
    
    To resolve this issue, Marshal.FreeHGlobal() is used instead of
    Interop.Libc.Free().
    
    Signed-off-by: SangYoun Kwak <sy.kwak@samsung.com>

--- src/Tizen.Sensor/Tizen.Sensor/Plugins/Gyroscope.cs
+++ src/Tizen.Sensor/Tizen.Sensor/Plugins/Gyroscope.cs
@@ -15,6 +15,7 @@
  */
 
 using System;
+using System.Runtime.InteropServices;
 
 namespace Tizen.Sensor
 {
@@ -116,7 +117,7 @@ namespace Tizen.Sensor
                 count = 0;
             }
             else
-                Interop.Libc.Free(list);
+                Marshal.FreeHGlobal(list);
             return count;
         }
 
