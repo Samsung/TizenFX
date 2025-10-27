68648491395bb71ddf0bd02946e95b91cb909f10
Eunki, Hong <eunkiki.hong@samsung.com>
2025-10-27 16:51:10 +0900

    [NUI](BackgroundBlurEffect) Bind `SetSourceView` and `SetStopperView` API
    Let we support to set source view and stopper view, to increate performance of
    BackgroundBlurEffect.
    
    We can specify the range of blur area in scene tree if user want.
    
    Relative dali patch
     - https://review.tizen.org/gerrit/c/platform/core/uifw/dali-csharp-binder/+/332850
    
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI/src/internal/Interop/Interop.BackgroundBlurEffect.cs
+++ src/Tizen.NUI/src/internal/Interop/Interop.BackgroundBlurEffect.cs
@@ -51,6 +51,12 @@ namespace Tizen.NUI
             [DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_BackgroundBlurEffect_AddBlurOpacityAnimation")]
             public static extern void AddBlurOpacityAnimation(HandleRef effect, HandleRef animation, HandleRef alphaFunction, HandleRef timePeriod, float fromValue, float toValue);
 
+            [DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_BackgroundBlurEffect_SetSourceActor")]
+            public static extern void SetSourceView(HandleRef effect, HandleRef sourceView);
+
+            [DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_BackgroundBlurEffect_SetStopperActor")]
+            public static extern void SetStopperView(HandleRef effect, HandleRef stopperView);
+
             [DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_BackgroundBlurEffect_FinishedSignal_Connect")]
             public static extern void FinishedSignalConnect(HandleRef effect, HandleRef callback);
 
