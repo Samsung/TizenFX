0d4a8779d0b9acb8a3f9e1dcf099846c1f98bd32
newb1e-kim <jihoi.kim@samsung.com>
2025-11-20 12:04:59 +0900

    [UIGadget] Fix LifecycleEvents pending issue
    When TryDequeue Fail, the function return without resetting '_processing' flag. To handle the case, it should not return during the loop.
    
    Also, to handle the TryDequeue fail properly, it requires some retry count.

--- src/Tizen.Applications.UIGadget/Tizen.Applications/UIGadgetLifecycleManager.cs
+++ src/Tizen.Applications.UIGadget/Tizen.Applications/UIGadgetLifecycleManager.cs
@@ -57,11 +57,17 @@ namespace Tizen.Applications
             }
 
             _processing = true;
+            var retry = 3;
             while (!_lifecycleEvents.IsEmpty)
             {
                 if (!_lifecycleEvents.TryDequeue(out LifecycleEvent lifecycleEvent))
                 {
-                    return;
+                    if ((!_lifecycleEvents.IsEmpty) && (retry-- > 0)) {
+                        Log.Warn("Fail to deque lifecycle events, retry " + retry);
+                        continue;
+                    } else {
+                        break;
+                    }
                 }
 
                 var action = lifecycleEvent.Action;
