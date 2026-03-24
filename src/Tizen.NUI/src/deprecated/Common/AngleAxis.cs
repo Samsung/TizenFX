b5306b0e3c4f4322f29e16ce1be06cbb1d5eba04
Eunki Hong <eunkiki.hong@samsung.com>
2026-03-24 18:04:11 +0900

    [NUI] Public for `PropertyValue(Matrix)` constructor + Move `AngleAxis` to deprecated folder
    Signed-off-by: Eunki Hong <eunkiki.hong@samsung.com>

--- src/Tizen.NUI/src/deprecated/Common/AngleAxis.cs
+++ src/Tizen.NUI/src/deprecated/Common/AngleAxis.cs
@@ -17,6 +17,7 @@
 
 namespace Tizen.NUI
 {
+    [global::System.Obsolete("Do not use this, Use Rotation instead.")]
     internal class AngleAxis : Disposable
     {
         internal AngleAxis(global::System.IntPtr cPtr, bool cMemoryOwn) : base(cPtr, cMemoryOwn, false)
