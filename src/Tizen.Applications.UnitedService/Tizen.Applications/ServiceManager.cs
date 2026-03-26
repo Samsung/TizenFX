4c1426b0eeab9829ae2f7b915f5625fc61a75c78
Jay Cho <chojoong@gmail.com>
2026-03-26 16:57:03 +0900

    Refactor: Remove Unnecessary LINQ Allocation (#7535)

--- src/Tizen.Applications.UnitedService/Tizen.Applications/ServiceManager.cs
+++ src/Tizen.Applications.UnitedService/Tizen.Applications/ServiceManager.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2023 Samsung Electronics Co., Ltd All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the License);
@@ -84,7 +84,7 @@ namespace Tizen.Applications
                 return;
             }
 
-            foreach (string packageId in packages.Split(':').ToList())
+            foreach (string packageId in packages.Split(':'))
             {
                 ServiceInfo info = ServiceInfo.Create(packageId);
                 if (info != null)
