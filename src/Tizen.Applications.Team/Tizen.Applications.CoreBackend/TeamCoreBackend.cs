e1b35118a3685f38d41120b39c0a5e264fb0c1ac
newb1e-kim <jihoi.kim@samsung.com>
2026-04-17 17:24:54 +0900

    [Tizen.Applications] Implement Team Application C# Interface
    * [NUI] Add Tizen.Application.Team into FriendAssembly
    
    TeamApplication will create UI with NUI.
    
    However, it does not inherit NUIApplication. Instead of inheriting
    NUIApplcation, it use DALI native in backend. For developes to utilize
    C# NUI object in TeamApplication, the backend should depend on NUI
    deeply, require to be friend of the project.
    
    Signed-off-by: Jihoi Kim <jihoi.kim@samsung.com>
    
    * [APPFW] Implement Team Application C# Interface
    
    - Implement Interop, Wrapper Class to implement C# Team Application
    - Add OnCreate Error Handling
    - When the application start, TeamManager will hold the application
      instance to guarantee the object alive until exit.
    - Implement TeamViewApplication
    
    Signed-off-by: Jihoi Kim <jihoi.kim@samsung.com>
    
    * [APPFW] Add XML documentation and inhouse markers for Team Application
    
    - Add summary, param, returns, exception, remarks XML doc comments
      to public API surface of Tizen.Applications.Team module.
    - Mark public types and members with [EditorBrowsable(EditorBrowsableState.Never)]
      and inhouse comment line until ACR is completed.
    
    Co-authored-by: KimHyeonuk <hyeonuk.kim@samsung.com>
    
    ---------
    
    Signed-off-by: Jihoi Kim <jihoi.kim@samsung.com>
    Co-authored-by: 김현욱 <43038815+Hyeon-Uk@users.noreply.github.com>
    Co-authored-by: KimHyeonuk <hyeonuk.kim@samsung.com>

---
+++ src/Tizen.Applications.Team/Tizen.Applications.CoreBackend/TeamCoreBackend.cs
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2026 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace Tizen.Applications.CoreBackend
+{
+    /// <summary>
+    /// Represents the abstract base backend that drives a Team application instance.
+    /// </summary>
+    /// This will be public opened in next tizen after ACR done. (Before ACR, need to be hidden as inhouse API)
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public abstract class TeamCoreBackend : DefaultCoreBackend
+    {
+        /// <summary>
+        /// The native handle that identifies this Team member instance.
+        /// </summary>
+        protected IntPtr _memberHandle = IntPtr.Zero;
+
+        /// <summary>
+        /// The identifier of the loaded assembly associated with this Team member instance.
+        /// </summary>
+        protected IntPtr _loadObjId = IntPtr.Zero;
+
+        /// <summary>
+        /// The native handle to the arguments passed to this Team member instance.
+        /// </summary>
+        protected IntPtr _argHandle = IntPtr.Zero;
+
+        internal abstract IntPtr MemberHandle { get; }
+        internal abstract IntPtr LoadObjId { get; }
+        internal abstract IntPtr ArgHandle { get; }
+    }
+}
--- src/Tizen.NUI/src/internal/Common/FriendAssembly.cs
+++ src/Tizen.NUI/src/internal/Common/FriendAssembly.cs
@@ -22,6 +22,7 @@
 
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Tizen.Applications.Team, " + Tizen.NUI.PublicKey.TizenFX)]
 [assembly: InternalsVisibleTo("Tizen.Multimedia, " + Tizen.NUI.PublicKey.TizenFX)]
 [assembly: InternalsVisibleTo("Tizen.NUI.Wearable, " + Tizen.NUI.PublicKey.TizenFX)]
 [assembly: InternalsVisibleTo("Tizen.NUI.Components, " + Tizen.NUI.PublicKey.TizenFX)]
